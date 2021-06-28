c2f6a440480427f190f69eaa07126e71c67dd311
Jared Duffey <jared.duffey@bluequartz.net>
2021-06-28 12:16:46 -0400

    Added --diff-filter to exclude deleted files
    Signed-off-by: Jared Duffey <jared.duffey@bluequartz.net>

--- scripts/clang_format.py
+++ scripts/clang_format.py
@@ -35,7 +35,10 @@ def get_git_files() -> List[str]:
 def get_git_diff_files(previous_commit: str, current_commit: str) -> List[str]:
   try:
     result = subprocess.run(
-      ['git', 'diff', '--name-only', f'{previous_commit}...{current_commit}'], text=True, capture_output=True, check=True
+      ['git', 'diff', '--name-only', '--diff-filter=d', f'{previous_commit}...{current_commit}'],
+      text=True,
+      capture_output=True,
+      check=True
     )
   except subprocess.CalledProcessError as error:
     print(error.stderr)
