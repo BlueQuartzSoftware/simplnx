2209e636f84127d2fbf2c1abffd9c7238a89dd15
Jared Duffey <jared.duffey@bluequartz.net>
2021-08-19 12:42:11 -0400

    Added VectorParameter
    Signed-off-by: Jared Duffey <jared.duffey@bluequartz.net>

---
+++ src/complex/Core/Parameters/VectorParameter.cpp
@@ -0,0 +1,151 @@
+#include "VectorParameter.hpp"
+
+#include <fmt/core.h>
+
+#include <nlohmann/json.hpp>
+
+namespace
+{
+template <class...>
+constexpr std::false_type always_false{};
+}
+
+namespace complex
+{
+template <class T>
+VectorParameter<T>::VectorParameter(const std::string& name, const std::string& humanName, const std::string& helpText, const ValueType& defaultValue, const NamesType& names)
+: ValueParameter(name, humanName, helpText)
+, m_DefaultValue(defaultValue)
+, m_Names(names)
+{
+  if(m_DefaultValue.size() != m_Names.size())
+  {
+    throw std::runtime_error("VectorParameter: size of names is not equal to required size");
+  }
+}
+
+template <class T>
+Uuid VectorParameter<T>::uuid() const
+{
+  return ParameterTraits<VectorParameter<T>>::uuid;
+}
+
+template <class T>
+IParameter::AcceptedTypes VectorParameter<T>::acceptedTypes() const
+{
+  return {typeid(ValueType)};
+}
+
+template <class T>
+nlohmann::json VectorParameter<T>::toJson(const std::any& value) const
+{
+  const auto* vec = std::any_cast<ValueType>(&value);
+  if(vec == nullptr)
+  {
+    throw std::bad_any_cast();
+  }
+
+  auto jsonArray = nlohmann::json::array();
+  for(T value : *vec)
+  {
+    jsonArray.push_back(value);
+  }
+
+  return jsonArray;
+}
+
+template <class T>
+Result<std::any> VectorParameter<T>::fromJson(const nlohmann::json& json) const
+{
+  const std::string key = name();
+  if(!json.contains(key))
+  {
+    return {nonstd::make_unexpected(std::vector<Error>{{-1, fmt::format("JSON does not contain key \"{}\"", key)}})};
+  }
+  auto jsonValue = json.at(key);
+  if(!jsonValue.is_array())
+  {
+    return {nonstd::make_unexpected(std::vector<Error>{{-2, fmt::format("JSON value for key \"{}\" is not an array", key)}})};
+  }
+  ValueType vec;
+  for(usize i = 0; i < jsonValue.size(); i++)
+  {
+    const auto& element = jsonValue[i];
+    if constexpr(std::is_arithmetic_v<T>)
+    {
+      if(!element.is_number())
+      {
+        return {nonstd::make_unexpected(std::vector<Error>{{-3, fmt::format("JSON value for array index \"{}\" is not a number", i)}})};
+      }
+    }
+    else
+    {
+      static_assert(always_false<T>, "JSON conversion not implemented for this type");
+    }
+    vec.push_back(element.get<T>());
+  }
+  return {vec};
+}
+
+template <class T>
+IParameter::UniquePointer VectorParameter<T>::clone() const
+{
+  return std::make_unique<VectorParameter<T>>(name(), humanName(), helpText(), m_DefaultValue, m_Names);
+}
+
+template <class T>
+std::any VectorParameter<T>::defaultValue() const
+{
+  return defaultVector();
+}
+
+template <class T>
+const typename VectorParameter<T>::NamesType& VectorParameter<T>::names() const
+{
+  return m_Names;
+}
+
+template <class T>
+usize VectorParameter<T>::size() const
+{
+  return m_DefaultValue.size();
+}
+
+template <class T>
+const typename VectorParameter<T>::ValueType& VectorParameter<T>::defaultVector() const
+{
+  return m_DefaultValue;
+}
+
+template <class T>
+Result<> VectorParameter<T>::validate(const std::any& value) const
+{
+  const auto* castValue = std::any_cast<ValueType>(&value);
+  if(castValue == nullptr)
+  {
+    throw std::bad_any_cast();
+  }
+  return validateVector(*castValue);
+}
+
+template <class T>
+Result<> VectorParameter<T>::validateVector(const ValueType& value) const
+{
+  return {};
+}
+
+template class COMPLEX_TEMPLATE_EXPORT VectorParameter<i8>;
+template class COMPLEX_TEMPLATE_EXPORT VectorParameter<u8>;
+
+template class COMPLEX_TEMPLATE_EXPORT VectorParameter<i16>;
+template class COMPLEX_TEMPLATE_EXPORT VectorParameter<u16>;
+
+template class COMPLEX_TEMPLATE_EXPORT VectorParameter<i32>;
+template class COMPLEX_TEMPLATE_EXPORT VectorParameter<u32>;
+
+template class COMPLEX_TEMPLATE_EXPORT VectorParameter<i64>;
+template class COMPLEX_TEMPLATE_EXPORT VectorParameter<u64>;
+
+template class COMPLEX_TEMPLATE_EXPORT VectorParameter<f32>;
+template class COMPLEX_TEMPLATE_EXPORT VectorParameter<f64>;
+} // namespace complex
---
+++ src/complex/Core/Parameters/VectorParameter.hpp
@@ -0,0 +1,150 @@
+#pragma once
+
+#include <vector>
+
+#include "complex/Filter/ParameterTraits.hpp"
+#include "complex/Filter/ValueParameter.hpp"
+
+namespace complex
+{
+template <class T>
+class VectorParameter : public ValueParameter
+{
+public:
+  using ValueType = std::vector<T>;
+  using ElementType = typename ValueType::value_type;
+  using NamesType = std::vector<std::string>;
+
+  VectorParameter() = delete;
+  VectorParameter(const std::string& name, const std::string& humanName, const std::string& helpText, const ValueType& defaultValue, const NamesType& names);
+  ~VectorParameter() override = default;
+
+  VectorParameter(const VectorParameter&) = delete;
+  VectorParameter(VectorParameter&&) noexcept = delete;
+
+  VectorParameter& operator=(const VectorParameter&) = delete;
+  VectorParameter& operator=(VectorParameter&&) noexcept = delete;
+
+  /**
+   * @brief
+   * @return
+   */
+  [[nodiscard]] Uuid uuid() const override;
+
+  /**
+   * @brief
+   * @return
+   */
+  [[nodiscard]] AcceptedTypes acceptedTypes() const override;
+
+  /**
+   * @brief
+   * @param value
+   */
+  [[nodiscard]] nlohmann::json toJson(const std::any& value) const override;
+
+  /**
+   * @brief
+   * @return
+   */
+  [[nodiscard]] Result<std::any> fromJson(const nlohmann::json& json) const override;
+
+  /**
+   * @brief
+   * @return
+   */
+  [[nodiscard]] UniquePointer clone() const override;
+
+  /**
+   * @brief
+   * @return
+   */
+  [[nodiscard]] std::any defaultValue() const override;
+
+  /**
+   * @brief
+   * @return
+   */
+  [[nodiscard]] const ValueType& defaultVector() const;
+
+  /**
+   * @brief
+   * @return
+   */
+  [[nodiscard]] const NamesType& names() const;
+
+  /**
+   * @brief
+   * @return
+   */
+  [[nodiscard]] usize size() const;
+
+  /**
+   * @brief
+   * @param value
+   * @return
+   */
+  [[nodiscard]] Result<> validate(const std::any& value) const override;
+
+  /**
+   * @brief
+   * @param value
+   * @return
+   */
+  [[nodiscard]] Result<> validateVector(const ValueType& value) const;
+
+private:
+  ValueType m_DefaultValue = {};
+  NamesType m_Names;
+};
+
+extern template class VectorParameter<i8>;
+extern template class VectorParameter<u8>;
+
+extern template class VectorParameter<i16>;
+extern template class VectorParameter<u16>;
+
+extern template class VectorParameter<i32>;
+extern template class VectorParameter<u32>;
+
+extern template class VectorParameter<i64>;
+extern template class VectorParameter<u64>;
+
+extern template class VectorParameter<f32>;
+extern template class VectorParameter<f64>;
+
+using VectorInt8Parameter = VectorParameter<i8>;
+using VectorUInt8Parameter = VectorParameter<u8>;
+
+using VectorInt16Parameter = VectorParameter<i16>;
+using VectorUInt16Parameter = VectorParameter<u16>;
+
+using VectorInt32Parameter = VectorParameter<i32>;
+using VectorUInt32Parameter = VectorParameter<u32>;
+
+using VectorInt64Parameter = VectorParameter<i64>;
+using VectorUInt64Parameter = VectorParameter<u64>;
+
+using VectorFloat32Parameter = VectorParameter<f32>;
+using VectorFloat64Parameter = VectorParameter<f64>;
+} // namespace complex
+
+COMPLEX_DEF_PARAMETER_TRAITS(complex::VectorInt8Parameter, "9f5f9683-e492-4a79-8378-79d727b2356a");
+
+COMPLEX_DEF_PARAMETER_TRAITS(complex::VectorUInt8Parameter, "bff78ff3-35ef-482a-b3b1-df8806e7f7ef");
+
+COMPLEX_DEF_PARAMETER_TRAITS(complex::VectorInt16Parameter, "43810a29-1a5f-4472-bec6-41de9ffe27f7");
+
+COMPLEX_DEF_PARAMETER_TRAITS(complex::VectorUInt16Parameter, "2f1ba2f4-c5d5-403c-8b90-0bf60d2bde9b");
+
+COMPLEX_DEF_PARAMETER_TRAITS(complex::VectorInt32Parameter, "d3188e18-e383-4727-ab32-88b5fda56ae8");
+
+COMPLEX_DEF_PARAMETER_TRAITS(complex::VectorUInt32Parameter, "37322aa6-1a2f-4ecb-9aa1-8922d7ac1e49");
+
+COMPLEX_DEF_PARAMETER_TRAITS(complex::VectorInt64Parameter, "4ceaffc1-7326-4f65-a33a-eae263dc22d1");
+
+COMPLEX_DEF_PARAMETER_TRAITS(complex::VectorUInt64Parameter, "17309744-c4e8-4d1e-807e-e7012387f1ec");
+
+COMPLEX_DEF_PARAMETER_TRAITS(complex::VectorFloat32Parameter, "88f231a1-7956-41f5-98b7-4471705d2805");
+
+COMPLEX_DEF_PARAMETER_TRAITS(complex::VectorFloat64Parameter, "57cbdfdf-9d1a-4de8-95d7-71d0c01c5c96");
