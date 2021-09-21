e93132e3698e41fd44a66e0aa24c25567bf33f69
Jared Duffey <jared.duffey@bluequartz.net>
2021-09-21 11:00:11 -0400

    Switched to std::string_view in Arguments where appropriate
    * Arguments::insert now takes std::string by value and moves
    
    Signed-off-by: Jared Duffey <jared.duffey@bluequartz.net>

--- test/ArgumentsTest.cpp
+++ test/ArgumentsTest.cpp
@@ -2,22 +2,23 @@
 
 #include <catch2/catch.hpp>
 
+#include <complex/Common/StringLiteral.hpp>
 #include <complex/Filter/Arguments.hpp>
 
-using namespace complex::types;
+using namespace complex;
 
 TEST_CASE("ArgumentsTest")
 {
-  static constexpr char k_FooKey[] = "foo";
-  complex::Arguments args;
+  static constexpr StringLiteral k_FooKey = "foo";
+  Arguments args;
 
   SECTION("insert and retrieve primitive")
   {
     static constexpr i32 k_FooValue = 42;
 
-    args.insert(k_FooKey, k_FooValue);
+    args.insert(k_FooKey.str(), k_FooValue);
 
-    auto value = args.value<i32>(k_FooKey);
+    auto value = args.value<i32>(k_FooKey.view());
 
     REQUIRE(value == k_FooValue);
   }
@@ -25,9 +26,9 @@ TEST_CASE("ArgumentsTest")
   {
     const std::string k_FooValue = "value";
 
-    args.insert(k_FooKey, k_FooValue);
+    args.insert(k_FooKey.str(), k_FooValue);
 
-    auto value = args.value<std::string>(k_FooKey);
+    auto value = args.value<std::string>(k_FooKey.view());
 
     REQUIRE(value == k_FooValue);
   }
@@ -35,9 +36,9 @@ TEST_CASE("ArgumentsTest")
   {
     const std::string k_FooValue = "value";
 
-    args.insert(k_FooKey, k_FooValue);
+    args.insert(k_FooKey.str(), k_FooValue);
 
-    const auto& value = args.valueRef<std::string>(k_FooKey);
+    const auto& value = args.valueRef<std::string>(k_FooKey.view());
 
     REQUIRE(value == k_FooValue);
   }
@@ -45,9 +46,9 @@ TEST_CASE("ArgumentsTest")
   {
     i32 value = 42;
 
-    args.insert(k_FooKey, std::ref(value));
+    args.insert(k_FooKey.str(), std::ref(value));
 
-    auto& valueRefWrapper = args.ref<i32>(k_FooKey);
+    auto& valueRefWrapper = args.ref<i32>(k_FooKey.view());
 
     REQUIRE(&value == &valueRefWrapper);
     REQUIRE(value == valueRefWrapper);
@@ -60,9 +61,9 @@ TEST_CASE("ArgumentsTest")
   {
     i32 value = 42;
 
-    args.insert(k_FooKey, std::cref(value));
+    args.insert(k_FooKey.str(), std::cref(value));
 
-    const auto& valueRefWrapper = args.ref<const i32>(k_FooKey);
+    const auto& valueRefWrapper = args.ref<const i32>(k_FooKey.view());
 
     REQUIRE(&value == &valueRefWrapper);
     REQUIRE(value == valueRefWrapper);
@@ -71,13 +72,13 @@ TEST_CASE("ArgumentsTest")
   {
     static constexpr i32 k_Value = 42;
 
-    args.insert(k_FooKey, k_Value);
+    args.insert(k_FooKey.str(), k_Value);
 
-    REQUIRE(args.contains(k_FooKey));
+    REQUIRE(args.contains(k_FooKey.view()));
   }
   SECTION("not contains test")
   {
-    REQUIRE(!args.contains(k_FooKey));
+    REQUIRE(!args.contains(k_FooKey.view()));
   }
   SECTION("size test")
   {
