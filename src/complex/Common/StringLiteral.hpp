bbc147f492a70f614489810a97f1c8efae2eada4
Jared Duffey <jared.duffey@bluequartz.net>
2021-09-24 12:07:43 -0400

    Added check requiring StringLiteral be null-terminated
    * Added code example in documentation
    * Updated comments
    * Fixed constructor order
    * Removed length()
    * size() returns the size not including the null terminator
    
    Signed-off-by: Jared Duffey <jared.duffey@bluequartz.net>

--- src/complex/Common/StringLiteral.hpp
+++ src/complex/Common/StringLiteral.hpp
@@ -2,16 +2,36 @@
 
 #include "complex/Common/Types.hpp"
 
+#include <stdexcept>
 #include <string>
 #include <string_view>
 
 namespace complex
 {
+namespace detail
+{
+/**
+ * @brief Returns true if the given character array is null-terminated.
+ * @tparam T
+ * @param string
+ * @return
+ */
+template <class T, usize Size>
+constexpr bool HasNullTerminator(const T (&string)[Size]) noexcept
+{
+  return string[Size - 1] == static_cast<T>('\0');
+}
+} // namespace detail
+
 /**
  * @brief BasicStringLiteral is meant to be a safe container for a string literal allowing for easy access to its size/length.
  * This class should always contain a pointer to a static compile time null-terminated string literal.
  * Typical usage will be for static string constants. At this time the constructors allow non string literals to be passed in.
  * This is undesired behavior, but there is no way to overcome this in C++17 without using character parameter packs which come with their own issues.
+ * Example:
+ * @code
+ * static constexpr StringLiteral k_Foo = "foo";
+ * @endcode
  * @tparam T Character type
  */
 template <class T>
@@ -21,15 +41,20 @@ public:
   BasicStringLiteral() = delete;
 
   /**
-   * @brief Constructor that accepts a string literal of fixed size.
+   * @brief Constructor that accepts a string literal of fixed size. Should be made consteval in C++20.
+   * Requires string to be null-terminated.
    * @param string
    * @return
    */
   template <usize Size>
-  constexpr BasicStringLiteral(const T (&string)[Size]) noexcept
-  : m_Size(Size)
-  , m_String(string)
+  constexpr BasicStringLiteral(const T (&string)[Size])
+  : m_String(string)
+  , m_Size(Size)
   {
+    if(!detail::HasNullTerminator(string))
+    {
+      throw std::runtime_error("BasicStringLiteral must be null-terminated");
+    }
   }
 
   ~BasicStringLiteral() noexcept = default;
@@ -41,7 +66,7 @@ public:
   BasicStringLiteral& operator=(BasicStringLiteral&&) noexcept = default;
 
   /**
-   * @brief Returns the c-string pointer
+   * @brief Returns the c-string pointer.
    * @return const T*
    */
   constexpr const T* c_str() const noexcept
@@ -50,48 +75,39 @@ public:
   }
 
   /**
-   * @brief Returns the size of the string literal including the null terminator
+   * @brief Returns the size of the string literal not including the null terminator.
    * @return usize
    */
   constexpr usize size() const noexcept
-  {
-    return m_Size;
-  }
-
-  /**
-   * @brief Returns the size of the string literal not including the null terminator
-   * @return usize
-   */
-  constexpr usize length() const noexcept
   {
     return m_Size - 1;
   }
 
   /**
-   * @brief Returns a view of string literal not including the null terminator
+   * @brief Returns a view of string literal not including the null terminator.
    * @return std::basic_string_view<T>
    */
   constexpr std::basic_string_view<T> view() const
-  {
-    return std::basic_string_view<T>(m_String, length());
-  }
-
-  /**
-   * @brief Returns a view of string literal including the null terminator
-   * @return std::basic_string_view<T>
-   */
-  constexpr std::basic_string_view<T> c_view() const
   {
     return std::basic_string_view<T>(m_String, size());
   }
 
   /**
-   * @brief Returns a null-terminated heap allocated string
+   * @brief Returns a view of string literal including the null terminator.
+   * @return std::basic_string_view<T>
+   */
+  constexpr std::basic_string_view<T> c_view() const
+  {
+    return std::basic_string_view<T>(m_String, m_Size);
+  }
+
+  /**
+   * @brief Returns a null-terminated heap allocated string.
    * @return std::basic_string<T>
    */
   std::basic_string<T> str() const
   {
-    return std::basic_string<T>(m_String, length());
+    return std::basic_string<T>(m_String, size());
   }
 
 private:
