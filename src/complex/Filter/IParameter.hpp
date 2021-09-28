4e5a07e423d5cd82aa7f8c2215863450558c64ea
Jared Duffey <jared.duffey@bluequartz.net>
2021-09-28 09:59:41 -0400

    Changed some constants to inline
    Signed-off-by: Jared Duffey <jared.duffey@bluequartz.net>

--- src/complex/Filter/IParameter.hpp
+++ src/complex/Filter/IParameter.hpp
@@ -18,19 +18,17 @@
 
 namespace complex
 {
-
-namespace FilterParameter::Constants
-{
 /**
  * @brief These constants can be used as standard error codes when parsing JSON
  */
-constexpr const int32_t k_Json_Missing_Entry = -100;
-constexpr const int32_t k_Json_Value_Not_Object = -101;
-constexpr const int32_t k_Json_Value_Not_String = -102;
-constexpr const int32_t k_Json_Value_Not_Integer = -103;
-constexpr const int32_t k_Json_Value_Not_Unsigned = -104;
-constexpr const int32_t k_Json_Value_Not_Enumeration = -105;
-
+namespace FilterParameter::Constants
+{
+inline constexpr int32_t k_Json_Missing_Entry = -100;
+inline constexpr int32_t k_Json_Value_Not_Object = -101;
+inline constexpr int32_t k_Json_Value_Not_String = -102;
+inline constexpr int32_t k_Json_Value_Not_Integer = -103;
+inline constexpr int32_t k_Json_Value_Not_Unsigned = -104;
+inline constexpr int32_t k_Json_Value_Not_Enumeration = -105;
 } // namespace FilterParameter::Constants
 
 /**
