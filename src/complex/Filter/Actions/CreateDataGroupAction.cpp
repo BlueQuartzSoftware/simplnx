668fba678d3c4ff6b1daf13bf144a136f278af8e
Michael Jackson <mike.jackson@bluequartz.net>
2021-09-30 18:04:00 -0400

    Add output Action class CreateDataGroupAction.
    This is in support of other PR requests.
    
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

---
+++ src/complex/Filter/Actions/CreateDataGroupAction.cpp
@@ -0,0 +1,35 @@
+#include "CreateDataGroupAction.hpp"
+
+#include "complex/DataStructure/DataStructure.hpp"
+#include "complex/DataStructure/LinkedPath.hpp"
+
+#include <fmt/core.h>
+
+namespace complex
+{
+
+//------------------------------------------------------------------------------
+CreateDataGroupAction::CreateDataGroupAction(const DataPath& path)
+: m_Path(path)
+{
+}
+
+CreateDataGroupAction::~CreateDataGroupAction() noexcept = default;
+
+DataPath CreateDataGroupAction::parentPath() const
+{
+  return m_Path;
+}
+
+Result<> CreateDataGroupAction::apply(DataStructure& dataStructure, Mode mode) const
+{
+  LinkedPath linkedPath = dataStructure.makePath(m_Path);
+  if(!linkedPath.isValid())
+  {
+    return MakeErrorResult(-2, fmt::format("Path '{}' was not created.", m_Path.toString()));
+  }
+  // Assuming nothing went wrong, return an empty Result<> object
+  return {};
+}
+
+} // namespace complex
---
+++ src/complex/Filter/Actions/CreateDataGroupAction.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "complex/Filter/Output.hpp"
+
+namespace complex
+{
+
+class CreateDataGroupAction : public IDataAction
+{
+public:
+  CreateDataGroupAction() = delete;
+
+  CreateDataGroupAction(const DataPath& path);
+
+  ~CreateDataGroupAction() noexcept override;
+
+  CreateDataGroupAction(const CreateDataGroupAction&) = delete;
+  CreateDataGroupAction(CreateDataGroupAction&&) noexcept = delete;
+  CreateDataGroupAction& operator=(const CreateDataGroupAction&) = delete;
+  CreateDataGroupAction& operator=(CreateDataGroupAction&&) noexcept = delete;
+
+  /**
+   * @brief Applies this action's change to the given DataStructure in the given mode.
+   * Returns any warnings/errors. On error, DataStructure is not guaranteed to be consistent.
+   * @param dataStructure
+   * @return
+   */
+  Result<> apply(DataStructure& dataStructure, Mode mode) const override;
+
+  /**
+   * @brief Returns the NumericType of the DataArray to be created.
+   * @return
+   */
+  [[nodiscard]] DataPath parentPath() const;
+
+private:
+  DataPath m_Path = {};
+};
+
+} // namespace complex
