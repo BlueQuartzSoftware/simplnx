4fbfe0b851cddef88dbea2343138fe0a8666da0a
Jared Duffey <jared.duffey@bluequartz.net>
2021-10-01 16:11:50 -0400

    Moved actions into their own files
    Signed-off-by: Jared Duffey <jared.duffey@bluequartz.net>

--- src/complex/Core/Filters/ImportTextFilter.cpp
+++ src/complex/Core/Filters/ImportTextFilter.cpp
@@ -10,6 +10,7 @@
 #include "complex/Core/Parameters/NumberParameter.hpp"
 #include "complex/Core/Parameters/NumericTypeParameter.hpp"
 #include "complex/DataStructure/DataArray.hpp"
+#include "complex/Filter/Actions/CreateArrayAction.hpp"
 
 namespace fs = std::filesystem;
 using namespace complex;
--- src/complex/Filter/Output.hpp
+++ src/complex/Filter/Output.hpp
@@ -44,62 +44,6 @@ protected:
 };
 
 /**
- * @brief Action for creating DataArrays in a DataStructure
- */
-class CreateArrayAction : public IDataAction
-{
-public:
-  CreateArrayAction() = delete;
-
-  CreateArrayAction(NumericType type, const std::vector<usize>& dims, uint64 nComp, const DataPath& path);
-
-  ~CreateArrayAction() noexcept override;
-
-  CreateArrayAction(const CreateArrayAction&) = delete;
-  CreateArrayAction(CreateArrayAction&&) noexcept = delete;
-  CreateArrayAction& operator=(const CreateArrayAction&) = delete;
-  CreateArrayAction& operator=(CreateArrayAction&&) noexcept = delete;
-
-  /**
-   * @brief Applies this action's change to the given DataStructure in the given mode.
-   * Returns any warnings/errors. On error, DataStructure is not guaranteed to be consistent.
-   * @param dataStructure
-   * @return
-   */
-  Result<> apply(DataStructure& dataStructure, Mode mode) const override;
-
-  /**
-   * @brief Returns the NumericType of the DataArray to be created.
-   * @return
-   */
-  NumericType type() const;
-
-  /**
-   * @brief Returns the dimensions of the DataArray to be created.
-   * @return
-   */
-  std::vector<usize> dims() const;
-
-  /**
-   * @brief Returns the number of components of the DataArray to be created.
-   * @return
-   */
-  uint64 numComponents() const;
-
-  /**
-   * @brief Returns the path of the DataArray to be created.
-   * @return
-   */
-  DataPath path() const;
-
-private:
-  NumericType m_Type;
-  std::vector<usize> m_Dims;
-  uint64 m_NComp;
-  DataPath m_Path;
-};
-
-/**
  * @brief Container for IDataActions
  */
 struct OutputActions
