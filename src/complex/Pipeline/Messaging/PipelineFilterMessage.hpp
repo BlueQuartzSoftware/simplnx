9e43efca4bde30b26a96c75ab7a5bf3d46a3b375
Matthew Marine <matthew.marine@bluequartz.net>
2021-10-20 09:44:35 -0400

    Added PipelineFilterMessage
    * Added PipelineFilterMessage for IFilter::Message passing from PipelineFilter during preflight and execute.

---
+++ src/complex/Pipeline/Messaging/PipelineFilterMessage.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "complex/Filter/IFilter.hpp"
+#include "complex/Pipeline/Messaging/AbstractPipelineMessage.hpp"
+
+namespace complex
+{
+class PipelineFilter;
+
+/**
+ * @class PipelineFilterMessage
+ * @brief The PipelineFilterMessage class exists for alerting PipelineFilter
+ * listeners to IFilter::Messages.
+ */
+class COMPLEX_EXPORT PipelineFilterMessage : public AbstractPipelineMessage
+{
+public:
+  /**
+   * @brief Constructs a PipelineFilterMessage for the given node with the IFilter::Message.
+   * @param filter
+   * @param filterMessage
+   */
+  PipelineFilterMessage(PipelineFilter* filter, const IFilter::Message& filterMessage);
+
+  virtual ~PipelineFilterMessage();
+
+  /**
+   * @brief Returns the IFilter::Message
+   * @return IFilter::Message
+   */
+  IFilter::Message getFilterMessage();
+
+private:
+  IFilter::Message m_FilterMessage;
+};
+} // namespace complex
