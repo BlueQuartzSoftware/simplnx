0d15a05079c056f407bdf211761a3a7cd3978c5c
Michael Jackson <mike.jackson@bluequartz.net>
2021-11-28 23:22:17 -0500

    Add Laplacian Smoothing Filter (#52)
    * Add Laplacian Smoothing Filter.
    * Updated unit test. 
    * Adding reference XDMF files for visualizing results.
    
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

---
+++ src/Plugins/ComplexCore/src/ComplexCore/Filters/Algorithms/LaplacianSmoothing.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "ComplexCore/ComplexCore_export.hpp"
+
+#include "complex/DataStructure/DataPath.hpp"
+#include "complex/DataStructure/DataStructure.hpp"
+#include "complex/Filter/IFilter.hpp"
+
+#include <vector>
+
+namespace complex
+{
+
+struct LaplacianSmoothingInputValues
+{
+  DataPath pTriangleGeometryDataPath;
+  int32 pIterationSteps;
+  float32 pLambda;
+  bool pUseTaubinSmoothing;
+  float32 pMuFactor;
+  float32 pTripleLineLambda;
+  float32 pQuadPointLambda;
+  float32 pSurfacePointLambda;
+  float32 pSurfaceTripleLineLambda;
+  float32 pSurfaceQuadPointLambda;
+  DataPath pSurfaceMeshNodeTypeArrayPath;
+};
+
+/**
+ * @class ConditionalSetValue
+ * @brief This filter replaces values in the target array with a user specified value
+ * where a bool mask array specifies.
+ */
+
+class COMPLEXCORE_EXPORT LaplacianSmoothing
+{
+public:
+  LaplacianSmoothing(DataStructure& data, LaplacianSmoothingInputValues* inputValues, const IFilter* filter, const IFilter::MessageHandler& mesgHandler);
+  ~LaplacianSmoothing() noexcept;
+
+  LaplacianSmoothing(const LaplacianSmoothing&) = delete;
+  LaplacianSmoothing(LaplacianSmoothing&&) noexcept = delete;
+  LaplacianSmoothing& operator=(const LaplacianSmoothing&) = delete;
+  LaplacianSmoothing& operator=(LaplacianSmoothing&&) noexcept = delete;
+
+  Result<> operator()();
+
+private:
+  DataStructure& m_DataStructure;
+  const LaplacianSmoothingInputValues* m_InputValues = nullptr;
+  const IFilter* m_Filter = nullptr;
+  const IFilter::MessageHandler& m_MessageHandler;
+
+  std::vector<float> generateLambdaArray();
+  Result<> edgeBasedSmoothing();
+};
+
+} // namespace complex
