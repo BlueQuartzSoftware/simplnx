fc50746f2615f041ea20b2e2b2f78a94883ee89a
Jared Duffey <jared.duffey@bluequartz.net>
2021-11-30 14:40:32 -0500

    Changed CMake functions to use ARGS_ prefix for arguments
    Signed-off-by: Jared Duffey <jared.duffey@bluequartz.net>

--- cmake/Plugin.cmake
+++ cmake/Plugin.cmake
@@ -11,24 +11,24 @@ include(GNUInstallDirs)
 function(complex_COMPILE_PLUGIN)
   set(options)
   set(oneValueArgs PLUGIN_NAME PLUGIN_SOURCE_DIR)
-  cmake_parse_arguments(P "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+  cmake_parse_arguments(ARGS "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-  option(COMPLEX_PLUGIN_ENABLE_${P_PLUGIN_NAME} "Build the ${P_PLUGIN_NAME}" ON)
+  option(COMPLEX_PLUGIN_ENABLE_${ARGS_PLUGIN_NAME} "Build the ${ARGS_PLUGIN_NAME}" ON)
 
-  if(COMPLEX_PLUGIN_ENABLE_${P_PLUGIN_NAME})
-    add_subdirectory(${P_PLUGIN_SOURCE_DIR} ${PROJECT_BINARY_DIR}/Plugins/${P_PLUGIN_NAME})
-    get_property(PluginNumFilters GLOBAL PROPERTY ${P_PLUGIN_NAME}_filter_count)
+  if(COMPLEX_PLUGIN_ENABLE_${ARGS_PLUGIN_NAME})
+    add_subdirectory(${ARGS_PLUGIN_SOURCE_DIR} ${PROJECT_BINARY_DIR}/Plugins/${ARGS_PLUGIN_NAME})
+    get_property(PluginNumFilters GLOBAL PROPERTY ${ARGS_PLUGIN_NAME}_filter_count)
 
-    message(STATUS "${P_PLUGIN_NAME} [ENABLED] ${PluginNumFilters} Filters")
+    message(STATUS "${ARGS_PLUGIN_NAME} [ENABLED] ${PluginNumFilters} Filters")
     #- Now set up the dependency between the main application and each of the plugins so that
     #- things like Visual Studio are forced to rebuild the plugins when launching
     #- the sandbox application
-    if(COMPLEX_PLUGIN_ENABLE_${P_PLUGIN_NAME} AND TARGET complex::complex AND TARGET ${P_PLUGIN_NAME})
-      add_dependencies(${P_PLUGIN_NAME} complex::complex)
+    if(COMPLEX_PLUGIN_ENABLE_${ARGS_PLUGIN_NAME} AND TARGET complex::complex AND TARGET ${ARGS_PLUGIN_NAME})
+      add_dependencies(${ARGS_PLUGIN_NAME} complex::complex)
     endif()
 
   else()
-    message(STATUS "${P_PLUGIN_NAME} [DISABLED]: Use -DCOMPLEX_PLUGIN_ENABLE_${P_PLUGIN_NAME}=ON to Enable Plugin")
+    message(STATUS "${ARGS_PLUGIN_NAME} [DISABLED]: Use -DCOMPLEX_PLUGIN_ENABLE_${ARGS_PLUGIN_NAME}=ON to Enable Plugin")
   endif()
 endfunction()
 
@@ -40,51 +40,51 @@ endfunction()
 function(complex_add_plugin)
   set(options)
   set(oneValueArgs PLUGIN_NAME)
-  cmake_parse_arguments(P "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+  cmake_parse_arguments(ARGS "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-  #message(STATUS "complex_add_plugin: ${P_PLUGIN_NAME}")
-  #message(STATUS "COMPLEX_${P_PLUGIN_NAME}_SOURCE_DIR: ${COMPLEX_${P_PLUGIN_NAME}_SOURCE_DIR}")
-  if(NOT DEFINED COMPLEX_${P_PLUGIN_NAME}_SOURCE_DIR OR NOT EXISTS "${COMPLEX_${P_PLUGIN_NAME}_SOURCE_DIR}")
-    #message(STATUS "COMPLEX_${P_PLUGIN_NAME}_SOURCE_DIR was NOT Defined. Searching for Plugins in 'COMPLEX_PLUGIN_SEARCH_DIRS'")
+  #message(STATUS "complex_add_plugin: ${ARGS_PLUGIN_NAME}")
+  #message(STATUS "COMPLEX_${ARGS_PLUGIN_NAME}_SOURCE_DIR: ${COMPLEX_${ARGS_PLUGIN_NAME}_SOURCE_DIR}")
+  if(NOT DEFINED COMPLEX_${ARGS_PLUGIN_NAME}_SOURCE_DIR OR NOT EXISTS "${COMPLEX_${ARGS_PLUGIN_NAME}_SOURCE_DIR}")
+    #message(STATUS "COMPLEX_${ARGS_PLUGIN_NAME}_SOURCE_DIR was NOT Defined. Searching for Plugins in 'COMPLEX_PLUGIN_SEARCH_DIRS'")
     foreach(pluginSearchDir ${COMPLEX_PLUGIN_SEARCH_DIRS})
       #message(STATUS "|-Searching:${pluginSearchDir} ")
-      if(EXISTS ${pluginSearchDir}/${P_PLUGIN_NAME}/CMakeLists.txt)
-        set(COMPLEX_${P_PLUGIN_NAME}_SOURCE_DIR ${pluginSearchDir}/${P_PLUGIN_NAME} CACHE PATH "")
-        #message(STATUS "  |-Plugin: Defining COMPLEX_${P_PLUGIN_NAME}_SOURCE_DIR to ${COMPLEX_${P_PLUGIN_NAME}_SOURCE_DIR}")
+      if(EXISTS ${pluginSearchDir}/${ARGS_PLUGIN_NAME}/CMakeLists.txt)
+        set(COMPLEX_${ARGS_PLUGIN_NAME}_SOURCE_DIR ${pluginSearchDir}/${ARGS_PLUGIN_NAME} CACHE PATH "")
+        #message(STATUS "  |-Plugin: Defining COMPLEX_${ARGS_PLUGIN_NAME}_SOURCE_DIR to ${COMPLEX_${ARGS_PLUGIN_NAME}_SOURCE_DIR}")
         break()
       endif()
     endforeach()
 
-    #message(STATUS "${complex_SOURCE_DIR}/../complex_plugins/${P_PLUGIN_NAME}/CMakeLists.txt")
-    if(EXISTS ${complex_SOURCE_DIR}/../complex_plugins/${P_PLUGIN_NAME}/CMakeLists.txt)
-      set(COMPLEX_${P_PLUGIN_NAME}_SOURCE_DIR ${complex_SOURCE_DIR}/../complex_plugins/${P_PLUGIN_NAME} CACHE PATH "")
-      #message(STATUS "  |-Plugin: Defining COMPLEX_${P_PLUGIN_NAME}_SOURCE_DIR to ${COMPLEX_${P_PLUGIN_NAME}_SOURCE_DIR}")
+    #message(STATUS "${complex_SOURCE_DIR}/../complex_plugins/${ARGS_PLUGIN_NAME}/CMakeLists.txt")
+    if(EXISTS ${complex_SOURCE_DIR}/../complex_plugins/${ARGS_PLUGIN_NAME}/CMakeLists.txt)
+      set(COMPLEX_${ARGS_PLUGIN_NAME}_SOURCE_DIR ${complex_SOURCE_DIR}/../complex_plugins/${ARGS_PLUGIN_NAME} CACHE PATH "")
+      #message(STATUS "  |-Plugin: Defining COMPLEX_${ARGS_PLUGIN_NAME}_SOURCE_DIR to ${COMPLEX_${ARGS_PLUGIN_NAME}_SOURCE_DIR}")
     endif()
 
   endif()
 
   # Mark these variables as advanced
-  mark_as_advanced(COMPLEX_${P_PLUGIN_NAME}_SOURCE_DIR)
+  mark_as_advanced(COMPLEX_${ARGS_PLUGIN_NAME}_SOURCE_DIR)
 
   # Now that we have defined where the user's plugin directory is at we
   # need to make sure it has a CMakeLists.txt file in it
-  if(EXISTS ${COMPLEX_${P_PLUGIN_NAME}_SOURCE_DIR}/CMakeLists.txt)
-    set(${P_PLUGIN_NAME}_IMPORT_FILE COMPLEX_${P_PLUGIN_NAME}_SOURCE_DIR/CMakeLists.txt)
+  if(EXISTS ${COMPLEX_${ARGS_PLUGIN_NAME}_SOURCE_DIR}/CMakeLists.txt)
+    set(${ARGS_PLUGIN_NAME}_IMPORT_FILE COMPLEX_${ARGS_PLUGIN_NAME}_SOURCE_DIR/CMakeLists.txt)
   endif()
 
   # By this point we should have everything defined and ready to go...
-  if(DEFINED COMPLEX_${P_PLUGIN_NAME}_SOURCE_DIR AND DEFINED ${P_PLUGIN_NAME}_IMPORT_FILE)
-    #message(STATUS "Plugin: Adding Plugin ${COMPLEX_${P_PLUGIN_NAME}_SOURCE_DIR}")
-    complex_COMPILE_PLUGIN(PLUGIN_NAME ${P_PLUGIN_NAME}
-      PLUGIN_SOURCE_DIR ${COMPLEX_${P_PLUGIN_NAME}_SOURCE_DIR}
+  if(DEFINED COMPLEX_${ARGS_PLUGIN_NAME}_SOURCE_DIR AND DEFINED ${ARGS_PLUGIN_NAME}_IMPORT_FILE)
+    #message(STATUS "Plugin: Adding Plugin ${COMPLEX_${ARGS_PLUGIN_NAME}_SOURCE_DIR}")
+    complex_COMPILE_PLUGIN(PLUGIN_NAME ${ARGS_PLUGIN_NAME}
+      PLUGIN_SOURCE_DIR ${COMPLEX_${ARGS_PLUGIN_NAME}_SOURCE_DIR}
     )
     # Increment the plugin count
-    get_property(P_PLUGIN_COUNT GLOBAL PROPERTY COMPLEX_PLUGIN_COUNT)
-    math(EXPR P_PLUGIN_COUNT "${P_PLUGIN_COUNT}+1")
-    set_property(GLOBAL PROPERTY COMPLEX_PLUGIN_COUNT ${P_PLUGIN_COUNT})
+    get_property(PLUGIN_COUNT GLOBAL PROPERTY COMPLEX_PLUGIN_COUNT)
+    math(EXPR PLUGIN_COUNT "${PLUGIN_COUNT}+1")
+    set_property(GLOBAL PROPERTY COMPLEX_PLUGIN_COUNT ${PLUGIN_COUNT})
   else()
-    set(COMPLEX_${P_PLUGIN_NAME}_SOURCE_DIR ${pluginSearchDir} CACHE PATH "" FORCE)
-    message(STATUS "${P_PLUGIN_NAME} [DISABLED]. Missing Source Directory. Use -DCOMPLEX_${P_PLUGIN_NAME}_SOURCE_DIR=/Path/To/PluginDir")
+    set(COMPLEX_${ARGS_PLUGIN_NAME}_SOURCE_DIR ${pluginSearchDir} CACHE PATH "" FORCE)
+    message(STATUS "${ARGS_PLUGIN_NAME} [DISABLED]. Missing Source Directory. Use -DCOMPLEX_${ARGS_PLUGIN_NAME}_SOURCE_DIR=/Path/To/PluginDir")
   endif()
 endfunction()
 
