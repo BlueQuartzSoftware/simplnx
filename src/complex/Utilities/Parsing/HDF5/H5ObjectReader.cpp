b7e5766dde097132a149a147b853c3d388b11b33
Jared Duffey <jared.duffey@bluequartz.net>
2021-12-02 21:36:23 -0500

    Added utility function to get HDF5 object name
    * Wraps H5Iget_name calls
    
    Signed-off-by: Jared Duffey <jared.duffey@bluequartz.net>

--- src/complex/Utilities/Parsing/HDF5/H5ObjectReader.cpp
+++ src/complex/Utilities/Parsing/HDF5/H5ObjectReader.cpp
@@ -55,11 +55,9 @@ std::string H5::ObjectReader::getName() const
     return "";
   }
 
-  constexpr size_t size = 1024;
-  char buffer[size];
-  H5Iget_name(getId(), buffer, size);
+  std::string path = H5::GetNameFromId(getId());
 
-  return GetNameFromBuffer(buffer);
+  return path;
 }
 
 size_t H5::ObjectReader::getNumAttributes() const
