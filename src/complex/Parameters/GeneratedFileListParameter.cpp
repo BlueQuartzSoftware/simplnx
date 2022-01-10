1edc2e0f04f480624f3f241a36b0befdff34a1f6
Matthew Marine <matthew.marine@bluequartz.net>
2022-01-10 17:21:03 -0500

    Fixed CMake and build errors
    * Updated TBB cmake
    * Fixed fmt::format build errors

--- src/complex/Parameters/GeneratedFileListParameter.cpp
+++ src/complex/Parameters/GeneratedFileListParameter.cpp
@@ -106,7 +106,7 @@ Result<std::any> GeneratedFileListParameter::fromJson(const nlohmann::json& json
   if(ordering_check != static_cast<OrderingUnderlyingT>(Ordering::LowToHigh) && ordering_check != static_cast<OrderingUnderlyingT>(Ordering::HighToLow))
   {
     return MakeErrorResult<std::any>(FilterParameter::Constants::k_Json_Value_Not_Enumeration, fmt::format("{}JSON value for key '{}' was not a valid ordering Value. [{}|{}] allowed.", prefix.view(),
-                                                                                                           k_Ordering.view(), Ordering::LowToHigh, Ordering::HighToLow));
+                                                                                                           k_Ordering.view(), static_cast<int32>(Ordering::LowToHigh), static_cast<int32>(Ordering::HighToLow)));
   }
 
   if(!json[k_PaddingDigits].is_number_unsigned())
