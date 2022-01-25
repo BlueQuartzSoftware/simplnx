874156f8cdc047de05e8d3d8db1e569653813e25
Matthew Marine <matthew.marine@bluequartz.net>
2022-01-25 09:10:24 -0500

    Added AlignGeometries filter
    * Updated RectGridGeom with non-const getX/Y/ZBounds() methods.

---
+++ src/Plugins/ComplexCore/src/ComplexCore/Filters/AlignGeometries.cpp
@@ -0,0 +1,426 @@
+#include "AlignGeometries.hpp"
+
+#include "complex/DataStructure/Geometry/AbstractGeometry.hpp"
+#include "complex/DataStructure/Geometry/AbstractGeometry2D.hpp"
+#include "complex/DataStructure/Geometry/AbstractGeometry3D.hpp"
+#include "complex/DataStructure/Geometry/EdgeGeom.hpp"
+#include "complex/DataStructure/Geometry/ImageGeom.hpp"
+#include "complex/DataStructure/Geometry/RectGridGeom.hpp"
+#include "complex/DataStructure/Geometry/VertexGeom.hpp"
+#include "complex/Filter/Actions/CreateArrayAction.hpp"
+#include "complex/Parameters/ChoicesParameter.hpp"
+#include "complex/Parameters/DataGroupSelectionParameter.hpp"
+#include "complex/Utilities/DataArrayUtilities.hpp"
+
+#include <string>
+
+namespace
+{
+constexpr complex::int32 k_EMPTY_PARAMETER = -123;
+
+complex::FloatVec3 extractOrigin(const complex::AbstractGeometry* geometry)
+{
+  if(auto* image = dynamic_cast<const complex::ImageGeom*>(geometry))
+  {
+    return image->getOrigin();
+  }
+  if(auto* rectGrid = dynamic_cast<const complex::RectGridGeom*>(geometry))
+  {
+    const auto* xBounds = rectGrid->getXBounds()->getDataStore();
+    const auto* yBounds = rectGrid->getYBounds()->getDataStore();
+    const auto* zBounds = rectGrid->getZBounds()->getDataStore();
+    complex::FloatVec3 origin(std::numeric_limits<float>::max(), std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
+    for(size_t i = 0; i < xBounds->getNumberOfTuples(); i++)
+    {
+      if(xBounds->getValue(i) < origin[0])
+      {
+        origin[0] = xBounds->getValue(i);
+      }
+    }
+    for(size_t i = 0; i < yBounds->getNumberOfTuples(); i++)
+    {
+      if(yBounds->getValue(i) < origin[1])
+      {
+        origin[1] = yBounds->getValue(i);
+      }
+    }
+    for(size_t i = 0; i < zBounds->getNumberOfTuples(); i++)
+    {
+      if(zBounds->getValue(i) < origin[2])
+      {
+        origin[2] = zBounds->getValue(i);
+      }
+    }
+    return origin;
+  }
+  if(auto* vertex = dynamic_cast<const complex::VertexGeom*>(geometry))
+  {
+    const auto* vertices = vertex->getVertices()->getDataStore();
+    complex::FloatVec3 origin(std::numeric_limits<float>::max(), std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
+
+    for(size_t i = 0; i < vertex->getNumberOfVertices(); i++)
+    {
+      for(size_t j = 0; j < 3; j++)
+      {
+        if(vertices->getValue(3 * i + j) < origin[j])
+        {
+          origin[j] = vertices->getValue(3 * i + j);
+        }
+      }
+    }
+    return origin;
+  }
+  if(const auto* edge = dynamic_cast<const complex::EdgeGeom*>(geometry))
+  {
+    const auto* vertices = edge->getVertices()->getDataStore();
+    complex::FloatVec3 origin(std::numeric_limits<float>::max(), std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
+
+    for(size_t i = 0; i < edge->getNumberOfVertices(); i++)
+    {
+      for(size_t j = 0; j < 3; j++)
+      {
+        if(vertices->getValue(3 * i + j) < origin[j])
+        {
+          origin[j] = vertices->getValue(3 * i + j);
+        }
+      }
+    }
+    return origin;
+  }
+  if(const auto* geometry2d = dynamic_cast<const complex::AbstractGeometry2D*>(geometry))
+  {
+    const auto* vertices = geometry2d->getVertices()->getDataStore();
+    complex::FloatVec3 origin(std::numeric_limits<float>::max(), std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
+
+    for(size_t i = 0; i < geometry2d->getNumberOfVertices(); i++)
+    {
+      for(size_t j = 0; j < 3; j++)
+      {
+        if(vertices->getValue(3 * i + j) < origin[j])
+        {
+          origin[j] = vertices->getValue(3 * i + j);
+        }
+      }
+    }
+    return origin;
+  }
+  if(const auto* geometry3d = dynamic_cast<const complex::AbstractGeometry3D*>(geometry))
+  {
+    const auto* vertices = geometry3d->getVertices()->getDataStore();
+    complex::FloatVec3 origin(std::numeric_limits<float>::max(), std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
+
+    for(size_t i = 0; i < geometry3d->getNumberOfVertices(); i++)
+    {
+      for(size_t j = 0; j < 3; j++)
+      {
+        if(vertices->getValue(3 * i + j) < origin[j])
+        {
+          origin[j] = vertices->getValue(3 * i + j);
+        }
+      }
+    }
+    return origin;
+  }
+  complex::FloatVec3 origin(std::numeric_limits<float>::max(), std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
+  return origin;
+}
+
+complex::FloatVec3 extractCentroid(const complex::AbstractGeometry* geometry)
+{
+  complex::FloatVec3 centroid(0.0f, 0.0f, 0.0f);
+  if(const auto* image = dynamic_cast<const complex::ImageGeom*>(geometry))
+  {
+    complex::SizeVec3 dims = image->getDimensions();
+    complex::FloatVec3 origin = image->getOrigin();
+    complex::FloatVec3 res = image->getSpacing();
+
+    centroid[0] = (static_cast<float>(dims[0]) * res[0] / 2.0f) + origin[0];
+    centroid[1] = (static_cast<float>(dims[1]) * res[1] / 2.0f) + origin[1];
+    centroid[2] = (static_cast<float>(dims[2]) * res[2] / 2.0f) + origin[2];
+    return centroid;
+  }
+  if(const auto* rectGrid = dynamic_cast<const complex::RectGridGeom*>(geometry))
+  {
+    const auto* xBounds = rectGrid->getXBounds()->getDataStore();
+    const auto* yBounds = rectGrid->getYBounds()->getDataStore();
+    const auto* zBounds = rectGrid->getZBounds()->getDataStore();
+    float min[3] = {std::numeric_limits<float>::max(), std::numeric_limits<float>::max(), std::numeric_limits<float>::max()};
+    float max[3] = {std::numeric_limits<float>::min(), std::numeric_limits<float>::min(), std::numeric_limits<float>::min()};
+    for(size_t i = 0; i < xBounds->getNumberOfTuples(); i++)
+    {
+      if(xBounds->getValue(i) < min[0])
+      {
+        min[0] = xBounds->getValue(i);
+      }
+      if(xBounds->getValue(i) > max[0])
+      {
+        max[0] = xBounds->getValue(i);
+      }
+    }
+    for(size_t i = 0; i < yBounds->getNumberOfTuples(); i++)
+    {
+      if(yBounds->getValue(i) < min[1])
+      {
+        min[1] = yBounds->getValue(i);
+      }
+      if(yBounds->getValue(i) > max[1])
+      {
+        max[1] = yBounds->getValue(i);
+      }
+    }
+    for(size_t i = 0; i < zBounds->getNumberOfTuples(); i++)
+    {
+      if(zBounds->getValue(i) < min[2])
+      {
+        min[2] = zBounds->getValue(i);
+      }
+      if(zBounds->getValue(i) > max[2])
+      {
+        max[2] = zBounds->getValue(i);
+      }
+    }
+    centroid[0] = (max[0] - min[0]) / 2.0f;
+    centroid[1] = (max[1] - min[1]) / 2.0f;
+    centroid[2] = (max[2] - min[2]) / 2.0f;
+    return centroid;
+  }
+  if(const auto* vertex = dynamic_cast<const complex::VertexGeom*>(geometry))
+  {
+    const auto* vertices = vertex->getVertices()->getDataStore();
+    centroid[0] = 0.0f;
+    centroid[1] = 0.0f;
+    centroid[2] = 0.0f;
+    for(size_t i = 0; i < vertex->getNumberOfVertices(); i++)
+    {
+      centroid[0] += vertices->getValue(3 * i + 0);
+      centroid[1] += vertices->getValue(3 * i + 1);
+      centroid[2] += vertices->getValue(3 * i + 2);
+    }
+    centroid[0] /= static_cast<float>(vertex->getNumberOfVertices());
+    centroid[1] /= static_cast<float>(vertex->getNumberOfVertices());
+    centroid[2] /= static_cast<float>(vertex->getNumberOfVertices());
+    return centroid;
+  }
+  if(const auto* edge = dynamic_cast<const complex::EdgeGeom*>(geometry))
+  {
+    const auto& vertices = edge->getVertices()->getDataStoreRef();
+    centroid[0] = 0.0f;
+    centroid[1] = 0.0f;
+    centroid[2] = 0.0f;
+    for(size_t i = 0; i < edge->getNumberOfVertices(); i++)
+    {
+      centroid[0] += vertices[3 * i + 0];
+      centroid[1] += vertices[3 * i + 1];
+      centroid[2] += vertices[3 * i + 2];
+    }
+    centroid[0] /= static_cast<float>(edge->getNumberOfVertices());
+    centroid[1] /= static_cast<float>(edge->getNumberOfVertices());
+    centroid[2] /= static_cast<float>(edge->getNumberOfVertices());
+    return centroid;
+  }
+  if(auto* geometry2d = dynamic_cast<const complex::AbstractGeometry2D*>(geometry))
+  {
+    const auto& vertices = geometry2d->getVertices()->getDataStoreRef();
+    centroid[0] = 0.0f;
+    centroid[1] = 0.0f;
+    centroid[2] = 0.0f;
+    for(size_t i = 0; i < geometry2d->getNumberOfVertices(); i++)
+    {
+      centroid[0] += vertices[3 * i + 0];
+      centroid[1] += vertices[3 * i + 1];
+      centroid[2] += vertices[3 * i + 2];
+    }
+    centroid[0] /= static_cast<float>(geometry2d->getNumberOfVertices());
+    centroid[1] /= static_cast<float>(geometry2d->getNumberOfVertices());
+    centroid[2] /= static_cast<float>(geometry2d->getNumberOfVertices());
+    return centroid;
+  }
+  if(const auto* geometry3d = dynamic_cast<const complex::AbstractGeometry3D*>(geometry))
+  {
+    const auto& vertices = geometry3d->getVertices()->getDataStoreRef();
+    centroid[0] = 0.0f;
+    centroid[1] = 0.0f;
+    centroid[2] = 0.0f;
+    for(size_t i = 0; i < geometry3d->getNumberOfVertices(); i++)
+    {
+      centroid[0] += vertices[3 * i + 0];
+      centroid[1] += vertices[3 * i + 1];
+      centroid[2] += vertices[3 * i + 2];
+    }
+    centroid[0] /= static_cast<float>(geometry3d->getNumberOfVertices());
+    centroid[1] /= static_cast<float>(geometry3d->getNumberOfVertices());
+    centroid[2] /= static_cast<float>(geometry3d->getNumberOfVertices());
+    return centroid;
+    ;
+  }
+  return centroid;
+}
+
+void translateGeometry(complex::AbstractGeometry* geometry, const complex::FloatVec3& translation)
+{
+  if(auto* image = dynamic_cast<complex::ImageGeom*>(geometry))
+  {
+    complex::FloatVec3 origin = image->getOrigin();
+    origin[0] += translation[0];
+    origin[1] += translation[1];
+    origin[2] += translation[2];
+    image->setOrigin(origin);
+    return;
+  }
+  if(auto* rectGrid = dynamic_cast<complex::RectGridGeom*>(geometry))
+  {
+    auto& xBounds = rectGrid->getXBounds()->getDataStoreRef();
+    auto& yBounds = rectGrid->getYBounds()->getDataStoreRef();
+    auto& zBounds = rectGrid->getZBounds()->getDataStoreRef();
+    for(size_t i = 0; i < rectGrid->getNumXPoints(); i++)
+    {
+      xBounds[i] += translation[0];
+    }
+    for(size_t i = 0; i < rectGrid->getNumYPoints(); i++)
+    {
+      yBounds[i] += translation[1];
+    }
+    for(size_t i = 0; i < rectGrid->getNumZPoints(); i++)
+    {
+      zBounds[i] += translation[2];
+    }
+    return;
+  }
+  if(auto* vertex = dynamic_cast<complex::VertexGeom*>(geometry))
+  {
+    auto& vertices = vertex->getVertices()->getDataStoreRef();
+    for(size_t i = 0; i < vertex->getNumberOfVertices(); i++)
+    {
+      vertices[3 * i + 0] += translation[0];
+      vertices[3 * i + 1] += translation[1];
+      vertices[3 * i + 2] += translation[2];
+    }
+    return;
+  }
+  if(auto* edge = dynamic_cast<complex::EdgeGeom*>(geometry))
+  {
+    auto& vertices = edge->getVertices()->getDataStoreRef();
+    for(size_t i = 0; i < edge->getNumberOfVertices(); i++)
+    {
+      vertices[3 * i + 0] += translation[0];
+      vertices[3 * i + 1] += translation[1];
+      vertices[3 * i + 2] += translation[2];
+    }
+    return;
+  }
+  if(auto* geometry2d = dynamic_cast<complex::AbstractGeometry2D*>(geometry))
+  {
+    auto& vertices = geometry2d->getVertices()->getDataStoreRef();
+    for(size_t i = 0; i < geometry2d->getNumberOfVertices(); i++)
+    {
+      vertices[3 * i + 0] += translation[0];
+      vertices[3 * i + 1] += translation[1];
+      vertices[3 * i + 2] += translation[2];
+    }
+    return;
+  }
+  if(auto* geometry3d = dynamic_cast<complex::AbstractGeometry3D*>(geometry))
+  {
+    auto& vertices = geometry3d->getVertices()->getDataStoreRef();
+    for(size_t i = 0; i < geometry3d->getNumberOfVertices(); i++)
+    {
+      vertices[3 * i + 0] += translation[0];
+      vertices[3 * i + 1] += translation[1];
+      vertices[3 * i + 2] += translation[2];
+    }
+    return;
+  }
+}
+
+} // namespace
+
+namespace complex
+{
+
+std::string AlignGeometries::name() const
+{
+  return FilterTraits<AlignGeometries>::name;
+}
+
+std::string AlignGeometries::className() const
+{
+  return FilterTraits<AlignGeometries>::className;
+}
+
+Uuid AlignGeometries::uuid() const
+{
+  return FilterTraits<AlignGeometries>::uuid;
+}
+
+std::string AlignGeometries::humanName() const
+{
+  return "Align Geometries";
+}
+
+Parameters AlignGeometries::parameters() const
+{
+  Parameters params;
+  params.insert(std::make_unique<DataGroupSelectionParameter>(k_MovingGeometry_Key, "Moving Geometry", "Numeric Type of data to create", DataPath()));
+  params.insert(std::make_unique<DataGroupSelectionParameter>(k_TargetGeometry_Key, "Target Geometry", "Number of components", DataPath()));
+  params.insert(std::make_unique<ChoicesParameter>(k_AlignmentType_Key, "Alignment Type", "Alignment Type", 0, std::vector<std::string>{"Origin", "Centroid"}));
+  return params;
+}
+
+IFilter::UniquePointer AlignGeometries::clone() const
+{
+  return std::make_unique<AlignGeometries>();
+}
+
+IFilter::PreflightResult AlignGeometries::preflightImpl(const DataStructure& dataStructure, const Arguments& filterArgs, const MessageHandler& messageHandler) const
+{
+  auto movingGeometryPath = filterArgs.value<DataPath>(k_MovingGeometry_Key);
+  auto targetGeometryPath = filterArgs.value<DataPath>(k_TargetGeometry_Key);
+  auto alignmentType = filterArgs.value<uint64>(k_AlignmentType_Key);
+
+  auto* movingGeometry = dataStructure.getDataAs<AbstractGeometry>(movingGeometryPath);
+  auto* targetGeometry = dataStructure.getDataAs<AbstractGeometry>(targetGeometryPath);
+
+  if(alignmentType != 0 && alignmentType != 1)
+  {
+    std::string ss = fmt::format("Invalid selection for alignment type");
+    return {MakeErrorResult<OutputActions>(-1, ss)};
+  }
+
+  OutputActions actions;
+  return {std::move(actions)};
+}
+
+Result<> AlignGeometries::executeImpl(DataStructure& data, const Arguments& args, const PipelineFilter* pipelineNode, const MessageHandler& messageHandler) const
+{
+  auto movingGeometryPath = args.value<DataPath>(k_MovingGeometry_Key);
+  auto targetGeometryPath = args.value<DataPath>(k_TargetGeometry_Key);
+  auto alignmentType = args.value<uint64>(k_AlignmentType_Key);
+
+  auto* moving = data.getDataAs<AbstractGeometry>(movingGeometryPath);
+  auto* target = data.getDataAs<AbstractGeometry>(targetGeometryPath);
+
+  if(alignmentType == 0)
+  {
+    FloatVec3 movingOrigin = extractOrigin(moving);
+    FloatVec3 targetOrigin = extractOrigin(target);
+
+    float translation[3] = {targetOrigin[0] - movingOrigin[0], targetOrigin[1] - movingOrigin[1], targetOrigin[2] - movingOrigin[2]};
+    translateGeometry(moving, translation);
+  }
+  else if(alignmentType == 1)
+  {
+    FloatVec3 movingCentroid = extractCentroid(moving);
+    FloatVec3 targetCentroid = extractCentroid(target);
+
+    float translation[3] = {targetCentroid[0] - movingCentroid[0], targetCentroid[0] - movingCentroid[0], targetCentroid[0] - movingCentroid[0]};
+    translateGeometry(moving, translation);
+  }
+  else
+  {
+    std::string ss = fmt::format("Invalid selection for alignment type");
+    return {MakeErrorResult<>(-1, ss)};
+  }
+
+  return {};
+}
+} // namespace complex
---
+++ src/Plugins/ComplexCore/src/ComplexCore/Filters/AlignGeometries.hpp
@@ -0,0 +1,86 @@
+#pragma once
+
+#include "ComplexCore/ComplexCore_export.hpp"
+
+#include "complex/Common/StringLiteral.hpp"
+#include "complex/Filter/FilterTraits.hpp"
+#include "complex/Filter/IFilter.hpp"
+
+namespace complex
+{
+class COMPLEXCORE_EXPORT AlignGeometries : public IFilter
+{
+public:
+  AlignGeometries() = default;
+  ~AlignGeometries() noexcept override = default;
+
+  AlignGeometries(const AlignGeometries&) = delete;
+  AlignGeometries(AlignGeometries&&) noexcept = delete;
+
+  AlignGeometries& operator=(const AlignGeometries&) = delete;
+  AlignGeometries& operator=(AlignGeometries&&) noexcept = delete;
+
+  // Parameter Keys
+  static inline constexpr StringLiteral k_MovingGeometry_Key = "moving_geometry";
+  static inline constexpr StringLiteral k_TargetGeometry_Key = "target_geometry";
+  static inline constexpr StringLiteral k_AlignmentType_Key = "alignment_type";
+
+  /**
+   * @brief
+   * @return std::string
+   */
+  std::string name() const override;
+
+  /**
+   * @brief Returns the C++ classname of this filter.
+   * @return std::string
+   */
+  std::string className() const override;
+
+  /**
+   * @brief
+   * @return Uuid
+   */
+  Uuid uuid() const override;
+
+  /**
+   * @brief
+   * @return std::string
+   */
+  std::string humanName() const override;
+
+  /**
+   * @brief
+   * @return Parameters
+   */
+  Parameters parameters() const override;
+
+  /**
+   * @brief
+   * @return UniquePointer
+   */
+  UniquePointer clone() const override;
+
+protected:
+  /**
+   * @brief
+   * @param data
+   * @param filterArgs
+   * @param messageHandler
+   * @return Result<OutputActions>
+   */
+  PreflightResult preflightImpl(const DataStructure& data, const Arguments& filterArgs, const MessageHandler& messageHandler) const override;
+
+  /**
+   * @brief
+   * @param dataStructure
+   * @param args
+   * @param pipelineNode
+   * @param messageHandler
+   * @return Result<>
+   */
+  Result<> executeImpl(DataStructure& dataStructure, const Arguments& args, const PipelineFilter* pipelineNode, const MessageHandler& messageHandler) const override;
+};
+} // namespace complex
+
+COMPLEX_DEF_FILTER_TRAITS(complex, AlignGeometries, "ce1ee404-0336-536c-8aad-f9641c9458be");
