c2d2359f6779e05576411bf09c4962e85ed973b3
Michael Jackson <mike.jackson@bluequartz.net>
2022-01-26 16:44:03 -0500

    Rearrange parameter order in Example Filter 2
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- src/Plugins/ComplexCore/src/ComplexCore/Filters/ExampleFilter2.cpp
+++ src/Plugins/ComplexCore/src/ComplexCore/Filters/ExampleFilter2.cpp
@@ -61,26 +61,26 @@ Parameters ExampleFilter2::parameters() const
 {
   Parameters params;
   params.insertSeparator({"1rst Group of Parameters"});
-
-  params.insertSeparator({"2nd Group of Parameters"});
-  params.insert(std::make_unique<ArrayCreationParameter>(k_Param5, "Array Creation", "", ArrayCreationParameter::ValueType{}));
-  params.insert(std::make_unique<ArraySelectionParameter>(k_Param6, "Array Selection", "", ArraySelectionParameter::ValueType{}));
-
-  params.insertSeparator({"3rd Group of Parameters"});
   params.insertLinkableParameter(std::make_unique<BoolParameter>(k_Param7, "Bool Parameter", "", true));
   params.insert(std::make_unique<ChoicesParameter>(k_Param3, "ChoicesParameter", "", 0, ChoicesParameter::Choices{"foo", "bar", "baz"}));
 
-  params.insertSeparator({"4th Group of Parameters"});
-  params.insert(std::make_unique<DataGroupCreationParameter>(k_Param8, "DataGroupCreationParameter", "", DataPath{}));
-  params.insert(std::make_unique<DataGroupSelectionParameter>(k_Param9, "DataGroupSelectionParameter", "", DataPath{}));
-  params.insert(std::make_unique<DataPathSelectionParameter>(k_Param10, "DataPathSelectionParameter", "", DataPath{}));
-
-  params.insert(std::make_unique<GeometrySelectionParameter>(k_Param11, "GeometrySelectionParameter", "", DataPath{}, std::set<DataObject::Type>{}));
-  params.insert(std::make_unique<MultiArraySelectionParameter>(k_Param12, "MultiArraySelectionParameter", "", MultiArraySelectionParameter::ValueType{}));
+  params.insertSeparator({"2nd Group of Parameters"});
   DynamicTableParameter::ValueType dynamicTable{{{10, 20}, {30, 40}}, {"Col 1", "Col 2"}, {"Row 1", "Row2"}};
   dynamicTable.setMinCols(2);
   dynamicTable.setDynamicCols(true);
+  dynamicTable.setDynamicRows(true);
   params.insert(std::make_unique<DynamicTableParameter>(k_Param13, "DynamicTableParameter", "", dynamicTable));
+
+  // These should show up under the "Required Objects" Section in the GUI
+  params.insert(std::make_unique<DataGroupSelectionParameter>(k_Param9, "DataGroupSelectionParameter", "", DataPath{}));
+  params.insert(std::make_unique<DataPathSelectionParameter>(k_Param10, "DataPathSelectionParameter", "", DataPath{}));
+  params.insert(std::make_unique<ArraySelectionParameter>(k_Param6, "Array Selection", "", ArraySelectionParameter::ValueType{}));
+  params.insert(std::make_unique<GeometrySelectionParameter>(k_Param11, "GeometrySelectionParameter", "", DataPath{}, std::set<DataObject::Type>{}));
+  params.insert(std::make_unique<MultiArraySelectionParameter>(k_Param12, "MultiArraySelectionParameter", "", MultiArraySelectionParameter::ValueType{}));
+
+  // These should show up under the "Created Objects" section in the GUI
+  params.insert(std::make_unique<DataGroupCreationParameter>(k_Param8, "DataGroupCreationParameter", "", DataPath{}));
+  params.insert(std::make_unique<ArrayCreationParameter>(k_Param5, "Array Creation", "", ArrayCreationParameter::ValueType{}));
 
   return params;
 }
