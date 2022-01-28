6c43c590f459ade34b006b96ae9ef7f9d7d88d3c
Jared Duffey <jared.duffey@bluequartz.net>
2022-01-28 14:13:08 -0500

    Fixed formatting of FileSystemPathParameter
    Signed-off-by: Jared Duffey <jared.duffey@bluequartz.net>

--- src/complex/Parameters/FileSystemPathParameter.hpp
+++ src/complex/Parameters/FileSystemPathParameter.hpp
@@ -31,7 +31,8 @@ public:
   using ValueType = std::filesystem::path;
 
   FileSystemPathParameter() = delete;
-  FileSystemPathParameter(const std::string& name, const std::string& humanName, const std::string& helpText, const ValueType& defaultValue, const ExtensionsType& extensionsType, PathType pathType, bool shouldValidateExtension = true);
+  FileSystemPathParameter(const std::string& name, const std::string& humanName, const std::string& helpText, const ValueType& defaultValue, const ExtensionsType& extensionsType, PathType pathType,
+                          bool shouldValidateExtension = true);
   ~FileSystemPathParameter() override = default;
 
   FileSystemPathParameter(const FileSystemPathParameter&) = delete;
