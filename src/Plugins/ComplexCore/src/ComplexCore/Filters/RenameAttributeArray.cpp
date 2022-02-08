9c02522018581c7110c9c47332d261d1ef226b73
Matthew Marine <matthew.marine@bluequartz.net>
2022-02-08 14:13:58 -0500

    Applied PR changes
    Applied changes for pull request

--- src/Plugins/ComplexCore/src/ComplexCore/Filters/RenameAttributeArray.cpp
+++ src/Plugins/ComplexCore/src/ComplexCore/Filters/RenameAttributeArray.cpp
@@ -11,41 +11,6 @@ namespace
 {
 constexpr complex::int32 k_ARRAY_NOT_FOUND = -700;
 constexpr complex::int32 k_COULD_NOT_RENAME = -701;
-
-template <typename T>
-struct ULLConvertor
-{
-  T operator()(const std::string& initValue)
-  {
-    return static_cast<T>(std::stoull(initValue));
-  }
-};
-
-struct Float32Convertor
-{
-  complex::float32 operator()(const std::string& initValue)
-  {
-    return static_cast<complex::float32>(std::stof(initValue));
-  }
-};
-
-struct Float64Convertor
-{
-  complex::float64 operator()(const std::string& initValue)
-  {
-    return static_cast<complex::float64>(std::stod(initValue));
-  }
-};
-
-template <typename T, class Convertor>
-void CreateAndInitArray(const std::string& initValue, complex::DataStructure& data, const complex::DataPath& path)
-{
-  T value = static_cast<T>(Convertor()(initValue));
-  auto& dataArray = complex::ArrayRefFromPath<T>(data, path);
-  auto& v = *(dataArray.getDataStore());
-  std::fill(v.begin(), v.end(), value);
-}
-
 } // namespace
 
 namespace complex
@@ -89,13 +54,8 @@ IFilter::PreflightResult RenameAttributeArray::preflightImpl(const DataStructure
   auto arrayPath = filterArgs.value<DataPath>(k_ArrayPath_Key);
   auto newName = filterArgs.value<std::string>(k_NewName_Key);
 
-  auto dataArray = dataStructure.getDataAs<IDataArray>(arrayPath);
-  if(dataArray == nullptr)
-  {
-    std::string ss = fmt::format("Could not find DataArray at path '{}'", arrayPath.toString());
-    return {MakeErrorResult<OutputActions>(::k_ARRAY_NOT_FOUND, ss)};
-  }
-  if(!dataArray->canRename(newName))
+  auto& dataArray = dataStructure.getDataRefAs<IDataArray>(arrayPath);
+  if(!dataArray.canRename(newName))
   {
     std::string ss = fmt::format("Could not rename DataArray at path '{}' to '{}'", arrayPath.toString(), newName);
     return {MakeErrorResult<OutputActions>(::k_COULD_NOT_RENAME, ss)};
@@ -110,8 +70,8 @@ Result<> RenameAttributeArray::executeImpl(DataStructure& data, const Arguments&
   auto arrayPath = args.value<DataPath>(k_ArrayPath_Key);
   auto newName = args.value<std::string>(k_NewName_Key);
 
-  auto dataArray = data.getDataAs<IDataArray>(arrayPath);
-  dataArray->rename(newName);
+  auto& dataArray = data.getDataRefAs<IDataArray>(arrayPath);
+  dataArray.rename(newName);
 
   return {};
 }
