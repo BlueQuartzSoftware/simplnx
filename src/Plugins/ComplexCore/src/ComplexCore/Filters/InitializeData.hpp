06e7c5e5a74a0f104cfaa58260cecfc298a2f370
Jared Duffey <jared.duffey@bluequartz.net>
2022-02-16 17:16:19 -0500

    Added InitializeData filter
    Signed-off-by: Jared Duffey <jared.duffey@bluequartz.net>

---
+++ src/Plugins/ComplexCore/src/ComplexCore/Filters/InitializeData.hpp
@@ -0,0 +1,103 @@
+#pragma once
+
+#include "ComplexCore/ComplexCore_export.hpp"
+
+#include "complex/Common/StringLiteral.hpp"
+#include "complex/Filter/FilterTraits.hpp"
+#include "complex/Filter/IFilter.hpp"
+
+namespace complex
+{
+class COMPLEXCORE_EXPORT InitializeData : public IFilter
+{
+public:
+  InitializeData() = default;
+  ~InitializeData() noexcept override = default;
+
+  InitializeData(const InitializeData&) = delete;
+  InitializeData(InitializeData&&) noexcept = delete;
+
+  InitializeData& operator=(const InitializeData&) = delete;
+  InitializeData& operator=(InitializeData&&) noexcept = delete;
+
+  // Parameter Keys
+  static inline constexpr StringLiteral k_CellArrayPaths_Key = "cell_arrays";
+  static inline constexpr StringLiteral k_ImageGeometryPath_Key = "image_geom_path";
+  static inline constexpr StringLiteral k_XMin_Key = "x_min";
+  static inline constexpr StringLiteral k_YMin_Key = "y_min";
+  static inline constexpr StringLiteral k_ZMin_Key = "z_min";
+  static inline constexpr StringLiteral k_XMax_Key = "x_max";
+  static inline constexpr StringLiteral k_YMax_Key = "y_max";
+  static inline constexpr StringLiteral k_ZMax_Key = "z_max";
+  static inline constexpr StringLiteral k_InitType_Key = "init_type";
+  static inline constexpr StringLiteral k_InitValue_Key = "init_value";
+  static inline constexpr StringLiteral k_InitRange_Key = "init_range";
+
+  enum class InitType : uint64
+  {
+    Manual = 0,
+    Random,
+    RandomWithRange
+  };
+
+  /**
+   * @brief
+   * @return std::string
+   */
+  std::string name() const override;
+
+  /**
+   * @brief Returns the C++ classname of this filter.
+   * @return std::string
+   */
+  std::string className() const override;
+
+  /**
+   * @brief
+   * @return Uuid
+   */
+  Uuid uuid() const override;
+
+  /**
+   * @brief
+   * @return std::string
+   */
+  std::string humanName() const override;
+
+  /**
+   * @brief
+   * @return Parameters
+   */
+  Parameters parameters() const override;
+
+  /**
+   * @brief
+   * @return UniquePointer
+   */
+  UniquePointer clone() const override;
+
+protected:
+  /**
+   * @brief
+   * @param data
+   * @param args
+   * @param messageHandler
+   * @param shouldCancel
+   * @return PreflightResult
+   */
+  PreflightResult preflightImpl(const DataStructure& data, const Arguments& args, const MessageHandler& messageHandler, const std::atomic_bool& shouldCancel) const override;
+
+  /**
+   * @brief
+   * @param data
+   * @param args
+   * @param pipelineNode
+   * @param messageHandler
+   * @param shouldCancel
+   * @return Result<>
+   */
+  Result<> executeImpl(DataStructure& data, const Arguments& args, const PipelineFilter* pipelineNode, const MessageHandler& messageHandler, const std::atomic_bool& shouldCancel) const override;
+};
+} // namespace complex
+
+COMPLEX_DEF_FILTER_TRAITS(complex, InitializeData, "dfab9921-fea3-521c-99ba-48db98e43ff8");
