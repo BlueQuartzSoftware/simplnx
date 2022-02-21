7256e1f5d5a73b7dbc5e204a5c0e2cea374b4f50
Michael Jackson <mike.jackson@bluequartz.net>
2022-02-21 17:26:19 -0500

    ChoiceParameter: Update validation error message
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- test/UnitTestCommon/include/complex/UnitTest/UnitTestCommon.hpp
+++ test/UnitTestCommon/include/complex/UnitTest/UnitTestCommon.hpp
@@ -47,6 +47,7 @@ inline constexpr StringLiteral k_ImageGeometry("Image Geometry");
 inline constexpr StringLiteral k_VertexGeometry("Vertex Geometry");
 inline constexpr StringLiteral k_ConfidenceIndex("Confidence Index");
 inline constexpr StringLiteral k_EulerAngles("EulerAngles");
+inline constexpr StringLiteral k_AxisAngles("AxisAngles");
 inline constexpr StringLiteral k_FeatureGroupName("Feature Data");
 inline constexpr StringLiteral k_ActiveName("Active");
 inline constexpr StringLiteral k_SlipVector("SlipVector");
