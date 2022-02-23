c0621a0bf6af4e879ab4d7695b840880e02a30df
Joey Kleingers <joey.kleingers@bluequartz.net>
2022-02-23 15:01:05 -0500

    Finish unit test.
    Signed-off-by: Joey Kleingers <joey.kleingers@bluequartz.net>

--- src/Plugins/ComplexCore/test/CopyFeatureArrayToElementArrayTest.cpp
+++ src/Plugins/ComplexCore/test/CopyFeatureArrayToElementArrayTest.cpp
@@ -1,72 +1,73 @@
-/**
- * This file is auto generated from the original Core/CopyFeatureArrayToElementArray
- * runtime information. These are the steps that need to be taken to utilize this
- * unit test in the proper way.
- *
- * 1: Validate each of the default parameters that gets created.
- * 2: Inspect the actual filter to determine if the filter in its default state
- * would pass or fail BOTH the preflight() and execute() methods
- * 3: UPDATE the ```REQUIRE(result.result.valid());``` code to have the proper
- *
- * 4: Add additional unit tests to actually test each code path within the filter
- *
- * There are some example Catch2 ```TEST_CASE``` sections for your inspiration.
- *
- * NOTE the format of the ```TEST_CASE``` macro. Please stick to this format to
- * allow easier parsing of the unit tests.
- *
- * When you start working on this unit test remove "[CopyFeatureArrayToElementArray][.][UNIMPLEMENTED]"
- * from the TEST_CASE macro. This will enable this unit test to be run by default
- * and report errors.
- */
-
 #include <catch2/catch.hpp>
 
 #include "ComplexCore/Filters/CopyFeatureArrayToElementArray.hpp"
-#include "ComplexCore/Filters/RawBinaryReaderFilter.hpp"
-#include "complex/Parameters/ArrayCreationParameter.hpp"
-#include "complex/Parameters/ArraySelectionParameter.hpp"
-#include "complex/Parameters/DynamicTableParameter.hpp"
-
-#include "ComplexCore/ComplexCore_test_dirs.hpp"
-
-#include "complex/UnitTest/UnitTestCommon.hpp"
+#include "complex/DataStructure/DataArray.hpp"
+#include "complex/DataStructure/DataStore.hpp"
 
 using namespace complex;
-namespace fs = std::filesystem;
 
 namespace
 {
 const int k_EmptyArrayPath = -201;
-const std::string k_FeaturePhasesFileName = "Feature_Phases.raw";
-const std::string k_FeatureIdsFileName = "FeatureIds.raw";
-const DataPath k_FeaturePhasesPath({"Feature Phases"});
-const DataPath k_FeatureIDsPath({"Feature IDs"});
-const DataPath k_CreatedArrayPath({"New Array"});
 
-DataStructure createDataStructure()
+const std::string k_CellFeatureIdsArrayName("FeatureIds");
+const std::string k_CellTempArrayName("Cell Temperature");
+const std::string k_FeatureDataArrayName("Feature Temperature");
+
+template <typename T>
+void TestCopyFeatureArrayToElementArrayForType()
 {
   DataStructure ds;
+
+  // Create Cell FeatureIds array
+  Int32Array* cellFeatureIdsPtr = Int32Array::CreateWithStore<DataStore<int32>>(ds, k_CellFeatureIdsArrayName, {{10, 3}}, {1});
+  REQUIRE(cellFeatureIdsPtr != nullptr);
+  Int32Array& cellFeatureIds = *cellFeatureIdsPtr;
+
+  for(usize y = 0; y < 3; y++)
+  {
+    for(usize x = 0; x < 10; x++)
+    {
+      usize index = (10 * y) + x;
+      cellFeatureIds[index] = static_cast<int32>(y);
+    }
+  }
+
+  // Create a feature data array with 3 values
+  DataArray<T>* avgTempValuePtr = DataArray<T>::template CreateWithStore<DataStore<T>>(ds, k_FeatureDataArrayName, {3}, {1});
+  REQUIRE(avgTempValuePtr != nullptr);
+  DataArray<T>& avgTempValue = *avgTempValuePtr;
+
+  for(int i = 0; i < 3; i++)
+  {
+    avgTempValue[i] = static_cast<T>(0);
+  }
+
+  // Create filter and set arguments
+  CopyFeatureArrayToElementArray filter;
   Arguments args;
 
-  RawBinaryReaderFilter rbrFilter;
-  args.insertOrAssign(RawBinaryReaderFilter::k_InputFile_Key, fs::path(unit_test::k_TestDataSourceDir.str()).append(k_FeaturePhasesFileName));
-  args.insertOrAssign(RawBinaryReaderFilter::k_NumberOfComponents_Key, std::make_any<uint64>(1));
-  args.insertOrAssign(RawBinaryReaderFilter::k_ScalarType_Key, NumericType::int32);
-  args.insertOrAssign(RawBinaryReaderFilter::k_TupleDims_Key, DynamicTableParameter::ValueType({{{796}}, {}, {}}));
-  args.insertOrAssign(RawBinaryReaderFilter::k_CreatedAttributeArrayPath_Key, std::make_any<DataPath>(k_FeaturePhasesPath));
+  args.insertOrAssign(CopyFeatureArrayToElementArray::k_SelectedFeatureArrayPath_Key, std::make_any<DataPath>(DataPath({k_FeatureDataArrayName})));
+  args.insertOrAssign(CopyFeatureArrayToElementArray::k_FeatureIdsArrayPath_Key, std::make_any<DataPath>(DataPath({k_CellFeatureIdsArrayName})));
+  args.insertOrAssign(CopyFeatureArrayToElementArray::k_CreatedArrayName_Key, std::make_any<DataPath>(DataPath({k_CellTempArrayName})));
 
-  auto result = rbrFilter.execute(ds, args);
-  COMPLEX_RESULT_REQUIRE_VALID(result.result);
+  // Preflight the filter
+  auto preflightResult = filter.preflight(ds, args);
+  REQUIRE(preflightResult.outputActions.valid());
 
-  args.insertOrAssign(RawBinaryReaderFilter::k_InputFile_Key, fs::path(unit_test::k_TestDataSourceDir.str()).append(k_FeatureIdsFileName));
-  args.insertOrAssign(RawBinaryReaderFilter::k_TupleDims_Key, DynamicTableParameter::ValueType({{{1000000}}, {}, {}}));
-  args.insertOrAssign(RawBinaryReaderFilter::k_CreatedAttributeArrayPath_Key, std::make_any<DataPath>(k_FeatureIDsPath));
+  // Execute the filter
+  auto executeResult = filter.execute(ds, args);
+  REQUIRE(executeResult.result.valid());
 
-  result = rbrFilter.execute(ds, args);
-  COMPLEX_RESULT_REQUIRE_VALID(result.result);
-
-  return ds;
+  // Check the filter results
+  DataArray<T>& createdElementArray = ds.getDataRefAs<DataArray<T>>(DataPath({k_CellTempArrayName}));
+  for(usize i = 0; i < createdElementArray.getNumberOfTuples(); i++)
+  {
+    int32 featureId = cellFeatureIds[i];
+    T value = createdElementArray[i];
+    T featureValue = avgTempValue[featureId];
+    REQUIRE(value == featureValue);
+  }
 }
 } // namespace
 
@@ -103,26 +104,14 @@ TEST_CASE("Core::CopyFeatureArrayToElementArray: Instantiation and Parameter Che
 
 TEST_CASE("Core::CopyFeatureArrayToElementArray: Valid filter execution", "[Core][CopyFeatureArrayToElementArray]")
 {
-  // Instantiate the filter, a DataStructure object and an Arguments Object
-  CopyFeatureArrayToElementArray filter;
-  DataStructure ds = createDataStructure();
-  Arguments args;
-
-  // Create default Parameters for the filter.
-  args.insertOrAssign(CopyFeatureArrayToElementArray::k_SelectedFeatureArrayPath_Key, std::make_any<DataPath>(k_FeaturePhasesPath));
-  args.insertOrAssign(CopyFeatureArrayToElementArray::k_FeatureIdsArrayPath_Key, std::make_any<DataPath>(k_FeatureIDsPath));
-  args.insertOrAssign(CopyFeatureArrayToElementArray::k_CreatedArrayName_Key, std::make_any<DataPath>(k_CreatedArrayPath));
-
-  // Preflight the filter and check result
-  auto preflightResult = filter.preflight(ds, args);
-  REQUIRE(preflightResult.outputActions.valid());
-
-  // Execute the filter and check the result
-  auto executeResult = filter.execute(ds, args);
-  REQUIRE(executeResult.result.valid());
+  TestCopyFeatureArrayToElementArrayForType<uint8>();
+  TestCopyFeatureArrayToElementArrayForType<int8>();
+  TestCopyFeatureArrayToElementArrayForType<uint16>();
+  TestCopyFeatureArrayToElementArrayForType<int16>();
+  TestCopyFeatureArrayToElementArrayForType<uint32>();
+  TestCopyFeatureArrayToElementArrayForType<int32>();
+  TestCopyFeatureArrayToElementArrayForType<uint64>();
+  TestCopyFeatureArrayToElementArrayForType<int64>();
+  TestCopyFeatureArrayToElementArrayForType<float32>();
+  TestCopyFeatureArrayToElementArrayForType<float64>();
 }
-
-// TEST_CASE("Core::CopyFeatureArrayToElementArray: InValid filter execution", "[Core][CopyFeatureArrayToElementArray]")
-//{
-//
-//}
