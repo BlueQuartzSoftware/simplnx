a02776c0a969dc65b4ca49d99ea15ee7a2526fbc
Michael Jackson <mike.jackson@bluequartz.net>
2022-03-15 18:23:03 -0400

    PipelineRunner: print filter messages while running. Improve error reporting
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- src/PipelineRunner/src/PRObserver.cpp
+++ src/PipelineRunner/src/PRObserver.cpp
@@ -9,11 +9,56 @@ using namespace complex;
 using namespace complex::PipelineRunner;
 
 PipelineObserver::PipelineObserver(Pipeline* pipeline)
-: PipelineNodeObserver()
 {
   if(pipeline != nullptr)
   {
     startObservingNode(pipeline);
+  }
+  if(pipeline == nullptr)
+  {
+    return;
+  }
+  int32_t currentFilterIndex = 0;
+  for(auto iter = pipeline->begin(); iter != pipeline->end(); iter++)
+  {
+    std::shared_ptr<complex::AbstractPipelineNode> cxFilter = *iter;
+
+    m_SignalConnections.push_back(cxFilter->getFilterRunStateSignal().connect([currentFilterIndex](complex::AbstractPipelineNode* node, int32_t filterIndex, complex::RunState state) {
+      // std::cout << "[" << currentFilterIndex << "] Changed State" << std::endl;
+    }));
+
+    m_SignalConnections.push_back(cxFilter->getFilterUpdateSignal().connect([currentFilterIndex](complex::AbstractPipelineNode* node, int32_t, const std::string& message) {
+      std::cout << "[" << currentFilterIndex << "] " << node->getName() << ": " << message << std::endl;
+    }));
+
+    m_SignalConnections.push_back(cxFilter->getFilterProgressSignal().connect([currentFilterIndex](complex::AbstractPipelineNode* node, int32_t, int32_t progress, const std::string& message) {
+      std::cout << "[" << currentFilterIndex << "] " << node->getName() << ": " << progress << "% " << message << std::endl;
+    }));
+
+    m_SignalConnections.push_back(cxFilter->getFilterFaultSignal().connect([currentFilterIndex](complex::AbstractPipelineNode*, int32_t filterIndex, complex::FaultState state) {
+      if(state != complex::FaultState::None)
+      {
+        std::cout << "[" << currentFilterIndex << "] Errors thrown... Fault state= " << static_cast<int32_t>(state) << std::endl;
+      }
+    }));
+
+    m_SignalConnections.push_back(cxFilter->getFilterFaultDetailSignal().connect(
+        [currentFilterIndex](complex::AbstractPipelineNode*, int32_t filterIndex, const complex::WarningCollection& warnings, const complex::ErrorCollection& errors) {
+          if(!warnings.empty() || !errors.empty())
+          {
+            std::cout << "[" << currentFilterIndex << "] Warnings/Errors During Execution" << std::endl;
+          }
+          for(const auto& warn : warnings)
+          {
+            std::cout << "    Code: " << warn.code << "    Message: " << warn.message << std::endl;
+          }
+          for(const auto& error : errors)
+          {
+            std::cout << "    Code: " << error.code << "    Message: " << error.message << std::endl;
+          }
+        }));
+
+    currentFilterIndex++;
   }
 }
 
