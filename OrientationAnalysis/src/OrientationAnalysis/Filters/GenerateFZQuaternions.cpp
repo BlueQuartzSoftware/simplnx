007d60314248ff40b1f17928940d3c12242fe835
Michael Jackson <mike.jackson@bluequartz.net>
2022-03-15 11:32:40 -0400

    FILTER: GenerateFZQuaternions (#4)
    * Add filter GenerateFZQuaternions
    * Added unit test for Cubic to validate conversion
    * Disabled Parallel execution of the filter due to DataStore probably not being threadsafe.
    
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- OrientationAnalysis/src/OrientationAnalysis/Filters/GenerateFZQuaternions.cpp
+++ OrientationAnalysis/src/OrientationAnalysis/Filters/GenerateFZQuaternions.cpp
@@ -1,13 +1,111 @@
 #include "GenerateFZQuaternions.hpp"
 
+#include "complex/DataStructure/DataArray.hpp"
 #include "complex/DataStructure/DataPath.hpp"
 #include "complex/Filter/Actions/CreateArrayAction.hpp"
-#include "complex/Filter/Actions/EmptyAction.hpp"
 #include "complex/Parameters/ArrayCreationParameter.hpp"
 #include "complex/Parameters/ArraySelectionParameter.hpp"
 #include "complex/Parameters/BoolParameter.hpp"
+#include "complex/Utilities/ParallelDataAlgorithm.hpp"
+
+#include "EbsdLib/Core/EbsdLibConstants.h"
+#include "EbsdLib/LaueOps/LaueOps.h"
 
 using namespace complex;
+
+namespace
+{
+/**
+ * @brief The GenerateFZQuatsImpl class implements a threaded algorithm that computes the Fundamental Zone Quaternion
+ * for a given Quaternion and Laue Class (which is based from the crystalStructures array
+ */
+template <typename MaskArrayType>
+class GenerateFZQuatsImpl
+{
+public:
+  GenerateFZQuatsImpl(Float32Array& quats, Int32Array& phases, UInt32Array& crystalStructures, int32_t numPhases, MaskArrayType* goodVoxels, Float32Array& fzQuats,
+                      const std::atomic_bool& shouldCancel, std::atomic_int32_t& warningCount)
+  : m_Quats(quats)
+  , m_CellPhases(phases)
+  , m_CrystalStructures(crystalStructures)
+  , m_NumPhases(numPhases)
+  , m_GoodVoxels(goodVoxels)
+  , m_FZQuats(fzQuats)
+  , m_ShouldCancel(shouldCancel)
+  , m_WarningCount(warningCount)
+  {
+  }
+
+  virtual ~GenerateFZQuatsImpl() = default;
+
+  /**
+   * @brief convert
+   * @param start
+   * @param end
+   */
+  void convert(size_t start, size_t end) const
+  {
+    std::vector<LaueOps::Pointer> ops = LaueOps::GetAllOrientationOps();
+    int32_t phase = 0;
+    bool generateFZQuat = false;
+    size_t index = 0;
+
+    for(size_t i = start; i < end; i++)
+    {
+      if(m_ShouldCancel)
+      {
+        break;
+      }
+      phase = m_CellPhases[i];
+
+      generateFZQuat = true;
+      if(nullptr != m_GoodVoxels)
+      {
+        generateFZQuat = static_cast<bool>((*m_GoodVoxels)[i]);
+      }
+
+      // Sanity check the phase data to make sure we do not walk off the end of the array
+      if(phase >= m_NumPhases)
+      {
+        m_WarningCount++;
+      }
+
+      // Initialize the output to zero. There really isn't a good value to use.
+      index = i * 4;
+      m_FZQuats[index] = 0.0f;
+      m_FZQuats[index + 1] = 0.0f;
+      m_FZQuats[index + 2] = 0.0f;
+      m_FZQuats[index + 3] = 0.0f;
+
+      if(phase < m_NumPhases && generateFZQuat && m_CrystalStructures[phase] < EbsdLib::CrystalStructure::LaueGroupEnd)
+      {
+        QuatD quatD = QuatD(m_Quats[index], m_Quats[index + 1], m_Quats[index + 2], m_Quats[index + 3]); // Makes a copy into q
+        int32_t xtal = static_cast<int32_t>(m_CrystalStructures[phase]);                                 // get the Laue Group
+        quatD = ops[xtal]->getFZQuat(quatD);
+        m_FZQuats[index] = quatD.x();
+        m_FZQuats[index + 1] = quatD.y();
+        m_FZQuats[index + 2] = quatD.z();
+        m_FZQuats[index + 3] = quatD.w();
+      }
+    }
+  }
+
+  void operator()(const ComplexRange& range) const
+  {
+    convert(range.min(), range.max());
+  }
+
+private:
+  Float32Array& m_Quats;
+  Int32Array& m_CellPhases;
+  UInt32Array& m_CrystalStructures;
+  int32_t m_NumPhases = 0;
+  MaskArrayType* m_GoodVoxels;
+  Float32Array& m_FZQuats;
+  const std::atomic_bool& m_ShouldCancel;
+  std::atomic_int32_t& m_WarningCount;
+};
+} // namespace
 
 namespace complex
 {
@@ -45,19 +143,25 @@ std::vector<std::string> GenerateFZQuaternions::defaultTags() const
 Parameters GenerateFZQuaternions::parameters() const
 {
   Parameters params;
+
+  std::vector<std::string> names = LaueOps::GetLaueNames();
+  names.pop_back();
+
   // Create the parameter descriptors that are needed for this filter
-  params.insertLinkableParameter(std::make_unique<BoolParameter>(k_UseGoodVoxels_Key, "Apply to Good Elements Only (Bad Elements Will Be Black)", "", false));
   params.insertSeparator(Parameters::Separator{"Element Data"});
-  params.insert(std::make_unique<ArraySelectionParameter>(k_QuatsArrayPath_Key, "Quaternions", "", DataPath{}));
-  params.insert(std::make_unique<ArraySelectionParameter>(k_CellPhasesArrayPath_Key, "Phases", "", DataPath{}));
-  params.insert(std::make_unique<ArraySelectionParameter>(k_GoodVoxelsArrayPath_Key, "Mask", "", DataPath{}));
+  params.insert(std::make_unique<ArraySelectionParameter>(k_QuatsArrayPath_Key, "Input Quaternions", "The input quaternions to convert.", DataPath{}, false,
+                                                          ArraySelectionParameter::AllowedTypes{DataType::float32}));
+  params.insert(std::make_unique<ArraySelectionParameter>(k_CellPhasesArrayPath_Key, "Input Phases", "The phases of the data. The data should be the indices into the Crystal Structures Data Array.",
+                                                          DataPath{}, false, ArraySelectionParameter::AllowedTypes{DataType::int32}));
+  params.insertLinkableParameter(std::make_unique<BoolParameter>(k_UseGoodVoxels_Key, "Apply to Good Elements Only (Bad Elements Will Be Black)", "", false));
+  params.insert(std::make_unique<ArraySelectionParameter>(k_GoodVoxelsArrayPath_Key, "Input Mask [Optional]", "Optional Mask array where valid data is TRUE or 1.", DataPath{}, true,
+                                                          ArraySelectionParameter::AllowedTypes{DataType::int8, DataType::uint8, DataType::boolean}));
   params.insertSeparator(Parameters::Separator{"Ensemble Data"});
-  params.insert(std::make_unique<ArraySelectionParameter>(k_CrystalStructuresArrayPath_Key, "Crystal Structures", "", DataPath{}));
+  params.insert(std::make_unique<ArraySelectionParameter>(k_CrystalStructuresArrayPath_Key, "Crystal Structures", "", DataPath{}, false, ArraySelectionParameter::AllowedTypes{DataType::uint32}));
   params.insertSeparator(Parameters::Separator{"Element Data"});
   params.insert(std::make_unique<ArrayCreationParameter>(k_FZQuatsArrayPath_Key, "Created FZ Quaternions", "", DataPath{}));
   // Associate the Linkable Parameter(s) to the children parameters that they control
   params.linkParameters(k_UseGoodVoxels_Key, k_GoodVoxelsArrayPath_Key, true);
-
   return params;
 }
 
@@ -71,15 +175,7 @@ IFilter::UniquePointer GenerateFZQuaternions::clone() const
 IFilter::PreflightResult GenerateFZQuaternions::preflightImpl(const DataStructure& dataStructure, const Arguments& filterArgs, const MessageHandler& messageHandler,
                                                               const std::atomic_bool& shouldCancel) const
 {
-  /****************************************************************************
-   * Write any preflight sanity checking codes in this function
-   ***************************************************************************/
 
-  /**
-   * These are the values that were gathered from the UI or the pipeline file or
-   * otherwise passed into the filter. These are here for your convenience. If you
-   * do not need some of them remove them.
-   */
   auto pUseGoodVoxelsValue = filterArgs.value<bool>(k_UseGoodVoxels_Key);
   auto pQuatsArrayPathValue = filterArgs.value<DataPath>(k_QuatsArrayPath_Key);
   auto pCellPhasesArrayPathValue = filterArgs.value<DataPath>(k_CellPhasesArrayPath_Key);
@@ -87,49 +183,60 @@ IFilter::PreflightResult GenerateFZQuaternions::preflightImpl(const DataStructur
   auto pCrystalStructuresArrayPathValue = filterArgs.value<DataPath>(k_CrystalStructuresArrayPath_Key);
   auto pFZQuatsArrayPathValue = filterArgs.value<DataPath>(k_FZQuatsArrayPath_Key);
 
-  // Declare the preflightResult variable that will be populated with the results
-  // of the preflight. The PreflightResult type contains the output Actions and
-  // any preflight updated values that you want to be displayed to the user, typically
-  // through a user interface (UI).
-  PreflightResult preflightResult;
+  const Int32Array& phaseData = dataStructure.getDataRefAs<Int32Array>(pCellPhasesArrayPathValue);
+
+  if(phaseData.getNumberOfComponents() != 1)
+  {
+    return {MakeErrorResult<OutputActions>(-49000, fmt::format("Phase Array number of components is not 1: '{}'", phaseData.getNumberOfComponents()))};
+  }
+
+  const Float32Array& quatArray = dataStructure.getDataRefAs<Float32Array>(pQuatsArrayPathValue);
+  if(quatArray.getNumberOfComponents() != 4)
+  {
+    return {MakeErrorResult<OutputActions>(-49001, fmt::format("Quaternion Array number of components is not 4: '{}'", quatArray.getNumberOfComponents()))};
+  }
+
+  if(phaseData.getNumberOfTuples() != quatArray.getNumberOfTuples())
+  {
+    return {MakeErrorResult<OutputActions>(-49002,
+                                           fmt::format("Quaternion and Phase Arrays must have the same number of tuples. '{} != {}'", quatArray.getNumberOfTuples(), phaseData.getNumberOfTuples()))};
+  }
+
+  const UInt32Array& xtalArray = dataStructure.getDataRefAs<UInt32Array>(pCrystalStructuresArrayPathValue);
+  if(xtalArray.getNumberOfComponents() != 1)
+  {
+    return {MakeErrorResult<OutputActions>(-49003, fmt::format("Crystal Structure Array number of components is not 1: '{}'", xtalArray.getNumberOfComponents()))};
+  }
+
+  if(pUseGoodVoxelsValue)
+  {
+    const IDataArray& maskArray = dataStructure.getDataRefAs<IDataArray>(pGoodVoxelsArrayPathValue);
+    if(maskArray.getNumberOfComponents() != 1)
+    {
+      return {MakeErrorResult<OutputActions>(-49004, fmt::format("Mask Array number of components is not 1: '{}'", maskArray.getNumberOfComponents()))};
+    }
+    if(maskArray.getDataType() != complex::DataType::boolean && maskArray.getDataType() != complex::DataType::uint8 && maskArray.getDataType() != complex::DataType::int8)
+    {
+      return {MakeErrorResult<OutputActions>(-49005, fmt::format("Mask Array is not [BOOL (10) | UINT8 (2) | INT8 (1)]: '{}'", maskArray.getDataType()))};
+    }
+
+    if(maskArray.getNumberOfTuples() != quatArray.getNumberOfTuples())
+    {
+      return {MakeErrorResult<OutputActions>(-49006,
+                                             fmt::format("Quaternion and Mask arrays must have the same number of tuples. '{} != {}'", quatArray.getNumberOfTuples(), maskArray.getNumberOfTuples()))};
+    }
+  }
 
   // If your filter is making structural changes to the DataStructure then the filter
   // is going to create OutputActions subclasses that need to be returned. This will
   // store those actions.
   complex::Result<OutputActions> resultOutputActions;
 
-  // If your filter is going to pass back some `preflight updated values` then this is where you
-  // would create the code to store those values in the appropriate object. Note that we
-  // in line creating the pair (NOT a std::pair<>) of Key:Value that will get stored in
-  // the std::vector<PreflightValue> object.
   std::vector<PreflightValue> preflightUpdatedValues;
 
-  // If the filter needs to pass back some updated values via a key:value string:string set of values
-  // you can declare and update that string here.
-  // None found in this filter based on the filter parameters
-
-  // If this filter makes changes to the DataStructure in the form of
-  // creating/deleting/moving/renaming DataGroups, Geometries, DataArrays then you
-  // will need to use one of the `*Actions` classes located in complex/Filter/Actions
-  // to relay that information to the preflight and execute methods. This is done by
-  // creating an instance of the Action class and then storing it in the resultOutputActions variable.
-  // This is done through a `push_back()` method combined with a `std::move()`. For the
-  // newly initiated to `std::move` once that code is executed what was once inside the Action class
-  // instance variable is *no longer there*. The memory has been moved. If you try to access that
-  // variable after this line you will probably get a crash or have subtle bugs. To ensure that this
-  // does not happen we suggest using braces `{}` to scope each of the action's declaration and store
-  // so that the programmer is not tempted to use the action instance past where it should be used.
-  // You have to create your own Actions class if there isn't something specific for your filter's needs
-  // These are some proposed Actions based on the FilterParameters used. Please check them for correctness.
-  // This block is commented out because it needs some variables to be filled in.
-  {
-    // auto createArrayAction = std::make_unique<CreateArrayAction>(complex::NumericType::FILL_ME_IN, std::vector<usize>{NUM_TUPLES_VALUE}, NUM_COMPONENTS, pFZQuatsArrayPathValue);
-    // resultOutputActions.value().actions.push_back(std::move(createArrayAction));
-  }
-
-  // Store the preflight updated value(s) into the preflightUpdatedValues vector using
-  // the appropriate methods.
-  // None found based on the filter parameters
+  auto createArrayAction =
+      std::make_unique<CreateArrayAction>(complex::NumericType::float32, quatArray.getDataStore()->getTupleShape(), quatArray.getDataStore()->getComponentShape(), pFZQuatsArrayPathValue);
+  resultOutputActions.value().actions.push_back(std::move(createArrayAction));
 
   // Return both the resultOutputActions and the preflightUpdatedValues via std::move()
   return {std::move(resultOutputActions), std::move(preflightUpdatedValues)};
@@ -139,9 +246,6 @@ IFilter::PreflightResult GenerateFZQuaternions::preflightImpl(const DataStructur
 Result<> GenerateFZQuaternions::executeImpl(DataStructure& dataStructure, const Arguments& filterArgs, const PipelineFilter* pipelineNode, const MessageHandler& messageHandler,
                                             const std::atomic_bool& shouldCancel) const
 {
-  /****************************************************************************
-   * Extract the actual input values from the 'filterArgs' object
-   ***************************************************************************/
   auto pUseGoodVoxelsValue = filterArgs.value<bool>(k_UseGoodVoxels_Key);
   auto pQuatsArrayPathValue = filterArgs.value<DataPath>(k_QuatsArrayPath_Key);
   auto pCellPhasesArrayPathValue = filterArgs.value<DataPath>(k_CellPhasesArrayPath_Key);
@@ -149,10 +253,52 @@ Result<> GenerateFZQuaternions::executeImpl(DataStructure& dataStructure, const 
   auto pCrystalStructuresArrayPathValue = filterArgs.value<DataPath>(k_CrystalStructuresArrayPath_Key);
   auto pFZQuatsArrayPathValue = filterArgs.value<DataPath>(k_FZQuatsArrayPath_Key);
 
-  /****************************************************************************
-   * Write your algorithm implementation in this function
-   ***************************************************************************/
+  Int32Array& phaseArray = dataStructure.getDataRefAs<Int32Array>(pCellPhasesArrayPathValue);
+  Float32Array& quatArray = dataStructure.getDataRefAs<Float32Array>(pQuatsArrayPathValue);
+  UInt32Array& xtalArray = dataStructure.getDataRefAs<UInt32Array>(pCrystalStructuresArrayPathValue);
+  IDataArray* maskArray = dataStructure.getDataAs<IDataArray>(pGoodVoxelsArrayPathValue);
+  Float32Array& fzQuatArray = dataStructure.getDataRefAs<Float32Array>(pFZQuatsArrayPathValue);
 
+  std::atomic_int32_t warningCount = 0;
+  int32_t numPhases = static_cast<int32_t>(xtalArray.getNumberOfTuples());
+
+  // Parallel algorithm to find duplicate nodes
+  ParallelDataAlgorithm dataAlg;
+  dataAlg.setRange(0ULL, static_cast<size_t>(quatArray.getNumberOfTuples()));
+  dataAlg.setParallelizationEnabled(false);
+
+  if(pUseGoodVoxelsValue)
+  {
+    if(maskArray->getDataType() == DataType::boolean)
+    {
+      BoolArray* goodVoxelsArray = dataStructure.getDataAs<BoolArray>(pGoodVoxelsArrayPathValue);
+      dataAlg.execute(::GenerateFZQuatsImpl<BoolArray>(quatArray, phaseArray, xtalArray, numPhases, goodVoxelsArray, fzQuatArray, shouldCancel, warningCount));
+    }
+    else if(maskArray->getDataType() == DataType::uint8)
+    {
+      UInt32Array* goodVoxelsArray = dataStructure.getDataAs<UInt32Array>(pGoodVoxelsArrayPathValue);
+      dataAlg.execute(::GenerateFZQuatsImpl<UInt32Array>(quatArray, phaseArray, xtalArray, numPhases, goodVoxelsArray, fzQuatArray, shouldCancel, warningCount));
+    }
+    else if(maskArray->getDataType() == DataType::int8)
+    {
+      Int8Array* goodVoxelsArray = dataStructure.getDataAs<Int8Array>(pGoodVoxelsArrayPathValue);
+      dataAlg.execute(::GenerateFZQuatsImpl<Int8Array>(quatArray, phaseArray, xtalArray, numPhases, goodVoxelsArray, fzQuatArray, shouldCancel, warningCount));
+    }
+  }
+  else
+  {
+    dataAlg.execute(::GenerateFZQuatsImpl<Int8Array>(quatArray, phaseArray, xtalArray, numPhases, nullptr, fzQuatArray, shouldCancel, warningCount));
+  }
+
+  if(warningCount > 0)
+  {
+    std::string errorMessage = fmt::format("The Ensemble Phase information only references {} phase(s) but {} cell(s) had a phase value greater than {}. \
+This indicates a problem with the input cell phase data. DREAM.3D may have given INCORRECT RESULTS.",
+                                           numPhases - 1, warningCount, numPhases - 1);
+
+    return {MakeErrorResult<>(-49008, errorMessage)};
+  }
   return {};
 }
+
 } // namespace complex
--- OrientationAnalysis/test/GenerateFZQuaternionsTest.cpp
+++ OrientationAnalysis/test/GenerateFZQuaternionsTest.cpp
@@ -1,66 +1,125 @@
-/**
- * This file is auto generated from the original OrientationAnalysis/GenerateFZQuaternions
- * runtime information. These are the steps that need to be taken to utilize this
- * unit test in the proper way.
- *
- * 1: Validate each of the default parameters that gets created.
- * 2: Inspect the actual filter to determine if the filter in its default state
- * would pass or fail BOTH the preflight() and execute() methods
- * 3: UPDATE the ```REQUIRE(result.result.valid());``` code to have the proper
- *
- * 4: Add additional unit tests to actually test each code path within the filter
- *
- * There are some example Catch2 ```TEST_CASE``` sections for your inspiration.
- *
- * NOTE the format of the ```TEST_CASE``` macro. Please stick to this format to
- * allow easier parsing of the unit tests.
- *
- * When you start working on this unit test remove "[GenerateFZQuaternions][.][UNIMPLEMENTED]"
- * from the TEST_CASE macro. This will enable this unit test to be run by default
- * and report errors.
- */
-
 #include <catch2/catch.hpp>
 
-#include "complex/Parameters/ArrayCreationParameter.hpp"
-#include "complex/Parameters/ArraySelectionParameter.hpp"
+#include "complex/DataStructure/Geometry/ImageGeom.hpp"
+#include "complex/Filter/IFilter.hpp"
 #include "complex/Parameters/BoolParameter.hpp"
+#include "complex/UnitTest/UnitTestCommon.hpp"
+#include "complex/Utilities/DataArrayUtilities.hpp"
+
+#include "EbsdLib/Core/EbsdLibConstants.h"
 
 #include "OrientationAnalysis/Filters/GenerateFZQuaternions.hpp"
 #include "OrientationAnalysis/OrientationAnalysis_test_dirs.hpp"
 
 using namespace complex;
+using namespace complex::Constants;
 
-TEST_CASE("OrientationAnalysis::GenerateFZQuaternions: Instantiation and Parameter Check", "[OrientationAnalysis][GenerateFZQuaternions][.][UNIMPLEMENTED][!mayfail]")
+namespace
+{
+DataStructure CreateDataStructure()
+{
+  DataStructure dataGraph;
+
+  DataGroup* group = complex::DataGroup::Create(dataGraph, complex::Constants::k_SmallIN100);
+  DataGroup* scanData = complex::DataGroup::Create(dataGraph, complex::Constants::k_EbsdScanData, group->getId());
+
+  // Create an Image Geometry grid for the Scan Data
+  ImageGeom* imageGeom = ImageGeom::Create(dataGraph, k_SmallIn100ImageGeom, scanData->getId());
+  imageGeom->setSpacing({0.25f, 0.25f, 0.25f});
+  imageGeom->setOrigin({0.0f, 0.0f, 0.0f});
+  complex::SizeVec3 imageGeomDims = {100, 100, 2};
+  imageGeom->setDimensions(imageGeomDims); // Listed from slowest to fastest (Z, Y, X)
+
+  // Create some DataArrays; The DataStructure keeps a shared_ptr<> to the DataArray so DO NOT put
+  // it into another shared_ptr<>
+  std::vector<size_t> compDims = {4};
+  std::vector<size_t> tupleDims = {100, 100, 2};
+
+  std::string filePath = complex::unit_test::k_DataDir.str();
+
+  std::string fileName = "/quats.raw";
+  complex::ImportFromBinaryFile<float>(filePath + fileName, k_Quats, dataGraph, tupleDims, compDims, scanData->getId());
+
+  fileName = "/fz_quats.raw";
+  complex::ImportFromBinaryFile<float>(filePath + fileName, "FZ_QUATS_EXEMPLAR", dataGraph, tupleDims, compDims, scanData->getId());
+
+  Int32Array* phases_data = complex::UnitTest::CreateTestDataArray<int32>(dataGraph, k_Phases, tupleDims, {1}, scanData->getId());
+  phases_data->fill(1);
+
+  // Add in another group that is just information about the grid data.
+  DataGroup* phaseGroup = complex::DataGroup::Create(dataGraph, k_PhaseData, group->getId());
+  UInt32Array* laueClass = UInt32Array::CreateWithStore<UInt32DataStore>(dataGraph, k_LaueClass, {2}, {1}, phaseGroup->getId());
+  (*laueClass)[0] = EbsdLib::CrystalStructure::UnknownCrystalStructure;
+  (*laueClass)[1] = EbsdLib::CrystalStructure::Cubic_High;
+
+  return dataGraph;
+}
+
+void MessageHandlerFunction(const IFilter::Message& message)
+{
+}
+
+} // namespace
+
+TEST_CASE("OrientationAnalysis::GenerateFZQuaternions: Instantiation and Parameter Check", "[OrientationAnalysis][GenerateFZQuaternions]")
 {
   // Instantiate the filter, a DataStructure object and an Arguments Object
   GenerateFZQuaternions filter;
-  DataStructure ds;
+  DataStructure dataStructure = CreateDataStructure();
   Arguments args;
 
+  DataPath scanDataPath = DataPath({complex::Constants::k_SmallIN100, complex::Constants::k_EbsdScanData});
   // Create default Parameters for the filter.
+
+  args.insertOrAssign(GenerateFZQuaternions::k_QuatsArrayPath_Key, std::make_any<DataPath>(scanDataPath.createChildPath(k_Quats)));
+  args.insertOrAssign(GenerateFZQuaternions::k_FZQuatsArrayPath_Key, std::make_any<DataPath>(scanDataPath.createChildPath(k_FZQuats)));
+  args.insertOrAssign(GenerateFZQuaternions::k_CellPhasesArrayPath_Key, std::make_any<DataPath>(scanDataPath.createChildPath(k_Phases)));
+  args.insertOrAssign(GenerateFZQuaternions::k_CrystalStructuresArrayPath_Key, std::make_any<DataPath>(DataPath({k_SmallIN100, k_PhaseData, k_LaueClass})));
+
   args.insertOrAssign(GenerateFZQuaternions::k_UseGoodVoxels_Key, std::make_any<bool>(false));
-  args.insertOrAssign(GenerateFZQuaternions::k_QuatsArrayPath_Key, std::make_any<DataPath>(DataPath{}));
-  args.insertOrAssign(GenerateFZQuaternions::k_CellPhasesArrayPath_Key, std::make_any<DataPath>(DataPath{}));
   args.insertOrAssign(GenerateFZQuaternions::k_GoodVoxelsArrayPath_Key, std::make_any<DataPath>(DataPath{}));
-  args.insertOrAssign(GenerateFZQuaternions::k_CrystalStructuresArrayPath_Key, std::make_any<DataPath>(DataPath{}));
-  args.insertOrAssign(GenerateFZQuaternions::k_FZQuatsArrayPath_Key, std::make_any<DataPath>(DataPath{}));
 
   // Preflight the filter and check result
-  auto preflightResult = filter.preflight(ds, args);
+  auto preflightResult = filter.preflight(dataStructure, args);
+  if(preflightResult.outputActions.invalid())
+  {
+    for(const auto& error : preflightResult.outputActions.errors())
+    {
+      std::cout << error.code << ": " << error.message << std::endl;
+    }
+  }
   REQUIRE(preflightResult.outputActions.valid());
 
   // Execute the filter and check the result
-  auto executeResult = filter.execute(ds, args);
+  auto executeResult = filter.execute(dataStructure, args);
   REQUIRE(executeResult.result.valid());
+
+  // Compare Results
+  auto generatedFZQuats = dataStructure.getDataRefAs<Float32Array>(scanDataPath.createChildPath(k_FZQuats));
+  auto exemplarFZQuats = dataStructure.getDataRefAs<Float32Array>(scanDataPath.createChildPath("FZ_QUATS_EXEMPLAR"));
+  std::vector<size_t> misMatchIndices;
+  for(size_t idx = 0; idx < generatedFZQuats.getNumberOfTuples(); idx++)
+  {
+    if(generatedFZQuats[idx * 4] != exemplarFZQuats[idx * 4])
+    {
+      misMatchIndices.push_back(idx);
+      continue;
+    }
+    if(generatedFZQuats[idx * 4 + 1] != exemplarFZQuats[idx * 4 + 1])
+    {
+      misMatchIndices.push_back(idx);
+      continue;
+    }
+    if(generatedFZQuats[idx * 4 + 2] != exemplarFZQuats[idx * 4 + 2])
+    {
+      misMatchIndices.push_back(idx);
+      continue;
+    }
+    if(generatedFZQuats[idx * 4 + 3] != exemplarFZQuats[idx * 4 + 3])
+    {
+      misMatchIndices.push_back(idx);
+      continue;
+    }
+  }
+  REQUIRE(misMatchIndices.size() == 0);
 }
-
-// TEST_CASE("OrientationAnalysis::GenerateFZQuaternions: Valid filter execution")
-//{
-//
-//}
-
-// TEST_CASE("OrientationAnalysis::GenerateFZQuaternions: InValid filter execution")
-//{
-//
-//}
