534c0a75c8faa3a815bce80d0043422e4e588d66
Michael Jackson <mike.jackson@bluequartz.net>
2022-03-20 11:03:06 -0400

    ArraySelectionParameter: Change to std::set for AllowedTypes
    DataGroupSelectionParameter: Add std::set<> for AllowedTypes which will basically remain empty. Needed for UI compatibility.
    
    GeometrySelectionParameter: Add std::set<> for AllowedTypes.
    
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- src/complex/Parameters/ArraySelectionParameter.hpp
+++ src/complex/Parameters/ArraySelectionParameter.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
+#include <set>
 #include <string>
-#include <unordered_set>
 
 #include "complex/Filter/MutableDataParameter.hpp"
 #include "complex/Filter/ParameterTraits.hpp"
@@ -17,7 +17,7 @@ class COMPLEX_EXPORT ArraySelectionParameter : public MutableDataParameter
 {
 public:
   using ValueType = DataPath;
-  using AllowedTypes = std::unordered_set<DataType>;
+  using AllowedTypes = std::set<DataType>;
 
   ArraySelectionParameter() = delete;
   ArraySelectionParameter(const std::string& name, const std::string& humanName, const std::string& helpText, const ValueType& defaultValue, bool allowEmpty = false,
