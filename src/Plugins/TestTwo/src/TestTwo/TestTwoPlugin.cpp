a1daa2dc0f75ef81cb199cb0c6ed28844e1ed2fe
Michael Jackson <mike.jackson@bluequartz.net>
2022-04-25 18:08:41 -0400

    CMAKE: Autogenerate the filter registration codes (#203)
    * Autogenerate the filter registration codes and have the Plugin file use that.
    * Free Function now generates a std::vector<> of Filter Functions.
    * Plugin constructor loops over those filters and "addFilter()" for each.
    
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- src/Plugins/TestTwo/src/TestTwo/TestTwoPlugin.cpp
+++ src/Plugins/TestTwo/src/TestTwo/TestTwoPlugin.cpp
@@ -1,6 +1,5 @@
 #include "TestTwoPlugin.hpp"
-
-#include "TestTwo/Filters/Test2Filter.hpp"
+#include "TestTwo/TestTwo_filter_registration.hpp"
 
 using namespace complex;
 
@@ -12,7 +11,11 @@ constexpr AbstractPlugin::IdType k_ID = *Uuid::FromString("05cc618b-781f-4ac0-b9
 TestTwoPlugin::TestTwoPlugin()
 : AbstractPlugin(k_ID, "TestTwo", "Test Plugin", "BlueQuartz Software")
 {
-  addFilter([]() -> IFilter::UniquePointer { return std::make_unique<Test2Filter>(); });
+  std::vector<::FilterCreationFunc> filterFuncs = ::GetPluginFilterList();
+  for(const auto& filterFunc : filterFuncs)
+  {
+    addFilter(filterFunc);
+  }
 }
 
 TestTwoPlugin::~TestTwoPlugin() = default;
