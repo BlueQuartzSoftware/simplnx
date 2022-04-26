be0444ca051e98fafc29a1e8cfe2c0f8d968c614
Michael Jackson <mike.jackson@bluequartz.net>
2022-04-26 12:28:40 -0400

    GeometryParameter Updates for better error messaging (#205)
    * More verbose messages in the GeometrySelectionParameter
    * use fmt/range for easier to read code.
    
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>
    Co-authored-by: Jared Duffey <jared.duffey@bluequartz.net>

--- src/complex/Filter/IParameter.hpp
+++ src/complex/Filter/IParameter.hpp
@@ -37,6 +37,7 @@ inline constexpr int32 k_Validate_DuplicateValue = -203;
 inline constexpr int32 k_Validate_Does_Not_Exist = -204;
 inline constexpr int32 k_Validate_Type_Error = -205;
 inline constexpr int32 k_Validate_OutOfRange_Error = -206;
+inline constexpr int32 k_Validate_AllowedType_Error = -207;
 
 } // namespace FilterParameter::Constants
 
