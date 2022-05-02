098e7485778476f67cbea9deae8318fb217faf1e
Michael Jackson <mike.jackson@bluequartz.net>
2022-05-02 13:34:29 -0400

    Final Fixes to allow PacMan Sample Pipelines to generate matching result (#217)
    * SetImageGeomOriginScaling: Better ordering of parameters
    * CropImageGeometry: Fix issue where the updated origin was applied twice
    * CropVertexGeometry: Remove extra variable.
    * IterativeClosestPointFilter: Fix issues causing bad data to be produced.
    * CropImageGeometry: White space fix?
    * CropImageGeometry: Bug fixes, performance enhancements, code cleanup
    * PipelineTest: Add missing override keywords.
    
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>
    
    Co-authored-by: Jared Duffey <jared.duffey@bluequartz.net>

--- src/Plugins/ComplexCore/src/ComplexCore/Filters/IterativeClosestPointFilter.cpp
+++ src/Plugins/ComplexCore/src/ComplexCore/Filters/IterativeClosestPointFilter.cpp
@@ -9,10 +9,8 @@
 #include "complex/Filter/Actions/CreateArrayAction.hpp"
 #include "complex/Parameters/ArrayCreationParameter.hpp"
 #include "complex/Parameters/BoolParameter.hpp"
-#include "complex/Parameters/ChoicesParameter.hpp"
 #include "complex/Parameters/DataPathSelectionParameter.hpp"
 #include "complex/Parameters/NumberParameter.hpp"
-#include "complex/Parameters/NumericTypeParameter.hpp"
 
 #include "ComplexCore/utils/nanoflann.hpp"
 
@@ -30,11 +28,16 @@ struct VertexGeomAdaptor
 {
   const Derived& obj;
   Float32Array* verts;
+  size_t m_NumComponents = 0;
+  size_t m_NumTuples = 0;
 
   VertexGeomAdaptor(const Derived& obj_)
   : obj(obj_)
   {
+    // These values never change for the life time of this object so cache them now.
     verts = derived()->getVertices();
+    m_NumComponents = verts->getNumberOfComponents();
+    m_NumTuples = verts->getNumberOfTuples();
   }
 
   inline const Derived& derived() const
@@ -44,24 +47,13 @@ struct VertexGeomAdaptor
 
   inline usize kdtree_get_point_count() const
   {
-    return verts->getNumberOfTuples();
+    return m_NumTuples;
   }
 
   inline float kdtree_get_pt(const usize idx, const usize dim) const
   {
-    auto numComponents = verts->getNumberOfComponents();
-    auto offset = idx * numComponents;
-
-    if(dim == 0)
-    {
-      return (*verts)[offset + 0];
-    }
-    if(dim == 1)
-    {
-      return (*verts)[offset + 1];
-    }
-
-    return (*verts)[offset + 2];
+    auto offset = idx * m_NumComponents;
+    return (*verts)[offset + dim];
   }
 
   template <class BBOX>
@@ -95,11 +87,14 @@ std::string IterativeClosestPointFilter::humanName() const
 Parameters IterativeClosestPointFilter::parameters() const
 {
   Parameters params;
-  params.insert(std::make_unique<DataPathSelectionParameter>(k_MovingVertexPath_Key, "Moving Vertex Geometry", "Numeric Type of data to create", DataPath()));
-  params.insert(std::make_unique<DataPathSelectionParameter>(k_TargetVertexPath_Key, "Target Vertex Geometry", "Number of components", DataPath()));
+
   params.insert(std::make_unique<UInt64Parameter>(k_NumIterations_Key, "Number of Iterations", "Number of components", 1));
   params.insert(std::make_unique<BoolParameter>(k_ApplyTransformation_Key, "Apply Transformation to Moving Geometry", "Number of components", false));
-  params.insert(std::make_unique<ArrayCreationParameter>(k_TransformArrayPath_Key, "Transform Array", "Number of tuples", DataPath()));
+
+  params.insert(std::make_unique<DataPathSelectionParameter>(k_MovingVertexPath_Key, "Moving Vertex Geometry", "Numeric Type of data to create", DataPath()));
+  params.insert(std::make_unique<DataPathSelectionParameter>(k_TargetVertexPath_Key, "Target Vertex Geometry", "Number of components", DataPath()));
+
+  params.insert(std::make_unique<ArrayCreationParameter>(k_TransformArrayPath_Key, "Output Transform Array", "Number of tuples", DataPath()));
   return params;
 }
 
@@ -113,7 +108,7 @@ IFilter::PreflightResult IterativeClosestPointFilter::preflightImpl(const DataSt
   auto movingVertexPath = args.value<DataPath>(k_MovingVertexPath_Key);
   auto targetVertexPath = args.value<DataPath>(k_TargetVertexPath_Key);
   auto numIterations = args.value<uint64>(k_NumIterations_Key);
-  auto applytransformation = args.value<bool>(k_ApplyTransformation_Key);
+  //  auto applytransformation = args.value<bool>(k_ApplyTransformation_Key);
   auto transformArrayPath = args.value<DataPath>(k_TransformArrayPath_Key);
 
   if(data.getDataAs<VertexGeom>(movingVertexPath) == nullptr)
@@ -165,37 +160,33 @@ Result<> IterativeClosestPointFilter::executeImpl(DataStructure& data, const Arg
     return {nonstd::make_unexpected(std::vector<Error>{Error{k_MissingVertices, ss}})};
   }
 
-  auto* movingPtr = movingVertexGeom->getVertices();
-  auto* movingCopyPtr = movingVertexGeom->getVertices();
-  auto* targetPtr = targetVertexGeom->getVertices();
-
-  if(movingPtr == nullptr)
+  if(movingVertexGeom->getVertices() == nullptr)
   {
     auto ss = fmt::format("Moving Vertex Geometry does not contain a vertex array");
     return {nonstd::make_unexpected(std::vector<Error>{Error{k_MissingVertices, ss}})};
   }
-  if(targetPtr == nullptr)
+  if(targetVertexGeom->getVertices() == nullptr)
   {
     auto ss = fmt::format("Target Vertex Geometry does not contain a vertex array");
     return {nonstd::make_unexpected(std::vector<Error>{Error{k_MissingVertices, ss}})};
   }
 
-  auto* movingStore = movingCopyPtr->getDataStore();
-  std::vector<float32> movingData(movingStore->begin(), movingStore->end());
+  auto* movingPtr = movingVertexGeom->getVertices();
+  Float32Array& targetPtr = *(targetVertexGeom->getVertices());
 
+  auto* movingStore = movingPtr->getDataStore();
+  std::vector<float32> movingVector(movingStore->begin(), movingStore->end());
+  float32* movingCopyPtr = movingVector.data();
   DataStructure tmp;
 
   usize numMovingVerts = movingVertexGeom->getNumberOfVertices();
-  std::vector<usize> cDims(1, 3);
-  Float32Array* dynTarget = Float32Array::CreateWithStore<DataStore<float32>>(tmp, "tmp", {numMovingVerts}, cDims, true);
-  dynTarget->fill(0.0f);
-  auto* dynTargetPtr = dynTarget->getDataStore();
-  std::vector<float32> dynTargetData(dynTargetPtr->begin(), dynTargetPtr->end());
+  std::vector<float32> dynTarget(numMovingVerts * 3, 0.0F);
+  float* dynTargetPtr = dynTarget.data();
 
   using Adaptor = VertexGeomAdaptor<VertexGeom*>;
   const Adaptor adaptor(targetVertexGeom);
 
-  // notifyStatusMessage("Building kd-tree index...");
+  messageHandler("Building kd-tree index...");
 
   using KDtree = nanoflann::KDTreeSingleIndexAdaptor<nanoflann::L2_Adaptor<float32, Adaptor>, Adaptor, 3>;
   KDtree index(3, adaptor, nanoflann::KDTreeSingleIndexAdaptorParams(30));
@@ -215,40 +206,37 @@ Result<> IterativeClosestPointFilter::executeImpl(DataStructure& data, const Arg
   int64 progressInt = 0;
   int64 counter = 0;
 
-  auto targetData = targetPtr->getDataStore();
-
   for(usize i = 0; i < iters; i++)
   {
+    if(shouldCancel)
+    {
+      return {};
+    }
+
     for(usize j = 0; j < numMovingVerts; j++)
     {
       usize id;
       float dist;
       nanoflann::KNNResultSet<float> results(nn);
       results.init(&id, &dist);
-      index.findNeighbors(results, movingData.data() + (3 * j), nanoflann::SearchParams());
-      const usize dynOffset = 3 * j;
-      const usize targetOffset = 3 * id;
-      dynTargetPtr->setValue(dynOffset + 0, targetData->getValue(targetOffset + 0));
-      (*dynTargetPtr)[dynOffset + 1] = (*targetData)[targetOffset + 1];
-      (*dynTargetPtr)[dynOffset + 2] = (*targetData)[targetOffset + 2];
+      index.findNeighbors(results, movingCopyPtr + (3 * j), nanoflann::SearchParams());
+      dynTargetPtr[3 * j + 0] = targetPtr[3 * id + 0];
+      dynTargetPtr[3 * j + 1] = targetPtr[3 * id + 1];
+      dynTargetPtr[3 * j + 2] = targetPtr[3 * id + 2];
     }
 
-    Eigen::Map<PointCloud> moving_(movingData.data(), 3, numMovingVerts);
-    Eigen::Map<PointCloud> target_(dynTargetData.data(), 3, numMovingVerts);
+    Eigen::Map<PointCloud> moving_(movingCopyPtr, 3, numMovingVerts);
+    Eigen::Map<PointCloud> target_(dynTargetPtr, 3, numMovingVerts);
 
     UmeyamaTransform transform = Eigen::umeyama(moving_, target_, false);
 
     for(usize j = 0; j < numMovingVerts; j++)
     {
-      Eigen::Vector4f position((*movingCopyPtr)[3 * j + 0], (*movingCopyPtr)[3 * j + 1], (*movingCopyPtr)[3 * j + 2], 1);
+      Eigen::Vector4f position(movingCopyPtr[3 * j + 0], movingCopyPtr[3 * j + 1], movingCopyPtr[3 * j + 2], 1);
       Eigen::Vector4f transformedPosition = transform * position;
-      float* transformPositionData = transformedPosition.data();
-      for(usize k = 0; k < 3; k++)
-      {
-        (*movingCopyPtr)[3 * j + k] = transformPositionData[k];
-      }
+      std::memcpy(movingCopyPtr + (3 * j), transformedPosition.data(), sizeof(float) * 3);
     }
-
+    // Update the global transform
     globalTransform = transform * globalTransform;
 
     if(counter > prog)
@@ -269,7 +257,6 @@ Result<> IterativeClosestPointFilter::executeImpl(DataStructure& data, const Arg
     {
       Eigen::Vector4f position((*movingPtr)[3 * j + 0], (*movingPtr)[3 * j + 1], (*movingPtr)[3 * j + 2], 1);
       Eigen::Vector4f transformedPosition = globalTransform * position;
-      auto transformedPositionData = transformedPosition.data();
       for(usize k = 0; k < 3; k++)
       {
         (*movingPtr)[3 * j + k] = transformedPosition.data()[k];
