f6f377ae6abcf39c29b30946aa378d932678e6bf
Joey Kleingers <joey.kleingers@bluequartz.net>
2022-05-10 11:07:03 -0400

    Clang format.
    Signed-off-by: Joey Kleingers <joey.kleingers@bluequartz.net>

--- src/Plugins/ComplexCore/src/ComplexCore/Filters/ImportCSVDataFilter.cpp
+++ src/Plugins/ComplexCore/src/ComplexCore/Filters/ImportCSVDataFilter.cpp
@@ -146,62 +146,52 @@ Result<ParsersVector> createParsers(const DataTypeVector& dataTypes, const DataP
     DataType dataType = dataTypeOpt.value();
     switch(dataType)
     {
-    case complex::DataType::int8:
-    {
+    case complex::DataType::int8: {
       Int8Array& data = dataStructure.getDataRefAs<Int8Array>(arrayPath);
       dataParsers[i] = std::make_unique<Int8Parser>(data, name, i);
       break;
     }
-    case complex::DataType::uint8:
-    {
+    case complex::DataType::uint8: {
       UInt8Array& data = dataStructure.getDataRefAs<UInt8Array>(arrayPath);
       dataParsers[i] = std::make_unique<UInt8Parser>(data, name, i);
       break;
     }
-    case complex::DataType::int16:
-    {
+    case complex::DataType::int16: {
       Int16Array& data = dataStructure.getDataRefAs<Int16Array>(arrayPath);
       dataParsers[i] = std::make_unique<Int16Parser>(data, name, i);
       break;
     }
-    case complex::DataType::uint16:
-    {
+    case complex::DataType::uint16: {
       UInt16Array& data = dataStructure.getDataRefAs<UInt16Array>(arrayPath);
       dataParsers[i] = std::make_unique<UInt16Parser>(data, name, i);
       break;
     }
-    case complex::DataType::int32:
-    {
+    case complex::DataType::int32: {
       Int32Array& data = dataStructure.getDataRefAs<Int32Array>(arrayPath);
       dataParsers[i] = std::make_unique<Int32Parser>(data, name, i);
       break;
     }
-    case complex::DataType::uint32:
-    {
+    case complex::DataType::uint32: {
       UInt32Array& data = dataStructure.getDataRefAs<UInt32Array>(arrayPath);
       dataParsers[i] = std::make_unique<UInt32Parser>(data, name, i);
       break;
     }
-    case complex::DataType::int64:
-    {
+    case complex::DataType::int64: {
       Int64Array& data = dataStructure.getDataRefAs<Int64Array>(arrayPath);
       dataParsers[i] = std::make_unique<Int64Parser>(data, name, i);
       break;
     }
-    case complex::DataType::uint64:
-    {
+    case complex::DataType::uint64: {
       UInt64Array& data = dataStructure.getDataRefAs<UInt64Array>(arrayPath);
       dataParsers[i] = std::make_unique<UInt64Parser>(data, name, i);
       break;
     }
-    case complex::DataType::float32:
-    {
+    case complex::DataType::float32: {
       Float32Array& data = dataStructure.getDataRefAs<Float32Array>(arrayPath);
       dataParsers[i] = std::make_unique<Float32Parser>(data, name, i);
       break;
     }
-    case complex::DataType::float64:
-    {
+    case complex::DataType::float64: {
       Float64Array& data = dataStructure.getDataRefAs<Float64Array>(arrayPath);
       dataParsers[i] = std::make_unique<Float64Parser>(data, name, i);
       break;
