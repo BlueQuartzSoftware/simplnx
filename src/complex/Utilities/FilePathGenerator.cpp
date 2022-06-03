fdba35b09511270ced2d907d957a87809ca15483
Jessica Marquis <jessica.marquis@bluequartz.net>
2022-06-03 16:51:17 -0400

    Add GenerateFileList function

--- src/complex/Utilities/FilePathGenerator.cpp
+++ src/complex/Utilities/FilePathGenerator.cpp
@@ -53,5 +53,33 @@ std::pair<std::vector<std::string>, bool> GenerateAndValidateFileList(int32 star
 
   return std::make_pair(std::move(fileList), missingFiles);
 }
+
+// -----------------------------------------------------------------------------
+std::vector<std::string> GenerateFileList(int32 start, int32 end, int32 increment, Ordering order, std::string_view inputPath, std::string_view filePrefix, std::string_view fileSuffix,
+                                          std::string_view fileExtension, int32 paddingDigits)
+{
+  std::vector<std::string> fileList;
+  if(increment < 1)
+  {
+    return fileList;
+  }
+  int32 index = 0;
+  std::string format_string = fmt::format("{{}}/{{}}{{:0{}d}}{{}}{{}}", paddingDigits);
+  for(int32 i = 0; i < (end - start) + 1; i += increment)
+  {
+    if(order == Ordering::LowToHigh)
+    {
+      index = start + i;
+    }
+    else
+    {
+      index = end - i;
+    }
+    std::string filePath = fmt::format(format_string, inputPath, filePrefix, index, fileSuffix, fileExtension);
+    fileList.push_back(filePath);
+  }
+
+  return fileList;
+}
 } // namespace FilePathGenerator
 } // namespace complex
