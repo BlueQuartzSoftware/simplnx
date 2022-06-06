923404b989c7839ef3725f1dfe7560da0a45527f
Jessica Marquis <jessica.marquis@bluequartz.net>
2022-06-06 13:21:05 -0400

    Generate complete file list before validating & add option to 'fail quickly' (turned on by default)

--- src/complex/Utilities/FilePathGenerator.cpp
+++ src/complex/Utilities/FilePathGenerator.cpp
@@ -13,44 +13,21 @@ namespace FilePathGenerator
 {
 // -----------------------------------------------------------------------------
 std::pair<std::vector<std::string>, bool> GenerateAndValidateFileList(int32 start, int32 end, int32 increment, Ordering order, std::string_view inputPath, std::string_view filePrefix,
-                                                                      std::string_view fileSuffix, std::string_view fileExtension, int32 paddingDigits)
+                                                                      std::string_view fileSuffix, std::string_view fileExtension, int32 paddingDigits, bool failFast)
 {
-  std::vector<std::string> fileList;
-
-  if(!fs::exists(inputPath))
-  {
-    return std::make_pair(std::move(fileList), true);
-  }
-  if(increment < 1)
-  {
-    return std::make_pair(std::move(fileList), true);
-  }
-  int32 index = 0;
-
-  std::string format_string = fmt::format("{{}}/{{}}{{:0{}d}}{{}}{{}}", paddingDigits);
-
+  std::vector<std::string> fileList = GenerateFileList(start, end, increment, order, inputPath, filePrefix, fileSuffix, fileExtension, paddingDigits);
   bool missingFiles = false;
-  for(int32 i = 0; i < (end - start) + 1; i += increment)
+  for(const auto& file : fileList)
   {
-    if(order == Ordering::LowToHigh)
-    {
-      index = start + i;
-    }
-    else
-    {
-      index = end - i;
-    }
-
-    std::string filePath = fmt::format(format_string, inputPath, filePrefix, index, fileSuffix, fileExtension);
-
-    if(!fs::exists(filePath))
+    if(!std::filesystem::exists(file))
     {
       missingFiles = true;
+      if(failFast)
+      {
+        break;
+      }
     }
-
-    fileList.push_back(filePath);
   }
-
   return std::make_pair(std::move(fileList), missingFiles);
 }
 
@@ -59,6 +36,10 @@ std::vector<std::string> GenerateFileList(int32 start, int32 end, int32 incremen
                                           std::string_view fileExtension, int32 paddingDigits)
 {
   std::vector<std::string> fileList;
+  if(!fs::exists(inputPath))
+  {
+    return fileList;
+  }
   if(increment < 1)
   {
     return fileList;
