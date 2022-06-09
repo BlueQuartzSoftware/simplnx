514fb0ecea7b6870b8562c6596f6ad33c2b880cb
Michael Jackson <mike.jackson@bluequartz.net>
2022-06-09 14:10:51 -0400

    ITKImageProcessing: ITKAbsImage Filter added, tested, verified
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

---
+++ ITKImageProcessing/src/ITKImageProcessing/Filters/ITKAbsImage.cpp
@@ -0,0 +1,115 @@
+#include "ITKAbsImage.hpp"
+
+#include "ITKImageProcessing/Common/ITKArrayHelper.hpp"
+#include "ITKImageProcessing/Common/sitkCommon.hpp"
+
+#include "complex/Parameters/ArrayCreationParameter.hpp"
+#include "complex/Parameters/ArraySelectionParameter.hpp"
+#include "complex/Parameters/GeometrySelectionParameter.hpp"
+
+#include <itkAbsImageFilter.h>
+
+using namespace complex;
+
+namespace cxITKAbsImage
+{
+using ArrayOptionsT = ITK::ScalarPixelIdTypeList;
+// VectorPixelIDTypeList;
+
+struct ITKAbsImageFunctor
+{
+  template <class InputImageT, class OutputImageT, uint32 Dimension>
+  auto createFilter() const
+  {
+    using FilterT = itk::AbsImageFilter<InputImageT, OutputImageT>;
+    auto filter = FilterT::New();
+    return filter;
+  }
+};
+} // namespace cxITKAbsImage
+
+namespace complex
+{
+//------------------------------------------------------------------------------
+std::string ITKAbsImage::name() const
+{
+  return FilterTraits<ITKAbsImage>::name;
+}
+
+//------------------------------------------------------------------------------
+std::string ITKAbsImage::className() const
+{
+  return FilterTraits<ITKAbsImage>::className;
+}
+
+//------------------------------------------------------------------------------
+Uuid ITKAbsImage::uuid() const
+{
+  return FilterTraits<ITKAbsImage>::uuid;
+}
+
+//------------------------------------------------------------------------------
+std::string ITKAbsImage::humanName() const
+{
+  return "ITK Abs Image Filter";
+}
+
+//------------------------------------------------------------------------------
+std::vector<std::string> ITKAbsImage::defaultTags() const
+{
+  return {"ITKImageProcessing", "ITKAbsImage", "ITKImageIntensity", "ImageIntensity"};
+}
+
+//------------------------------------------------------------------------------
+Parameters ITKAbsImage::parameters() const
+{
+  Parameters params;
+
+  params.insertSeparator(Parameters::Separator{"Input Data Structure Items"});
+  params.insert(std::make_unique<GeometrySelectionParameter>(k_SelectedImageGeomPath_Key, "Image Geometry", "Select the Image Geometry Group from the DataStructure.", DataPath({"Image Geometry"}),
+                                                             GeometrySelectionParameter::AllowedTypes{AbstractGeometry::Type::Image}));
+  params.insert(std::make_unique<ArraySelectionParameter>(k_SelectedImageDataPath_Key, "Input Image Data Array", "The image data that will be processed by this filter.", DataPath{},
+                                                          complex::ITK::GetScalarPixelAllowedTypes()));
+
+  params.insertSeparator(Parameters::Separator{"Created Data Structure Items"});
+  params.insert(std::make_unique<ArrayCreationParameter>(k_OutputImageDataPath_Key, "Output Image Data Array", "The result of the processing will be stored in this Data Array.", DataPath{}));
+
+  return params;
+}
+
+//------------------------------------------------------------------------------
+IFilter::UniquePointer ITKAbsImage::clone() const
+{
+  return std::make_unique<ITKAbsImage>();
+}
+
+//------------------------------------------------------------------------------
+IFilter::PreflightResult ITKAbsImage::preflightImpl(const DataStructure& dataStructure, const Arguments& filterArgs, const MessageHandler& messageHandler, const std::atomic_bool& shouldCancel) const
+{
+  auto imageGeomPath = filterArgs.value<DataPath>(k_SelectedImageGeomPath_Key);
+  auto selectedInputArray = filterArgs.value<DataPath>(k_SelectedImageDataPath_Key);
+  auto outputArrayPath = filterArgs.value<DataPath>(k_OutputImageDataPath_Key);
+
+  Result<OutputActions> resultOutputActions = ITK::DataCheck<cxITKAbsImage::ArrayOptionsT>(dataStructure, selectedInputArray, imageGeomPath, outputArrayPath);
+
+  return {std::move(resultOutputActions)};
+}
+
+//------------------------------------------------------------------------------
+Result<> ITKAbsImage::executeImpl(DataStructure& dataStructure, const Arguments& filterArgs, const PipelineFilter* pipelineNode, const MessageHandler& messageHandler,
+                                  const std::atomic_bool& shouldCancel) const
+{
+  auto imageGeomPath = filterArgs.value<DataPath>(k_SelectedImageGeomPath_Key);
+  auto selectedInputArray = filterArgs.value<DataPath>(k_SelectedImageDataPath_Key);
+  auto outputArrayPath = filterArgs.value<DataPath>(k_OutputImageDataPath_Key);
+
+  cxITKAbsImage::ITKAbsImageFunctor itkFunctor = {};
+
+  // LINK GEOMETRY OUTPUT START
+  ImageGeom& imageGeom = dataStructure.getDataRefAs<ImageGeom>(imageGeomPath);
+  imageGeom.getLinkedGeometryData().addCellData(outputArrayPath);
+  // LINK GEOMETRY OUTPUT STOP
+
+  return ITK::Execute<cxITKAbsImage::ArrayOptionsT>(dataStructure, selectedInputArray, imageGeomPath, outputArrayPath, itkFunctor);
+}
+} // namespace complex
