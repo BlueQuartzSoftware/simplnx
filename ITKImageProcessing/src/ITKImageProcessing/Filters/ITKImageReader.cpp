45f325b24b2151a02dbc35036102a82f5536383c
Michael Jackson <mike.jackson@bluequartz.net>
2022-07-14 23:26:32 -0400

    ITKImageReader: Add .mha as an accepted file extension
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- ITKImageProcessing/src/ITKImageProcessing/Filters/ITKImageReader.cpp
+++ ITKImageProcessing/src/ITKImageProcessing/Filters/ITKImageReader.cpp
@@ -310,7 +310,7 @@ Parameters ITKImageReader::parameters() const
   Parameters params;
   params.insertSeparator(Parameters::Separator{"Filter Parameters"});
   params.insert(std::make_unique<FileSystemPathParameter>(k_FileName_Key, "File", "Input image file", fs::path(""),
-                                                          FileSystemPathParameter::ExtensionsType{{".png"}, {".tiff"}, {".tif"}, {".bmp"}, {".jpeg"}, {".jpg"}, {".nrrd"}},
+                                                          FileSystemPathParameter::ExtensionsType{{".png"}, {".tiff"}, {".tif"}, {".bmp"}, {".jpeg"}, {".jpg"}, {".nrrd"}, {".mha"}},
                                                           FileSystemPathParameter::PathType::InputFile, false));
 
   params.insertSeparator(Parameters::Separator{"Created Data Structure Items"});
