ace95f1ab4bb85bcf6e6300d206d236744c4acdd
Jessica Marquis <jessica.marquis@bluequartz.net>
2022-07-21 10:14:56 -0400

    Improve the unit test for FindArrayStatisticsFilter

--- src/Plugins/ComplexCore/test/FindArrayStatisticsTest.cpp
+++ src/Plugins/ComplexCore/test/FindArrayStatisticsTest.cpp
@@ -1,5 +1,8 @@
 #include <catch2/catch.hpp>
 
+#include <filesystem>
+namespace fs = std::filesystem;
+
 #include "complex/UnitTest/UnitTestCommon.hpp"
 
 #include "complex/Parameters/ArrayCreationParameter.hpp"
@@ -7,14 +10,35 @@
 #include "complex/Parameters/ArrayThresholdsParameter.hpp"
 #include "complex/Parameters/BoolParameter.hpp"
 #include "complex/Parameters/DataGroupSelectionParameter.hpp"
+#include "complex/Parameters/Dream3dImportParameter.hpp"
 #include "complex/Parameters/NumberParameter.hpp"
 
 #include "ComplexCore/ComplexCore_test_dirs.hpp"
 #include "ComplexCore/Filters/FindArrayStatisticsFilter.hpp"
-#include "ComplexCore/Filters/MultiThresholdObjects.hpp"
+#include "ComplexCore/Filters/ImportDREAM3DFilter.hpp"
 
 using namespace complex;
 using namespace complex::Constants;
+
+template <typename T>
+void CompareDataArrays(const IDataArray& left, const IDataArray& right)
+{
+  const auto& oldDataStore = left.getIDataStoreRefAs<AbstractDataStore<T>>();
+  const auto& newDataStore = right.getIDataStoreRefAs<AbstractDataStore<T>>();
+  usize start = 0;
+  usize end = oldDataStore.getSize();
+  bool same = true;
+  usize badIndex = 0;
+  for(usize i = start; i < end; i++)
+  {
+    if(oldDataStore[i] != newDataStore[i])
+    {
+      badIndex = i;
+      REQUIRE(oldDataStore[badIndex] == newDataStore[badIndex]);
+      break;
+    }
+  }
+}
 
 TEST_CASE("FindArrayStatisticsFilter: Instantiate Filter", "[ComplexCore][FindArrayStatisticsFilter]")
 {
@@ -69,77 +93,181 @@ TEST_CASE("FindArrayStatisticsFilter: Instantiate Filter", "[ComplexCore][FindAr
 
 TEST_CASE("FindArrayStatisticsFilter: Test Algorithm", "[ComplexCore][FindArrayStatisticsFilter]")
 {
-  DataPath statsGroup({Constants::k_SmallIN100, "Statistics"});
-  DataPath maskPath({Constants::k_SmallIN100, Constants::k_EbsdScanData, "Mask"});
-  DataPath confIndexPath({Constants::k_SmallIN100, Constants::k_EbsdScanData, Constants::k_ConfidenceIndex});
-  DataStructure dataGraph = UnitTest::CreateDataStructure();
-  dataGraph.makePath(statsGroup);
+  const std::string k_StatisticsGroupName("Statistics");
+  const std::string k_Mask("Mask");
+  const std::string k_SEMSignal("SEM Signal");
+  const std::string k_FeatureIdsName("FeatureIds");
+  const DataPath k_TopLevelItemPath({"Exemplar Data"});
+  const DataPath k_StatisticsGroupPath = k_TopLevelItemPath.createChildPath(k_StatisticsGroupName);
+  const DataPath k_CellAttributeMatrix = k_TopLevelItemPath.createChildPath("CellData");
+  const DataPath k_MaskArrayPath = k_CellAttributeMatrix.createChildPath(k_Mask);
+  const DataPath k_FeatureIdsPath = k_CellAttributeMatrix.createChildPath(k_FeatureIdsName);
+  const DataPath k_SEMSignalPath = k_CellAttributeMatrix.createChildPath(k_SEMSignal);
 
+  DataStructure exemplarDataStructure;
+  // Read Exemplar DREAM3D File Filter
   {
-    MultiThresholdObjects thresholdsFilter;
-    Arguments thresholdArgs;
+    ImportDREAM3DFilter filter;
 
-    ArrayThresholdSet arrayThresholdset;
-    ArrayThresholdSet::CollectionType thresholds;
+    Dream3dImportParameter::ImportData parameter;
+    parameter.FilePath = fs::path(fmt::format("{}/TestFiles/find_array_statistics.dream3d", unit_test::k_DREAM3DDataDir));
 
-    std::shared_ptr<ArrayThreshold> ciThreshold = std::make_shared<ArrayThreshold>();
-    ciThreshold->setArrayPath(confIndexPath);
-    ciThreshold->setComparisonType(ArrayThreshold::ComparisonType::GreaterThan);
-    ciThreshold->setComparisonValue(0.1);
-    thresholds.push_back(ciThreshold);
-
-    arrayThresholdset.setArrayThresholds(thresholds);
-
-    constexpr StringLiteral k_ArrayThresholds_Key = "array_thresholds";
-    constexpr StringLiteral k_CreatedDataPath_Key = "created_data_path";
-    thresholdArgs.insertOrAssign(k_ArrayThresholds_Key, std::make_any<ArrayThresholdsParameter::ValueType>(arrayThresholdset));
-    thresholdArgs.insertOrAssign(k_CreatedDataPath_Key, std::make_any<DataPath>(maskPath));
+    Arguments args;
+    args.insertOrAssign(ImportDREAM3DFilter::k_ImportFileData, std::make_any<Dream3dImportParameter::ImportData>(parameter));
 
     // Preflight the filter and check result
-    auto preflightResult = thresholdsFilter.preflight(dataGraph, thresholdArgs);
+    auto preflightResult = filter.preflight(exemplarDataStructure, args);
     REQUIRE(preflightResult.outputActions.valid());
 
     // Execute the filter and check the result
-    auto executeResult = thresholdsFilter.execute(dataGraph, thresholdArgs);
+    auto executeResult = filter.execute(exemplarDataStructure, args);
     REQUIRE(executeResult.result.valid());
   }
 
-  FindArrayStatisticsFilter filter;
-  Arguments args;
-  args.insertOrAssign(FindArrayStatisticsFilter::k_FindHistogram_Key, std::make_any<bool>(false));
-  args.insertOrAssign(FindArrayStatisticsFilter::k_MinRange_Key, std::make_any<float64>(2.3456789));
-  args.insertOrAssign(FindArrayStatisticsFilter::k_MaxRange_Key, std::make_any<float64>(2.3456789));
-  args.insertOrAssign(FindArrayStatisticsFilter::k_UseFullRange_Key, std::make_any<bool>(false));
-  args.insertOrAssign(FindArrayStatisticsFilter::k_NumBins_Key, std::make_any<int32>(1234356));
-  args.insertOrAssign(FindArrayStatisticsFilter::k_FindLength_Key, std::make_any<bool>(false));
-  args.insertOrAssign(FindArrayStatisticsFilter::k_FindMin_Key, std::make_any<bool>(false));
-  args.insertOrAssign(FindArrayStatisticsFilter::k_FindMax_Key, std::make_any<bool>(false));
-  args.insertOrAssign(FindArrayStatisticsFilter::k_FindMean_Key, std::make_any<bool>(false));
-  args.insertOrAssign(FindArrayStatisticsFilter::k_FindMedian_Key, std::make_any<bool>(false));
-  args.insertOrAssign(FindArrayStatisticsFilter::k_FindStdDeviation_Key, std::make_any<bool>(false));
-  args.insertOrAssign(FindArrayStatisticsFilter::k_FindSummation_Key, std::make_any<bool>(false));
-  args.insertOrAssign(FindArrayStatisticsFilter::k_UseMask_Key, std::make_any<bool>(false));
-  // args.insertOrAssign(FindArrayStatisticsFilter::k_ComputeByIndex_Key, std::make_any<bool>(false));
-  args.insertOrAssign(FindArrayStatisticsFilter::k_StandardizeData_Key, std::make_any<bool>(false));
-  args.insertOrAssign(FindArrayStatisticsFilter::k_SelectedArrayPath_Key, std::make_any<DataPath>(confIndexPath));
-  // args.insertOrAssign(FindArrayStatisticsFilter::k_FeatureIdsArrayPath_Key, std::make_any<DataPath>(DataPath{Constants::k_FeatureIds}));
-  args.insertOrAssign(FindArrayStatisticsFilter::k_MaskArrayPath_Key, std::make_any<DataPath>(maskPath));
-  args.insertOrAssign(FindArrayStatisticsFilter::k_DestinationAttributeMatrix_Key, std::make_any<DataPath>(statsGroup));
-  args.insertOrAssign(FindArrayStatisticsFilter::k_HistogramArrayName_Key, std::make_any<std::string>("Histogram"));
-  args.insertOrAssign(FindArrayStatisticsFilter::k_LengthArrayName_Key, std::make_any<std::string>("Length"));
-  args.insertOrAssign(FindArrayStatisticsFilter::k_MinimumArrayName_Key, std::make_any<std::string>("Minimum"));
-  args.insertOrAssign(FindArrayStatisticsFilter::k_MaximumArrayName_Key, std::make_any<std::string>("Maximum"));
-  args.insertOrAssign(FindArrayStatisticsFilter::k_MeanArrayName_Key, std::make_any<std::string>("Mean"));
-  args.insertOrAssign(FindArrayStatisticsFilter::k_MedianArrayName_Key, std::make_any<std::string>("Median"));
-  args.insertOrAssign(FindArrayStatisticsFilter::k_StdDeviationArrayName_Key, std::make_any<std::string>("Standard Deviation"));
-  args.insertOrAssign(FindArrayStatisticsFilter::k_SummationArrayName_Key, std::make_any<std::string>("Summation"));
-  args.insertOrAssign(FindArrayStatisticsFilter::k_StandardizedArrayName_Key, std::make_any<std::string>("Standardization"));
+  DataStructure dataStructure;
+  // read data set
+  {
+    ImportDREAM3DFilter filter;
 
-  // Preflight the filter and check result
-  auto preflightResult = filter.preflight(dataGraph, args);
-  REQUIRE(preflightResult.outputActions.valid());
+    Dream3dImportParameter::ImportData parameter;
+    parameter.FilePath = fs::path(fmt::format("{}/TestFiles/bad_data_neighbor_orientation_check.dream3d", unit_test::k_DREAM3DDataDir));
 
-  // Execute the filter and check the result
-  auto executeResult = filter.execute(dataGraph, args);
-  REQUIRE(executeResult.result.valid());
+    Arguments args;
+    args.insertOrAssign(ImportDREAM3DFilter::k_ImportFileData, std::make_any<Dream3dImportParameter::ImportData>(parameter));
+
+    // Preflight the filter and check result
+    auto preflightResult = filter.preflight(dataStructure, args);
+    REQUIRE(preflightResult.outputActions.valid());
+
+    // Execute the filter and check the result
+    auto executeResult = filter.execute(dataStructure, args);
+    REQUIRE(executeResult.result.valid());
+  }
+
+  // create a data group to store the statistics
+  DataGroup* statisticsData = DataGroup::Create(dataStructure, k_StatisticsGroupName, dataStructure.getData(k_TopLevelItemPath)->getId());
+
+  // Find Array Statistics Filter
+  {
+    FindArrayStatisticsFilter filter;
+    Arguments args;
+    args.insertOrAssign(FindArrayStatisticsFilter::k_FindHistogram_Key, std::make_any<bool>(true));
+    args.insertOrAssign(FindArrayStatisticsFilter::k_MinRange_Key, std::make_any<float64>(-1300.0));
+    args.insertOrAssign(FindArrayStatisticsFilter::k_MaxRange_Key, std::make_any<float64>(0.0));
+    args.insertOrAssign(FindArrayStatisticsFilter::k_UseFullRange_Key, std::make_any<bool>(true));
+    args.insertOrAssign(FindArrayStatisticsFilter::k_NumBins_Key, std::make_any<int32>(8));
+    args.insertOrAssign(FindArrayStatisticsFilter::k_FindLength_Key, std::make_any<bool>(true));
+    args.insertOrAssign(FindArrayStatisticsFilter::k_FindMin_Key, std::make_any<bool>(true));
+    args.insertOrAssign(FindArrayStatisticsFilter::k_FindMax_Key, std::make_any<bool>(true));
+    args.insertOrAssign(FindArrayStatisticsFilter::k_FindMean_Key, std::make_any<bool>(true));
+    args.insertOrAssign(FindArrayStatisticsFilter::k_FindMedian_Key, std::make_any<bool>(true));
+    args.insertOrAssign(FindArrayStatisticsFilter::k_FindStdDeviation_Key, std::make_any<bool>(true));
+    args.insertOrAssign(FindArrayStatisticsFilter::k_FindSummation_Key, std::make_any<bool>(true));
+    args.insertOrAssign(FindArrayStatisticsFilter::k_UseMask_Key, std::make_any<bool>(true));
+    // args.insertOrAssign(FindArrayStatisticsFilter::k_ComputeByIndex_Key, std::make_any<bool>(true));
+    args.insertOrAssign(FindArrayStatisticsFilter::k_StandardizeData_Key, std::make_any<bool>(true));
+    args.insertOrAssign(FindArrayStatisticsFilter::k_SelectedArrayPath_Key, std::make_any<DataPath>(k_SEMSignalPath));
+    // args.insertOrAssign(FindArrayStatisticsFilter::k_FeatureIdsArrayPath_Key, std::make_any<DataPath>(k_FeatureIdsPath));
+    args.insertOrAssign(FindArrayStatisticsFilter::k_MaskArrayPath_Key, std::make_any<DataPath>(k_MaskArrayPath));
+    args.insertOrAssign(FindArrayStatisticsFilter::k_DestinationAttributeMatrix_Key, std::make_any<DataPath>(k_StatisticsGroupPath));
+    args.insertOrAssign(FindArrayStatisticsFilter::k_HistogramArrayName_Key, std::make_any<std::string>("Histogram"));
+    args.insertOrAssign(FindArrayStatisticsFilter::k_LengthArrayName_Key, std::make_any<std::string>("Length"));
+    args.insertOrAssign(FindArrayStatisticsFilter::k_MinimumArrayName_Key, std::make_any<std::string>("Minimum"));
+    args.insertOrAssign(FindArrayStatisticsFilter::k_MaximumArrayName_Key, std::make_any<std::string>("Maximum"));
+    args.insertOrAssign(FindArrayStatisticsFilter::k_MeanArrayName_Key, std::make_any<std::string>("Mean"));
+    args.insertOrAssign(FindArrayStatisticsFilter::k_MedianArrayName_Key, std::make_any<std::string>("Median"));
+    args.insertOrAssign(FindArrayStatisticsFilter::k_StdDeviationArrayName_Key, std::make_any<std::string>("Standard Deviation"));
+    args.insertOrAssign(FindArrayStatisticsFilter::k_SummationArrayName_Key, std::make_any<std::string>("Summation"));
+    args.insertOrAssign(FindArrayStatisticsFilter::k_StandardizedArrayName_Key, std::make_any<std::string>("Standardization"));
+
+    // Preflight the filter and check result
+    auto preflightResult = filter.preflight(dataStructure, args);
+    REQUIRE(preflightResult.outputActions.valid());
+
+    // Execute the filter and check the result
+    auto executeResult = filter.execute(dataStructure, args);
+    REQUIRE(executeResult.result.valid());
+  }
+
+  // Loop and compare each array from the 'Exemplar Data / CellData' to the 'Test Data / CellData' group
+  {
+    auto& cellDataGroup = dataStructure.getDataRefAs<DataGroup>(k_CellAttributeMatrix);
+    std::vector<DataPath> selectedCellArrays;
+
+    // Create the vector of selected cell DataPaths
+    for(auto& child : cellDataGroup)
+    {
+      selectedCellArrays.push_back(k_CellAttributeMatrix.createChildPath(child.second->getName()));
+    }
+
+    for(const auto& cellArrayPath : selectedCellArrays)
+    {
+      const auto& generatedDataArray = dataStructure.getDataRefAs<IDataArray>(cellArrayPath);
+      DataType type = generatedDataArray.getDataType();
+
+      // Now generate the path to the exemplar data set in the exemplar data structure.
+      std::vector<std::string> generatedPathVector = cellArrayPath.getPathVector();
+      DataPath exemplarDataArrayPath(generatedPathVector);
+      auto& exemplarDataArray = exemplarDataStructure.getDataRefAs<IDataArray>(exemplarDataArrayPath);
+      DataType exemplarType = exemplarDataArray.getDataType();
+
+      if(type != exemplarType)
+      {
+        std::cout << fmt::format("DataArray {} and {} do not have the same type: {} vs {}. Data Will not be compared.", generatedDataArray.getName(), exemplarDataArray.getName(), type, exemplarType)
+                  << std::endl;
+        continue;
+      }
+
+      switch(type)
+      {
+      case DataType::boolean: {
+        CompareDataArrays<bool>(generatedDataArray, exemplarDataArray);
+        break;
+      }
+      case DataType::int8: {
+        CompareDataArrays<int8>(generatedDataArray, exemplarDataArray);
+        break;
+      }
+      case DataType::int16: {
+        CompareDataArrays<int16>(generatedDataArray, exemplarDataArray);
+        break;
+      }
+      case DataType::int32: {
+        CompareDataArrays<int32>(generatedDataArray, exemplarDataArray);
+        break;
+      }
+      case DataType::int64: {
+        CompareDataArrays<int64>(generatedDataArray, exemplarDataArray);
+        break;
+      }
+      case DataType::uint8: {
+        CompareDataArrays<uint8>(generatedDataArray, exemplarDataArray);
+        break;
+      }
+      case DataType::uint16: {
+        CompareDataArrays<uint16>(generatedDataArray, exemplarDataArray);
+        break;
+      }
+      case DataType::uint32: {
+        CompareDataArrays<uint32>(generatedDataArray, exemplarDataArray);
+        break;
+      }
+      case DataType::uint64: {
+        CompareDataArrays<uint64>(generatedDataArray, exemplarDataArray);
+        break;
+      }
+      case DataType::float32: {
+        CompareDataArrays<float32>(generatedDataArray, exemplarDataArray);
+        break;
+      }
+      case DataType::float64: {
+        CompareDataArrays<float64>(generatedDataArray, exemplarDataArray);
+        break;
+      }
+      default: {
+        throw std::runtime_error("Invalid DataType");
+      }
+      }
+    }
+  }
 }
