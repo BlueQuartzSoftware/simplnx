de3ce3bad9a2fedd19e6c36abe4d2504847bc7e9
Jessica Marquis <83971431+jmarquisbq@users.noreply.github.com>
2022-07-22 09:26:22 -0400

    FindArrayStatisticsFilter  (#258)
    * [FindArrayStatistics]: Add filter to complex core
    * Implement/port FindArrayStatisticsFilter
    * Add FindArrayStatisticsFilter unit test
    * Add some comments to the FindArrayStatistics algorithm
    * Improve the unit test for FindArrayStatisticsFilter
    * Fix/simplify unit test
    * Add missing include for macos build
    Co-authored-by: Michael Jackson <mike.jackson@bluequartz.net>

---
+++ src/Plugins/ComplexCore/src/ComplexCore/Filters/Algorithms/FindArrayStatistics.hpp
@@ -0,0 +1,76 @@
+#pragma once
+
+#include "ComplexCore/ComplexCore_export.hpp"
+
+#include "complex/DataStructure/DataArray.hpp"
+#include "complex/DataStructure/DataPath.hpp"
+#include "complex/DataStructure/DataStructure.hpp"
+#include "complex/Filter/IFilter.hpp"
+
+namespace complex
+{
+
+struct COMPLEXCORE_EXPORT FindArrayStatisticsInputValues
+{
+  bool FindHistogram;
+  float64 MinRange;
+  float64 MaxRange;
+  bool UseFullRange;
+  int32 NumBins;
+  bool FindLength;
+  bool FindMin;
+  bool FindMax;
+  bool FindMean;
+  bool FindMedian;
+  bool FindStdDeviation;
+  bool FindSummation;
+  bool UseMask;
+  bool ComputeByIndex;
+  bool StandardizeData;
+  DataPath SelectedArrayPath;
+  DataPath FeatureIdsArrayPath;
+  DataPath MaskArrayPath;
+  DataPath DestinationAttributeMatrix;
+  DataPath HistogramArrayName;
+  DataPath LengthArrayName;
+  DataPath MinimumArrayName;
+  DataPath MaximumArrayName;
+  DataPath MeanArrayName;
+  DataPath MedianArrayName;
+  DataPath StdDeviationArrayName;
+  DataPath SummationArrayName;
+  DataPath StandardizedArrayName;
+};
+
+/**
+ * @class ConditionalSetValue
+ * @brief This filter replaces values in the target array with a user specified value
+ * where a bool mask array specifies.
+ */
+
+class COMPLEXCORE_EXPORT FindArrayStatistics
+{
+public:
+  FindArrayStatistics(DataStructure& dataStructure, const IFilter::MessageHandler& mesgHandler, const std::atomic_bool& shouldCancel, FindArrayStatisticsInputValues* inputValues);
+  ~FindArrayStatistics() noexcept;
+
+  FindArrayStatistics(const FindArrayStatistics&) = delete;
+  FindArrayStatistics(FindArrayStatistics&&) noexcept = delete;
+  FindArrayStatistics& operator=(const FindArrayStatistics&) = delete;
+  FindArrayStatistics& operator=(FindArrayStatistics&&) noexcept = delete;
+
+  Result<> operator()();
+
+  const std::atomic_bool& getCancel();
+
+  template <typename T>
+  Result<> findStats(const DataArray<T>& inputArray, std::vector<IDataArray*>& arrays, int32 numFeatures);
+
+private:
+  DataStructure& m_DataStructure;
+  const FindArrayStatisticsInputValues* m_InputValues = nullptr;
+  const std::atomic_bool& m_ShouldCancel;
+  const IFilter::MessageHandler& m_MessageHandler;
+};
+
+} // namespace complex
