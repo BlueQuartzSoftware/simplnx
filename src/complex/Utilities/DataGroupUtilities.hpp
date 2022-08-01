86658f5e43a45b24c956a69e1afba4e1bcd0df1e
Michael Jackson <mike.jackson@bluequartz.net>
2022-08-01 13:31:05 -0400

    DataGroupUtilities: Add function to return vector<IDataArray> of specific DataArrays (#269)
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- src/complex/Utilities/DataGroupUtilities.hpp
+++ src/complex/Utilities/DataGroupUtilities.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
 #include "complex/DataStructure/DataArray.hpp"
-#include "complex/DataStructure/DataGroup.hpp"
-#include "complex/DataStructure/DataMap.hpp"
-#include "complex/DataStructure/DataStore.hpp"
+#include "complex/DataStructure/DataPath.hpp"
 #include "complex/DataStructure/DataStructure.hpp"
+#include "complex/DataStructure/IDataArray.hpp"
 #include "complex/complex_export.hpp"
 
+#include <memory>
 #include <vector>
 
 namespace complex
@@ -22,5 +22,14 @@ namespace complex
  * @return
  */
 COMPLEX_EXPORT bool RemoveInactiveObjects(DataStructure& dataStructure, DataPath& featureDataGroupPath, const std::vector<bool>& activeObjects, Int32Array& cellFeatureIds, size_t currentFeatureCount);
+
+/**
+ * @brief This function will gather all of the sibling DataArrays to the input DataPath, then filter out all the 'IgnoredDataPaths`
+ * @param dataStructure The DataStructure to operate on
+ * @param dataArrayPath The DataArrayPath to find all sibling DataArrays to
+ * @param ignoredDataPaths Vector of DataPaths that should be remove from the final vector.
+ * @return
+ */
+COMPLEX_EXPORT std::vector<std::shared_ptr<IDataArray>> GenerateDataArrayList(const DataStructure& dataStructure, const DataPath& dataArrayPath, const std::vector<DataPath>& ignoredDataPaths);
 
 } // namespace complex
