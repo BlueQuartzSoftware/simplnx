8d4463a3716223d80c4596da585111f8885b37db
Michael Jackson <mike.jackson@bluequartz.net>
2022-08-09 12:20:12 -0400

    FindNeighbors: Update unit test to use exemplary file for comparisons
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- src/Plugins/ComplexCore/test/FindNeighborsTest.cpp
+++ src/Plugins/ComplexCore/test/FindNeighborsTest.cpp
@@ -1,88 +1,125 @@
 #include <catch2/catch.hpp>
 
 #include "ComplexCore/Filters/FindNeighbors.hpp"
-#include "complex/Parameters/DynamicTableParameter.hpp"
 #include "complex/UnitTest/UnitTestCommon.hpp"
+#include "complex/Utilities/Parsing/HDF5/H5FileWriter.hpp"
 
 #include "ComplexCore/ComplexCore_test_dirs.hpp"
 
+namespace fs = std::filesystem;
+
 using namespace complex;
+using namespace complex::Constants;
 
 namespace
 {
-const std::string k_ImageGeomName = "Image Geom";
-const std::string k_FeatureIdsName = "Feature IDs";
-const std::string k_BoundaryCellsName = "Boundary Cells";
-const std::string k_NumNeighborsName = "Num Neighbors";
-const std::string k_NeighborListName = "Neighbor List";
-const std::string k_SharedSurfaceAreaName = "Shared Surface Area";
-const std::string k_SurfaceFeaturesName = "Surface Features";
-const std::string k_CellFeatureName = "Feature Data";
 
-DataStructure createTestData()
+template <typename T>
+void CompareArrays(const DataStructure& dataStructure, const DataPath& exemplaryDataPath, const DataPath& computedPath)
 {
-  DataStructure data;
-  auto* imageGeom = ImageGeom::Create(data, k_ImageGeomName);
-  imageGeom->setDimensions({5, 5, 5});
-  imageGeom->setOrigin({0, 0, 0});
-  imageGeom->setSpacing({1, 1, 1});
+  // DataPath exemplaryDataPath = featureGroup.createChildPath("SurfaceFeatures");
+  REQUIRE_NOTHROW(dataStructure.getDataRefAs<DataArray<T>>(exemplaryDataPath));
+  REQUIRE_NOTHROW(dataStructure.getDataRefAs<DataArray<T>>(computedPath));
 
-  IDataStore::ShapeType tupleShape{5, 5, 5};
-  IDataStore::ShapeType compDims{1};
+  const auto& featureArrayExemplary = dataStructure.getDataRefAs<DataArray<T>>(exemplaryDataPath);
+  const auto& createdFeatureArray = dataStructure.getDataRefAs<DataArray<T>>(computedPath);
+  REQUIRE(createdFeatureArray.getNumberOfTuples() == featureArrayExemplary.getNumberOfTuples());
 
+  for(usize i = 0; i < featureArrayExemplary.getSize(); i++)
   {
-    auto* featureIdsArray = Int32Array::CreateWithStore<Int32DataStore>(data, k_FeatureIdsName, tupleShape, compDims, imageGeom->getId());
-    auto& featureIds = featureIdsArray->getDataStoreRef();
-    for(usize i = 0; i < featureIds.getSize(); ++i)
+    REQUIRE(featureArrayExemplary[i] == createdFeatureArray[i]);
+  }
+}
+
+template <typename T>
+void CompareNeighborLists(const DataStructure& dataStructure, const DataPath& exemplaryDataPath, const DataPath& computedPath)
+{
+  // DataPath exemplaryDataPath = featureGroup.createChildPath("SurfaceFeatures");
+  REQUIRE_NOTHROW(dataStructure.getDataRefAs<NeighborList<T>>(exemplaryDataPath));
+  REQUIRE_NOTHROW(dataStructure.getDataRefAs<NeighborList<T>>(computedPath));
+
+  const auto& exemplaryList = dataStructure.getDataRefAs<NeighborList<T>>(exemplaryDataPath);
+  const auto& computedNeighborList = dataStructure.getDataRefAs<NeighborList<T>>(computedPath);
+  REQUIRE(computedNeighborList.getNumberOfTuples() == exemplaryList.getNumberOfTuples());
+
+  for(usize i = 0; i < exemplaryList.getSize(); i++)
+  {
+    const auto exemplary = exemplaryList.getList(i);
+    const auto computed = computedNeighborList.getList(i);
+    if(exemplary.get() != nullptr && computed.get() != nullptr)
     {
-      featureIds[i] = i / 3;
+      REQUIRE(exemplary->size() == computed->size());
     }
   }
-
-  DataGroup* featureGroup = DataGroup::Create(data, k_CellFeatureName, imageGeom->getId());
-
-  DataPath activeArrayPath({k_ImageGeomName, k_CellFeatureName, "Actives"});
-
-  auto* featureArray = Int32Array::CreateWithStore<Int32DataStore>(data, "Actives", tupleShape, compDims, featureGroup->getId());
-  featureArray->fill(5);
-
-  return data;
 }
+
 } // namespace
 
-TEST_CASE("ComplexCore::FindNeighbors(Instantiate)", "[ComplexCore][FindNeighbors]")
+TEST_CASE("ComplexCore::FindNeighbors", "[ComplexCore][FindNeighbors]")
 {
-  static constexpr uint64 k_NComp = 3;
-  static constexpr uint64 k_NumTuples = 25;
-  const static DynamicTableData::TableDataType k_TupleDims = {{static_cast<double>(k_NumTuples)}};
 
-  static const DataPath k_ImageGeom({k_ImageGeomName});
-  static const DataPath k_FeatureIds({k_ImageGeomName, k_FeatureIdsName});
-  static const DataPath k_BoundaryCells({k_ImageGeomName, k_BoundaryCellsName});
-  static const DataPath k_SurfaceFeatures({k_ImageGeomName, k_SurfaceFeaturesName});
-  static const DataPath k_NumNeighbors({k_ImageGeomName, k_NumNeighborsName});
-  static const DataPath k_NeighborList({k_ImageGeomName, k_NeighborListName});
-  static const DataPath k_SharedSurfaceArea({k_ImageGeomName, k_SharedSurfaceAreaName});
-  static const DataPath k_CellFeatures({k_ImageGeomName, k_CellFeatureName});
+  // Read the Small IN100 Data set
+  auto baseDataFilePath = fs::path(fmt::format("{}/6_5_test_data_1.dream3d", unit_test::k_TestDataSourceDir));
+  DataStructure dataStructure = UnitTest::LoadDataStructure(baseDataFilePath);
 
-  FindNeighbors filter;
-  DataStructure ds = createTestData();
-  Arguments args;
+  DataPath smallIn100Group({complex::Constants::k_DataContainer});
+  DataPath cellDataAttributeMatrix = smallIn100Group.createChildPath(k_CellData);
+  DataPath featureIdsDataPath({k_DataContainer, k_CellData, k_FeatureIds});
+  DataPath cellFeatureAttributeMatrixPath({k_DataContainer, k_CellFeatureData});
+  DataPath numNeighborPath = cellFeatureAttributeMatrixPath.createChildPath("NumNeighbors2");
+  DataPath neighborListPath = cellFeatureAttributeMatrixPath.createChildPath("NeighborList2");
+  DataPath sharedSurfaceAreaListPath = cellFeatureAttributeMatrixPath.createChildPath("SharedSurfaceAreaList2");
+  DataPath boundaryCellsPath = cellFeatureAttributeMatrixPath.createChildPath("BoundaryCells_computed");
+  DataPath surfaceFeaturesPath = cellFeatureAttributeMatrixPath.createChildPath("SurfaceFeatures_computed");
 
-  args.insert(FindNeighbors::k_StoreBoundary_Key, std::make_any<bool>(false));
-  args.insert(FindNeighbors::k_StoreSurface_Key, std::make_any<bool>(false));
-  args.insert(FindNeighbors::k_ImageGeom_Key, std::make_any<DataPath>(k_ImageGeom));
-  args.insert(FindNeighbors::k_FeatureIds_Key, std::make_any<DataPath>(k_FeatureIds));
-  args.insert(FindNeighbors::k_CellFeatures_Key, std::make_any<DataPath>(k_CellFeatures));
-  args.insert(FindNeighbors::k_BoundaryCells_Key, std::make_any<DataPath>(k_BoundaryCells));
-  args.insert(FindNeighbors::k_NumNeighbors_Key, std::make_any<DataPath>(k_NumNeighbors));
-  args.insert(FindNeighbors::k_NeighborList_Key, std::make_any<DataPath>(k_NeighborList));
-  args.insert(FindNeighbors::k_SharedSurfaceArea_Key, std::make_any<DataPath>(k_SharedSurfaceArea));
-  args.insert(FindNeighbors::k_SurfaceFeatures_Key, std::make_any<DataPath>(k_SurfaceFeatures));
+  {
+    FindNeighbors filter;
+    Arguments args;
 
-  auto preflightResult = filter.preflight(ds, args);
-  COMPLEX_RESULT_REQUIRE_VALID(preflightResult.outputActions);
+    args.insertOrAssign(FindNeighbors::k_ImageGeom_Key, std::make_any<DataPath>(smallIn100Group));
+    args.insertOrAssign(FindNeighbors::k_FeatureIds_Key, std::make_any<DataPath>(featureIdsDataPath));
+    args.insertOrAssign(FindNeighbors::k_CellFeatures_Key, std::make_any<DataPath>(cellFeatureAttributeMatrixPath));
 
-  auto result = filter.execute(ds, args);
-  COMPLEX_RESULT_REQUIRE_VALID(result.result);
+    args.insertOrAssign(FindNeighbors::k_StoreBoundary_Key, std::make_any<bool>(true));
+    args.insertOrAssign(FindNeighbors::k_BoundaryCells_Key, std::make_any<DataPath>(boundaryCellsPath));
+
+    args.insertOrAssign(FindNeighbors::k_StoreSurface_Key, std::make_any<bool>(true));
+    args.insertOrAssign(FindNeighbors::k_SurfaceFeatures_Key, std::make_any<DataPath>(surfaceFeaturesPath));
+
+    args.insertOrAssign(FindNeighbors::k_NumNeighbors_Key, std::make_any<DataPath>(numNeighborPath));
+    args.insertOrAssign(FindNeighbors::k_NeighborList_Key, std::make_any<DataPath>(neighborListPath));
+    args.insertOrAssign(FindNeighbors::k_SharedSurfaceArea_Key, std::make_any<DataPath>(sharedSurfaceAreaListPath));
+
+    // Preflight the filter and check result
+    auto preflightResult = filter.preflight(dataStructure, args);
+    COMPLEX_RESULT_REQUIRE_VALID(preflightResult.outputActions);
+
+    // Execute the filter and check the result
+    auto executeResult = filter.execute(dataStructure, args);
+    COMPLEX_RESULT_REQUIRE_VALID(executeResult.result);
+  }
+
+  // Output
+  {
+    DataPath featureGroup = smallIn100Group.createChildPath(complex::Constants::k_CellFeatureData);
+    DataPath exemplaryDataPath = featureGroup.createChildPath("SurfaceFeatures");
+    CompareArrays<bool>(dataStructure, exemplaryDataPath, surfaceFeaturesPath);
+
+    exemplaryDataPath = featureGroup.createChildPath("NumNeighbors2");
+    CompareArrays<int32>(dataStructure, exemplaryDataPath, numNeighborPath);
+
+    exemplaryDataPath = featureGroup.createChildPath("NeighborList2");
+    CompareNeighborLists<int32>(dataStructure, exemplaryDataPath, neighborListPath);
+
+    exemplaryDataPath = featureGroup.createChildPath("SharedSurfaceAreaList2");
+    CompareNeighborLists<float32>(dataStructure, exemplaryDataPath, sharedSurfaceAreaListPath);
+  }
+
+  {
+    // Write out the DataStructure for later viewing/debugging
+    Result<H5::FileWriter> result = H5::FileWriter::CreateFile(fmt::format("{}/find_neighbors_test.dream3d", unit_test::k_BinaryDir));
+    H5::FileWriter fileWriter = std::move(result.value());
+    herr_t err = dataStructure.writeHdf5(fileWriter);
+    REQUIRE(err >= 0);
+  }
 }
