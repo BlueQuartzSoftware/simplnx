2a8e92c0c8dc2aef99aa8ac3657ed09298f40008
bpenniebq <106270421+bpenniebq@users.noreply.github.com>
2022-08-11 10:32:51 -0400

    Added getTypeName function to NeighborList header to display type in hover info (#270)
    * Added getTypeName function to NeighborList to display type in hover info
    * Templated NeighborListFactory like DataArrayFactory to handle different types and fix test errors
    * Added Test Case for invalid input arrays
    * fixed wording in error message
    
    
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>
    Co-authored-by: Michael Jackson <mike.jackson@bluequartz.net>

--- src/Plugins/ComplexCore/test/FindNeighborListStatisticsTest.cpp
+++ src/Plugins/ComplexCore/test/FindNeighborListStatisticsTest.cpp
@@ -177,3 +177,51 @@ TEST_CASE("ComplexCore::FindNeighborListStatistics: Test Algorithm", "[FindNeigh
   REQUIRE(sumVal2 == 24753.0f);
   REQUIRE(sumVal3 == 22391.0f);
 }
+
+TEST_CASE("ComplexCore::FindNeighborListStatistics: Invalid Input Array", "[FindNeighborListStatistics]")
+{
+  DataStructure dataStructure;
+  DataGroup* topLevelGroup = DataGroup::Create(dataStructure, "TestData");
+  DataGroup* statsGroup = DataGroup::Create(dataStructure, "Statistics", topLevelGroup->getId());
+  DataPath statsDataPath({"TestData", "Statistics"});
+  DataPath inputArrayPath({"TestData", "Data Array"});
+  DataPath lengthOutputPath = statsDataPath.createChildPath("Length");
+  DataPath minimumOutputPath = statsDataPath.createChildPath("Minimum");
+  DataPath maximumOutputPath = statsDataPath.createChildPath("Maximum");
+  DataPath meanOutputPath = statsDataPath.createChildPath("Mean");
+  DataPath medianOutputPath = statsDataPath.createChildPath("Median");
+  DataPath stdDevOutputPath = statsDataPath.createChildPath("Standard Deviation");
+  DataPath summationOutputPath = statsDataPath.createChildPath("Summation");
+
+  auto dataStore = std::make_unique<DataStore<float32>>(std::vector<usize>{0}, std::vector<usize>{3}, 0.0f);
+  auto* dataArray = DataArray<float32>::Create(dataStructure, "Data Array", std::move(dataStore), topLevelGroup->getId());
+  dataArray->fill(10.0f);
+
+  FindNeighborListStatistics filter;
+  Arguments args;
+
+  args.insertOrAssign(FindNeighborListStatistics::k_FindLength_Key, std::make_any<bool>(true));
+  args.insertOrAssign(FindNeighborListStatistics::k_FindMinimum_Key, std::make_any<bool>(true));
+  args.insertOrAssign(FindNeighborListStatistics::k_FindMaximum_Key, std::make_any<bool>(true));
+  args.insertOrAssign(FindNeighborListStatistics::k_FindMean_Key, std::make_any<bool>(true));
+  args.insertOrAssign(FindNeighborListStatistics::k_FindMedian_Key, std::make_any<bool>(true));
+  args.insertOrAssign(FindNeighborListStatistics::k_FindStandardDeviation_Key, std::make_any<bool>(true));
+  args.insertOrAssign(FindNeighborListStatistics::k_FindSummation_Key, std::make_any<bool>(true));
+
+  args.insertOrAssign(FindNeighborListStatistics::k_InputArray_Key, std::make_any<DataPath>(inputArrayPath));
+  args.insertOrAssign(FindNeighborListStatistics::k_Length_Key, std::make_any<DataPath>(lengthOutputPath));
+  args.insertOrAssign(FindNeighborListStatistics::k_Minimum_Key, std::make_any<DataPath>(minimumOutputPath));
+  args.insertOrAssign(FindNeighborListStatistics::k_Maximum_Key, std::make_any<DataPath>(maximumOutputPath));
+  args.insertOrAssign(FindNeighborListStatistics::k_Mean_Key, std::make_any<DataPath>(meanOutputPath));
+  args.insertOrAssign(FindNeighborListStatistics::k_Median_Key, std::make_any<DataPath>(medianOutputPath));
+  args.insertOrAssign(FindNeighborListStatistics::k_StandardDeviation_Key, std::make_any<DataPath>(stdDevOutputPath));
+  args.insertOrAssign(FindNeighborListStatistics::k_Summation_Key, std::make_any<DataPath>(summationOutputPath));
+
+  // Preflight the filter and check result
+  auto preflightResult = filter.preflight(dataStructure, args);
+  REQUIRE(preflightResult.outputActions.invalid());
+
+  // Execute the filter and check the result
+  auto executeResult = filter.execute(dataStructure, args);
+  REQUIRE(executeResult.result.invalid());
+}
