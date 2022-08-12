f7f636181cd4a852b4201f3b7a208417025096dc
Matthew Marine <matthew.marine@bluequartz.net>
2022-08-12 08:54:09 -0400

    Added MoveData filter (#276)
    * Added filter, action, and unit test
    * Filter moves a DataObject from one parent to another using the DataPath provided for the target DataObject.
    * Cannot move to one of the DataObject's existing parents.

---
+++ src/Plugins/ComplexCore/src/ComplexCore/Filters/MoveData.hpp
@@ -0,0 +1,86 @@
+#pragma once
+
+#include "ComplexCore/ComplexCore_export.hpp"
+
+#include "complex/Common/StringLiteral.hpp"
+#include "complex/Filter/FilterTraits.hpp"
+#include "complex/Filter/IFilter.hpp"
+
+namespace complex
+{
+class COMPLEXCORE_EXPORT MoveData : public IFilter
+{
+public:
+  MoveData() = default;
+  ~MoveData() noexcept override = default;
+
+  MoveData(const MoveData&) = delete;
+  MoveData(MoveData&&) noexcept = delete;
+
+  MoveData& operator=(const MoveData&) = delete;
+  MoveData& operator=(MoveData&&) noexcept = delete;
+
+  // Parameter Keys
+  static inline constexpr StringLiteral k_Data_Key = "data";
+  static inline constexpr StringLiteral k_OriginalParent_Key = "original_parent";
+  static inline constexpr StringLiteral k_NewParent_Key = "new_parent";
+
+  /**
+   * @brief
+   * @return std::name
+   */
+  std::string name() const override;
+
+  /**
+   * @brief Returns the C++ classname of this filter.
+   * @return std::string
+   */
+  std::string className() const override;
+
+  /**
+   * @brief
+   * @return Uuid
+   */
+  Uuid uuid() const override;
+
+  /**
+   * @brief
+   * @return std::string
+   */
+  std::string humanName() const override;
+
+  /**
+   * @brief
+   * @return Parameters
+   */
+  Parameters parameters() const override;
+
+  /**
+   * @brief
+   * @return IFilter::UniquePointer
+   */
+  UniquePointer clone() const override;
+
+protected:
+  /**
+   * @brief
+   * @param data
+   * @param args
+   * @param messageHandler
+   * @return PreflightResult
+   */
+  PreflightResult preflightImpl(const DataStructure& data, const Arguments& args, const MessageHandler& messageHandler, const std::atomic_bool& shouldCancel) const override;
+
+  /**
+   * @brief
+   * @param data
+   * @param args
+   * @param pipelineNode
+   * @param messageHandler
+   * @return Result<>
+   */
+  Result<> executeImpl(DataStructure& data, const Arguments& args, const PipelineFilter* pipelineNode, const MessageHandler& messageHandler, const std::atomic_bool& shouldCancel) const override;
+};
+} // namespace complex
+
+COMPLEX_DEF_FILTER_TRAITS(complex, MoveData, "651e5894-ab7c-4176-b7f0-ea466c521753");
---
+++ src/Plugins/ComplexCore/test/MoveDataTest.cpp
@@ -0,0 +1,79 @@
+#include <catch2/catch.hpp>
+
+#include "ComplexCore/Filters/MoveData.hpp"
+
+#include "complex/DataStructure/DataGroup.hpp"
+#include "complex/UnitTest/UnitTestCommon.hpp"
+
+#include <set>
+
+using namespace complex;
+
+namespace
+{
+constexpr StringLiteral k_Group1Name = "Group1";
+constexpr StringLiteral k_Group2Name = "Group2";
+constexpr StringLiteral k_Group3Name = "Group3";
+constexpr StringLiteral k_Group4Name = "Group4";
+
+DataStructure createDataStructure()
+{
+  DataStructure data;
+  auto group1 = DataGroup::Create(data, k_Group1Name);
+  auto group2 = DataGroup::Create(data, k_Group2Name);
+  auto group3 = DataGroup::Create(data, k_Group3Name, group2->getId());
+  auto group4 = DataGroup::Create(data, k_Group4Name, group2->getId());
+
+  data.setAdditionalParent(group4->getId(), group3->getId());
+  return data;
+}
+} // namespace
+
+TEST_CASE("ComplexCore::MoveData Successful", "[Complex::Core][MoveData]")
+{
+  MoveData filter;
+  Arguments args;
+  DataStructure dataGraph = createDataStructure();
+
+  const DataPath k_Group1Path({k_Group1Name});
+  const DataPath k_Group3Path({k_Group2Name, k_Group3Name});
+
+  args.insertOrAssign(MoveData::k_Data_Key, std::make_any<DataPath>(k_Group3Path));
+  args.insertOrAssign(MoveData::k_NewParent_Key, std::make_any<DataPath>(k_Group1Path));
+
+  // Preflight the filter and check result
+  auto preflightResult = filter.preflight(dataGraph, args);
+  COMPLEX_RESULT_REQUIRE_VALID(preflightResult.outputActions);
+
+  // Execute the filter and check the result
+  auto executeResult = filter.execute(dataGraph, args);
+  COMPLEX_RESULT_REQUIRE_VALID(executeResult.result);
+
+  const auto* group1 = dataGraph.getDataAs<DataGroup>(k_Group1Path);
+  REQUIRE(group1->getDataMap().getSize() == 1);
+
+  const auto* group2 = dataGraph.getDataAs<DataGroup>(DataPath({k_Group2Name}));
+  REQUIRE(group2->getDataMap().getSize() == 1);
+
+  REQUIRE(dataGraph.getDataAs<DataGroup>(k_Group3Path) == nullptr);
+
+  const DataPath newGroup3Path = k_Group1Path.createChildPath(k_Group3Name);
+  REQUIRE(dataGraph.getDataAs<DataGroup>(newGroup3Path) != nullptr);
+}
+
+TEST_CASE("ComplexCore::MoveData Unsuccessful", "[Complex::Core][MoveData]")
+{
+  MoveData filter;
+  Arguments args;
+  DataStructure dataGraph = createDataStructure();
+
+  const DataPath k_Group2Path({k_Group2Name});
+  const DataPath k_Group3Path({k_Group3Name});
+
+  args.insertOrAssign(MoveData::k_Data_Key, std::make_any<DataPath>(k_Group3Path));
+  args.insertOrAssign(MoveData::k_NewParent_Key, std::make_any<DataPath>(k_Group2Path));
+
+  // Preflight the filter and check result
+  auto preflightResult = filter.preflight(dataGraph, args);
+  COMPLEX_RESULT_REQUIRE_INVALID(preflightResult.outputActions);
+}
