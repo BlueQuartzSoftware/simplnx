ee16ecdbf906282b8470ef7e7a81c20dddafd642
Jessica Marquis <83971431+jmarquisbq@users.noreply.github.com>
2022-08-16 10:37:19 -0400

    Implement ReadH5EbsdFilter unit test (#24)
    * Implement ReadH5EbsdFilter unit test
    * Update ReadH5EbsdTest to use SmallIN100 test utilities

---
+++ OrientationAnalysis/test/ReadH5EbsdTest.cpp
@@ -0,0 +1,157 @@
+#include <catch2/catch.hpp>
+
+#include "complex/Core/Application.hpp"
+#include "complex/Parameters/ArrayCreationParameter.hpp"
+#include "complex/Parameters/DataGroupCreationParameter.hpp"
+#include "complex/Parameters/Dream3dImportParameter.hpp"
+#include "complex/UnitTest/UnitTestCommon.hpp"
+
+#include "complex_plugins/Utilities/SmallIN100Utilties.hpp"
+
+#include "OrientationAnalysis/Filters/ReadH5EbsdFilter.hpp"
+#include "OrientationAnalysis/OrientationAnalysis_test_dirs.hpp"
+#include "OrientationAnalysis/Parameters/H5EbsdReaderParameter.h"
+
+TEST_CASE("OrientationAnalysis::ReadH5Ebsd: Instantiation and Parameter Check", "[OrientationAnalysis][ReadH5Ebsd]")
+{
+  ReadH5EbsdFilter filter;
+  DataStructure ds;
+  Arguments args;
+
+  H5EbsdReaderParameter::ValueType h5ebsdParamVal;
+
+  args.insertOrAssign(ReadH5EbsdFilter::k_ReadH5EbsdFilter_Key, std::make_any<H5EbsdReaderParameter::ValueType>(h5ebsdParamVal));
+  args.insertOrAssign(ReadH5EbsdFilter::k_DataContainerName_Key, std::make_any<DataPath>(DataPath{}));
+  args.insertOrAssign(ReadH5EbsdFilter::k_CellAttributeMatrixName_Key, std::make_any<DataPath>(DataPath{}));
+  args.insertOrAssign(ReadH5EbsdFilter::k_CellEnsembleAttributeMatrixName_Key, std::make_any<DataPath>(DataPath{}));
+
+  // Preflight the filter and check result
+  auto preflightResult = filter.preflight(ds, args);
+  COMPLEX_RESULT_REQUIRE_INVALID(preflightResult.outputActions);
+
+  // Execute the filter and check the result
+  auto executeResult = filter.execute(ds, args);
+  COMPLEX_RESULT_REQUIRE_INVALID(executeResult.result);
+}
+
+TEST_CASE("OrientationAnalysis::ReadH5Ebsd: Valid filter execution", "[OrientationAnalysis][ReadH5Ebsd]")
+{
+  std::shared_ptr<make_shared_enabler> app = std::make_shared<make_shared_enabler>();
+  app->loadPlugins(unit_test::k_BuildDir.view(), true);
+  auto* filterList = Application::Instance()->getFilterList();
+
+  // Read Exemplar DREAM3D File
+  auto exemplarFilePath = fs::path(fmt::format("{}/TestFiles/Small_IN100.dream3d", unit_test::k_DREAM3DDataDir));
+  DataStructure exemplarDataStructure = complex::LoadDataStructure(exemplarFilePath);
+
+  // ReadH5EbsdFilter
+  DataStructure dataStructure;
+  {
+    ReadH5EbsdFilter filter;
+    Arguments args;
+
+    H5EbsdReaderParameter::ValueType h5ebsdParamVal;
+    h5ebsdParamVal.inputFilePath = fmt::format("{}/TestFiles/SmallIN100.h5ebsd", unit_test::k_DREAM3DDataDir);
+    h5ebsdParamVal.startSlice = 1;
+    h5ebsdParamVal.endSlice = 117;
+    h5ebsdParamVal.eulerRepresentation = EbsdLib::AngleRepresentation::Radians;
+    h5ebsdParamVal.hdf5DataPaths = {k_ConfidenceIndex, k_EulerAngles, k_Fit, k_ImageQuality, k_Phases, k_SEMSignal};
+    h5ebsdParamVal.useRecommendedTransform = true;
+
+    args.insertOrAssign(ReadH5EbsdFilter::k_ReadH5EbsdFilter_Key, std::make_any<H5EbsdReaderParameter::ValueType>(h5ebsdParamVal));
+    args.insertOrAssign(ReadH5EbsdFilter::k_DataContainerName_Key, std::make_any<DataPath>(k_DataContainerPath));
+    args.insertOrAssign(ReadH5EbsdFilter::k_CellAttributeMatrixName_Key, std::make_any<DataPath>(k_CellAttributeMatrix));
+    args.insertOrAssign(ReadH5EbsdFilter::k_CellEnsembleAttributeMatrixName_Key, std::make_any<DataPath>(k_CellEnsembleAttributeMatrixPath));
+
+    // Preflight the filter and check result
+    auto preflightResult = filter.preflight(dataStructure, args);
+    COMPLEX_RESULT_REQUIRE_VALID(preflightResult.outputActions);
+
+    // Execute the filter and check the result
+    auto executeResult = filter.execute(dataStructure, args);
+    COMPLEX_RESULT_REQUIRE_VALID(executeResult.result);
+  }
+
+  // Loop and compare each array from the 'Exemplar Data / CellData' to the 'Data Container / CellData' group
+  {
+    auto& cellDataGroup = dataStructure.getDataRefAs<DataGroup>(k_CellAttributeMatrix);
+    auto& cellEnsembleDataGroup = dataStructure.getDataRefAs<DataGroup>(k_CellEnsembleAttributeMatrixPath);
+    std::vector<DataPath> selectedArrays;
+
+    // Create the vector of selected cell DataPaths
+    for(auto& child : cellDataGroup)
+    {
+      selectedArrays.push_back(k_CellAttributeMatrix.createChildPath(child.second->getName()));
+    }
+    for(auto& child : cellEnsembleDataGroup)
+    {
+      selectedArrays.push_back(k_CellEnsembleAttributeMatrixPath.createChildPath(child.second->getName()));
+    }
+
+    for(const auto& arrayPath : selectedArrays)
+    {
+      const auto& generatedDataArray = dataStructure.getDataRefAs<IDataArray>(arrayPath);
+      DataType type = generatedDataArray.getDataType();
+      auto& exemplarDataArray = exemplarDataStructure.getDataRefAs<IDataArray>(arrayPath);
+      DataType exemplarType = exemplarDataArray.getDataType();
+
+      if(type != exemplarType)
+      {
+        std::cout << fmt::format("DataArray {} and {} do not have the same type: {} vs {}. Data Will not be compared.", generatedDataArray.getName(), exemplarDataArray.getName(), type, exemplarType)
+                  << std::endl;
+        continue;
+      }
+
+      switch(type)
+      {
+      case DataType::boolean: {
+        CompareDataArrays<bool>(generatedDataArray, exemplarDataArray);
+        break;
+      }
+      case DataType::int8: {
+        CompareDataArrays<int8>(generatedDataArray, exemplarDataArray);
+        break;
+      }
+      case DataType::int16: {
+        CompareDataArrays<int16>(generatedDataArray, exemplarDataArray);
+        break;
+      }
+      case DataType::int32: {
+        CompareDataArrays<int32>(generatedDataArray, exemplarDataArray);
+        break;
+      }
+      case DataType::int64: {
+        CompareDataArrays<int64>(generatedDataArray, exemplarDataArray);
+        break;
+      }
+      case DataType::uint8: {
+        CompareDataArrays<uint8>(generatedDataArray, exemplarDataArray);
+        break;
+      }
+      case DataType::uint16: {
+        CompareDataArrays<uint16>(generatedDataArray, exemplarDataArray);
+        break;
+      }
+      case DataType::uint32: {
+        CompareDataArrays<uint32>(generatedDataArray, exemplarDataArray);
+        break;
+      }
+      case DataType::uint64: {
+        CompareDataArrays<uint64>(generatedDataArray, exemplarDataArray);
+        break;
+      }
+      case DataType::float32: {
+        CompareDataArrays<float32>(generatedDataArray, exemplarDataArray);
+        break;
+      }
+      case DataType::float64: {
+        CompareDataArrays<float64>(generatedDataArray, exemplarDataArray);
+        break;
+      }
+      default: {
+        throw std::runtime_error("Invalid DataType");
+      }
+      }
+    }
+  }
+}
