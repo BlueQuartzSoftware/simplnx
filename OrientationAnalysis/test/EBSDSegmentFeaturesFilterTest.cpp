9cf1bb12ea20a88a47ec0225ef4563b3f7edce65
Jessica Marquis <83971431+jmarquisbq@users.noreply.github.com>
2022-08-16 11:22:09 -0400

    Implement unit test for EBSDSegmentFeatures filter (#25)
    * Implement unit test for EBSDSegmentFeatures filter
    * Use Dream3dIO parsing utility to read legacy dream3d test file
    * Fix TEST_CASE name
    * Update EBSDSegmentFeaturesTest to use SmallIN100 test utility fcts

--- OrientationAnalysis/test/EBSDSegmentFeaturesFilterTest.cpp
+++ OrientationAnalysis/test/EBSDSegmentFeaturesFilterTest.cpp
@@ -1,68 +1,97 @@
-/**
- * This file is auto generated from the original Reconstruction/EBSDSegmentFeatures
- * runtime information. These are the steps that need to be taken to utilize this
- * unit test in the proper way.
- *
- * 1: Validate each of the default parameters that gets created.
- * 2: Inspect the actual filter to determine if the filter in its default state
- * would pass or fail BOTH the preflight() and execute() methods
- * 3: UPDATE the ```REQUIRE(result.result.valid());``` code to have the proper
- *
- * 4: Add additional unit tests to actually test each code path within the filter
- *
- * There are some example Catch2 ```TEST_CASE``` sections for your inspiration.
- *
- * NOTE the format of the ```TEST_CASE``` macro. Please stick to this format to
- * allow easier parsing of the unit tests.
- *
- * When you start working on this unit test remove "[EBSDSegmentFeatures][.][UNIMPLEMENTED]"
- * from the TEST_CASE macro. This will enable this unit test to be run by default
- * and report errors.
- */
-
 #include <catch2/catch.hpp>
 
+#include "complex/Core/Application.hpp"
 #include "complex/Parameters/ArrayCreationParameter.hpp"
+#include "complex/Parameters/ArraySelectionParameter.hpp"
+#include "complex/Parameters/ArrayThresholdsParameter.hpp"
 #include "complex/Parameters/BoolParameter.hpp"
+#include "complex/Parameters/ChoicesParameter.hpp"
+#include "complex/Parameters/Dream3dImportParameter.hpp"
+#include "complex/Parameters/FileSystemPathParameter.hpp"
+#include "complex/Parameters/GeometrySelectionParameter.hpp"
+#include "complex/UnitTest/UnitTestCommon.hpp"
+#include "complex/Utilities/Parsing/DREAM3D/Dream3dIO.hpp"
+#include "complex/Utilities/Parsing/HDF5/H5FileReader.hpp"
 
 #include "OrientationAnalysis/Filters/EBSDSegmentFeaturesFilter.hpp"
 #include "OrientationAnalysis/OrientationAnalysis_test_dirs.hpp"
 
+#include "complex_plugins/EbsdLibConstants.hpp"
+#include "complex_plugins/Utilities/SmallIN100Utilties.hpp"
+#include "complex_plugins/Utilities/TestUtilities.hpp"
+
+#include <filesystem>
+namespace fs = std::filesystem;
+
 using namespace complex;
 
-TEST_CASE("Reconstruction::EBSDSegmentFeatures: Instantiation and Parameter Check", "[Reconstruction][EBSDSegmentFeatures][.][UNIMPLEMENTED][!mayfail]")
+TEST_CASE("OrientationAnalysis::EBSDSegmentFeatures: Instantiation and Parameter Check", "[OrientationAnalysis][EBSDSegmentFeatures]")
 {
-  // Instantiate the filter, a DataStructure object and an Arguments Object
-  EBSDSegmentFeaturesFilter filter;
-  DataStructure ds;
-  Arguments args;
+  std::shared_ptr<make_shared_enabler> app = std::make_shared<make_shared_enabler>();
+  app->loadPlugins(unit_test::k_BuildDir.view(), true);
+  auto* filterList = Application::Instance()->getFilterList();
 
-  // Create default Parameters for the filter.
-  args.insertOrAssign(EBSDSegmentFeaturesFilter::k_MisorientationTolerance_Key, std::make_any<float32>(5.0F));
-  args.insertOrAssign(EBSDSegmentFeaturesFilter::k_UseGoodVoxels_Key, std::make_any<bool>(true));
-  args.insertOrAssign(EBSDSegmentFeaturesFilter::k_QuatsArrayPath_Key, std::make_any<DataPath>(DataPath{}));
-  args.insertOrAssign(EBSDSegmentFeaturesFilter::k_CellPhasesArrayPath_Key, std::make_any<DataPath>(DataPath{}));
-  args.insertOrAssign(EBSDSegmentFeaturesFilter::k_GoodVoxelsPath_Key, std::make_any<DataPath>(DataPath{}));
-  args.insertOrAssign(EBSDSegmentFeaturesFilter::k_CrystalStructuresArrayPath_Key, std::make_any<DataPath>(DataPath{}));
-  args.insertOrAssign(EBSDSegmentFeaturesFilter::k_FeatureIdsArrayName_Key, std::make_any<DataPath>(DataPath{}));
-  args.insertOrAssign(EBSDSegmentFeaturesFilter::k_CellFeatureAttributeMatrixName_Key, std::make_any<DataPath>(DataPath{}));
-  args.insertOrAssign(EBSDSegmentFeaturesFilter::k_ActiveArrayName_Key, std::make_any<DataPath>(DataPath{}));
+  // Read Exemplar DREAM3D File Filter
+  auto exemplarFilePath = fs::path(fmt::format("{}/TestFiles/6_6_ebsd_segment_features.dream3d", unit_test::k_DREAM3DDataDir));
+  DataStructure exemplarDataStructure = complex::LoadDataStructure(exemplarFilePath);
 
-  // Preflight the filter and check result
-  auto preflightResult = filter.preflight(ds, args);
-  REQUIRE(preflightResult.outputActions.valid());
+  // Read the Small IN100 Data set
+  auto baseDataFilePath = fs::path(fmt::format("{}/TestFiles/Small_IN100.dream3d", unit_test::k_DREAM3DDataDir));
+  DataStructure dataStructure = LoadDataStructure(baseDataFilePath);
 
-  // Execute the filter and check the result
-  auto executeResult = filter.execute(ds, args);
-  REQUIRE(executeResult.result.valid());
+  // MultiThreshold Objects Filter (From ComplexCore Plugins)
+  SmallIn100::ExecuteMultiThresholdObjects(dataStructure, *filterList);
+
+  // Convert Orientations Filter (From OrientationAnalysis Plugin)
+  SmallIn100::ExecuteConvertOrientations(dataStructure, *filterList);
+
+  // Align Sections Misorientation Filter (From OrientationAnalysis Plugin)
+  SmallIn100::ExecuteAlignSectionsMisorientation(dataStructure, *filterList, fs::path(fmt::format("{}/AlignSectionsMisorientation_1.txt", unit_test::k_BinaryDir)));
+
+  // Identify Sample Filter
+  SmallIn100::ExecuteIdentifySample(dataStructure, *filterList);
+
+  // Align Sections Feature Centroid Filter
+  SmallIn100::ExecuteAlignSectionsFeatureCentroid(dataStructure, *filterList, fs::path(fmt::format("{}/AlignSectionsFeatureCentroid_1.txt", unit_test::k_BinaryDir)));
+
+  // Bad Data Neighbor Orientation Check Filter
+  SmallIn100::ExecuteBadDataNeighborOrientationCheck(dataStructure, *filterList);
+
+  // Neighbor Orientation Correlation Filter
+  SmallIn100::ExecuteNeighborOrientationCorrelation(dataStructure, *filterList);
+
+  // EBSD Segment Features/Semgent Features (Misorientation) Filter
+  {
+    EBSDSegmentFeaturesFilter filter;
+    Arguments args;
+
+    // Create default Parameters for the filter.
+    args.insertOrAssign(EBSDSegmentFeaturesFilter::k_MisorientationTolerance_Key, std::make_any<float32>(5.0F));
+    args.insertOrAssign(EBSDSegmentFeaturesFilter::k_UseGoodVoxels_Key, std::make_any<bool>(true));
+    args.insertOrAssign(EBSDSegmentFeaturesFilter::k_GridGeomPath_Key, std::make_any<DataPath>(k_DataContainerPath));
+    args.insertOrAssign(EBSDSegmentFeaturesFilter::k_QuatsArrayPath_Key, std::make_any<DataPath>(k_QuatsArrayPath));
+    args.insertOrAssign(EBSDSegmentFeaturesFilter::k_CellPhasesArrayPath_Key, std::make_any<DataPath>(k_PhasesArrayPath));
+    args.insertOrAssign(EBSDSegmentFeaturesFilter::k_GoodVoxelsPath_Key, std::make_any<DataPath>(k_MaskArrayPath));
+    args.insertOrAssign(EBSDSegmentFeaturesFilter::k_CrystalStructuresArrayPath_Key, std::make_any<DataPath>(k_CrystalStructuresArrayPath));
+    args.insertOrAssign(EBSDSegmentFeaturesFilter::k_FeatureIdsArrayName_Key, std::make_any<DataPath>(k_FeatureIdsArrayPath));
+    args.insertOrAssign(EBSDSegmentFeaturesFilter::k_CellFeatureAttributeMatrixName_Key, std::make_any<DataPath>(k_CellFeatureAttributeMatrix));
+    args.insertOrAssign(EBSDSegmentFeaturesFilter::k_ActiveArrayName_Key, std::make_any<DataPath>(k_ActiveArrayPath));
+    args.insertOrAssign(EBSDSegmentFeaturesFilter::k_RandomizeFeatures_Key, std::make_any<bool>(false));
+
+    // Preflight the filter and check result
+    auto preflightResult = filter.preflight(dataStructure, args);
+    COMPLEX_RESULT_REQUIRE_VALID(preflightResult.outputActions);
+
+    // Execute the filter and check the result
+    auto executeResult = filter.execute(dataStructure, args);
+    COMPLEX_RESULT_REQUIRE_VALID(executeResult.result);
+  }
+
+  // Loop and compare each array from the 'Exemplar Data / CellData' to the 'Data Container / CellData' group
+  {
+    const auto& generatedDataArray = dataStructure.getDataRefAs<Int32Array>(k_FeatureIdsArrayPath);
+    const auto& exemplarDataArray = exemplarDataStructure.getDataRefAs<Int32Array>(DataPath({"Small IN100", k_SmallIN100ScanData, k_FeatureIds}));
+
+    CompareDataArrays<int32>(generatedDataArray, exemplarDataArray);
+  }
 }
-
-// TEST_CASE("Reconstruction::EBSDSegmentFeatures: Valid filter execution")
-//{
-//
-//}
-
-// TEST_CASE("Reconstruction::EBSDSegmentFeatures: InValid filter execution")
-//{
-//
-//}
