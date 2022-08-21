6a725ce7f93eef68aa72dcaffc091f4cb669304a
Michael Jackson <mike.jackson@bluequartz.net>
2022-08-21 12:39:41 -0400

    Fix issues where ComplexRange and tbb::blocked_range might be reversed.
    Remove license info since that is at the top of the project.
    
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- src/complex/Common/ComplexRange2D.cpp
+++ src/complex/Common/ComplexRange2D.cpp
@@ -1,36 +1,3 @@
-/* ============================================================================
- * Copyright (c) 2019 BlueQuartz Software, LLC
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of BlueQuartz Software, the US Air Force, nor the names of its
- * contributors may be used to endorse or promote products derived from this software
- * without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
- * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * The code contained herein was partially funded by the following contracts:
- *    United States Air Force Prime Contract FA8650-15-D-5231
- *
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
 #include "complex/Common/ComplexRange2D.hpp"
 
 #include <stdexcept>
@@ -46,8 +13,8 @@ ComplexRange2D::ComplexRange2D()
 // -----------------------------------------------------------------------------
 //
 // -----------------------------------------------------------------------------
-ComplexRange2D::ComplexRange2D(size_t initRow, size_t initCol, size_t endRow, size_t endCol)
-: m_Range({{initRow, initCol, endRow, endCol}})
+ComplexRange2D::ComplexRange2D(size_t xStart, size_t xEnd, size_t yStart, size_t yEnd)
+: m_Range({{xStart, xEnd, yStart, yEnd}})
 {
 }
 
@@ -56,7 +23,7 @@ ComplexRange2D::ComplexRange2D(size_t initRow, size_t initCol, size_t endRow, si
 //
 // -----------------------------------------------------------------------------
 ComplexRange2D::ComplexRange2D(const tbb::blocked_range2d<size_t, size_t>& r)
-: m_Range({{r.rows().begin(), r.cols().begin(), r.rows().end(), r.cols().end()}})
+: m_Range({{r.cols().begin(), r.cols().end(), r.rows().begin(), r.rows().end()}})
 {
 }
 #endif
@@ -130,8 +97,8 @@ size_t ComplexRange2D::size() const
 // -----------------------------------------------------------------------------
 bool ComplexRange2D::empty() const
 {
-  const bool emptyRows = (m_Range[0] == m_Range[2]) && (m_Range[0] == 0);
-  const bool emptyCols = (m_Range[1] == m_Range[3]) && (m_Range[1] == 0);
+  const bool emptyRows = (m_Range[2] == m_Range[3]) && (m_Range[2] == 0);
+  const bool emptyCols = (m_Range[0] == m_Range[1]) && (m_Range[0] == 0);
   return emptyRows && emptyCols;
 }
 
@@ -144,5 +111,5 @@ size_t ComplexRange2D::operator[](size_t index) const
   {
     return m_Range[index];
   }
-  throw std::range_error("Range out of bounds");
+  throw std::range_error("Index out of Range error. Must be less than 4.");
 }
--- src/complex/Common/ComplexRange3D.cpp
+++ src/complex/Common/ComplexRange3D.cpp
@@ -1,36 +1,3 @@
-/* ============================================================================
- * Copyright (c) 2019 BlueQuartz Software, LLC
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of BlueQuartz Software, the US Air Force, nor the names of its
- * contributors may be used to endorse or promote products derived from this software
- * without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
- * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * The code contained herein was partially funded by the following contracts:
- *    United States Air Force Prime Contract FA8650-15-D-5231
- *
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
 #include "complex/Common/ComplexRange3D.hpp"
 
 #include <stdexcept>
@@ -65,7 +32,7 @@ ComplexRange3D::ComplexRange3D(size_t xMin, size_t xMax, size_t yMin, size_t yMa
 //
 // -----------------------------------------------------------------------------
 ComplexRange3D::ComplexRange3D(const tbb::blocked_range3d<size_t>& r)
-: m_Range({{r.pages().begin(), r.pages().end(), r.rows().begin(), r.rows().end(), r.cols().begin(), r.cols().end()}})
+: m_Range({{r.cols().begin(), r.cols().end(), r.rows().begin(), r.rows().end(), r.pages().begin(), r.pages().end()}})
 {
 }
 #endif
@@ -120,5 +87,6 @@ size_t ComplexRange3D::operator[](size_t index) const
     return m_Range[index];
   }
 
-  throw std::range_error("Range must be 0 or 1");
+  throw std::range_error("Index out of Range. Must be less than 6");
+  throw std::range_error("Index out of Range. Must be less than 6");
 }
