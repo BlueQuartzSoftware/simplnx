3ab21123887604e06827dadd693bf8a5405fa8fc
Michael Jackson <mike.jackson@bluequartz.net>
2022-09-16 00:09:59 -0400

    Clang format these files.
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- src/complex/Utilities/FilePathGenerator.cpp
+++ src/complex/Utilities/FilePathGenerator.cpp
@@ -32,9 +32,7 @@ std::pair<std::vector<std::string>, bool> GenerateAndValidateFileList(int32 star
 }
 
 // -----------------------------------------------------------------------------
-std::vector<std::string> GenerateFileList(int32 start, int32 end, int32 increment,
-                                          Ordering order, std::string_view inputPath,
-                                          std::string_view filePrefix, std::string_view fileSuffix,
+std::vector<std::string> GenerateFileList(int32 start, int32 end, int32 increment, Ordering order, std::string_view inputPath, std::string_view filePrefix, std::string_view fileSuffix,
                                           std::string_view fileExtension, int32 paddingDigits)
 {
   std::vector<std::string> fileList;
