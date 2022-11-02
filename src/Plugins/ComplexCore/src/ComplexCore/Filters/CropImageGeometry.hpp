936f0cea1ec8868f4d439982f3c693c04e3358c2
Jessica Marquis <83971431+jmarquisbq@users.noreply.github.com>
2022-11-02 15:03:27 -0400

    Filter: Crop Image Geometry bug fixes and enhancements (#337)
    * Add in support for removing original geometry in CropImageGeomFilter & copy over all data
    * Update CropImageGeom unit test with new input parameter
    * Update CropImageGeometry filter doc
    * Return the information for the original and cropped image geometry in preflight
    * Update CropImageGeometryFilter copy actions to new CopyDataObjectAction api
    
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>
    Co-authored-by: Michael Jackson <mike.jackson@bluequartz.net>

--- src/Plugins/ComplexCore/src/ComplexCore/Filters/CropImageGeometry.hpp
+++ src/Plugins/ComplexCore/src/ComplexCore/Filters/CropImageGeometry.hpp
@@ -29,6 +29,7 @@ public:
   static inline constexpr StringLiteral k_RenumberFeatures_Key = "renumber_features";
   static inline constexpr StringLiteral k_FeatureIds_Key = "feature_ids";
   static inline constexpr StringLiteral k_CellFeatureAttributeMatrix_Key = "cell_feature_attribute_matrix";
+  static inline constexpr StringLiteral k_RemoveOriginalGeometry_Key = "RemoveOriginalGeometry";
 
   /**
    * @brief
