78823a80733547596fbf20209e35332592e83ac0
Nathan Young <109472155+nyoungbq@users.noreply.github.com>
2022-12-02 09:42:58 -0500

    FILTER: Fully Implement FindVolFractionsFilter and FindNumFeaturesFilter (#375)
    Fully implemented FindVolFractionsFilter and FindNumFeaturesFilter

---
+++ src/Plugins/ComplexCore/test/FindNumFeaturesTest.cpp
@@ -0,0 +1,105 @@
+#include <catch2/catch.hpp>
+
+#include "complex/Core/Application.hpp"
+#include "complex/Parameters/ArrayCreationParameter.hpp"
+#include "complex/Parameters/ArraySelectionParameter.hpp"
+#include "complex/UnitTest/UnitTestCommon.hpp"
+
+#include "ComplexCore/ComplexCore_test_dirs.hpp"
+#include "ComplexCore/Filters/FindNumFeaturesFilter.hpp"
+
+namespace fs = std::filesystem;
+using namespace complex;
+
+namespace
+{
+const std::string k_FeatureCounts("Feature Count");
+const std::string k_FeatureCountsNX("Feature Count NX");
+
+const DataPath k_FeaturePhasesPath({Constants::k_DataContainer, Constants::k_FeatureData, Constants::k_Phases});
+const DataPath k_IncorrectFeaturePhasesPath({Constants::k_DataContainer, Constants::k_CellData, Constants::k_Phases});
+
+const DataPath k_FeatureCountsPath({Constants::k_DataContainer, Constants::k_CellEnsembleData, k_FeatureCounts});
+const DataPath k_FeatureCountsPathNX({Constants::k_DataContainer, Constants::k_CellEnsembleData, k_FeatureCountsNX});
+
+const fs::path k_BaseDataFilePath = fs::path(fmt::format("{}/TestFiles/6_6_volume_fraction_feature_count.dream3d", unit_test::k_DREAM3DDataDir));
+} // namespace
+
+TEST_CASE("ComplexCore::FindNumFeaturesFilter: Instantiation and Parameter Check", "[ComplexCore][FindNumFeaturesFilter]")
+{
+  // Instantiate the filter, a DataStructure object and an Arguments Object
+  FindNumFeaturesFilter filter;
+  Arguments args;
+
+  DataStructure ds = UnitTest::LoadDataStructure(k_BaseDataFilePath);
+
+  // Create default Parameters for the filter.
+  args.insertOrAssign(FindNumFeaturesFilter::k_FeaturePhasesArrayPath_Key, std::make_any<DataPath>(k_FeaturePhasesPath));
+  args.insertOrAssign(FindNumFeaturesFilter::k_NumFeaturesArrayPath_Key, std::make_any<DataPath>(k_FeatureCountsPathNX));
+
+  // Preflight the filter and check result
+  auto preflightResult = filter.preflight(ds, args);
+  REQUIRE(preflightResult.outputActions.valid());
+
+  // Execute the filter and check the result
+  auto executeResult = filter.execute(ds, args);
+  REQUIRE(executeResult.result.valid());
+}
+
+TEST_CASE("ComplexCore::FindNumFeaturesFilter: Valid filter execution", "[ComplexCore][FindNumFeaturesFilter]")
+{
+  // Instantiate the filter, a DataStructure object and an Arguments Object
+  FindNumFeaturesFilter filter;
+  Arguments args;
+
+  DataStructure ds = UnitTest::LoadDataStructure(k_BaseDataFilePath);
+
+  // Create default Parameters for the filter.
+  args.insertOrAssign(FindNumFeaturesFilter::k_FeaturePhasesArrayPath_Key, std::make_any<DataPath>(k_FeaturePhasesPath));
+  args.insertOrAssign(FindNumFeaturesFilter::k_NumFeaturesArrayPath_Key, std::make_any<DataPath>(k_FeatureCountsPathNX));
+
+  // Preflight the filter and check result
+  auto preflightResult = filter.preflight(ds, args);
+  REQUIRE(preflightResult.outputActions.valid());
+
+  // Execute the filter and check the result
+  auto executeResult = filter.execute(ds, args);
+  REQUIRE(executeResult.result.valid());
+
+  auto& d3dFeatureCountsArrayRef = ds.getDataRefAs<Int32Array>(k_FeatureCountsPath);
+  auto& nxFeatureCountsArrayRef = ds.getDataRefAs<Int32Array>(k_FeatureCountsPathNX);
+
+  for(usize index = 0; index < d3dFeatureCountsArrayRef.getSize(); index++)
+  {
+    REQUIRE(d3dFeatureCountsArrayRef[index] == nxFeatureCountsArrayRef[index]);
+  }
+}
+
+TEST_CASE("ComplexCore::FindNumFeaturesFilter: InValid filter execution", "[ComplexCore][FindNumFeaturesFilter]")
+{
+  // Instantiate the filter, a DataStructure object and an Arguments Object
+  FindNumFeaturesFilter filter;
+  Arguments args;
+
+  DataStructure ds = UnitTest::LoadDataStructure(k_BaseDataFilePath);
+
+  // Create default Parameters for the filter.
+  args.insertOrAssign(FindNumFeaturesFilter::k_FeaturePhasesArrayPath_Key, std::make_any<DataPath>(k_IncorrectFeaturePhasesPath));
+  args.insertOrAssign(FindNumFeaturesFilter::k_NumFeaturesArrayPath_Key, std::make_any<DataPath>(k_FeatureCountsPathNX));
+
+  // Preflight the filter and check result
+  auto preflightResult = filter.preflight(ds, args);
+  REQUIRE(preflightResult.outputActions.valid());
+
+  // Execute the filter and check the result
+  auto executeResult = filter.execute(ds, args);
+  REQUIRE(executeResult.result.valid());
+
+  auto& d3dFeatureCountsArrayRef = ds.getDataRefAs<Int32Array>(k_FeatureCountsPath);
+  auto& nxFeatureCountsArrayRef = ds.getDataRefAs<Int32Array>(k_FeatureCountsPathNX);
+
+  for(usize index = 1; index < d3dFeatureCountsArrayRef.getSize(); index++)
+  {
+    REQUIRE(d3dFeatureCountsArrayRef[index] != nxFeatureCountsArrayRef[index]);
+  }
+}
---
+++ src/Plugins/ComplexCore/test/FindVolFractionsTest.cpp
@@ -0,0 +1,108 @@
+#include <catch2/catch.hpp>
+
+#include "complex/Core/Application.hpp"
+#include "complex/Parameters/ArrayCreationParameter.hpp"
+#include "complex/Parameters/ArraySelectionParameter.hpp"
+#include "complex/UnitTest/UnitTestCommon.hpp"
+
+#include "ComplexCore/ComplexCore_test_dirs.hpp"
+#include "ComplexCore/Filters/FindVolFractionsFilter.hpp"
+
+namespace fs = std::filesystem;
+using namespace complex;
+
+namespace
+{
+const std::string k_VolumeFractions("Volume Fractions");
+const std::string k_VolumeFractionsNX("Volume Fractions NX");
+
+const DataPath k_CellPhasesPath({Constants::k_DataContainer, Constants::k_CellData, Constants::k_Phases});
+const DataPath k_IncorrectCellPhasesPath({Constants::k_DataContainer, Constants::k_FeatureData, Constants::k_Phases});
+
+const DataPath k_VolumeFractionsPath({Constants::k_DataContainer, Constants::k_CellEnsembleData, k_VolumeFractions});
+const DataPath k_VolumeFractionsPathNX({Constants::k_DataContainer, Constants::k_CellEnsembleData, k_VolumeFractionsNX});
+
+const fs::path k_BaseDataFilePath = fs::path(fmt::format("{}/TestFiles/6_6_volume_fraction_feature_count.dream3d", unit_test::k_DREAM3DDataDir));
+} // namespace
+
+TEST_CASE("ComplexCore::FindVolFractionsFilter: Instantiation and Parameter Check", "[ComplexCore]")
+{
+  // Instantiate the filter, a DataStructure object and an Arguments Object
+  FindVolFractionsFilter filter;
+  Arguments args;
+
+  DataStructure ds = UnitTest::LoadDataStructure(k_BaseDataFilePath);
+
+  // Create default Parameters for the filter.
+  args.insertOrAssign(FindVolFractionsFilter::k_CellPhasesArrayPath_Key, std::make_any<DataPath>(k_CellPhasesPath));
+  args.insertOrAssign(FindVolFractionsFilter::k_VolFractionsArrayPath_Key, std::make_any<DataPath>(k_VolumeFractionsPathNX));
+
+  // Preflight the filter and check result
+  auto preflightResult = filter.preflight(ds, args);
+  REQUIRE(preflightResult.outputActions.valid());
+
+  // Execute the filter and check the result
+  auto executeResult = filter.execute(ds, args);
+  REQUIRE(executeResult.result.valid());
+}
+
+TEST_CASE("ComplexCore::FindVolFractionsFilter: Valid filter execution", "[ComplexCore]")
+{
+  // Instantiate the filter, a DataStructure object and an Arguments Object
+  FindVolFractionsFilter filter;
+  Arguments args;
+
+  DataStructure ds = UnitTest::LoadDataStructure(k_BaseDataFilePath);
+
+  // Create default Parameters for the filter.
+  args.insertOrAssign(FindVolFractionsFilter::k_CellPhasesArrayPath_Key, std::make_any<DataPath>(k_CellPhasesPath));
+  args.insertOrAssign(FindVolFractionsFilter::k_VolFractionsArrayPath_Key, std::make_any<DataPath>(k_VolumeFractionsPathNX));
+
+  // Preflight the filter and check result
+  auto preflightResult = filter.preflight(ds, args);
+  REQUIRE(preflightResult.outputActions.valid());
+
+  // Execute the filter and check the result
+  auto executeResult = filter.execute(ds, args);
+  REQUIRE(executeResult.result.valid());
+
+  auto& d3dVolumeFractionsArrayRef = ds.getDataRefAs<Float32Array>(k_VolumeFractionsPath);
+  auto& nxVolumeFractionsArrayRef = ds.getDataRefAs<Float32Array>(k_VolumeFractionsPathNX);
+
+  for(usize index = 0; index < d3dVolumeFractionsArrayRef.getSize(); index++)
+  {
+    float32 result = fabsf(d3dVolumeFractionsArrayRef[index] - nxVolumeFractionsArrayRef[index]);
+    REQUIRE(result < UnitTest::EPSILON);
+  }
+}
+
+TEST_CASE("ComplexCore::FindVolFractionsFilter: InValid filter execution", "[ComplexCore]")
+{
+  // Instantiate the filter, a DataStructure object and an Arguments Object
+  FindVolFractionsFilter filter;
+  Arguments args;
+
+  auto baseDataFilePath = fs::path(fmt::format("{}/TestFiles/6_6_volFractions_and_numFeatures_test.dream3d", unit_test::k_DREAM3DDataDir));
+  DataStructure ds = UnitTest::LoadDataStructure(k_BaseDataFilePath);
+
+  // Create default Parameters for the filter.
+  args.insertOrAssign(FindVolFractionsFilter::k_CellPhasesArrayPath_Key, std::make_any<DataPath>(k_IncorrectCellPhasesPath));
+  args.insertOrAssign(FindVolFractionsFilter::k_VolFractionsArrayPath_Key, std::make_any<DataPath>(k_VolumeFractionsPathNX));
+
+  // Preflight the filter and check result
+  auto preflightResult = filter.preflight(ds, args);
+  REQUIRE(preflightResult.outputActions.valid());
+
+  // Execute the filter and check the result
+  auto executeResult = filter.execute(ds, args);
+  REQUIRE(executeResult.result.valid());
+
+  auto& d3dVolumeFractionsArrayRef = ds.getDataRefAs<Float32Array>(k_VolumeFractionsPath);
+  auto& nxVolumeFractionsArrayRef = ds.getDataRefAs<Float32Array>(k_VolumeFractionsPathNX);
+
+  for(usize index = 0; index < d3dVolumeFractionsArrayRef.getSize(); index++)
+  {
+    float32 result = fabsf(d3dVolumeFractionsArrayRef[index] - nxVolumeFractionsArrayRef[index]);
+    REQUIRE(result > UnitTest::EPSILON);
+  }
+}
