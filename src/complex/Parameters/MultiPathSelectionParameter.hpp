43085846035bab468151ff1e6fa22698483af11c
Jessica Marquis <jessica.marquis@bluequartz.net>
2022-12-08 13:53:58 -0500

    Add MultiPathSelectionParameter for selecting multiple objects of any type

---
+++ src/complex/Parameters/MultiPathSelectionParameter.hpp
@@ -0,0 +1,101 @@
+#pragma once
+
+#include "complex/DataStructure/IArray.hpp"
+#include "complex/Filter/MutableDataParameter.hpp"
+#include "complex/Filter/ParameterTraits.hpp"
+
+namespace complex
+{
+/**
+ * @brief This Filter Parameter represents multiple DataArrays as input.
+ */
+class COMPLEX_EXPORT MultiPathSelectionParameter : public MutableDataParameter
+{
+public:
+  using ValueType = std::vector<DataPath>;
+
+  MultiPathSelectionParameter() = delete;
+  MultiPathSelectionParameter(const std::string& name, const std::string& humanName, const std::string& helpText, const ValueType& defaultValue);
+  ~MultiPathSelectionParameter() override = default;
+
+  MultiPathSelectionParameter(const MultiPathSelectionParameter&) = delete;
+  MultiPathSelectionParameter(MultiPathSelectionParameter&&) noexcept = delete;
+
+  MultiPathSelectionParameter& operator=(const MultiPathSelectionParameter&) = delete;
+  MultiPathSelectionParameter& operator=(MultiPathSelectionParameter&&) noexcept = delete;
+
+  /**
+   * @brief Returns the parameter's uuid.
+   * @return
+   */
+  Uuid uuid() const override;
+
+  /**
+   * @brief Returns a list of accpeted input types.
+   * @return
+   */
+  AcceptedTypes acceptedTypes() const override;
+
+  /**
+   * @brief Converts the given value to JSON.
+   * Throws if value is not an accepted type.
+   * @param value
+   */
+  nlohmann::json toJson(const std::any& value) const override;
+
+  /**
+   * @brief Converts the given JSON to a std::any containing the appropriate input type.
+   * Returns any warnings/errors.
+   * @return
+   */
+  Result<std::any> fromJson(const nlohmann::json& json) const override;
+
+  /**
+   * @brief Creates a copy of the parameter.
+   * @return
+   */
+  UniquePointer clone() const override;
+
+  /**
+   * @brief Returns the user defined default value.
+   * @return
+   */
+  std::any defaultValue() const override;
+
+  /**
+   * @brief
+   * @return
+   */
+  ValueType defaultPath() const;
+
+  /**
+   * @brief Validates the given value against the given DataStructure. Returns warnings/errors.
+   * @param dataStructure The active DataStructure to use during validation
+   * @param value The value to validate
+   * @return
+   */
+  Result<> validate(const DataStructure& dataStructure, const std::any& value) const override;
+
+  /**
+   * @brief Validates the given value against the given DataStructure. Returns warnings/errors.
+   * @param dataStructure The active DataStructure to use during validation
+   * @param value The value to validate
+   * @return
+   */
+  Result<> validatePaths(const DataStructure& dataStructure, const ValueType& value) const;
+
+  /**
+   * @brief Takes the value and a mutable DataStructure and attempts store the actual derived DataObject in the std::any.
+   * Returns any warnings/errors.
+   * @param dataStructure
+   * @param value
+   * @return
+   */
+  Result<std::any> resolve(DataStructure& dataStructure, const std::any& value) const override;
+
+private:
+  ValueType m_DefaultValue = {};
+};
+} // namespace complex
+
+COMPLEX_DEF_PARAMETER_TRAITS(complex::MultiPathSelectionParameter, "b5632f4f-fc13-4234-beb2-8fd8820eb6b6");
