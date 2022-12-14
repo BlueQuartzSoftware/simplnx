27190876eff8285e8fa399d301f75da3c7d9b301
Michael Jackson <mike.jackson@bluequartz.net>
2022-12-14 11:19:39 -0500

    TimeUtilities: remove dependency on StringUtilities.
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- src/complex/Utilities/TimeUtilities.hpp
+++ src/complex/Utilities/TimeUtilities.hpp
@@ -1,19 +1,25 @@
 #pragma once
 
-#include "complex/Utilities/StringUtilities.hpp"
-
 #include <fmt/core.h>
 #include <fmt/ranges.h>
+
 #include <string>
 
 namespace complex
 {
+
+/**
+ * @brief Converts a millisecond count to Hours:Minutes:Seconds type of display. This assumes
+ * that the time would not be longer than 99 hours, 99 minutes, 99 seconds.
+ * @param millis Input millisecond count
+ * @return std::string in the form of HH:MM:SS
+ */
 inline std::string ConvertMillisToHrsMinSecs(unsigned long long int millis)
 {
-  unsigned long long int Hours = millis / (1000 * 60 * 60);
-  unsigned long long intMinutes = (millis % (1000 * 60 * 60)) / (1000 * 60);
-  unsigned long long intSeconds = ((millis % (1000 * 60 * 60)) % (1000 * 60)) / 1000;
-  std::string str = fmt::format("{:0>2}:{:0>2}:{:0>2}", StringUtilities::number(Hours), StringUtilities::number(intMinutes), StringUtilities::number(intSeconds));
+  const unsigned long long int hours = millis / (1000 * 60 * 60);
+  const unsigned long long intMinutes = (millis % (1000 * 60 * 60)) / (1000 * 60);
+  const unsigned long long intSeconds = ((millis % (1000 * 60 * 60)) % (1000 * 60)) / 1000;
+  std::string str = fmt::format("{:0>2}:{:0>2}:{:0>2}", hours, intMinutes, intSeconds);
   return str;
 }
 
