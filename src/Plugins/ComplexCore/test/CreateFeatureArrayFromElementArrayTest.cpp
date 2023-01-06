975acfa2da592d1dfa0775371ba223a266f9b8bf
Nathan Young <109472155+nyoungbq@users.noreply.github.com>
2023-01-06 10:02:21 -0500

    ENH: Implement Dynamic Filter Instantiation Test Case (#406)

--- src/Plugins/ComplexCore/test/CreateFeatureArrayFromElementArrayTest.cpp
+++ src/Plugins/ComplexCore/test/CreateFeatureArrayFromElementArrayTest.cpp
@@ -1,25 +1,3 @@
-/**
- * This file is auto generated from the original Core/CreateFeatureArrayFromElementArray
- * runtime information. These are the steps that need to be taken to utilize this
- * unit test in the proper way.
- *
- * 1: Validate each of the default parameters that gets created.
- * 2: Inspect the actual filter to determine if the filter in its default state
- * would pass or fail BOTH the preflight() and execute() methods
- * 3: UPDATE the ```REQUIRE(result.result.valid());``` code to have the proper
- *
- * 4: Add additional unit tests to actually test each code path within the filter
- *
- * There are some example Catch2 ```TEST_CASE``` sections for your inspiration.
- *
- * NOTE the format of the ```TEST_CASE``` macro. Please stick to this format to
- * allow easier parsing of the unit tests.
- *
- * When you start working on this unit test remove "[CreateFeatureArrayFromElementArray][.][UNIMPLEMENTED]"
- * from the TEST_CASE macro. This will enable this unit test to be run by default
- * and report errors.
- */
-
 #include <catch2/catch.hpp>
 
 #include "complex/Parameters/ArrayCreationParameter.hpp"
@@ -89,27 +67,6 @@ void testElementArray(const DataPath& cellDataPath)
   }
 }
 } // namespace
-
-TEST_CASE("ComplexCore::CreateFeatureArrayFromElementArray: Instantiation and Parameter Check", "[Core][CreateFeatureArrayFromElementArray]")
-{
-  // Instantiate the filter, a DataStructure object and an Arguments Object
-  CreateFeatureArrayFromElementArray filter;
-  DataStructure dataStructure;
-  Arguments args;
-
-  // Create default Parameters for the filter.
-  args.insertOrAssign(CreateFeatureArrayFromElementArray::k_SelectedCellArrayPath_Key, std::make_any<DataPath>(DataPath{}));
-  args.insertOrAssign(CreateFeatureArrayFromElementArray::k_CellFeatureIdsArrayPath_Key, std::make_any<DataPath>(DataPath{}));
-  args.insertOrAssign(CreateFeatureArrayFromElementArray::k_CreatedArrayName_Key, std::make_any<DataPath>(DataPath{}));
-
-  // Preflight the filter and check result
-  auto preflightResult = filter.preflight(dataStructure, args);
-  COMPLEX_RESULT_REQUIRE_INVALID(preflightResult.outputActions);
-
-  // Execute the filter and check the result
-  auto executeResult = filter.execute(dataStructure, args);
-  COMPLEX_RESULT_REQUIRE_INVALID(executeResult.result);
-}
 
 TEST_CASE("ComplexCore::CreateFeatureArrayFromElementArray: Valid filter execution - 1 Component")
 {
