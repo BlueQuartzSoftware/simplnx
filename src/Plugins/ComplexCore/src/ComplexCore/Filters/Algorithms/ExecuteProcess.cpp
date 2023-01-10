ffa53c475e04bd7a267f1724f56aaa0dd5fcecb8
Jessica Marquis <jessica.marquis@bluequartz.net>
2023-01-10 13:36:38 -0500

    [WIP: not working yet!] Initial implementation using reproc instead of QProcess

--- src/Plugins/ComplexCore/src/ComplexCore/Filters/Algorithms/ExecuteProcess.cpp
+++ src/Plugins/ComplexCore/src/ComplexCore/Filters/Algorithms/ExecuteProcess.cpp
@@ -4,6 +4,13 @@
 #include "complex/DataStructure/DataGroup.hpp"
 #include "complex/Utilities/StringUtilities.hpp"
 
+#include "reproc++/include/reproc++/drain.hpp"
+#include "reproc++/include/reproc++/reproc.hpp"
+#include "reproc++/include/reproc++/run.hpp"
+
+#include <future>
+#include <mutex>
+
 using namespace complex;
 
 // -----------------------------------------------------------------------------
@@ -28,8 +35,100 @@ const std::atomic_bool& ExecuteProcess::getCancel()
 Result<> ExecuteProcess::operator()()
 {
   std::vector<std::string> arguments = ExecuteProcess::splitArgumentsString(m_InputValues->Arguments);
-  std::string command = arguments[0];
-  arguments.erase(arguments.begin());
+  auto args = reproc::arguments::arguments(arguments);
+  reproc::process process;
+
+  if(m_InputValues->Blocking)
+  {
+    reproc::options options;
+    reproc::stop_actions stop = {
+        {reproc::stop::noop, reproc::milliseconds(0)}, {reproc::stop::terminate, reproc::milliseconds(m_InputValues->Timeout)}, {reproc::stop::kill, reproc::milliseconds(m_InputValues->Timeout)}};
+    options.stop = stop;
+    std::error_code ec = process.start(args, options);
+
+    if(ec == std::errc::no_such_file_or_directory)
+    {
+      return MakeErrorResult<>(-56410, fmt::format("Program {} not found. Make sure it's available from the PATH.", arguments[0]));
+    }
+    else if(ec)
+    {
+      return MakeErrorResult<>(-56410, fmt::format("An error occured while starting process '{}'\n{} : {}", m_InputValues->Arguments, ec.value(), ec.message()));
+    }
+
+    std::string output;
+    reproc::sink::string sink(output);
+    ec = reproc::drain(process, sink, reproc::sink::null);
+    // m_MessageHandler(IFilter::Message::Type::Info, output);
+
+    int status = 0;
+    std::tie(status, ec) = process.wait(reproc::infinite);
+    if(ec)
+    {
+      return MakeErrorResult<>(-56411, fmt::format("An error occured while executing process '{}'\n{} : {}", m_InputValues->Arguments, ec.value(), ec.message()));
+    }
+
+    m_MessageHandler(IFilter::Message::Type::Info, output);
+  }
+  else
+  {
+    reproc::options options;
+    reproc::stop_actions stop = {{reproc::stop::terminate, reproc::milliseconds(2000)}, {reproc::stop::kill, reproc::milliseconds(10000)}};
+    options.stop = stop;
+    std::error_code ec = process.start(args, options);
+
+    if(ec == std::errc::no_such_file_or_directory)
+    {
+      return MakeErrorResult<>(-56410, fmt::format("Program {} not found. Make sure it's available from the PATH.", arguments[0]));
+    }
+    else if(ec)
+    {
+      return MakeErrorResult<>(-56410, fmt::format("An error occured while starting process '{}'\n{} : {}", m_InputValues->Arguments, ec.value(), ec.message()));
+    }
+
+    std::string output;
+    std::mutex mutex;
+    auto drain_async = std::async(std::launch::async, [&process, &output, &mutex]() {
+      reproc::sink::thread_safe::string sink(output, mutex);
+      return reproc::drain(process, sink, sink);
+    });
+
+    int status = 0;
+
+    // Show new output every 2 seconds.
+    while(drain_async.wait_for(std::chrono::seconds(2)) != std::future_status::ready)
+    {
+      std::lock_guard<std::mutex> lock(mutex);
+      m_MessageHandler(IFilter::Message::Type::Info, output);
+      // Clear output that's already been flushed
+      output.clear();
+
+      if(getCancel())
+      {
+        std::tie(status, ec) = process.stop(options.stop);
+        if(ec)
+        {
+          return MakeErrorResult<>(-56411, fmt::format("An error occured while stopping process '{}'\n{} : {}", m_InputValues->Arguments, ec.value(), ec.message()));
+        }
+      }
+    }
+
+    // Flush any remaining output of `process`.
+    m_MessageHandler(IFilter::Message::Type::Info, output);
+
+    // Check if any errors occurred in the background thread.
+    ec = drain_async.get();
+    if(ec)
+    {
+      return MakeErrorResult<>(-56412, fmt::format("An error occured while executing process '{}'\n{} : {}", m_InputValues->Arguments, ec.value(), ec.message()));
+    }
+
+    // status = 0;
+    // std::tie(status, ec) = process.wait(reproc::infinite);
+    // if(ec)
+    //{
+    //   return MakeErrorResult<>(-56411, fmt::format("An error occured while stopping process '{}'\n{} : {}", m_InputValues->Arguments, ec.value(), ec.message()));
+    // }
+  }
 
   return {};
 }
