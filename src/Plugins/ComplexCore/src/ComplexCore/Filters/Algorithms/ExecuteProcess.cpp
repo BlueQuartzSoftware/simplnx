92a02428f532da4f5ba84a005668da8c1a1bc08f
Jessica Marquis <jessica.marquis@bluequartz.net>
2023-01-12 15:29:19 -0500

    Upate the ExecuteProcess algorithm for blocking/nonblocking

--- src/Plugins/ComplexCore/src/ComplexCore/Filters/Algorithms/ExecuteProcess.cpp
+++ src/Plugins/ComplexCore/src/ComplexCore/Filters/Algorithms/ExecuteProcess.cpp
@@ -34,72 +34,96 @@ const std::atomic_bool& ExecuteProcess::getCancel()
 // -----------------------------------------------------------------------------
 Result<> ExecuteProcess::operator()()
 {
-  std::vector<std::string> arguments = ExecuteProcess::splitArgumentsString(m_InputValues->Arguments);
-  auto args = reproc::arguments::arguments(arguments);
-  reproc::process process;
-
-  // if(m_InputValues->Blocking)
-  //{
-  reproc::stop_actions stop = {{reproc::stop::terminate, reproc::milliseconds(m_InputValues->Timeout)}, {reproc::stop::kill, reproc::milliseconds(m_InputValues->Timeout)}};
-  reproc::options options;
-  options.stop = stop;
-  std::error_code ec = process.start(args, options);
-
-  if(ec == std::errc::no_such_file_or_directory)
-  {
-    return MakeErrorResult<>(-56410, fmt::format("Program {} not found. Make sure it's available from the PATH.", arguments[0]));
-  }
-  else if(ec)
-  {
-    return MakeErrorResult<>(-56410, fmt::format("An error occured while starting process '{}'\n{} : {}", m_InputValues->Arguments, ec.value(), ec.message()));
-  }
-
-  // redirect the process output to the output string so we can save it to a log file
-  std::string output;
-  std::mutex mutex;
-  auto drain_async = std::async(std::launch::async, [&process, &output, &mutex]() {
-    reproc::sink::thread_safe::string sink(output, mutex);
-    return reproc::drain(process, sink, sink);
-  });
-  int status = 0;
+  // open the log file for storing the process output
   std::ofstream outFile;
   outFile.open(m_InputValues->LogFile, std::ios_base::out);
   if(!outFile.is_open())
   {
     return MakeErrorResult<>(-56411, fmt::format("Error creating output log file with file path {}", m_InputValues->LogFile.string()));
   }
-  // Show new output every 2 seconds.
-  while(drain_async.wait_for(std::chrono::seconds(1)) != std::future_status::ready)
-  {
-    std::lock_guard<std::mutex> lock(mutex);
-    outFile << output;
-    //  Clear output that's already been flushed
-    output.clear();
 
-    if(getCancel())
+  // gather the command/arguments and process options
+  std::vector<std::string> arguments = ExecuteProcess::splitArgumentsString(m_InputValues->Arguments);
+  auto args = reproc::arguments::arguments(arguments);
+
+  reproc::process process;
+  reproc::stop_actions stop = {{reproc::stop::terminate, reproc::milliseconds(m_InputValues->Timeout)}, {reproc::stop::kill, reproc::milliseconds(m_InputValues->Timeout)}};
+  reproc::options options;
+  options.stop = stop;
+  options.nonblocking = !m_InputValues->Blocking;
+  std::error_code ec = process.start(args, options);
+
+  if(ec == std::errc::no_such_file_or_directory)
+  {
+    outFile.close();
+    return MakeErrorResult<>(-56410, fmt::format("Program {} not found. Make sure it's available from the PATH.", arguments[0]));
+  }
+  else if(ec)
+  {
+    outFile.close();
+    return MakeErrorResult<>(-56410, fmt::format("An error occured while starting process '{}'\n{} : {}", m_InputValues->Arguments, ec.value(), ec.message()));
+  }
+
+  std::string output;
+  if(m_InputValues->Blocking)
+  {
+    reproc::sink::string sink(output);
+    ec = reproc::drain(process, sink, reproc::sink::null);
+    if(ec)
     {
-      break;
+      outFile.close();
+      return MakeErrorResult<>(-56413, fmt::format("An error occured while executing process '{}'\n{} : {}", m_InputValues->Arguments, ec.value(), ec.message()));
+    }
+    outFile << output;
+  }
+  else
+  {
+    // redirect the process output to the output string so we can save it to a log file
+    std::mutex mutex;
+    auto drain_async = std::async(std::launch::async, [&process, &output, &mutex]() {
+      reproc::sink::thread_safe::string sink(output, mutex);
+      return reproc::drain(process, sink, sink);
+    });
+
+    // Show new output every 2 seconds.
+    while(drain_async.wait_for(std::chrono::seconds(1)) != std::future_status::ready)
+    {
+      std::lock_guard<std::mutex> lock(mutex);
+      outFile << output;
+      //  Clear output that's already been flushed
+      output.clear();
+
+      if(getCancel())
+      {
+        break;
+      }
+    }
+    // Flush any remaining output of `process`.
+    outFile << output;
+
+    // Check if any errors occurred in the background thread.
+    ec = drain_async.get();
+    if(ec)
+    {
+      outFile.close();
+      return MakeErrorResult<>(-56413, fmt::format("An error occured while executing process '{}'\n{} : {}", m_InputValues->Arguments, ec.value(), ec.message()));
     }
   }
-  // Flush any remaining output of `process`.
-  outFile << output;
-  outFile.close();
 
-  // Check if any errors occurred in the background thread.
-  ec = drain_async.get();
-  if(ec)
-  {
-    return MakeErrorResult<>(-56413, fmt::format("An error occured while executing process '{}'\n{} : {}", m_InputValues->Arguments, ec.value(), ec.message()));
-  }
-
-  status = 0;
+  int status = 0;
   std::tie(status, ec) = process.wait(reproc::infinite);
   if(ec)
   {
+    outFile.close();
     return MakeErrorResult<>(-56414, fmt::format("An error occured while executing process '{}'\n{} : {}", m_InputValues->Arguments, ec.value(), ec.message()));
   }
-  //}
 
+  if(m_InputValues->Blocking)
+  {
+    outFile << output;
+  }
+
+  outFile.close();
   return {};
 }
 
