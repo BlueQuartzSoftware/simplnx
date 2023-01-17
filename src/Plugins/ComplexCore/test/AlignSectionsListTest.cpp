64c9f138b8b90e2efa226b7329fb7e2b876b1a61
Jessica Marquis <83971431+jmarquisbq@users.noreply.github.com>
2023-01-17 09:55:56 -0500

    FILTER: Align Sections List (#410)
    * Add AlignSectionsListFilter
    * Add AlignSectionsList unit test
    * Add AlignSectionsList documentation
    * Abstract out alignment shift file reading & improve parsing/format checking
    * Update AlignSectionsList unit test
    * More documentation updates

---
+++ src/Plugins/ComplexCore/test/AlignSectionsListTest.cpp
@@ -0,0 +1,85 @@
+#include <catch2/catch.hpp>
+
+#include "complex/Parameters/BoolParameter.hpp"
+#include "complex/Parameters/DataGroupSelectionParameter.hpp"
+#include "complex/Parameters/FileSystemPathParameter.hpp"
+#include "complex/UnitTest/UnitTestCommon.hpp"
+
+#include "ComplexCore/ComplexCore_test_dirs.hpp"
+#include "ComplexCore/Filters/AlignSectionsListFilter.hpp"
+
+#include <filesystem>
+namespace fs = std::filesystem;
+using namespace complex;
+
+TEST_CASE("ComplexCore::AlignSectionsListFilter: Valid filter execution", "[ComplexCore][AlignSectionsListFilter]")
+{
+  std::shared_ptr<UnitTest::make_shared_enabler> app = std::make_shared<UnitTest::make_shared_enabler>();
+  app->loadPlugins(unit_test::k_BuildDir.view(), true);
+  auto* filterList = Application::Instance()->getFilterList();
+
+  // Read Exemplar DREAM3D File Filter
+  auto exemplarFilePath = fs::path(fmt::format("{}/6_6_align_sections_misorientation.dream3d", unit_test::k_TestFilesDir));
+  DataStructure exemplarDataStructure = UnitTest::LoadDataStructure(exemplarFilePath);
+
+  // Read the Small IN100 Data set
+  auto baseDataFilePath = fs::path(fmt::format("{}/Small_IN100.dream3d", unit_test::k_TestFilesDir));
+  DataStructure dataStructure = UnitTest::LoadDataStructure(baseDataFilePath);
+
+  // MultiThreshold Objects Filter (From ComplexCore Plugins)
+  SmallIn100::ExecuteMultiThresholdObjects(dataStructure, *filterList);
+
+  // Align Sections List Filter
+  {
+    // Instantiate the filter and an Arguments Object
+    AlignSectionsListFilter filter;
+    Arguments args;
+
+    // Create default Parameters for the filter.
+    args.insertOrAssign(AlignSectionsListFilter::k_InputFile_Key,
+                        std::make_any<FileSystemPathParameter::ValueType>(fs::path(fmt::format("{}/align_sections_misorientation.txt", unit_test::k_TestFilesDir))));
+    args.insertOrAssign(AlignSectionsListFilter::k_DREAM3DAlignmentFile_Key, std::make_any<bool>(true));
+    args.insertOrAssign(AlignSectionsListFilter::k_SelectedImageGeometry_Key, std::make_any<DataPath>(complex::Constants::k_DataContainerPath));
+
+    // Preflight the filter and check result
+    auto preflightResult = filter.preflight(dataStructure, args);
+    REQUIRE(preflightResult.outputActions.valid());
+
+    // Execute the filter and check the result
+    auto executeResult = filter.execute(dataStructure, args);
+    REQUIRE(executeResult.result.valid());
+  }
+
+  UnitTest::CompareExemplarToGeneratedData(dataStructure, exemplarDataStructure, Constants::k_CellAttributeMatrix, Constants::k_ExemplarDataContainer);
+}
+
+TEST_CASE("ComplexCore::AlignSectionsListFilter: Invalid filter execution", "[ComplexCore][AlignSectionsListFilter]")
+{
+  // Read the Small IN100 Data set
+  auto baseDataFilePath = fs::path(fmt::format("{}/Small_IN100.dream3d", unit_test::k_TestFilesDir));
+  DataStructure dataStructure = UnitTest::LoadDataStructure(baseDataFilePath);
+  // Instantiate the filter and an Arguments Object
+  AlignSectionsListFilter filter;
+  Arguments args;
+
+  args.insertOrAssign(AlignSectionsListFilter::k_InputFile_Key,
+                      std::make_any<FileSystemPathParameter::ValueType>(fs::path(fmt::format("{}/write_ascii_data_exemplars/float32/0_0_exemplar_0.txt", unit_test::k_TestFilesDir))));
+  args.insertOrAssign(AlignSectionsListFilter::k_SelectedImageGeometry_Key, std::make_any<DataPath>(complex::Constants::k_DataContainerPath));
+
+  SECTION("Invalid DREAM3D Alignment file format")
+  {
+    args.insertOrAssign(AlignSectionsListFilter::k_DREAM3DAlignmentFile_Key, std::make_any<bool>(true));
+  }
+  SECTION("Invalid user Alignment file format")
+  {
+    args.insertOrAssign(AlignSectionsListFilter::k_DREAM3DAlignmentFile_Key, std::make_any<bool>(false));
+  }
+
+  // Preflight the filter and check result
+  auto preflightResult = filter.preflight(dataStructure, args);
+  REQUIRE(preflightResult.outputActions.valid());
+
+  // Execute the filter and check the result
+  auto executeResult = filter.execute(dataStructure, args);
+  REQUIRE(executeResult.result.invalid());
+}
