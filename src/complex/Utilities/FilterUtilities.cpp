f4d22ef0f7ad56dd15091257c791f79002fcaef6
Michael Jackson <mike.jackson@bluequartz.net>
2023-01-19 16:15:16 -0500

    Initial work to validate all output paths have the intermediate directories created
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

---
+++ src/complex/Utilities/FilterUtilities.cpp
@@ -0,0 +1,27 @@
+#include "FilterUtilities.hpp"
+
+#include <fmt/core.h>
+
+#include <string>
+
+namespace complex
+{
+
+// -----------------------------------------------------------------------------
+Result<> CreateOutputDirectories(const fs::path& outputPath)
+{
+  if(!fs::exists(outputPath))
+  {
+    std::error_code errorCode;
+    // So this looks wierd but this can happen on
+    // platforms where /tmp is a symlink to /private/tmp. So the original path is
+    // /tmp/foo but what was created was /private/tmp/foo. This logic should fix that issue.
+    if(!fs::create_directories(outputPath, errorCode) && !fs::exists(outputPath))
+    {
+      return MakeErrorResult(-4010, fmt::format("Unable to create output directory {}. Error code from operating system is {}", outputPath.string(), errorCode.value()));
+    }
+  }
+  return {};
+}
+
+} // namespace complex
