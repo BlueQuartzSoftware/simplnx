d506779194ec41d491328a9574c7082a171932b4
Michael Jackson <mike.jackson@bluequartz.net>
2023-01-26 18:13:12 -0500

    Filter: Move AlignSectionsFeatureCentroids to ComplexCore (#90)
    * Fixes to the AlignSectionsFeatureCentroid filter and test
    
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- Core/src/Core/CoreLegacyUUIDMapping.hpp
+++ Core/src/Core/CoreLegacyUUIDMapping.hpp
@@ -4,7 +4,6 @@
 #include <string>
 
 // clang-format off
-#include "Core/Filters/AlignSectionsFeatureCentroidFilter.hpp"
 #include "Core/Filters/FindNeighborhoodsFilter.hpp"
 #include "Core/Filters/RemoveFlaggedFeaturesFilter.hpp"
 #include "Core/Filters/ErodeDilateBadDataFilter.hpp"
@@ -15,7 +14,6 @@ namespace complex
   static const std::map<complex::Uuid, complex::Uuid> k_SIMPL_to_Core
   {
     // syntax std::make_pair {Dream3d UUID , Dream3dnx UUID}, // dream3d-class-name
-    {complex::Uuid::FromString("886f8b46-51b6-5682-a289-6febd10b7ef0").value(), complex::FilterTraits<AlignSectionsFeatureCentroidFilter>::uuid}, // AlignSectionsFeatureCentroid
     {complex::Uuid::FromString("697ed3de-db33-5dd1-a64b-04fb71e7d63e").value(), complex::FilterTraits<FindNeighborhoodsFilter>::uuid}, // FindNeighborhoods
     {complex::Uuid::FromString("a8463056-3fa7-530b-847f-7f4cb78b8602").value(), complex::FilterTraits<RemoveFlaggedFeaturesFilter>::uuid}, // RemoveFlaggedFeatures
     {complex::Uuid::FromString("3adfe077-c3c9-5cd0-ad74-cf5f8ff3d254").value(), complex::FilterTraits<ErodeDilateBadDataFilter>::uuid}, // ErodeDilateBadData
