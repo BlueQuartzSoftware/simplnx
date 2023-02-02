7f84aff9cb249ed9f88dc2d0f2b441f85f3235a0
nyoungbq <nathan.young@bluequartz.net>
2023-02-02 10:05:29 -0500

    Strip switches out of old filters

--- src/Plugins/ComplexCore/src/ComplexCore/Filters/InitializeData.cpp
+++ src/Plugins/ComplexCore/src/ComplexCore/Filters/InitializeData.cpp
@@ -8,6 +8,7 @@
 #include "complex/Parameters/MultiArraySelectionParameter.hpp"
 #include "complex/Parameters/NumberParameter.hpp"
 #include "complex/Parameters/VectorParameter.hpp"
+#include "complex/Utilities/FilterUtilities.hpp"
 
 #include <fmt/core.h>
 
@@ -42,38 +43,41 @@ InitializeData::InitType ConvertIndexToInitType(uint64 index)
   }
 }
 
-template <class T>
-std::optional<Error> CheckInitialization(const IDataArray& dataArray, InitializeData::InitType initType, float64 initValue, const std::pair<float64, float64>& initRange)
+struct CheckInitializationFunctor
 {
-  std::string arrayName = dataArray.getName();
-
-  if(initType == InitializeData::InitType::Manual)
+  template <class T>
+  std::optional<Error> operator()(const IDataArray& dataArray, InitializeData::InitType initType, float64 initValue, const std::pair<float64, float64>& initRange)
   {
-    if(initValue < static_cast<double>(std::numeric_limits<T>().lowest()) || initValue > static_cast<double>(std::numeric_limits<T>().max()))
-    {
-      return Error{-4000, fmt::format("{}: The initialization value could not be converted. The valid range is {} to {}", arrayName, std::numeric_limits<T>::min(), std::numeric_limits<T>::max())};
-    }
-  }
-  else if(initType == InitializeData::InitType::RandomWithRange)
-  {
-    float64 min = initRange.first;
-    float64 max = initRange.second;
-    if(min > max)
-    {
-      return Error{-5550, fmt::format("{}: Invalid initialization range.  Minimum value is larger than maximum value.", arrayName)};
-    }
-    if(min < static_cast<double>(std::numeric_limits<T>().lowest()) || max > static_cast<double>(std::numeric_limits<T>().max()))
-    {
-      return Error{-4001, fmt::format("{}: The initialization range can only be from {} to {}", arrayName, std::numeric_limits<T>::min(), std::numeric_limits<T>::max())};
-    }
-    if(min == max)
-    {
-      return Error{-4002, fmt::format("{}: The initialization range must have differing values", arrayName)};
-    }
-  }
+    std::string arrayName = dataArray.getName();
 
-  return {};
-}
+    if(initType == InitializeData::InitType::Manual)
+    {
+      if(initValue < static_cast<double>(std::numeric_limits<T>().lowest()) || initValue > static_cast<double>(std::numeric_limits<T>().max()))
+      {
+        return Error{-4000, fmt::format("{}: The initialization value could not be converted. The valid range is {} to {}", arrayName, std::numeric_limits<T>::min(), std::numeric_limits<T>::max())};
+      }
+    }
+    else if(initType == InitializeData::InitType::RandomWithRange)
+    {
+      float64 min = initRange.first;
+      float64 max = initRange.second;
+      if(min > max)
+      {
+        return Error{-5550, fmt::format("{}: Invalid initialization range.  Minimum value is larger than maximum value.", arrayName)};
+      }
+      if(min < static_cast<double>(std::numeric_limits<T>().lowest()) || max > static_cast<double>(std::numeric_limits<T>().max()))
+      {
+        return Error{-4001, fmt::format("{}: The initialization range can only be from {} to {}", arrayName, std::numeric_limits<T>::min(), std::numeric_limits<T>::max())};
+      }
+      if(min == max)
+      {
+        return Error{-4002, fmt::format("{}: The initialization range must have differing values", arrayName)};
+      }
+    }
+
+    return {};
+  }
+};
 
 template <class T>
 auto CreateRandomGenerator(T rangeMin, T rangeMax)
@@ -95,49 +99,52 @@ auto CreateRandomGenerator(T rangeMin, T rangeMax)
   }
 }
 
-template <class T>
-void InitializeArray(IDataArray& dataArray, const std::array<usize, 3>& dims, uint64 xMin, uint64 xMax, uint64 yMin, uint64 yMax, uint64 zMin, uint64 zMax, InitializeData::InitType initType,
-                     float64 initValue, const RangeType& initRange)
+struct InitializeArrayFunctor
 {
-  T rangeMin;
-  T rangeMax;
-  if(initType == InitializeData::InitType::RandomWithRange)
+  template <class T>
+  void operator()(IDataArray& dataArray, const std::array<usize, 3>& dims, uint64 xMin, uint64 xMax, uint64 yMin, uint64 yMax, uint64 zMin, uint64 zMax, InitializeData::InitType initType,
+                  float64 initValue, const RangeType& initRange)
   {
-    rangeMin = static_cast<T>(initRange.first);
-    rangeMax = static_cast<T>(initRange.second);
-  }
-  else
-  {
-    rangeMin = std::numeric_limits<T>().min();
-    rangeMax = std::numeric_limits<T>().max();
-  }
-
-  auto& dataStore = dataArray.getIDataStoreRefAs<AbstractDataStore<T>>();
-
-  auto&& [distribution, generator] = CreateRandomGenerator(rangeMin, rangeMax);
-
-  for(uint64 k = zMin; k < zMax + 1; k++)
-  {
-    for(uint64 j = yMin; j < yMax + 1; j++)
+    T rangeMin;
+    T rangeMax;
+    if(initType == InitializeData::InitType::RandomWithRange)
     {
-      for(uint64 i = xMin; i < xMax + 1; i++)
-      {
-        usize index = (k * dims[0] * dims[1]) + (j * dims[0]) + i;
+      rangeMin = static_cast<T>(initRange.first);
+      rangeMax = static_cast<T>(initRange.second);
+    }
+    else
+    {
+      rangeMin = std::numeric_limits<T>().min();
+      rangeMax = std::numeric_limits<T>().max();
+    }
 
-        if(initType == InitializeData::InitType::Manual)
+    auto& dataStore = dataArray.getIDataStoreRefAs<AbstractDataStore<T>>();
+
+    auto&& [distribution, generator] = CreateRandomGenerator(rangeMin, rangeMax);
+
+    for(uint64 k = zMin; k < zMax + 1; k++)
+    {
+      for(uint64 j = yMin; j < yMax + 1; j++)
+      {
+        for(uint64 i = xMin; i < xMax + 1; i++)
         {
-          T num = static_cast<T>(initValue);
-          dataStore.fillTuple(index, num);
-        }
-        else
-        {
-          T randNum = distribution(generator);
-          dataStore.fillTuple(index, randNum);
+          usize index = (k * dims[0] * dims[1]) + (j * dims[0]) + i;
+
+          if(initType == InitializeData::InitType::Manual)
+          {
+            T num = static_cast<T>(initValue);
+            dataStore.fillTuple(index, num);
+          }
+          else
+          {
+            T randNum = distribution(generator);
+            dataStore.fillTuple(index, randNum);
+          }
         }
       }
     }
   }
-}
+};
 } // namespace
 
 namespace complex
@@ -182,7 +189,7 @@ Parameters InitializeData::parameters() const
 
   params.insertSeparator(Parameters::Separator{"Required Data Objects"});
 
-  params.insert(std::make_unique<MultiArraySelectionParameter>(k_CellArrayPaths_Key, "Cell Arrays", "The cell data arrays in which to initialize a subvolume to zeros", std::vector<DataPath>{},
+  params.insert(std::make_unique<MultiArraySelectionParameter>(k_CellArrayPaths_Key, "Cell Arrays", "The cell data arrays in which to initialize a sub-volume to zeros", std::vector<DataPath>{},
                                                                MultiArraySelectionParameter::AllowedTypes{IArray::ArrayType::DataArray}, complex::GetAllDataTypes()));
   params.insert(std::make_unique<GeometrySelectionParameter>(k_ImageGeometryPath_Key, "Image Geometry", "The geometry containing the cell data for which to initialize", DataPath{},
                                                              GeometrySelectionParameter::AllowedTypes{IGeometry::Type::Image}));
@@ -235,7 +242,7 @@ IFilter::PreflightResult InitializeData::preflightImpl(const DataStructure& data
     errors.push_back(Error{-5553, fmt::format("Z Max ({}) less than Z Min ({})", zMax, zMin)});
   }
 
-  const ImageGeom& imageGeom = data.getDataRefAs<ImageGeom>(imageGeomPath);
+  const auto& imageGeom = data.getDataRefAs<ImageGeom>(imageGeomPath);
 
   if(xMax > (static_cast<int64>(imageGeom.getNumXCells()) - 1))
   {
@@ -256,8 +263,7 @@ IFilter::PreflightResult InitializeData::preflightImpl(const DataStructure& data
 
   for(const DataPath& path : cellArrayPaths)
   {
-    const IDataArray& dataArray = data.getDataRefAs<IDataArray>(path);
-
+    const auto& dataArray = data.getDataRefAs<IDataArray>(path);
     std::vector<usize> tupleShape = dataArray.getIDataStoreRef().getTupleShape();
 
     if(tupleShape.size() != reversedImageDims.size())
@@ -266,94 +272,10 @@ IFilter::PreflightResult InitializeData::preflightImpl(const DataStructure& data
       continue;
     }
 
-    DataType type = dataArray.getDataType();
-
-    switch(type)
+    std::optional<Error> maybeError = ExecuteDataFunction(CheckInitializationFunctor{}, dataArray.getDataType(), dataArray, initType, initValue, initRange);
+    if(maybeError.has_value())
     {
-    case DataType::int8: {
-      std::optional<Error> maybeError = CheckInitialization<int8>(dataArray, initType, initValue, initRange);
-      if(maybeError.has_value())
-      {
-        errors.push_back(std::move(*maybeError));
-      }
-      break;
-    }
-    case DataType::int16: {
-      std::optional<Error> maybeError = CheckInitialization<int16>(dataArray, initType, initValue, initRange);
-      if(maybeError.has_value())
-      {
-        errors.push_back(std::move(*maybeError));
-      }
-      break;
-    }
-    case DataType::int32: {
-      std::optional<Error> maybeError = CheckInitialization<int32>(dataArray, initType, initValue, initRange);
-      if(maybeError.has_value())
-      {
-        errors.push_back(std::move(*maybeError));
-      }
-      break;
-    }
-    case DataType::int64: {
-      std::optional<Error> maybeError = CheckInitialization<int64>(dataArray, initType, initValue, initRange);
-      if(maybeError.has_value())
-      {
-        errors.push_back(std::move(*maybeError));
-      }
-      break;
-    }
-    case DataType::uint8: {
-      std::optional<Error> maybeError = CheckInitialization<uint8>(dataArray, initType, initValue, initRange);
-      if(maybeError.has_value())
-      {
-        errors.push_back(std::move(*maybeError));
-      }
-      break;
-    }
-    case DataType::uint16: {
-      std::optional<Error> maybeError = CheckInitialization<uint16>(dataArray, initType, initValue, initRange);
-      if(maybeError.has_value())
-      {
-        errors.push_back(std::move(*maybeError));
-      }
-      break;
-    }
-    case DataType::uint32: {
-      std::optional<Error> maybeError = CheckInitialization<uint32>(dataArray, initType, initValue, initRange);
-      if(maybeError.has_value())
-      {
-        errors.push_back(std::move(*maybeError));
-      }
-      break;
-    }
-    case DataType::uint64: {
-      std::optional<Error> maybeError = CheckInitialization<uint64>(dataArray, initType, initValue, initRange);
-      if(maybeError.has_value())
-      {
-        errors.push_back(std::move(*maybeError));
-      }
-      break;
-    }
-    case DataType::float32: {
-      std::optional<Error> maybeError = CheckInitialization<float32>(dataArray, initType, initValue, initRange);
-      if(maybeError.has_value())
-      {
-        errors.push_back(std::move(*maybeError));
-      }
-      break;
-    }
-    case DataType::float64: {
-      std::optional<Error> maybeError = CheckInitialization<float64>(dataArray, initType, initValue, initRange);
-      if(maybeError.has_value())
-      {
-        errors.push_back(std::move(*maybeError));
-      }
-      break;
-    }
-    default: {
-      errors.push_back(Error{-5560, fmt::format("'{}' is an invalid type", path.toString())});
-      break;
-    }
+      errors.push_back(std::move(*maybeError));
     }
   }
 
@@ -392,58 +314,11 @@ Result<> InitializeData::executeImpl(DataStructure& data, const Arguments& args,
 
   for(const DataPath& path : cellArrayPaths)
   {
-    IDataArray& dataArray = data.getDataRefAs<IDataArray>(path);
+    auto& iDataArray = data.getDataRefAs<IDataArray>(path);
 
-    DataType type = dataArray.getDataType();
+    ExecuteNeighborFunction(InitializeArrayFunctor{}, iDataArray.getDataType(), iDataArray, dims, xMin, xMax, yMin, yMax, zMin, zMin, initType, initValue, initRange);
 
-    switch(type)
-    {
-    case DataType::int8: {
-      InitializeArray<int8>(dataArray, dims, xMin, xMax, yMin, yMax, zMin, zMin, initType, initValue, initRange);
-      break;
-    }
-    case DataType::int16: {
-      InitializeArray<int16>(dataArray, dims, xMin, xMax, yMin, yMax, zMin, zMin, initType, initValue, initRange);
-      break;
-    }
-    case DataType::int32: {
-      InitializeArray<int32>(dataArray, dims, xMin, xMax, yMin, yMax, zMin, zMin, initType, initValue, initRange);
-      break;
-    }
-    case DataType::int64: {
-      InitializeArray<int64>(dataArray, dims, xMin, xMax, yMin, yMax, zMin, zMin, initType, initValue, initRange);
-      break;
-    }
-    case DataType::uint8: {
-      InitializeArray<uint8>(dataArray, dims, xMin, xMax, yMin, yMax, zMin, zMin, initType, initValue, initRange);
-      break;
-    }
-    case DataType::uint16: {
-      InitializeArray<uint16>(dataArray, dims, xMin, xMax, yMin, yMax, zMin, zMin, initType, initValue, initRange);
-      break;
-    }
-    case DataType::uint32: {
-      InitializeArray<uint32>(dataArray, dims, xMin, xMax, yMin, yMax, zMin, zMin, initType, initValue, initRange);
-      break;
-    }
-    case DataType::uint64: {
-      InitializeArray<uint64>(dataArray, dims, xMin, xMax, yMin, yMax, zMin, zMin, initType, initValue, initRange);
-      break;
-    }
-    case DataType::float32: {
-      InitializeArray<float32>(dataArray, dims, xMin, xMax, yMin, yMax, zMin, zMin, initType, initValue, initRange);
-      break;
-    }
-    case DataType::float64: {
-      InitializeArray<float32>(dataArray, dims, xMin, xMax, yMin, yMax, zMin, zMin, initType, initValue, initRange);
-      break;
-    }
-    default: {
-      throw std::runtime_error(fmt::format("InitializeData: Invalid array type for '{}'", path.toString()));
-    }
-    }
-
-    // Delay the execution by 1 second to avoid the exact same seedings for each array
+    // Delay the execution by 1 second to avoid the exact same seeding for each array
     {
       using namespace std::chrono_literals;
       std::this_thread::sleep_for(1s);
