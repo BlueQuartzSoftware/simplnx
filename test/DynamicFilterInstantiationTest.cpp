5dcd969efa026c0495fe090123bf867a30c70991
nyoungbq <nathan.young@bluequartz.net>
2023-02-03 12:04:19 -0500

    clang formatting

--- test/DynamicFilterInstantiationTest.cpp
+++ test/DynamicFilterInstantiationTest.cpp
@@ -36,7 +36,7 @@ TEST_CASE("Filter List Instantiation")
       args.insert(name, param->defaultValue());
     }
 
-      REQUIRE(!filter->defaultTags().empty());
+    REQUIRE(!filter->defaultTags().empty());
 
     auto preflightResult = filter->preflight(dataStructure, args);
     REQUIRE((preflightResult.outputActions.valid() || preflightResult.outputActions.invalid())); // Preflight exists
