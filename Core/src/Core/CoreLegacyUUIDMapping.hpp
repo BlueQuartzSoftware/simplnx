32c71770613d7eac7b9d3e808ae256359414f2a7
Michael Jackson <mike.jackson@bluequartz.net>
2023-02-15 12:12:20 -0500

    Move ErodeDilateBadData to ComplexCore (#94)
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- Core/src/Core/CoreLegacyUUIDMapping.hpp
+++ Core/src/Core/CoreLegacyUUIDMapping.hpp
@@ -6,7 +6,6 @@
 // clang-format off
 #include "Core/Filters/FindNeighborhoodsFilter.hpp"
 #include "Core/Filters/RemoveFlaggedFeaturesFilter.hpp"
-#include "Core/Filters/ErodeDilateBadDataFilter.hpp"
 // @@__HEADER__TOKEN__DO__NOT__DELETE__@@
 
 namespace complex
@@ -16,7 +15,6 @@ namespace complex
     // syntax std::make_pair {Dream3d UUID , Dream3dnx UUID}, // dream3d-class-name
     {complex::Uuid::FromString("697ed3de-db33-5dd1-a64b-04fb71e7d63e").value(), complex::FilterTraits<FindNeighborhoodsFilter>::uuid}, // FindNeighborhoods
     {complex::Uuid::FromString("a8463056-3fa7-530b-847f-7f4cb78b8602").value(), complex::FilterTraits<RemoveFlaggedFeaturesFilter>::uuid}, // RemoveFlaggedFeatures
-    {complex::Uuid::FromString("3adfe077-c3c9-5cd0-ad74-cf5f8ff3d254").value(), complex::FilterTraits<ErodeDilateBadDataFilter>::uuid}, // ErodeDilateBadData
     // @@__MAP__UPDATE__TOKEN__DO__NOT__DELETE__@@
   };
 
