38388345f4e69936e7ab93b2e790752f08417724
Joey Kleingers <joey.kleingers@bluequartz.net>
2023-02-15 16:34:41 -0500

    Clang format.
    Signed-off-by: Joey Kleingers <joey.kleingers@bluequartz.net>

--- src/Plugins/ComplexCore/src/ComplexCore/Filters/ImportBinaryCTNorthstarFilter.cpp
+++ src/Plugins/ComplexCore/src/ComplexCore/Filters/ImportBinaryCTNorthstarFilter.cpp
@@ -376,7 +376,7 @@ Parameters ImportBinaryCTNorthstarFilter::parameters() const
 
   params.insert(std::make_unique<ChoicesParameter>(k_LengthUnit_Key, "Length Unit", "The length unit that will be set into the created image geometry", 0, IGeometry::GetAllLengthUnitStrings()));
 
-  params.insertSeparator(Parameters::Separator{"Created Objects"});
+  params.insertSeparator(Parameters::Separator{"Created Data Objects"});
   params.insert(
       std::make_unique<DataGroupCreationParameter>(k_ImageGeometryPath_Key, "Image Geometry Path", "The path that will be used to create the Image Geometry.", DataPath{{"CT Image Geometry"}}));
   params.insert(std::make_unique<DataObjectNameParameter>(k_CellAttributeMatrixName_Key, "Cell Attribute Matrix Name", "The name used to create the Cell Attribute Matrix.", "CT Scan Data"));
@@ -523,7 +523,8 @@ IFilter::PreflightResult ImportBinaryCTNorthstarFilter::preflightImpl(const Data
 }
 
 //------------------------------------------------------------------------------
-Result<> ImportBinaryCTNorthstarFilter::executeImpl(DataStructure& dataStructure, const Arguments& filterArgs, const PipelineFilter* pipelineNode, const MessageHandler& messageHandler, const std::atomic_bool& shouldCancel) const
+Result<> ImportBinaryCTNorthstarFilter::executeImpl(DataStructure& dataStructure, const Arguments& filterArgs, const PipelineFilter* pipelineNode, const MessageHandler& messageHandler,
+                                                    const std::atomic_bool& shouldCancel) const
 {
   ImportBinaryCTNorthstarInputValues inputValues;
 
