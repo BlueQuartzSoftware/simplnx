140e26e1960cdb5103012bda4165ff65fa321dc8
nyoungbq <nathan.young@bluequartz.net>
2023-03-07 13:09:16 -0500

    Functional messaging classes added

--- src/complex/Utilities/MessageUtilities.hpp
+++ src/complex/Utilities/MessageUtilities.hpp
@@ -8,60 +8,72 @@
 
 namespace complex
 {
-class COMPLEX_EXPORT AbstractMessenger
+class COMPLEX_EXPORT ThreadSafeMessenger
 {
 public:
-  AbstractMessenger() = default;
-  virtual ~AbstractMessenger() = default;
-
-  virtual void setProgressMessage(std::string&& string) = 0;
-
-  virtual void setUpdateDelay(usize milliseconds) = 0;
-
-  virtual void setProgressIncrement(usize count) = 0;
-
-  virtual void setTotalElements(usize totalElements) = 0;
-
-  virtual usize getProgressIncrement() = 0;
-
-  virtual usize getTotalElements() = 0;
-};
-
-class COMPLEX_EXPORT ThreadSafeMessenger : public AbstractMessenger
-{
-public:
-  ThreadSafeMessenger(const IFilter::MessageHandler& messageHandler, std::string&& progressMessage = "Executing... ", usize milliDelay = 1000);
+  ThreadSafeMessenger(const IFilter::MessageHandler& messageHandler, std::string&& progressMessage = "Executing...", usize milliDelay = 1000);
   ~ThreadSafeMessenger() = default;
 
-  void updateProgress(size_t counter);
+  void updateProgress(usize counter);
+
+  void setProgressMessage(std::string&& progressMessage);
+
+  void setUpdateDelay(usize milliseconds);
+
+  void setProgressIncrement(usize count);
+
+  void setTotalElements(usize totalElements);
+
+  usize getProgressIncrement();
+
+  usize getTotalElements();
 
 private:
   const IFilter::MessageHandler& m_MessageHandler;
-  std::string m_ProgressMessage = "Executing... ";
+  std::string m_ProgressMessage = "Executing...";
   usize m_MilliDelay; // Default = 1 second
-
-  size_t m_ProgressCounter = 0;
-  size_t m_TotalElements = 0;
+  usize m_ProgressCounter = 0;
+  usize m_ProgressIncrement = 100;
+  usize m_TotalElements = 0;
 
   mutable std::mutex m_ProgressMessage_Mutex;
   std::chrono::steady_clock::time_point m_InitialTime = std::chrono::steady_clock::now();
 };
 
-class COMPLEX_EXPORT ThreadSafeMultiTaskMessenger : public AbstractMessenger
+class COMPLEX_EXPORT ThreadSafeMultiTaskMessenger
 {
+  using ArrayValues = std::array<usize, 3>; // {progress counter, progress increment, total elements}
+
+  /*!!!!The C++11 standard guarantees that const method access to containers is safe from different threads (ie, both use const methods)!!!!*/
+
 public:
-  ThreadSafeMultiTaskMessenger(const IFilter::MessageHandler& messageHandler, usize milliDelay = 1000);
+  ThreadSafeMultiTaskMessenger(const IFilter::MessageHandler& messageHandler, std::string&& progressMessage = "Executing...", usize milliDelay = 1000);
   ~ThreadSafeMultiTaskMessenger() = default;
 
-  void updateProgress(size_t counter);
+  bool updateProgress(usize counter, uint64 id);
+
+  void setProgressMessage(std::string&& progressMessage);
+
+  void setUpdateDelay(usize milliseconds);
+
+  bool addArray(uint64 id, usize progressIncrement, usize totalElements, std::string&& arrayName);
+
+  bool setProgressIncrement(usize progressIncrement, uint64 id);
+
+  bool setTotalElements(usize totalElements, uint64 id);
+
+  usize getProgressIncrement(uint64 id) const;
+
+  usize getTotalElements(uint64 id) const;
+
+  // may need to implement a function in the future for non dataObjects to check that the id isn't already in the map
 
 private:
   const IFilter::MessageHandler& m_MessageHandler;
-  std::string m_ProgressMessage = "Executing... ";
+  std::string m_ProgressMessage = "Executing...";
   usize m_MilliDelay; // Default = 1 second
 
-  std::vector<usize> m_ProgressCounter = {0};
-  std::vector<usize> m_TotalElements = {0};
+  std::map<uint64, std::pair<ArrayValues, std::string>> m_Data = {};
 
   mutable std::mutex m_ProgressMessage_Mutex;
   std::chrono::steady_clock::time_point m_InitialTime = std::chrono::steady_clock::now();
