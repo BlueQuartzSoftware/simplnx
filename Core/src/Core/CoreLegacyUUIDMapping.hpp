0f9f6c6621822a7363b04a716c702114bbfde226
Nathan Young <nathan.young@bluequartz.net>
2023-03-07 14:33:00 -0500

    FILTER: Move find neighborhoods to complex/ComplexCore (#109)

--- Core/src/Core/CoreLegacyUUIDMapping.hpp
+++ Core/src/Core/CoreLegacyUUIDMapping.hpp
@@ -4,7 +4,6 @@
 #include <string>
 
 // clang-format off
-#include "Core/Filters/FindNeighborhoodsFilter.hpp"
 #include "Core/Filters/RemoveFlaggedFeaturesFilter.hpp"
 // @@__HEADER__TOKEN__DO__NOT__DELETE__@@
 
@@ -13,7 +12,6 @@ namespace complex
   static const std::map<complex::Uuid, complex::Uuid> k_SIMPL_to_Core
   {
     // syntax std::make_pair {Dream3d UUID , Dream3dnx UUID}, // dream3d-class-name
-    {complex::Uuid::FromString("697ed3de-db33-5dd1-a64b-04fb71e7d63e").value(), complex::FilterTraits<FindNeighborhoodsFilter>::uuid}, // FindNeighborhoods
     {complex::Uuid::FromString("a8463056-3fa7-530b-847f-7f4cb78b8602").value(), complex::FilterTraits<RemoveFlaggedFeaturesFilter>::uuid}, // RemoveFlaggedFeatures
     // @@__MAP__UPDATE__TOKEN__DO__NOT__DELETE__@@
   };
