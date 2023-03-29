82f8e1111505d0941c16c8e0707b5cf8eb8f8a14
Matthew Marine <matthew.marine@bluequartz.net>
2023-03-29 10:57:24 -0400

    Fixed debug / release CLI command

--- src/Plugins/ComplexCore/test/ExecuteProcessTest.cpp
+++ src/Plugins/ComplexCore/test/ExecuteProcessTest.cpp
@@ -23,9 +23,10 @@ TEST_CASE("ComplexCore::ExecuteProcessFilter: Valid filter execution")
   Arguments args;
 
   fs::path processOutput(fmt::format("{}/ExecuteProcessUnitTestOutput.txt", unit_test::k_BuildDir));
-  std::string testCommand = fmt::format("{}/DREAM3DNX-cli --help", unit_test::k_BuildDir);
 #if NDEBUG // release build
-  testCommand = fmt::format("{}/DREAM3DNX-cli_d --help", unit_test::k_BuildDir);
+  std::string testCommand = fmt::format("{}/DREAM3DNX-cli --help", unit_test::k_BuildDir);
+#else
+  std::string testCommand = fmt::format("{}/DREAM3DNX-cli_d --help", unit_test::k_BuildDir);
 #endif
 
   // Create default Parameters for the filter.
