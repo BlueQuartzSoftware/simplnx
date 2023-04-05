6022dd22f024edf2708b4dde8328fe5858c85e6f
Michael Jackson <mike.jackson@bluequartz.net>
2023-04-04 21:58:27 -0400

    Fix Win32 batch file generation. use proper slashes for Windows

--- src/Plugins/OrientationAnalysis/test/ctest_pipeline_driver.bat
+++ src/Plugins/OrientationAnalysis/test/ctest_pipeline_driver.bat
@@ -36,16 +36,15 @@ IF "@MSVC_IDE@" == "1" (
 echo CONFIG_DIR %CONFIG_DIR%
 echo EXE_SFX %EXE_SFX%
 
-echo on
-@echo "Prebuilt Pipeline Test Starting"
-@echo "    @test@.d3dpipeline"
+::echo on
+@echo Prebuilt Pipeline Test Starting
+@echo     @test@.d3dpipeline
 
-@echo "Running Executable at @CMAKE_RUNTIME_OUTPUT_DIRECTORY@/%CONFIG_DIR%/@PIPELINE_RUNNER_NAME@%EXE_SFX%@EXE_EXT@"
+@echo Running Executable at '@CMAKE_RUNTIME_OUTPUT_DIRECTORY@/%CONFIG_DIR%/@PIPELINE_RUNNER_NAME@%EXE_SFX%@EXE_EXT@'
 
 cd @CMAKE_RUNTIME_OUTPUT_DIRECTORY@\%CONFIG_DIR%
 
 @PIPELINE_RUNNER_NAME@%EXE_SFX%@EXE_EXT@ "@ARGS_PIPELINE_PATH@"
-
 
 ::-----------------------------------------------------------------------------
 :: These files need to be deleted after the test has completed
