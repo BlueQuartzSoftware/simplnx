bd3e821293ba57d294134fa1f79087efda1fcc8f
Nathan Young <nathan.young@bluequartz.net>
2023-04-14 17:34:26 -0400

    STYLE: Reorder Some ITK Parameters (#565)
    ITK Mask Image Filter
    ITK Gradient Magnitude Image Filter

--- src/Plugins/ITKImageProcessing/src/ITKImageProcessing/Filters/ITKGradientMagnitudeImage.cpp
+++ src/Plugins/ITKImageProcessing/src/ITKImageProcessing/Filters/ITKGradientMagnitudeImage.cpp
@@ -71,12 +71,16 @@ Parameters ITKGradientMagnitudeImage::parameters() const
   Parameters params;
 
   params.insertSeparator(Parameters::Separator{"Input Parameters"});
+  params.insert(std::make_unique<BoolParameter>(k_UseImageSpacing_Key, "UseImageSpacing", "Whether or not the filter will use the spacing of the input image in its calculations", true));
+
+  params.insertSeparator(Parameters::Separator{"Required Data Objects"});
   params.insert(std::make_unique<GeometrySelectionParameter>(k_SelectedImageGeomPath_Key, "Image Geometry", "Select the Image Geometry Group from the DataStructure.", DataPath{},
                                                              GeometrySelectionParameter::AllowedTypes{IGeometry::Type::Image}));
   params.insert(
       std::make_unique<ArraySelectionParameter>(k_SelectedImageDataPath_Key, "Input Image", "The image data that will be processed by this filter.", DataPath{}, complex::GetAllNumericTypes()));
+
+  params.insertSeparator(Parameters::Separator{"Created Data Objects"});
   params.insert(std::make_unique<ArrayCreationParameter>(k_OutputImageDataPath_Key, "Output Image", "The result of the processing will be stored in this Data Array.", DataPath{}));
-  params.insert(std::make_unique<BoolParameter>(k_UseImageSpacing_Key, "UseImageSpacing", "Whether or not the filter will use the spacing of the input image in its calculations", true));
 
   return params;
 }
