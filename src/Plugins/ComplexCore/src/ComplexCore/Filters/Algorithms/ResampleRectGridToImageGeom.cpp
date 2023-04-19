53894b485160e0b33c9c01105f41151364464c08
Jessica Marquis <jessica.marquis@bluequartz.net>
2023-04-19 16:00:29 -0400

    Implement preflight & execute fcts for ResampleRectGridToImageGeom filter

--- src/Plugins/ComplexCore/src/ComplexCore/Filters/Algorithms/ResampleRectGridToImageGeom.cpp
+++ src/Plugins/ComplexCore/src/ComplexCore/Filters/Algorithms/ResampleRectGridToImageGeom.cpp
@@ -1,6 +1,10 @@
 #include "ResampleRectGridToImageGeom.hpp"
 
 #include "complex/DataStructure/DataArray.hpp"
+#include "complex/DataStructure/Geometry/ImageGeom.hpp"
+#include "complex/DataStructure/Geometry/RectGridGeom.hpp"
+#include "complex/Utilities/DataArrayUtilities.hpp"
+#include "complex/Utilities/ParallelTaskAlgorithm.hpp"
 
 using namespace complex;
 
@@ -26,5 +30,117 @@ const std::atomic_bool& ResampleRectGridToImageGeom::getCancel()
 // -----------------------------------------------------------------------------
 Result<> ResampleRectGridToImageGeom::operator()()
 {
+  const auto& rectGridGeom = m_DataStructure.getDataRefAs<RectGridGeom>(m_InputValues->RectilinearGridPath);
+  auto& imageGeom = m_DataStructure.getDataRefAs<ImageGeom>(m_InputValues->ImageGeometryPath);
+  imageGeom.setUnits(rectGridGeom.getUnits());
+
+  auto originResults = rectGridGeom.getOrigin();
+  if(originResults.invalid())
+  {
+    return MakeErrorResult(-7367, fmt::format("Could not retrieve origin value from input rectilinear grid geometry at path {}", m_InputValues->RectilinearGridPath.toString()));
+  }
+  const Float32Array* xGridValues = rectGridGeom.getXBounds();
+  const Float32Array* yGridValues = rectGridGeom.getYBounds();
+  const Float32Array* zGridValues = rectGridGeom.getZBounds();
+  FloatVec3 origin = rectGridGeom.getOrigin().value();
+  const SizeVec3 rectGridDims = rectGridGeom.getDimensions();
+  const SizeVec3 imageGeomDims = imageGeom.getDimensions();
+  const std::vector<float32> max = {xGridValues->back(), yGridValues->back(), zGridValues->back()};
+  const std::vector<float32> imageGeomSpacing = {(max[0] - origin[0]) / imageGeomDims[0], (max[1] - origin[1]) / imageGeomDims[1], (max[2] - origin[2]) / imageGeomDims[2]};
+  const FloatVec3 halfSpacing = {imageGeomSpacing[0] * 0.5f, imageGeomSpacing[1] * 0.5f, imageGeomSpacing[2] * 0.5f};
+
+  imageGeom.setSpacing(imageGeomSpacing);
+  imageGeom.setOrigin(origin);
+
+  usize rgIdxStart = 1;
+  std::vector<usize> xIdx(imageGeomDims[0], 0);
+  for(usize x = 0; x < imageGeomDims[0]; x++)
+  {
+    float32 coord = origin[0] + (x * imageGeomSpacing[0]) + halfSpacing[0];
+    for(usize rgIdx = rgIdxStart; rgIdx < xGridValues->size(); rgIdx++)
+    {
+      if(coord > xGridValues->at(rgIdx - 1) && coord <= xGridValues->at(rgIdx))
+      {
+        xIdx[x] = rgIdx - 1;
+        rgIdxStart = rgIdx;
+        break;
+      }
+    }
+  }
+
+  rgIdxStart = 1;
+  std::vector<usize> yIdx(imageGeomDims[1], 0);
+  for(usize y = 0; y < imageGeomDims[1]; y++)
+  {
+    float32 coord = origin[1] + (y * imageGeomSpacing[1]) + halfSpacing[1];
+    for(usize rgIdx = rgIdxStart; rgIdx < yGridValues->size(); rgIdx++)
+    {
+      if(coord > yGridValues->at(rgIdx - 1) && coord <= yGridValues->at(rgIdx))
+      {
+        yIdx[y] = rgIdx - 1;
+        rgIdxStart = rgIdx;
+        break;
+      }
+    }
+  }
+
+  rgIdxStart = 1;
+  std::vector<usize> zIdx(imageGeomDims[2], 0);
+  for(usize z = 0; z < imageGeomDims[2]; z++)
+  {
+    float32 coord = origin[2] + (z * imageGeomSpacing[2]) + halfSpacing[2];
+    for(usize rgIdx = rgIdxStart; rgIdx < zGridValues->size(); rgIdx++)
+    {
+      if(coord > zGridValues->at(rgIdx - 1) && coord <= zGridValues->at(rgIdx))
+      {
+        zIdx[z] = rgIdx - 1;
+        rgIdxStart = rgIdx;
+        break;
+      }
+    }
+  }
+
+  // Store the mapped XYZ index into the RectGrid data ararys
+  std::vector<int64> newToOldIdxs(imageGeom.getNumberOfCells());
+  usize currIdx = 0;
+  for(const usize z : zIdx)
+  {
+    for(const usize y : yIdx)
+    {
+      for(const usize x : xIdx)
+      {
+        // Compute the index into the RectGrid Data Array
+        const usize idx = (rectGridDims[0] * rectGridDims[1] * z) + (rectGridDims[0] * y) + x;
+        // Compute the index into the new Idx Array
+        newToOldIdxs[currIdx++] = idx;
+      }
+    }
+  }
+  if(getCancel())
+  {
+    return {};
+  }
+
+  ParallelTaskAlgorithm taskRunner;
+  auto& destCellDataAM = imageGeom.getCellDataRef();
+
+  // copy over/resample the cell data
+  for(const auto& srcArrayPath : m_InputValues->SelectedDataArrayPaths)
+  {
+    if(m_ShouldCancel)
+    {
+      return {};
+    }
+
+    const auto& srcArray = m_DataStructure.getDataRefAs<IArray>(srcArrayPath);
+    const std::string srcName = srcArray.getName();
+    auto& destDataArray = dynamic_cast<IDataArray&>(destCellDataAM.at(srcName));
+    m_MessageHandler(fmt::format("Resample Rect Grid To Image Geom || Copying Data Array {}", srcName));
+
+    CopyFromArray::RunParallelCopyUsingIndexList(destDataArray, taskRunner, srcArray, newToOldIdxs);
+  }
+
+  taskRunner.wait(); // This will spill over if the number of DataArrays to process does not divide evenly by the number of threads.
+
   return {};
 }
--- src/Plugins/ComplexCore/src/ComplexCore/Filters/ResampleRectGridToImageGeomFilter.cpp
+++ src/Plugins/ComplexCore/src/ComplexCore/Filters/ResampleRectGridToImageGeomFilter.cpp
@@ -4,13 +4,19 @@
 
 #include "complex/DataStructure/DataPath.hpp"
 #include "complex/DataStructure/Geometry/ImageGeom.hpp"
-#include "complex/Filter/Actions/CreateDataGroupAction.hpp"
-#include "complex/Filter/Actions/EmptyAction.hpp"
+#include "complex/DataStructure/Geometry/RectGridGeom.hpp"
+#include "complex/DataStructure/INeighborList.hpp"
+#include "complex/DataStructure/StringArray.hpp"
+#include "complex/Filter/Actions/CreateArrayAction.hpp"
+#include "complex/Filter/Actions/CreateImageGeometryAction.hpp"
+#include "complex/Filter/Actions/CreateNeighborListAction.hpp"
+#include "complex/Filter/Actions/CreateStringArrayAction.hpp"
 #include "complex/Parameters/DataGroupCreationParameter.hpp"
 #include "complex/Parameters/DataObjectNameParameter.hpp"
 #include "complex/Parameters/GeometrySelectionParameter.hpp"
 #include "complex/Parameters/MultiArraySelectionParameter.hpp"
 #include "complex/Parameters/VectorParameter.hpp"
+#include "complex/Utilities/GeometryHelpers.hpp"
 
 using namespace complex;
 
@@ -85,18 +91,91 @@ IFilter::PreflightResult ResampleRectGridToImageGeomFilter::preflightImpl(const 
   auto pImageGeomCellAttributeMatrixNameValue = filterArgs.value<std::string>(k_ImageGeomCellAttributeMatrix_Key);
 
   PreflightResult preflightResult;
-  complex::Result<OutputActions> resultOutputActions;
+  Result<OutputActions> resultOutputActions;
   std::vector<PreflightValue> preflightUpdatedValues;
-  std::string rectGridGeometryDesc;
-  std::string createdGeometryDescription;
 
-  //{
-  //  auto createDataGroupAction = std::make_unique<CreateDataGroupAction>(pImageGeometryPathValue);
-  //  resultOutputActions.value().actions.push_back(std::move(createDataGroupAction));
-  //}
+  // check for valid inputs
+  if(pSelectedDataArrayPathsValue.empty())
+  {
+    return MakePreflightErrorResult(-7360, "At least one attribute array must be selected");
+  }
 
-  preflightUpdatedValues.push_back({"RectGridGeometryDesc", rectGridGeometryDesc});
-  preflightUpdatedValues.push_back({"CreatedGeometryDescription", createdGeometryDescription});
+  if(!dataStructure.validateNumberOfTuples(pSelectedDataArrayPathsValue))
+  {
+    return MakePreflightErrorResult(-7361, "One or more of the selected attribute arrays have mismatching numbers of tuples. All selected attribute arrays must come from the same attribute matrix.");
+  }
+
+  if(pDimensionsValue[0] < 0)
+  {
+    return MakePreflightErrorResult(-7362, fmt::format("The x dimension value ({}) for the created image geometry must be positive", pDimensionsValue[0]));
+  }
+  if(pDimensionsValue[1] < 0)
+  {
+    return MakePreflightErrorResult(-7363, fmt::format("The y dimension value ({}) for the created image geometry must be positive", pDimensionsValue[1]));
+  }
+  if(pDimensionsValue[2] < 0)
+  {
+    return MakePreflightErrorResult(-7364, fmt::format("The z dimension value ({}) for the created image geometry must be positive", pDimensionsValue[2]));
+  }
+
+  const std::vector<usize> dims = {static_cast<usize>(pDimensionsValue[0]), static_cast<usize>(pDimensionsValue[1]), static_cast<usize>(pDimensionsValue[2])};
+
+  // Create the re-sampled image geometry - origin & spacing values will be calculated & updated in execute
+  {
+    auto createDataGroupAction = std::make_unique<CreateImageGeometryAction>(pImageGeometryPathValue, dims, std::vector<float32>{0.0f, 0.0f, 0.0f}, std::vector<float32>{1.0f, 1.0f, 1.0f},
+                                                                             pImageGeomCellAttributeMatrixNameValue);
+    resultOutputActions.value().actions.push_back(std::move(createDataGroupAction));
+  }
+
+  // create the IArrays for copying over to the re-sampled image geometry
+  const auto& rectGridGeom = dataStructure.getDataRefAs<RectGridGeom>(pRectilinearGridPathValue);
+  const DataPath srcCellDataPath = pRectilinearGridPathValue.createChildPath(rectGridGeom.getCellData()->getName());
+  const DataPath destCellDataPath = pImageGeometryPathValue.createChildPath(pImageGeomCellAttributeMatrixNameValue);
+  usize totalPoints = dims[0] * dims[1] * dims[2];
+  for(const auto& path : pSelectedDataArrayPathsValue)
+  {
+    const DataPath destPath = destCellDataPath.createChildPath(path.getTargetName());
+    const auto* srcArray = dataStructure.getDataAs<IArray>(path);
+
+    if(srcCellDataPath != path.getParent())
+    {
+      resultOutputActions.warnings().push_back(
+          {-7365, fmt::format("Data Object at path '{}' is not a cell level array and thus cannot be copied over to the re-sampled image geometry", path.toString())});
+    }
+
+    IArray::ArrayType arrayType = srcArray->getArrayType();
+    if(arrayType == IArray::ArrayType::DataArray)
+    {
+      const auto* srcDataArray = dataStructure.getDataAs<IDataArray>(path);
+      auto createArrayAction = std::make_unique<CreateArrayAction>(srcDataArray->getDataType(), dims, srcDataArray->getComponentShape(), destPath);
+      resultOutputActions.value().actions.push_back(std::move(createArrayAction));
+    }
+    else if(arrayType == IArray::ArrayType::NeighborListArray)
+    {
+      const auto* srcDataArray = dataStructure.getDataAs<INeighborList>(path);
+      auto createArrayAction = std::make_unique<CreateNeighborListAction>(srcDataArray->getDataType(), totalPoints, destPath);
+      resultOutputActions.value().actions.push_back(std::move(createArrayAction));
+    }
+    else if(arrayType == IArray::ArrayType::StringArray)
+    {
+      const auto* srcDataArray = dataStructure.getDataAs<StringArray>(path);
+      auto createArrayAction = std::make_unique<CreateStringArrayAction>(dims, destPath);
+      resultOutputActions.value().actions.push_back(std::move(createArrayAction));
+    }
+    else
+    {
+      resultOutputActions.warnings().push_back({-7366, fmt::format("Data Object at path '{}' is not an IArray type and thus cannot be copied over to the re-sampled image geometry", path.toString())});
+    }
+  }
+
+  // Generate geometry info for preflight updated values
+  const SizeVec3 srcDims = rectGridGeom.getDimensions();
+  std::stringstream rectGridGeometryDesc;
+  rectGridGeometryDesc << srcDims[0] << " Voxels\n" << srcDims[1] << " Voxels\n" << srcDims[2] << " Voxels\n";
+  preflightUpdatedValues.push_back({"Rect Grid Geometry Info", rectGridGeometryDesc.str()});
+  std::stringstream createdGeometryDesc;
+  createdGeometryDesc << dims[0] << " Voxels\n" << dims[1] << " Voxels\n" << dims[2] << " Voxels\n";
+  preflightUpdatedValues.push_back({"Created Image Geometry Info", createdGeometryDesc.str()});
 
   return {std::move(resultOutputActions), std::move(preflightUpdatedValues)};
 }
