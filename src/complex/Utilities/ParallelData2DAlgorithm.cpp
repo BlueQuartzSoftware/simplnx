8d762761c38aa68df4c2d2af612809d29fdc860a
Matthew Marine <matthew.marine@bluequartz.net>
2023-05-08 09:08:18 -0400

    Updated ParallelData algorithms
    * Updated 2D and 3D versions of ParallelData algorithms for out of core use where the pattern of traversal can improve processing speeds.
    * This can be applied even when not running in parallel.

--- src/complex/Utilities/ParallelData2DAlgorithm.cpp
+++ src/complex/Utilities/ParallelData2DAlgorithm.cpp
@@ -27,7 +27,7 @@ Range2D ParallelData2DAlgorithm::getRange() const
 }
 
 // -----------------------------------------------------------------------------
-void ParallelData2DAlgorithm::setRange(const Range2D& range)
+void ParallelData2DAlgorithm::setRange(const RangeType& range)
 {
   m_Range = range;
 }
@@ -36,4 +36,16 @@ void ParallelData2DAlgorithm::setRange(const Range2D& range)
 void ParallelData2DAlgorithm::setRange(size_t minCols, size_t maxCols, size_t minRows, size_t maxRows)
 {
   m_Range = {minCols, maxCols, minRows, maxRows};
+}
+
+// -----------------------------------------------------------------------------
+std::optional<Range2D> ParallelData2DAlgorithm::getChunkSize() const
+{
+  return m_ChunkSize;
+}
+
+// -----------------------------------------------------------------------------
+void ParallelData2DAlgorithm::setChunkSize(std::optional<RangeType> chunkSize)
+{
+  m_ChunkSize = chunkSize;
 }
