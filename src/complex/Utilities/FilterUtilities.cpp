4ebb8452d3bff52ce2f3ef64b7a9dd706ece7325
Jessica Marquis <jessica.marquis@bluequartz.net>
2023-05-16 09:48:17 -0400

    FILTER: Add FindGBCDMetricBased & FindGBPDMetricBased filters (#578)
    * Add FindGBCDMetricBasedFilte
    * Add FindGBPDMetricBased Filter
    * Add example pipeline
    * New test files. 
    * Updating documentation
    * Added script for Pole Figure Generation using GMT.
    * Convert GBPDMetric to use double precision for internal calculations
    
    ---------
    
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>
    Co-authored-by: Michael Jackson <mike.jackson@bluequartz.net>

--- src/complex/Utilities/FilterUtilities.cpp
+++ src/complex/Utilities/FilterUtilities.cpp
@@ -13,7 +13,7 @@ Result<> CreateOutputDirectories(const fs::path& outputPath)
   if(!fs::exists(outputPath))
   {
     std::error_code errorCode;
-    // So this looks wierd but this can happen on
+    // So this looks weird but this can happen on
     // platforms where /tmp is a symlink to /private/tmp. So the original path is
     // /tmp/foo but what was created was /private/tmp/foo. This logic should fix that issue.
     if(!fs::create_directories(outputPath, errorCode) && !fs::exists(outputPath))
