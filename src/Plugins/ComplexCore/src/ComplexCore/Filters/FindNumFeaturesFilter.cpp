8324667ec08aabc2829b5755de5ab541a19977d8
Jessica Marquis <jessica.marquis@bluequartz.net>
2023-05-31 11:53:28 -0400

    Update FindNumFeatures filter to use DataObjectName parameter

--- src/Plugins/ComplexCore/src/ComplexCore/Filters/FindNumFeaturesFilter.cpp
+++ src/Plugins/ComplexCore/src/ComplexCore/Filters/FindNumFeaturesFilter.cpp
@@ -4,8 +4,9 @@
 #include "complex/DataStructure/DataArray.hpp"
 #include "complex/DataStructure/DataPath.hpp"
 #include "complex/Filter/Actions/CreateArrayAction.hpp"
-#include "complex/Parameters/ArrayCreationParameter.hpp"
 #include "complex/Parameters/ArraySelectionParameter.hpp"
+#include "complex/Parameters/DataGroupSelectionParameter.hpp"
+#include "complex/Parameters/DataObjectNameParameter.hpp"
 
 using namespace complex;
 
@@ -50,8 +51,13 @@ Parameters FindNumFeaturesFilter::parameters() const
   params.insert(std::make_unique<ArraySelectionParameter>(k_FeaturePhasesArrayPath_Key, "Feature Phases", "Array specifying which Ensemble each Feature belongs",
                                                           DataPath({"DataContainer", "FeatureData", "Phases"}), complex::GetAllDataTypes()));
 
+  params.insertSeparator(Parameters::Separator{"Required Objects: Ensemble Data"});
+  params.insert(std::make_unique<DataGroupSelectionParameter>(k_EnsembleAttributeMatrixPath_Key, "Ensemble Attribute Matrix",
+                                                              "The path to the ensemble attribute matrix where the number of features array will be stored",
+                                                              DataPath({"DataContainer", "EnsembleData"}), DataGroupSelectionParameter::AllowedTypes{BaseGroup::GroupType::AttributeMatrix}));
+
   params.insertSeparator(Parameters::Separator{"Created Objects: Ensemble Data"});
-  params.insert(std::make_unique<ArrayCreationParameter>(k_NumFeaturesArrayPath_Key, "Number of Features", "The number of Features that belong to each Ensemble", DataPath({"Number of Features"})));
+  params.insert(std::make_unique<DataObjectNameParameter>(k_NumFeaturesArrayPath_Key, "Number of Features", "The number of Features that belong to each Ensemble", "Number of Features"));
 
   return params;
 }
@@ -67,19 +73,20 @@ IFilter::PreflightResult FindNumFeaturesFilter::preflightImpl(const DataStructur
                                                               const std::atomic_bool& shouldCancel) const
 {
   auto pFeaturePhasesArrayPathValue = filterArgs.value<DataPath>(k_FeaturePhasesArrayPath_Key);
-  auto pNumFeaturesArrayPathValue = filterArgs.value<DataPath>(k_NumFeaturesArrayPath_Key);
+  auto pEnsembleDataPathValue = filterArgs.value<DataPath>(k_EnsembleAttributeMatrixPath_Key);
+  auto pNumFeaturesArrayPathValue = pEnsembleDataPathValue.createChildPath(filterArgs.value<std::string>(k_NumFeaturesArrayPath_Key));
 
   PreflightResult preflightResult;
   complex::Result<OutputActions> resultOutputActions;
   std::vector<PreflightValue> preflightUpdatedValues;
 
-  const auto* featureData = dataStructure.getDataAs<AttributeMatrix>(pNumFeaturesArrayPathValue.getParent());
-  if(featureData == nullptr)
+  const auto* cellEnsembleData = dataStructure.getDataAs<AttributeMatrix>(pEnsembleDataPathValue);
+  if(cellEnsembleData == nullptr)
   {
-    return {MakeErrorResult<OutputActions>(-47630, fmt::format("Could not find selected feature Attribute Matrix at path '{}'", pNumFeaturesArrayPathValue.getParent().toString()))};
+    return {MakeErrorResult<OutputActions>(-47630, fmt::format("Could not find selected ensemble Attribute Matrix at path '{}'", pEnsembleDataPathValue.toString()))};
   }
 
-  auto createArrayAction = std::make_unique<CreateArrayAction>(DataType::int32, featureData->getShape(), std::vector<usize>{1}, pNumFeaturesArrayPathValue);
+  auto createArrayAction = std::make_unique<CreateArrayAction>(DataType::int32, cellEnsembleData->getShape(), std::vector<usize>{1}, pNumFeaturesArrayPathValue);
   resultOutputActions.value().actions.push_back(std::move(createArrayAction));
 
   return {std::move(resultOutputActions), std::move(preflightUpdatedValues)};
@@ -89,8 +96,12 @@ IFilter::PreflightResult FindNumFeaturesFilter::preflightImpl(const DataStructur
 Result<> FindNumFeaturesFilter::executeImpl(DataStructure& dataStructure, const Arguments& filterArgs, const PipelineFilter* pipelineNode, const MessageHandler& messageHandler,
                                             const std::atomic_bool& shouldCancel) const
 {
-  const auto& featurePhasesArrayRef = dataStructure.getDataRefAs<Int32Array>(filterArgs.value<DataPath>(k_FeaturePhasesArrayPath_Key));
-  auto& numFeaturesArrayRef = dataStructure.getDataRefAs<Int32Array>(filterArgs.value<DataPath>(k_NumFeaturesArrayPath_Key));
+  auto pFeaturePhasesArrayPathValue = filterArgs.value<DataPath>(k_FeaturePhasesArrayPath_Key);
+  auto pEnsembleDataPathValue = filterArgs.value<DataPath>(k_EnsembleAttributeMatrixPath_Key);
+  auto pNumFeaturesArrayPathValue = pEnsembleDataPathValue.createChildPath(filterArgs.value<std::string>(k_NumFeaturesArrayPath_Key));
+
+  const auto& featurePhasesArrayRef = dataStructure.getDataRefAs<Int32Array>(pFeaturePhasesArrayPathValue);
+  auto& numFeaturesArrayRef = dataStructure.getDataRefAs<Int32Array>(pNumFeaturesArrayPathValue);
 
   for(usize index = 1; index < featurePhasesArrayRef.getNumberOfTuples(); index++)
   {
