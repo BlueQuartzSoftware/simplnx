14fe8314432199226679e45f3ec69f098c65e2fd
nyoungbq <nathan.young@bluequartz.net>
2023-06-07 10:23:36 -0400

    clang formatting

--- src/Plugins/ComplexCore/test/FindFeaturePhasesBinaryTest.cpp
+++ src/Plugins/ComplexCore/test/FindFeaturePhasesBinaryTest.cpp
@@ -13,7 +13,7 @@ const std::string k_BinaryFeaturePhasesName = "BinaryFeaturePhases";
 
 const DataPath k_ExemplarArray = DataPath({Constants::k_SmallIN100, Constants::k_EbsdScanData, "BinaryPhases"});
 const DataPath k_GenereatedArray = DataPath({Constants::k_SmallIN100, Constants::k_EbsdScanData, k_BinaryFeaturePhasesName});
-}
+} // namespace
 
 TEST_CASE("ComplexCore::FindFeaturePhasesBinaryFilter: Valid Filter Execution", "[ComplexCore][FindFeaturePhasesBinaryFilter]")
 {
