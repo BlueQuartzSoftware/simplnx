c0d1574bd0604670266a71afac0b1a7071e7ca40
nyoungbq <nathan.young@bluequartz.net>
2023-06-07 10:08:16 -0400

    Complete unit test

--- src/Plugins/ComplexCore/test/FindFeaturePhasesBinaryTest.cpp
+++ src/Plugins/ComplexCore/test/FindFeaturePhasesBinaryTest.cpp
@@ -1,5 +1,7 @@
 #include <catch2/catch.hpp>
 
+#include "complex/UnitTest/UnitTestCommon.hpp"
+
 #include "ComplexCore/ComplexCore_test_dirs.hpp"
 #include "ComplexCore/Filters/FindFeaturePhasesBinaryFilter.hpp"
 
@@ -8,27 +10,35 @@ using namespace complex;
 namespace
 {
 const std::string k_BinaryFeaturePhasesName = "BinaryFeaturePhases";
+
+const DataPath k_ExemplarArray = DataPath({Constants::k_SmallIN100, Constants::k_EbsdScanData, "BinaryPhases"});
+const DataPath k_GenereatedArray = DataPath({Constants::k_SmallIN100, Constants::k_EbsdScanData, k_BinaryFeaturePhasesName});
 }
 
 TEST_CASE("ComplexCore::FindFeaturePhasesBinaryFilter: Valid Filter Execution", "[ComplexCore][FindFeaturePhasesBinaryFilter]")
 {
-  // Instantiate the filter, a DataStructure object and an Arguments Object
-  FindFeaturePhasesBinaryFilter filter;
-  DataStructure dataStructure;
-  Arguments args;
+  DataStructure dataStructure = UnitTest::LoadDataStructure(fs::path(fmt::format("{}/bin_feature_phases/6_6_find_feature_phases_binary.dream3d", unit_test::k_TestFilesDir)));
 
-  // Create default Parameters for the filter.
-  args.insertOrAssign(FindFeaturePhasesBinaryFilter::k_FeatureIdsArrayPath_Key, std::make_any<DataPath>(DataPath{}));
-  args.insertOrAssign(FindFeaturePhasesBinaryFilter::k_GoodVoxelsArrayPath_Key, std::make_any<DataPath>(DataPath{}));
-  args.insertOrAssign(FindFeaturePhasesBinaryFilter::k_CellDataAMPath_Key, std::make_any<DataPath>(DataPath{}));
+  {
+    // Instantiate the filter, a DataStructure object and an Arguments Object
+    FindFeaturePhasesBinaryFilter filter;
+    Arguments args;
 
-  args.insertOrAssign(FindFeaturePhasesBinaryFilter::k_FeaturePhasesArrayName_Key, std::make_any<std::string>(k_BinaryFeaturePhasesName));
+    // Create default Parameters for the filter.
+    args.insertOrAssign(FindFeaturePhasesBinaryFilter::k_FeatureIdsArrayPath_Key, std::make_any<DataPath>(DataPath({Constants::k_SmallIN100, Constants::k_EbsdScanData, Constants::k_FeatureIds})));
+    args.insertOrAssign(FindFeaturePhasesBinaryFilter::k_GoodVoxelsArrayPath_Key, std::make_any<DataPath>(DataPath({Constants::k_SmallIN100, Constants::k_EbsdScanData, Constants::k_Mask})));
+    args.insertOrAssign(FindFeaturePhasesBinaryFilter::k_CellDataAMPath_Key, std::make_any<DataPath>(DataPath({Constants::k_SmallIN100, Constants::k_EbsdScanData})));
 
-  // Preflight the filter and check result
-  auto preflightResult = filter.preflight(dataStructure, args);
-  REQUIRE(preflightResult.outputActions.valid());
+    args.insertOrAssign(FindFeaturePhasesBinaryFilter::k_FeaturePhasesArrayName_Key, std::make_any<std::string>(k_BinaryFeaturePhasesName));
 
-  // Execute the filter and check the result
-  auto executeResult = filter.execute(dataStructure, args);
-  REQUIRE(executeResult.result.valid());
+    // Preflight the filter and check result
+    auto preflightResult = filter.preflight(dataStructure, args);
+    REQUIRE(preflightResult.outputActions.valid());
+
+    // Execute the filter and check the result
+    auto executeResult = filter.execute(dataStructure, args);
+    REQUIRE(executeResult.result.valid());
+  }
+
+  UnitTest::CompareArrays<int32>(dataStructure, k_ExemplarArray, k_GenereatedArray);
 }
