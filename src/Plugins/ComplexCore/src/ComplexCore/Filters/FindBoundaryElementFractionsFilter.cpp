79f1fc262558660521c3d81f7923d1e68cc29c5f
nyoungbq <nathan.young@bluequartz.net>
2023-06-07 14:35:30 -0400

    remove algorithm class since it is a trivial run

--- src/Plugins/ComplexCore/src/ComplexCore/Filters/FindBoundaryElementFractionsFilter.cpp
+++ src/Plugins/ComplexCore/src/ComplexCore/Filters/FindBoundaryElementFractionsFilter.cpp
@@ -1,9 +1,6 @@
 #include "FindBoundaryElementFractionsFilter.hpp"
 
-#include "ComplexCore/Filters/Algorithms/FindBoundaryElementFractions.hpp"
-
 #include "complex/DataStructure/DataPath.hpp"
-#include "complex/Filter/Actions/EmptyAction.hpp"
 #include "complex/Filter/Actions/CreateArrayAction.hpp"
 #include "complex/Parameters/ArrayCreationParameter.hpp"
 #include "complex/Parameters/ArraySelectionParameter.hpp"
@@ -79,41 +76,13 @@ IFilter::UniquePointer FindBoundaryElementFractionsFilter::clone() const
 //------------------------------------------------------------------------------
 IFilter::PreflightResult FindBoundaryElementFractionsFilter::preflightImpl(const DataStructure& dataStructure, const Arguments& filterArgs, const MessageHandler& messageHandler, const std::atomic_bool& shouldCancel) const
 {
-  /****************************************************************************
-   * Write any preflight sanity checking codes in this function
-   ***************************************************************************/
-
-  /**
-   * These are the values that were gathered from the UI or the pipeline file or
-   * otherwise passed into the filter. These are here for your convenience. If you
-   * do not need some of them remove them.
-   */
   auto pFeatureIdsArrayPathValue = filterArgs.value<DataPath>(k_FeatureIdsArrayPath_Key);
   auto pBoundaryCellsArrayPathValue = filterArgs.value<DataPath>(k_BoundaryCellsArrayPath_Key);
   auto pBoundaryCellFractionsArrayPathValue = filterArgs.value<DataPath>(k_BoundaryCellFractionsArrayPath_Key);
 
-
-
-  // Declare the preflightResult variable that will be populated with the results
-  // of the preflight. The PreflightResult type contains the output Actions and
-  // any preflight updated values that you want to be displayed to the user, typically
-  // through a user interface (UI).
   PreflightResult preflightResult;
-
-  // If your filter is making structural changes to the DataStructure then the filter
-  // is going to create OutputActions subclasses that need to be returned. This will
-  // store those actions.
   complex::Result<OutputActions> resultOutputActions;
-
-  // If your filter is going to pass back some `preflight updated values` then this is where you
-  // would create the code to store those values in the appropriate object. Note that we
-  // in line creating the pair (NOT a std::pair<>) of Key:Value that will get stored in
-  // the std::vector<PreflightValue> object.
   std::vector<PreflightValue> preflightUpdatedValues;
-
-  // If the filter needs to pass back some updated values via a key:value string:string set of values
-  // you can declare and update that string here.
-  // None found in this filter based on the filter parameters
 
   // If this filter makes changes to the DataStructure in the form of
   // creating/deleting/moving/renaming DataGroups, Geometries, DataArrays then you
@@ -134,10 +103,6 @@ IFilter::PreflightResult FindBoundaryElementFractionsFilter::preflightImpl(const
     // resultOutputActions.value().actions.push_back(std::move(createArrayAction));
   }
 
-  // Store the preflight updated value(s) into the preflightUpdatedValues vector using
-  // the appropriate methods.
-  // None found based on the filter parameters
-
   // Return both the resultOutputActions and the preflightUpdatedValues via std::move()
   return {std::move(resultOutputActions), std::move(preflightUpdatedValues)};
 }
@@ -145,14 +110,6 @@ IFilter::PreflightResult FindBoundaryElementFractionsFilter::preflightImpl(const
 //------------------------------------------------------------------------------
 Result<> FindBoundaryElementFractionsFilter::executeImpl(DataStructure& dataStructure, const Arguments& filterArgs, const PipelineFilter* pipelineNode, const MessageHandler& messageHandler, const std::atomic_bool& shouldCancel) const
 {
-
-  FindBoundaryElementFractionsInputValues inputValues;
-
-    inputValues.FeatureIdsArrayPath = filterArgs.value<DataPath>(k_FeatureIdsArrayPath_Key);
-  inputValues.BoundaryCellsArrayPath = filterArgs.value<DataPath>(k_BoundaryCellsArrayPath_Key);
-  inputValues.BoundaryCellFractionsArrayPath = filterArgs.value<DataPath>(k_BoundaryCellFractionsArrayPath_Key);
-
-
-  return FindBoundaryElementFractions(dataStructure, messageHandler, shouldCancel, &inputValues)();
+  return {};
 }
 } // namespace complex
