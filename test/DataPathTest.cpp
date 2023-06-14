8ec034039db9e2fb88397a1cde3b488b565be687
nyoungbq <nathan.young@bluequartz.net>
2023-06-14 11:59:51 -0400

    Initial pound out

---
+++ test/DataPathTest.cpp
@@ -0,0 +1,22 @@
+#include <catch2/catch.hpp>
+
+#include "complex/DataStructure/DataPath.hpp"
+
+using namespace complex;
+
+namespace
+{
+const std::vector<std::string> k_CommonStringsVec = {};
+const std::vector<std::string> k_EdgeCaseStringsVec = {};
+const std::vector<std::string> k_InvalidStringsVec = {};
+}
+
+TEST_CASE("Valid DataPath Creation")
+{
+
+}
+
+TEST_CASE("Invalid DataPath Creation")
+{
+
+}
