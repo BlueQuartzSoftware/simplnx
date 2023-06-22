111a97de431cb38714a70e3afb8fb1653e43e468
nyoungbq <nathan.young@bluequartz.net>
2023-06-22 13:24:58 -0400

    Update CMakeLists.txt and GenerateVectorColorsTest.cpp

--- src/Plugins/ComplexCore/test/GenerateVectorColorsTest.cpp
+++ src/Plugins/ComplexCore/test/GenerateVectorColorsTest.cpp
@@ -1,32 +1,40 @@
 #include <catch2/catch.hpp>
 
-#include "complex/Parameters/ArrayCreationParameter.hpp"
-#include "complex/Parameters/ArraySelectionParameter.hpp"
-#include "complex/Parameters/BoolParameter.hpp"
-
 #include "ComplexCore/ComplexCore_test_dirs.hpp"
 #include "ComplexCore/Filters/GenerateVectorColorsFilter.hpp"
 
+#include "complex/UnitTest/UnitTestCommon.hpp"
+
 using namespace complex;
 
-TEST_CASE("ComplexCore::GenerateVectorColorsFilter: Valid Filter Execution", "[ComplexCore][GenerateVectorColorsFilter][.][UNIMPLEMENTED][!mayfail]")
+namespace
 {
-  // Instantiate the filter, a DataStructure object and an Arguments Object
-  GenerateVectorColorsFilter filter;
-  DataStructure ds;
-  Arguments args;
+DataPath ebsdPath = DataPath({Constants::k_SmallIN100, Constants::k_EbsdScanData});
+DataPath eulerAnglesPath = ebsdPath.createChildPath(Constants::k_EulerAngles);
+const std::string k_VecColorsNX = "Vector Colors";
+} // namespace
 
-  // Create default Parameters for the filter.
-  args.insertOrAssign(GenerateVectorColorsFilter::k_UseGoodVoxels_Key, std::make_any<bool>(false));
-  args.insertOrAssign(GenerateVectorColorsFilter::k_VectorsArrayPath_Key, std::make_any<DataPath>(DataPath{}));
-  args.insertOrAssign(GenerateVectorColorsFilter::k_GoodVoxelsArrayPath_Key, std::make_any<DataPath>(DataPath{}));
-  args.insertOrAssign(GenerateVectorColorsFilter::k_CellVectorColorsArrayName_Key, std::make_any<DataPath>(DataPath{}));
+TEST_CASE("ComplexCore::GenerateVectorColorsFilter: Valid Filter Execution", "[ComplexCore][GenerateVectorColorsFilter]")
+{
+  DataStructure dataStructure = UnitTest::LoadDataStructure(fs::path(fmt::format("{}/generate_vector_colors/6_6_generate_vector_colors.dream3d", unit_test::k_TestFilesDir)));
+  {
+    // Instantiate the filter, a DataStructure object and an Arguments Object
+    GenerateVectorColorsFilter filter;
+    Arguments args;
 
-  // Preflight the filter and check result
-  auto preflightResult = filter.preflight(ds, args);
-  REQUIRE(preflightResult.outputActions.valid());
+    // Create default Parameters for the filter.
+    args.insertOrAssign(GenerateVectorColorsFilter::k_UseGoodVoxels_Key, std::make_any<bool>(false));
+    args.insertOrAssign(GenerateVectorColorsFilter::k_VectorsArrayPath_Key, std::make_any<DataPath>(eulerAnglesPath));
+    args.insertOrAssign(GenerateVectorColorsFilter::k_CellVectorColorsArrayName_Key, std::make_any<std::string>(k_VecColorsNX));
 
-  // Execute the filter and check the result
-  auto executeResult = filter.execute(ds, args);
-  REQUIRE(executeResult.result.valid());
+    // Preflight the filter and check result
+    auto preflightResult = filter.preflight(dataStructure, args);
+    REQUIRE(preflightResult.outputActions.valid());
+
+    // Execute the filter and check the result
+    auto executeResult = filter.execute(dataStructure, args);
+    REQUIRE(executeResult.result.valid());
+  }
+
+  UnitTest::CompareArrays<uint8>(dataStructure, ebsdPath.createChildPath("VectorColor"), ebsdPath.createChildPath(k_VecColorsNX));
 }
