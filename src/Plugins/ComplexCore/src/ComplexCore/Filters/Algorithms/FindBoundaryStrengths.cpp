3e8b57f96aa6751a733431e7d2cb6fa92de8f66e
nyoungbq <nathan.young@bluequartz.net>
2023-06-26 08:41:07 -0400

    Update FindBoundaryStrengths.cpp to comply with clang tidy

--- src/Plugins/ComplexCore/src/ComplexCore/Filters/Algorithms/FindBoundaryStrengths.cpp
+++ src/Plugins/ComplexCore/src/ComplexCore/Filters/Algorithms/FindBoundaryStrengths.cpp
@@ -40,36 +40,36 @@ Result<> FindBoundaryStrengths::operator()()
 
   auto& mPrimes = m_DataStructure.getDataRefAs<Float32Array>(m_InputValues->SurfaceMeshmPrimesArrayName);
   auto& f1s = m_DataStructure.getDataRefAs<Float32Array>(m_InputValues->SurfaceMeshF1sArrayName);
-  auto& f1spts = m_DataStructure.getDataRefAs<Float32Array>(m_InputValues->SurfaceMeshF1sptsArrayName);
+  auto& f1sPts = m_DataStructure.getDataRefAs<Float32Array>(m_InputValues->SurfaceMeshF1sptsArrayName);
   auto& f7s = m_DataStructure.getDataRefAs<Float32Array>(m_InputValues->SurfaceMeshF7sArrayName);
 
   usize numTriangles = surfaceMeshFaceLabels.getNumberOfTuples();
 
-  float32 mPrime_1 = 0.0f, mPrime_2 = 0.0f, F1_1 = 0.0f, F1_2 = 0.0f, F1spt_1 = 0.0f, F1spt_2 = 0.0f, F7_1 = 0.0f, F7_2 = 0.0f;
-  int32 gname1 = 0, gname2 = 0;
+  float32 mPrime_1, mPrime_2, F1_1, F1_2, F1spt_1, F1spt_2, F7_1, F7_2;
+  int32 gName1, gName2;
 
   float64 LD[3] = {m_InputValues->Loading[0], m_InputValues->Loading[1], m_InputValues->Loading[2]};
   MatrixMath::Normalize3x1(LD);
 
   for(usize i = 0; i < numTriangles; i++)
   {
-    gname1 = surfaceMeshFaceLabels[i * 2];
-    gname2 = surfaceMeshFaceLabels[i * 2 + 1];
-    if(gname1 > 0 && gname2 > 0)
+    gName1 = surfaceMeshFaceLabels[i * 2];
+    gName2 = surfaceMeshFaceLabels[i * 2 + 1];
+    if(gName1 > 0 && gName2 > 0)
     {
-      QuatD q1(avgQuats[gname1 * 4], avgQuats[gname1 * 4 + 1], avgQuats[gname1 * 4 + 2], avgQuats[gname1 * 4 + 3]);
-      QuatD q2(avgQuats[gname2 * 4], avgQuats[gname2 * 4 + 1], avgQuats[gname2 * 4 + 2], avgQuats[gname2 * 4 + 3]);
+      QuatD q1(avgQuats[gName1 * 4], avgQuats[gName1 * 4 + 1], avgQuats[gName1 * 4 + 2], avgQuats[gName1 * 4 + 3]);
+      QuatD q2(avgQuats[gName2 * 4], avgQuats[gName2 * 4 + 1], avgQuats[gName2 * 4 + 2], avgQuats[gName2 * 4 + 3]);
 
-      if(crystalStructures[featurePhases[gname1]] == crystalStructures[featurePhases[gname2]] && featurePhases[gname1] > 0)
+      if(crystalStructures[featurePhases[gName1]] == crystalStructures[featurePhases[gName2]] && featurePhases[gName1] > 0)
       {
-        mPrime_1 = orientationOps[crystalStructures[featurePhases[gname1]]]->getmPrime(q1, q2, LD);
-        mPrime_2 = orientationOps[crystalStructures[featurePhases[gname1]]]->getmPrime(q2, q1, LD);
-        F1_1 = orientationOps[crystalStructures[featurePhases[gname1]]]->getF1(q1, q2, LD, true);
-        F1_2 = orientationOps[crystalStructures[featurePhases[gname1]]]->getF1(q2, q1, LD, true);
-        F1spt_1 = orientationOps[crystalStructures[featurePhases[gname1]]]->getF1spt(q1, q2, LD, true);
-        F1spt_2 = orientationOps[crystalStructures[featurePhases[gname1]]]->getF1spt(q2, q1, LD, true);
-        F7_1 = orientationOps[crystalStructures[featurePhases[gname1]]]->getF7(q1, q2, LD, true);
-        F7_2 = orientationOps[crystalStructures[featurePhases[gname1]]]->getF7(q2, q1, LD, true);
+        mPrime_1 = static_cast<float32>(orientationOps[crystalStructures[featurePhases[gName1]]]->getmPrime(q1, q2, LD));
+        mPrime_2 = static_cast<float32>(orientationOps[crystalStructures[featurePhases[gName1]]]->getmPrime(q2, q1, LD));
+        F1_1 = static_cast<float32>(orientationOps[crystalStructures[featurePhases[gName1]]]->getF1(q1, q2, LD, true));
+        F1_2 = static_cast<float32>(orientationOps[crystalStructures[featurePhases[gName1]]]->getF1(q2, q1, LD, true));
+        F1spt_1 = static_cast<float32>(orientationOps[crystalStructures[featurePhases[gName1]]]->getF1spt(q1, q2, LD, true));
+        F1spt_2 = static_cast<float32>(orientationOps[crystalStructures[featurePhases[gName1]]]->getF1spt(q2, q1, LD, true));
+        F7_1 = static_cast<float32>(orientationOps[crystalStructures[featurePhases[gName1]]]->getF7(q1, q2, LD, true));
+        F7_2 = static_cast<float32>(orientationOps[crystalStructures[featurePhases[gName1]]]->getF7(q2, q1, LD, true));
       }
       else
       {
@@ -99,8 +99,8 @@ Result<> FindBoundaryStrengths::operator()()
     mPrimes[2 * i + 1] = mPrime_2;
     f1s[2 * i] = F1_1;
     f1s[2 * i + 1] = F1_2;
-    f1spts[2 * i] = F1spt_1;
-    f1spts[2 * i + 1] = F1spt_2;
+    f1sPts[2 * i] = F1spt_1;
+    f1sPts[2 * i + 1] = F1spt_2;
     f7s[2 * i] = F7_1;
     f7s[2 * i + 1] = F7_2;
   }
