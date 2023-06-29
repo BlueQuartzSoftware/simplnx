3e8a1290f29ba20b26b17fd0514f07e42251b4a0
Michael Jackson <mike.jackson@bluequartz.net>
2023-06-28 21:14:25 -0400

    Add code to pick out a specific .ang file from an archive to copy
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- src/Plugins/ComplexCore/test/AbaqusHexahedronWriterTest.cpp
+++ src/Plugins/ComplexCore/test/AbaqusHexahedronWriterTest.cpp
@@ -19,7 +19,7 @@ const DataPath k_FeatureIdsPath = DataPath({Constants::k_DataContainer}).createC
 
 const std::string k_ExemplarDir = fmt::format("{}/abaqus_hexahedron_writer_test", unit_test::k_TestFilesDir);
 
-std::vector<char> readIn(fs::path filePath)
+std::vector<char> readIn(const fs::path& filePath)
 {
   std::ifstream file(filePath.string(), std::ios_base::binary);
 
@@ -27,7 +27,7 @@ std::vector<char> readIn(fs::path filePath)
   {
     // get file size
     file.seekg(0, std::ios::end);
-    std::streampos length = file.tellg();
+    const std::streampos length = file.tellg();
     file.seekg(0, std::ios::beg);
 
     // read whole file into a vector
@@ -42,81 +42,90 @@ std::vector<char> readIn(fs::path filePath)
 
 void CompareResults() // compare hash of both file strings
 {
-  fs::path writtenFilePath = fs::path(std::string(unit_test::k_BinaryTestOutputDir) + "/6_6_Abaqus_Hexahedron_Writer_Test.inp");
+  const fs::path writtenFilePath = fs::path(std::string(unit_test::k_BinaryTestOutputDir) + "/6_6_Abaqus_Hexahedron_Writer_Test.inp");
   REQUIRE(fs::exists(writtenFilePath));
-  fs::path exemplarFilePath = fs::path(k_ExemplarDir + "/6_6_Abaqus_Hexahedron_Writer_Test.inp");
+  const fs::path exemplarFilePath = fs::path(k_ExemplarDir + "/6_6_Abaqus_Hexahedron_Writer_Test.inp");
   REQUIRE(fs::exists(exemplarFilePath));
   REQUIRE(readIn(writtenFilePath) == readIn(exemplarFilePath));
-  fs::path writtenFilePath2 = fs::path(std::string(unit_test::k_BinaryTestOutputDir) + "/6_6_Abaqus_Hexahedron_Writer_Test_elems.inp");
+  const fs::path writtenFilePath2 = fs::path(std::string(unit_test::k_BinaryTestOutputDir) + "/6_6_Abaqus_Hexahedron_Writer_Test_elems.inp");
   REQUIRE(fs::exists(writtenFilePath2));
-  fs::path exemplarFilePath2 = fs::path(k_ExemplarDir + "/6_6_Abaqus_Hexahedron_Writer_Test_elems.inp");
+  const fs::path exemplarFilePath2 = fs::path(k_ExemplarDir + "/6_6_Abaqus_Hexahedron_Writer_Test_elems.inp");
   REQUIRE(fs::exists(exemplarFilePath2));
   REQUIRE(readIn(writtenFilePath2) == readIn(exemplarFilePath2));
-  fs::path writtenFilePath3 = fs::path(std::string(unit_test::k_BinaryTestOutputDir) + "/6_6_Abaqus_Hexahedron_Writer_Test_elset.inp");
+  const fs::path writtenFilePath3 = fs::path(std::string(unit_test::k_BinaryTestOutputDir) + "/6_6_Abaqus_Hexahedron_Writer_Test_elset.inp");
   REQUIRE(fs::exists(writtenFilePath3));
-  fs::path exemplarFilePath3 = fs::path(k_ExemplarDir + "/6_6_Abaqus_Hexahedron_Writer_Test_elset.inp");
+  const fs::path exemplarFilePath3 = fs::path(k_ExemplarDir + "/6_6_Abaqus_Hexahedron_Writer_Test_elset.inp");
   REQUIRE(fs::exists(exemplarFilePath3));
   REQUIRE(readIn(writtenFilePath3) == readIn(exemplarFilePath3));
-  fs::path writtenFilePath4 = fs::path(std::string(unit_test::k_BinaryTestOutputDir) + "/6_6_Abaqus_Hexahedron_Writer_Test_nodes.inp");
+  const fs::path writtenFilePath4 = fs::path(std::string(unit_test::k_BinaryTestOutputDir) + "/6_6_Abaqus_Hexahedron_Writer_Test_nodes.inp");
   REQUIRE(fs::exists(writtenFilePath4));
-  fs::path exemplarFilePath4 = fs::path(k_ExemplarDir + "/6_6_Abaqus_Hexahedron_Writer_Test_nodes.inp");
+  const fs::path exemplarFilePath4 = fs::path(k_ExemplarDir + "/6_6_Abaqus_Hexahedron_Writer_Test_nodes.inp");
   REQUIRE(fs::exists(exemplarFilePath4));
   REQUIRE(readIn(writtenFilePath4) == readIn(exemplarFilePath4));
-  fs::path writtenFilePath5 = fs::path(std::string(unit_test::k_BinaryTestOutputDir) + "/6_6_Abaqus_Hexahedron_Writer_Test_sects.inp");
+  const fs::path writtenFilePath5 = fs::path(std::string(unit_test::k_BinaryTestOutputDir) + "/6_6_Abaqus_Hexahedron_Writer_Test_sects.inp");
   REQUIRE(fs::exists(writtenFilePath5));
-  fs::path exemplarFilePath5 = fs::path(k_ExemplarDir + "/6_6_Abaqus_Hexahedron_Writer_Test_sects.inp");
+  const fs::path exemplarFilePath5 = fs::path(k_ExemplarDir + "/6_6_Abaqus_Hexahedron_Writer_Test_sects.inp");
   REQUIRE(fs::exists(exemplarFilePath5));
   REQUIRE(readIn(writtenFilePath5) == readIn(exemplarFilePath5));
 }
 } // namespace
 
+/**
+ * @brief This class will decompress a tar.gz file using the locally installed copy of cmake and when
+ * then class goes out of scope the extracted contents will be deleted from disk.
+ */
 class TestFileSentinel
 {
 public:
-  TestFileSentinel(const std::string& inputArchiveName, const std::string& expectedTopLevelOutput)
-      : m_InputArchiveName(inputArchiveName)
-  , m_ExpectedTopLevelOutput(expectedTopLevelOutput)
+  TestFileSentinel(std::string inputArchiveName, std::string expectedTopLevelOutput)
+  : m_InputArchiveName(std::move(inputArchiveName))
+  , m_ExpectedTopLevelOutput(std::move(expectedTopLevelOutput))
   {
+    const int result = decompress();
+    REQUIRE(result == 0);
   }
+
   ~TestFileSentinel()
   {
-    const std::string k_RemoveFileCommand = fmt::format(R"(cd "{}" && "{}" -E rm -rf "{}/{}")", unit_test::k_TestFilesDir, unit_test::k_CMakeExecutable, unit_test::k_TestFilesDir, m_ExpectedTopLevelOutput);
-    const int result = std::system(k_RemoveFileCommand.c_str());
+    const std::string kRemoveFileCommand =
+        fmt::format(R"(cd "{}" && "{}" -E rm -rf "{}/{}")", unit_test::k_TestFilesDir, unit_test::k_CMakeExecutable, unit_test::k_TestFilesDir, m_ExpectedTopLevelOutput);
+    const int result = std::system(kRemoveFileCommand.c_str());
     if(result != 0)
     {
-      std::cout << "Removing decompressed data failed. The command was:\n  " << k_RemoveFileCommand << std::endl;
+      std::cout << "Removing decompressed data failed. The command was:\n  " << kRemoveFileCommand << std::endl;
     }
   }
 
+  TestFileSentinel(const TestFileSentinel&) = delete;            // Copy Constructor Not Implemented
+  TestFileSentinel(TestFileSentinel&&) = delete;                 // Move Constructor Not Implemented
+  TestFileSentinel& operator=(const TestFileSentinel&) = delete; // Copy Assignment Not Implemented
+  TestFileSentinel& operator=(TestFileSentinel&&) = delete;      // Move Assignment Not Implemented
+
   int decompress()
   {
-    const std::string k_DecompressCommand = fmt::format(R"(cd "{}" && "{}" -E tar xvzf "{}/{}")", unit_test::k_TestFilesDir, unit_test::k_CMakeExecutable, unit_test::k_TestFilesDir,m_InputArchiveName  );
-    return  std::system(k_DecompressCommand.c_str());
+    const std::string kDecompressCommand =
+        fmt::format(R"(cd "{}" && "{}" -E tar xvzf "{}/{}")", unit_test::k_TestFilesDir, unit_test::k_CMakeExecutable, unit_test::k_TestFilesDir, m_InputArchiveName);
+    return std::system(kDecompressCommand.c_str());
   }
 
 private:
-  const std::string m_InputArchiveName;
-  const std::string m_ExpectedTopLevelOutput;
+  std::string m_InputArchiveName;
+  std::string m_ExpectedTopLevelOutput;
 };
 
 TEST_CASE("ComplexCore::AbaqusHexahedronWriterFilter: Valid Filter Execution", "[ComplexCore][AbaqusHexahedronWriterFilter]")
 {
 
-  const std::string k_DataInputArchive = "6_6_find_feature_centroids.tar.gz";
-  const std::string k_ExpectedOutputTopLevel = "6_6_find_feature_centroids.dream3d";
-  TestFileSentinel testDataSentinel(k_DataInputArchive, k_ExpectedOutputTopLevel);
-  int result = testDataSentinel.decompress();
-  REQUIRE(result == 0);
+  const std::string kDataInputArchive = "6_6_find_feature_centroids.tar.gz";
+  const std::string kExpectedOutputTopLevel = "6_6_find_feature_centroids.dream3d";
+  const TestFileSentinel testDataSentinel(kDataInputArchive, kExpectedOutputTopLevel);
 
-  const std::string k_DataInputArchive1 = "abaqus_hexahedron_writer_test.tar.gz";
-  const std::string k_ExpectedOutputTopLevel1 = "abaqus_hexahedron_writer_test";
-  TestFileSentinel testDataSentinel1(k_DataInputArchive1, k_ExpectedOutputTopLevel1);
-  result = testDataSentinel1.decompress();
-  REQUIRE(result == 0);
-
+  const std::string kDataInputArchive1 = "abaqus_hexahedron_writer_test.tar.gz";
+  const std::string kExpectedOutputTopLevel1 = "abaqus_hexahedron_writer_test";
+  const TestFileSentinel testDataSentinel1(kDataInputArchive1, kExpectedOutputTopLevel1);
 
   // Instantiate the filter, a DataStructure object and an Arguments Object
-  AbaqusHexahedronWriterFilter filter;
+  const AbaqusHexahedronWriterFilter filter;
   DataStructure dataStructure = UnitTest::LoadDataStructure(fs::path(fmt::format("{}/6_6_find_feature_centroids.dream3d", unit_test::k_TestFilesDir)));
   Arguments args;
 
@@ -137,6 +146,4 @@ TEST_CASE("ComplexCore::AbaqusHexahedronWriterFilter: Valid Filter Execution", "
   REQUIRE(executeResult.result.valid());
 
   ::CompareResults();
-
-
 }
