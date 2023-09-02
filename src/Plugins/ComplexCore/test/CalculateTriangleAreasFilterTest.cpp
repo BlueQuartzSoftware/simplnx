e51d69f0026922bd9bd344bddfb9acbd9a137692
Michael Jackson <mike.jackson@bluequartz.net>
2023-09-02 07:46:34 -0400

    ENH: Changed STLFileReader filter to allow user setting of FaceNormals. (#680)
    * ENH: Changed STLFileReader filter to allow user setting of FaceNormals.
    
    * remove ability to set the SharedVertexList and SharedTriangleList names. Those
    should stay constant where possible.
    * The parameter key values also changed which may mess up some pipelines and the
    python bindings.
    
    ---------
    
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- src/Plugins/ComplexCore/test/CalculateTriangleAreasFilterTest.cpp
+++ src/Plugins/ComplexCore/test/CalculateTriangleAreasFilterTest.cpp
@@ -36,7 +36,7 @@ TEST_CASE("ComplexCore::CalculateTriangleAreasFilter", "[ComplexCore][CalculateT
 
     // Create default Parameters for the filter.
     args.insertOrAssign(StlFileReaderFilter::k_StlFilePath_Key, std::make_any<FileSystemPathParameter::ValueType>(fs::path(inputFile)));
-    args.insertOrAssign(StlFileReaderFilter::k_GeometryDataPath_Key, std::make_any<DataPath>(triangleGeomDataPath));
+    args.insertOrAssign(StlFileReaderFilter::k_TriangleGeometryName_Key, std::make_any<DataPath>(triangleGeomDataPath));
 
     // Preflight the filter and check result
     auto preflightResult = filter.preflight(dataStructure, args);
