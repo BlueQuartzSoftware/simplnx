2b0567c026d433948f932586dba6ad8b25871573
Jared Duffey <jared.duffey@bluequartz.net>
2023-09-12 15:35:55 -0400

    Minor cleanup to fix_pyi_file.py
    Signed-off-by: Jared Duffey <jared.duffey@bluequartz.net>

--- wrapping/python/fix_pyi_file.py
+++ wrapping/python/fix_pyi_file.py
@@ -1,5 +1,4 @@
 import argparse
-import uuid
 
 from pathlib import Path
 
@@ -9,16 +8,13 @@ def main() -> None:
 
     args = parser.parse_args()
 
-    input_file_path = f'{args.input_file}'
-    file_contents: str
-    with open(input_file_path, 'r') as input_file:
+    input_file_path: Path = args.input_file
+    with open(input_file_path, 'r+') as input_file:
         file_contents = input_file.read()
         file_contents = file_contents.replace('from typing import Any, Callable, ClassVar, Iterator, List, Optional, Tuple', 'from typing import Any, Callable, ClassVar, Iterator, List, Optional, Set, Tuple')
         file_contents = file_contents.replace('def load_python_plugin(arg0: module_) -> None: ...', 'def load_python_plugin(arg0: Any) -> None: ...')
-
-    with open(input_file_path, 'w') as cpp_file:
-        cpp_file.write(file_contents)
-
+        input_file.seek(0)
+        input_file.write(file_contents)
 
 if __name__ == '__main__':
-  main()
+    main()
