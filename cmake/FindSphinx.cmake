7e3ac6abf303f5bff0995e8d8505166a80f494fe
Michael Jackson <mike.jackson@bluequartz.net>
2023-10-23 22:23:03 -0400

    PY: Use SPHINX_BUILD_EXECUTABLE instead of SPHINX_EXECUTABLE
    It is more accurate and is consistent with DREAM3DNX
    
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- cmake/FindSphinx.cmake
+++ cmake/FindSphinx.cmake
@@ -1,8 +1,9 @@
-find_program(SPHINX_EXECUTABLE
-  NAMES sphinx-build
-  DOC "Path to sphinx-build executable"
-)
+get_filename_component(PYTHON_EXE_PATH "${Python3_EXECUTABLE}" DIRECTORY CACHE)
+set(SPHINX_PYTHON_EXECUTABLE "${Python3_EXECUTABLE}")
+find_program(SPHINX_BUILD_EXECUTABLE sphinx-build
+    NAMES sphinx-build sphinx-build.exe
+    PATHS "${PYTHON_EXE_PATH};${PYTHON_EXE_PATH}/Scripts")
 
 include(FindPackageHandleStandardArgs)
 
-find_package_handle_standard_args(Sphinx "Failed to find sphinx-build executable" SPHINX_EXECUTABLE)
+find_package_handle_standard_args(Sphinx "Failed to find sphinx-build executable" SPHINX_BUILD_EXECUTABLE)
