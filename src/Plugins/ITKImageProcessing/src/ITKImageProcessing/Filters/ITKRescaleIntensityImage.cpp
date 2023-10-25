640e9c3203cbfde01bb0c99129015455615b692b
Matthew Marine <matthew.marine@bluequartz.net>
2023-10-25 14:37:56 -0400

    Finished ITK conversion
    * Added Rescale Intensity Image
    * Added ITK Legacy UUID Mapping

--- src/Plugins/ITKImageProcessing/src/ITKImageProcessing/Filters/ITKRescaleIntensityImage.cpp
+++ src/Plugins/ITKImageProcessing/src/ITKImageProcessing/Filters/ITKRescaleIntensityImage.cpp
@@ -8,6 +8,8 @@
 #include "complex/Parameters/GeometrySelectionParameter.hpp"
 #include "complex/Parameters/NumberParameter.hpp"
 
+#include "complex/Utilities/SIMPLConversion.hpp"
+
 #include <itkRescaleIntensityImageFilter.h>
 
 using namespace complex;
@@ -127,5 +129,35 @@ Result<> ITKRescaleIntensityImage::executeImpl(DataStructure& dataStructure, con
   imageGeom.getLinkedGeometryData().addCellData(outputArrayPath);
 
   return ITK::Execute<cxITKRescaleIntensityImage::ArrayOptionsType>(dataStructure, selectedInputArray, imageGeomPath, outputArrayPath, itkFunctor, shouldCancel);
+}
+
+namespace
+{
+namespace SIMPL
+{
+constexpr StringLiteral k_OutputTypeKey = "OutputType";
+constexpr StringLiteral k_OutputMinimumKey = "OutputMinimum";
+constexpr StringLiteral k_OutputMaximumKey = "OutputMaximum";
+constexpr StringLiteral k_SelectedCellArrayPathKey = "SelectedCellArrayPath";
+constexpr StringLiteral k_NewCellArrayNameKey = "NewCellArrayName";
+} // namespace SIMPL
+} // namespace
+
+Result<Arguments> ITKRescaleIntensityImage::FromSIMPLJson(const nlohmann::json& json)
+{
+  Arguments args = ITKRescaleIntensityImage().getDefaultArguments();
+
+  std::vector<Result<>> results;
+
+  // results.push_back(SIMPLConversion::ConvertParameter<SIMPLConversion::ChoiceFilterParameterConverter>(args, json, SIMPL::k_OutputTypeKey, "@COMPLEX_PARAMETER_KEY@"));
+  results.push_back(SIMPLConversion::ConvertParameter<SIMPLConversion::DoubleFilterParameterConverter>(args, json, SIMPL::k_OutputMinimumKey, k_OutputMinimum_Key));
+  results.push_back(SIMPLConversion::ConvertParameter<SIMPLConversion::DoubleFilterParameterConverter>(args, json, SIMPL::k_OutputMaximumKey, k_OutputMaximum_Key));
+  results.push_back(SIMPLConversion::ConvertParameter<SIMPLConversion::DataContainerSelectionFilterParameterConverter>(args, json, SIMPL::k_SelectedCellArrayPathKey, k_SelectedImageGeomPath_Key));
+  results.push_back(SIMPLConversion::ConvertParameter<SIMPLConversion::DataArraySelectionFilterParameterConverter>(args, json, SIMPL::k_SelectedCellArrayPathKey, k_SelectedImageDataPath_Key));
+  results.push_back(SIMPLConversion::ConvertParameter<SIMPLConversion::StringFilterParameterConverter>(args, json, SIMPL::k_NewCellArrayNameKey, k_OutputImageDataPath_Key));
+
+  Result<> conversionResult = MergeResults(std::move(results));
+
+  return ConvertResultTo<Arguments>(std::move(conversionResult), std::move(args));
 }
 } // namespace complex
