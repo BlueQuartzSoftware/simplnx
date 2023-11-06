69ac9edf52f6764d0689f9b3341870b5b956790b
Alex Jackson <alex.jackson@bluequartz.net>
2023-11-06 16:06:45 -0500

    Pipelines Modified

--- wrapping/python/examples/Example_Pipelines/CombineSTLFiles.py
+++ wrapping/python/examples/Example_Pipelines/CombineSTLFiles.py
@@ -8,25 +8,38 @@ import numpy as np
 #Create a Data Structure
 data_structure = cx.DataStructure()
 
-#Filter 1
-
-result = cx.CombineStlFilesFilter.execute(
+# Filter 1
+# Instantiate Filter 
+filter = cx.CombineStlFilesFilter()
+# Execute Filter with Parameters
+result = filter.execute(
     data_structure=data_structure,
     face_attribute_matrix_name="Face Data",
     face_normals_array_name="Face Normals",
-    stl_files_path=cx.DataPath("Data/STL_Models"),
+    stl_files_path="Data/STL_Models",
     triangle_data_container_name=cx.DataPath("TriangleGeometry"),
     vertex_attribute_matrix_name="Vertex Data"
 )
+if len(result.warnings) !=0:
+    print(f'{filter.name()} Warnings: {result.warnings}')
+if len(result.errors) != 0:
+    print(f'{filter.name()} Errors: {result.errors}')
+    quit()
+else:
+    print(f"{filter.name()} No errors running the filter")
 
-#Filter 2
-
+# Filter 2
+# Instantiate Filter 
+filter = cx.ExportDREAM3DFilter()
+# Execute Filter with Parameters
 output_file_path = "Data/Output/CombinedStlFiles.dream3d"
-result = cx.ExportDREAM3DFilter.execute(data_structure=data_structure, 
+result = filter.execute(data_structure=data_structure, 
                                         export_file_path=output_file_path, 
                                         write_xdmf_file=True)
+if len(result.warnings) !=0:
+    print(f'{filter.name()} Warnings: {result.warnings}')
 if len(result.errors) != 0:
-    print('Errors: {}', result.errors)
-    print('Warnings: {}', result.warnings)
+    print(f'{filter.name()} Errors: {result.errors}')
+    quit()
 else:
-    print("No errors running the filter")
+    print(f"{filter.name()} No errors running the filter")
--- wrapping/python/examples/Example_Pipelines/Read_Write_File.py
+++ wrapping/python/examples/Example_Pipelines/Read_Write_File.py
@@ -59,3 +59,83 @@ if len(result.errors) != 0:
     print('Warnings: {}', result.warnings)
 else:
     print("No errors running the MultiThresholdObjects")
+
+
+
+
+#Copy Paste New Exit Command to All Filters
+
+# Filter 1
+# Instantiate Filter 
+filter = cx.CombineStlFilesFilter()
+# Execute Filter with Parameters
+result = filter.execute(
+    data_structure=data_structure,
+    face_attribute_matrix_name="Face Data",
+    face_normals_array_name="Face Normals",
+    stl_files_path="Data/STL_Models",
+    triangle_data_container_name=cx.DataPath("TriangleGeometry"),
+    vertex_attribute_matrix_name="Vertex Data"
+)
+if len(result.warnings) !=0:
+    print(f'{filter.name()} Warnings: {result.warnings}')
+if len(result.errors) != 0:
+    print(f'{filter.name()} Errors: {result.errors}')
+    quit()
+else:
+    print(f"{filter.name()} No errors running the filter")
+
+#Change PathLike to Strings "blahblah",
+#Copy Paste New Exit Command
+#Change Result = Filter.Execute
+
+
+
+
+#Filter Example
+
+import complex as cx
+
+import itkimageprocessing as cxitk
+import orientationanalysis as cxor
+
+import numpy as np
+
+#Create a Data Structure
+data_structure = cx.DataStructure()
+
+# Filter 1
+# Instantiate Filter 
+filter = cx.CombineStlFilesFilter()
+# Execute Filter with Parameters
+result = filter.execute(
+    data_structure=data_structure,
+    face_attribute_matrix_name="Face Data",
+    face_normals_array_name="Face Normals",
+    stl_files_path="Data/STL_Models",
+    triangle_data_container_name=cx.DataPath("TriangleGeometry"),
+    vertex_attribute_matrix_name="Vertex Data"
+)
+if len(result.warnings) !=0:
+    print(f'{filter.name()} Warnings: {result.warnings}')
+if len(result.errors) != 0:
+    print(f'{filter.name()} Errors: {result.errors}')
+    quit()
+else:
+    print(f"{filter.name()} No errors running the filter")
+
+# Filter 2
+# Instantiate Filter 
+filter = cx.ExportDREAM3DFilter()
+# Execute Filter with Parameters
+output_file_path = "Data/Output/CombinedStlFiles.dream3d"
+result = filter.execute(data_structure=data_structure, 
+                                        export_file_path=output_file_path, 
+                                        write_xdmf_file=True)
+if len(result.warnings) !=0:
+    print(f'{filter.name()} Warnings: {result.warnings}')
+if len(result.errors) != 0:
+    print(f'{filter.name()} Errors: {result.errors}')
+    quit()
+else:
+    print(f"{filter.name()} No errors running the filter")
