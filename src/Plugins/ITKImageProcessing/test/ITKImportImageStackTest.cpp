de9204f0955c0a81eb3d74a1fb5f413893d672ea
nyoungbq <nathan.young@bluequartz.net>
2024-01-12 17:00:02 -0500

    Initial test updates

--- src/Plugins/ITKImageProcessing/test/ITKImportImageStackTest.cpp
+++ src/Plugins/ITKImageProcessing/test/ITKImportImageStackTest.cpp
@@ -20,6 +20,46 @@ namespace
 const std::string k_ImageStackDir = unit_test::k_DataDir.str() + "/ImageStack";
 const DataPath k_ImageGeomPath = {{"ImageGeometry"}};
 const DataPath k_ImageDataPath = k_ImageGeomPath.createChildPath(ImageGeom::k_CellDataName).createChildPath("ImageData");
+
+void FillFlippedArgs(Arguments& args)
+{
+  std::vector<float32> origin = {1.0f, 4.0f, 8.0f};
+  std::vector<float32> spacing = {0.3f, 0.2f, 0.9f};
+
+  args.insertOrAssign(ITKImportImageStack::k_Origin_Key, std::make_any<std::vector<float32>>(origin));
+  args.insertOrAssign(ITKImportImageStack::k_Spacing_Key, std::make_any<std::vector<float32>>(spacing));
+  args.insertOrAssign(ITKImportImageStack::k_ImageGeometryPath_Key, std::make_any<DataPath>(k_ImageGeomPath));
+}
+
+Result<> CompareFlippedGeometries(const ImageGeom* baseGeom, const ImageGeom* xFlipGeom, const ImageGeom* yFlipGeom)
+{
+  Result<> result = {};
+
+  REQUIRE(imageGeom != nullptr);
+
+  SizeVec3 imageDims = imageGeom->getDimensions();
+  FloatVec3 imageOrigin = imageGeom->getOrigin();
+  FloatVec3 imageSpacing = imageGeom->getSpacing();
+
+  std::array<usize, 3> dims = {524, 390, 3};
+
+  REQUIRE(imageDims[0] == dims[0]);
+  REQUIRE(imageDims[1] == dims[1]);
+  REQUIRE(imageDims[2] == dims[2]);
+
+  REQUIRE(imageOrigin[0] == Approx(origin[0]));
+  REQUIRE(imageOrigin[1] == Approx(origin[1]));
+  REQUIRE(imageOrigin[2] == Approx(origin[2]));
+
+  REQUIRE(imageSpacing[0] == Approx(spacing[0]));
+  REQUIRE(imageSpacing[1] == Approx(spacing[1]));
+  REQUIRE(imageSpacing[2] == Approx(spacing[2]));
+
+  const auto* imageData = dataStructure.getDataAs<UInt8Array>(k_ImageDataPath);
+  REQUIRE(imageData != nullptr);
+
+  return result;
+}
 } // namespace
 
 TEST_CASE("ITKImageProcessing::ITKImportImageStack: NoInput", "[ITKImageProcessing][ITKImportImageStack]")
@@ -153,4 +193,128 @@ TEST_CASE("ITKImageProcessing::ITKImportImageStack: CompareImage", "[ITKImagePro
 
   const std::string md5Hash = ITKTestBase::ComputeMd5Hash(dataStructure, k_ImageDataPath);
   REQUIRE(md5Hash == "2620b39f0dcaa866602c2591353116a4");
+}
+
+TEST_CASE("ITKImageProcessing::ITKImportImageStack: Flipped Image Even-Even X/Y", "[ITKImageProcessing][ITKImportImageStack]")
+{
+  ITKImportImageStack filter;
+  DataStructure dataStructure;
+  Arguments args;
+
+  GeneratedFileListParameter::ValueType fileListInfo;
+  fileListInfo.inputPath = k_ImageStackDir;
+  fileListInfo.startIndex = 11;
+  fileListInfo.endIndex = 13;
+  fileListInfo.incrementIndex = 1;
+  fileListInfo.fileExtension = ".tif";
+  fileListInfo.filePrefix = "slice_";
+  fileListInfo.fileSuffix = "";
+  fileListInfo.paddingDigits = 2;
+  fileListInfo.ordering = GeneratedFileListParameter::Ordering::LowToHigh;
+
+  args.insertOrAssign(ITKImportImageStack::k_InputFileListInfo_Key, std::make_any<GeneratedFileListParameter::ValueType>(fileListInfo));
+  ::FillFlippedArgs(args);
+
+  auto preflightResult = filter.preflight(dataStructure, args);
+  SIMPLNX_RESULT_REQUIRE_VALID(preflightResult.outputActions)
+
+  auto executeResult = filter.execute(dataStructure, args);
+  SIMPLNX_RESULT_REQUIRE_VALID(executeResult.result)
+
+  auto compareResult =
+      ::CompareFlippedGeometries(dataStructure.getDataAs<ImageGeom>(k_BaseGeomPath), dataStructure.getDataAs<ImageGeom>(k_XFlippedGeomPath), dataStructure.getDataAs<ImageGeom>(k_YFlippedGeomPath));
+  SIMPLNX_RESULT_REQUIRE_VALID(compareResult.result)
+}
+
+TEST_CASE("ITKImageProcessing::ITKImportImageStack: Flipped Image Even-Odd X/Y", "[ITKImageProcessing][ITKImportImageStack]")
+{
+  ITKImportImageStack filter;
+  DataStructure dataStructure;
+  Arguments args;
+
+  GeneratedFileListParameter::ValueType fileListInfo;
+  fileListInfo.inputPath = k_ImageStackDir;
+  fileListInfo.startIndex = 11;
+  fileListInfo.endIndex = 13;
+  fileListInfo.incrementIndex = 1;
+  fileListInfo.fileExtension = ".tif";
+  fileListInfo.filePrefix = "slice_";
+  fileListInfo.fileSuffix = "";
+  fileListInfo.paddingDigits = 2;
+  fileListInfo.ordering = GeneratedFileListParameter::Ordering::LowToHigh;
+
+  args.insertOrAssign(ITKImportImageStack::k_InputFileListInfo_Key, std::make_any<GeneratedFileListParameter::ValueType>(fileListInfo));
+  ::FillFlippedArgs(args);
+
+  auto preflightResult = filter.preflight(dataStructure, args);
+  SIMPLNX_RESULT_REQUIRE_VALID(preflightResult.outputActions)
+
+  auto executeResult = filter.execute(dataStructure, args);
+  SIMPLNX_RESULT_REQUIRE_VALID(executeResult.result)
+
+  auto compareResult =
+      ::CompareFlippedGeometries(dataStructure.getDataAs<ImageGeom>(k_BaseGeomPath), dataStructure.getDataAs<ImageGeom>(k_XFlippedGeomPath), dataStructure.getDataAs<ImageGeom>(k_YFlippedGeomPath));
+  SIMPLNX_RESULT_REQUIRE_VALID(compareResult.result)
+}
+
+TEST_CASE("ITKImageProcessing::ITKImportImageStack: Flipped Image Odd-Even X/Y", "[ITKImageProcessing][ITKImportImageStack]")
+{
+  ITKImportImageStack filter;
+  DataStructure dataStructure;
+  Arguments args;
+
+  GeneratedFileListParameter::ValueType fileListInfo;
+  fileListInfo.inputPath = k_ImageStackDir;
+  fileListInfo.startIndex = 11;
+  fileListInfo.endIndex = 13;
+  fileListInfo.incrementIndex = 1;
+  fileListInfo.fileExtension = ".tif";
+  fileListInfo.filePrefix = "slice_";
+  fileListInfo.fileSuffix = "";
+  fileListInfo.paddingDigits = 2;
+  fileListInfo.ordering = GeneratedFileListParameter::Ordering::LowToHigh;
+
+  args.insertOrAssign(ITKImportImageStack::k_InputFileListInfo_Key, std::make_any<GeneratedFileListParameter::ValueType>(fileListInfo));
+  ::FillFlippedArgs(args);
+
+  auto preflightResult = filter.preflight(dataStructure, args);
+  SIMPLNX_RESULT_REQUIRE_VALID(preflightResult.outputActions)
+
+  auto executeResult = filter.execute(dataStructure, args);
+  SIMPLNX_RESULT_REQUIRE_VALID(executeResult.result)
+
+  auto compareResult =
+      ::CompareFlippedGeometries(dataStructure.getDataAs<ImageGeom>(k_BaseGeomPath), dataStructure.getDataAs<ImageGeom>(k_XFlippedGeomPath), dataStructure.getDataAs<ImageGeom>(k_YFlippedGeomPath));
+  SIMPLNX_RESULT_REQUIRE_VALID(compareResult.result)
+}
+
+TEST_CASE("ITKImageProcessing::ITKImportImageStack: Flipped Image Odd-Odd X/Y", "[ITKImageProcessing][ITKImportImageStack]")
+{
+  ITKImportImageStack filter;
+  DataStructure dataStructure;
+  Arguments args;
+
+  GeneratedFileListParameter::ValueType fileListInfo;
+  fileListInfo.inputPath = k_ImageStackDir;
+  fileListInfo.startIndex = 11;
+  fileListInfo.endIndex = 13;
+  fileListInfo.incrementIndex = 1;
+  fileListInfo.fileExtension = ".tif";
+  fileListInfo.filePrefix = "slice_";
+  fileListInfo.fileSuffix = "";
+  fileListInfo.paddingDigits = 2;
+  fileListInfo.ordering = GeneratedFileListParameter::Ordering::LowToHigh;
+
+  args.insertOrAssign(ITKImportImageStack::k_InputFileListInfo_Key, std::make_any<GeneratedFileListParameter::ValueType>(fileListInfo));
+  ::FillFlippedArgs(args);
+
+  auto preflightResult = filter.preflight(dataStructure, args);
+  SIMPLNX_RESULT_REQUIRE_VALID(preflightResult.outputActions)
+
+  auto executeResult = filter.execute(dataStructure, args);
+  SIMPLNX_RESULT_REQUIRE_VALID(executeResult.result)
+
+  auto compareResult =
+      ::CompareFlippedGeometries(dataStructure.getDataAs<ImageGeom>(k_BaseGeomPath), dataStructure.getDataAs<ImageGeom>(k_XFlippedGeomPath), dataStructure.getDataAs<ImageGeom>(k_YFlippedGeomPath));
+  SIMPLNX_RESULT_REQUIRE_VALID(compareResult.result)
 }
