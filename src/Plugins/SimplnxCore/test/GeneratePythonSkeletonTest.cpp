ba184037d433fb59b869429cb60f41e267097736
Joey Kleingers <joey.kleingers@bluequartz.net>
2024-01-15 14:27:13 -0500

    Update unit test.
    Signed-off-by: Joey Kleingers <joey.kleingers@bluequartz.net>

--- src/Plugins/SimplnxCore/test/GeneratePythonSkeletonTest.cpp
+++ src/Plugins/SimplnxCore/test/GeneratePythonSkeletonTest.cpp
@@ -2,12 +2,9 @@
 
 #include "SimplnxCore/Filters/GeneratePythonSkeletonFilter.hpp"
 #include "SimplnxCore/SimplnxCore_test_dirs.hpp"
-
+//
 #include "simplnx/Parameters/ArrayCreationParameter.hpp"
 #include "simplnx/Parameters/FileSystemPathParameter.hpp"
-#include "simplnx/UnitTest/UnitTestCommon.hpp"
-#include "simplnx/Utilities/ColorPresetsUtilities.hpp"
-#include "simplnx/Utilities/StringUtilities.hpp"
 
 namespace fs = std::filesystem;
 using namespace nx::core;
@@ -22,23 +19,47 @@ TEST_CASE("SimplnxCore::GeneratePythonSkeleton")
     GeneratePythonSkeletonFilter filter;
     Arguments args;
 
+    const std::filesystem::path k_OutputDirectory = fs::path(unit_test::k_BinaryTestOutputDir);
     const std::string k_PluginName = "ExampleToolbox";
-    const std::string k_PluginHumanName = "Example Toolbox";
-    const std::filesystem::path k_OutputDirectory = {unit_test::k_BinaryTestOutputDir};
-    const std::string k_FilterNames = "SomeFilter,OtherFilter,ThirdFilter";
 
-    // Create default Parameters for the filter.
-    args.insertOrAssign(GeneratePythonSkeletonFilter::k_PluginName_Key, std::make_any<std::string>(k_PluginName));
-    args.insertOrAssign(GeneratePythonSkeletonFilter::k_PluginHumanName_Key, std::make_any<std::string>(k_PluginHumanName));
-    args.insertOrAssign(GeneratePythonSkeletonFilter::k_PluginOutputDirectory_Key, std::make_any<FileSystemPathParameter::ValueType>("/Users/mjackson/Workspace1/simplnx/wrapping/python/plugins"));
-    args.insertOrAssign(GeneratePythonSkeletonFilter::k_PluginFilterNames, std::make_any<std::string>(k_FilterNames));
+    // Generate filters into a new plugin
+    {
+      const std::string k_PluginHumanName = "Example Toolbox";
+      const std::string k_FilterNames = "FirstFilter,SecondFilter";
 
-    // Preflight the filter and check result
-    auto preflightResult = filter.preflight(dataStructure, args);
-    REQUIRE(preflightResult.outputActions.valid());
+      // Create default Parameters for the filter.
+      args.insertOrAssign(GeneratePythonSkeletonFilter::k_UseExistingPlugin_Key, false);
+      args.insertOrAssign(GeneratePythonSkeletonFilter::k_PluginName_Key, std::make_any<std::string>(k_PluginName));
+      args.insertOrAssign(GeneratePythonSkeletonFilter::k_PluginHumanName_Key, std::make_any<std::string>(k_PluginHumanName));
+      args.insertOrAssign(GeneratePythonSkeletonFilter::k_PluginOutputDirectory_Key, std::make_any<FileSystemPathParameter::ValueType>(k_OutputDirectory.string()));
+      args.insertOrAssign(GeneratePythonSkeletonFilter::k_PluginFilterNames, std::make_any<std::string>(k_FilterNames));
 
-    // Execute the filter and check the result
-    auto executeResult = filter.execute(dataStructure, args);
-    REQUIRE(executeResult.result.valid());
+      // Preflight the filter and check result
+      auto preflightResult = filter.preflight(dataStructure, args);
+      REQUIRE(preflightResult.outputActions.valid());
+
+      // Execute the filter and check the result
+      auto executeResult = filter.execute(dataStructure, args);
+      REQUIRE(executeResult.result.valid());
+    }
+
+    // Generate filters into an existing plugin
+    {
+      const std::string k_FilterNames = "ThirdFilter,FourthFilter";
+      const std::filesystem::path k_InputDirectory = k_OutputDirectory / k_PluginName;
+
+      // Create default Parameters for the filter.
+      args.insertOrAssign(GeneratePythonSkeletonFilter::k_UseExistingPlugin_Key, true);
+      args.insertOrAssign(GeneratePythonSkeletonFilter::k_PluginInputDirectory_Key, std::make_any<FileSystemPathParameter::ValueType>(k_InputDirectory.string()));
+      args.insertOrAssign(GeneratePythonSkeletonFilter::k_PluginFilterNames, std::make_any<std::string>(k_FilterNames));
+
+      // Preflight the filter and check result
+      auto preflightResult = filter.preflight(dataStructure, args);
+      REQUIRE(preflightResult.outputActions.valid());
+
+      // Execute the filter and check the result
+      auto executeResult = filter.execute(dataStructure, args);
+      REQUIRE(executeResult.result.valid());
+    }
   }
 }
