98e45031a627ee22a5dc9c31ccffb27c0c5cbf2c
nyoungbq <nathan.young@bluequartz.net>
2024-02-02 17:25:44 -0500

    Generate JSON String

---
+++ src/simplnx/Parameters/util/ColorTable.in.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "simplnx/Common/Result.hpp"
+#include "simplnx/Common/StringLiteral.hpp"
+#include "simplnx/Common/Types.hpp"
+#include "simplnx/simplnx_export.hpp"
+
+#include <nlohmann/json.hpp>
+
+#include <optional>
+#include <string>
+
+namespace 
+{
+constexpr nx::core::StringLiteral k_ColorTableJsonStr = R"~(@COLOR_TABLE_JSON@)~";
+}
+
+namespace nx::core
+{
+/**
+ * @brief ColorTable
+ */
+class SIMPLNX_EXPORT ColorTable
+{
+};
+
+}
