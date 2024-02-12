e3a47dcc14e7cb317b7df5ef4c3d888faefbda82
Michael Jackson <mike.jackson@bluequartz.net>
2024-02-12 13:06:03 -0500

    COMP: Fix VS compiler warnings

--- src/Plugins/SimplnxCore/test/SurfaceNetsTest.cpp
+++ src/Plugins/SimplnxCore/test/SurfaceNetsTest.cpp
@@ -112,8 +112,8 @@ TEST_CASE("SimplnxCore::SurfaceNetsFilter: With Smoothing", "[SimplnxCore][Surfa
     // Create default Parameters for the filter.
 
     args.insertOrAssign(SurfaceNetsFilter::k_ApplySmoothing_Key, std::make_any<bool>(true));
-    args.insertOrAssign(SurfaceNetsFilter::k_MaxDistanceFromVoxelCenter_Key, std::make_any<float32>(1.0));
-    args.insertOrAssign(SurfaceNetsFilter::k_RelaxationFactor_Key, std::make_any<float32>(0.5));
+    args.insertOrAssign(SurfaceNetsFilter::k_MaxDistanceFromVoxelCenter_Key, std::make_any<float32>(1.0f));
+    args.insertOrAssign(SurfaceNetsFilter::k_RelaxationFactor_Key, std::make_any<float32>(0.5f));
 
     const DataPath gridGeomDataPath({k_DataContainer});
     args.insertOrAssign(SurfaceNetsFilter::k_GridGeometryDataPath_Key, std::make_any<DataPath>(gridGeomDataPath));
