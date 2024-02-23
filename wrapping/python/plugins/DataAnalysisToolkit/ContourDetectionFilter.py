0daf4435ebb7d0df94b2b28fcc45fbb42ee690d4
Michael Jackson <mike.jackson@bluequartz.net>
2024-02-23 16:45:29 -0500

    Commit OpenCV based filter.
    Requirements:
    
    - OpenCV 4.8
    - Python 3.12.x
    
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- wrapping/python/plugins/DataAnalysisToolkit/ContourDetectionFilter.py
+++ wrapping/python/plugins/DataAnalysisToolkit/ContourDetectionFilter.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 from typing import List
 
-class ContourStatisticsFilter:
+class ContourDetectionFilter:
   """
   This section should contain the 'keys' that store each parameter. The value of the key should be snake_case. The name
   of the value should be ALL_CAPITOL_KEY
@@ -12,6 +12,8 @@ class ContourStatisticsFilter:
   FEATURE_ATTR_MATRIX_PATH_KEY = 'feature_attr_matrix_path'
   CONTOUR_PERIMETERS_ARRAY_NAME_KEY = 'contour_perimeters_array_name'
   CONTOUR_AREAS_ARRAY_NAME_KEY = 'contour_areas_array_name'
+  THRESHOLD_VALUE_KEY = 'threshold_value'
+  FEATURE_ID_ARRAY_NAME_KEY = 'feature_id_array_name'
 
   def uuid(self) -> nx.Uuid:
     """This returns the UUID of the filter. Each filter has a unique UUID value
@@ -25,35 +27,35 @@ class ContourStatisticsFilter:
     :return: The filter's human name
     :rtype: string
     """
-    return 'Contour Statistics Filter (Python)'
+    return 'Contour Detection Filter (Python)'
 
   def class_name(self) -> str:
     """The returns the name of the class that implements the filter
     :return: The name of the implementation class
     :rtype: string
     """
-    return 'ContourStatisticsFilter'
+    return 'ContourDetectionFilter'
 
   def name(self) -> str:
     """The returns the name of filter
     :return: The name of the filter
     :rtype: string
     """
-    return 'ContourStatisticsFilter'
+    return 'ContourDetectionFilter'
 
   def default_tags(self) -> List[str]:
     """This returns the default tags for this filter
     :return: The default tags for the filter
     :rtype: list
     """
-    return ['python', 'contour', 'statistics', 'detection', 'opencv', 'cv2']
+    return ['python', 'contour', 'detection', 'opencv']
 
   def clone(self):
     """Clones the filter
     :return: A new instance of the filter
-    :rtype:  ContourStatisticsFilter
+    :rtype:  ContourDetectionFilter
     """
-    return ContourStatisticsFilter()
+    return ContourDetectionFilter()
 
   def parameters(self) -> nx.Parameters:
     """This function defines the parameters that are needed by the filter. Parameters collect the values from the user
@@ -62,12 +64,14 @@ class ContourStatisticsFilter:
     params = nx.Parameters()
 
     params.insert(nx.Parameters.Separator("Required Data Objects"))
-    params.insert(nx.ArraySelectionParameter(ContourStatisticsFilter.INPUT_IMAGE_ARRAY_KEY, 'Input Image Data Array', 'The input binary image array that will be used to detect contours.', nx.DataPath([]), set([nx.DataType.int8, nx.DataType.uint8, nx.DataType.int16, nx.DataType.uint16, nx.DataType.int32, nx.DataType.uint32, nx.DataType.int64, nx.DataType.uint64]), [[1]]))
+    params.insert(nx.ArraySelectionParameter(ContourDetectionFilter.INPUT_IMAGE_ARRAY_KEY, 'Input Image Data Array', 'The input binary image array that will be used to detect contours.', nx.DataPath([]), set([nx.DataType.uint8]), [[1]]))
+    params.insert(nx.Float64Parameter(ContourDetectionFilter.THRESHOLD_VALUE_KEY, 'Threshold Value', "", 50.0))
 
     params.insert(nx.Parameters.Separator("Created Data Objects"))
-    params.insert(nx.DataGroupCreationParameter(ContourStatisticsFilter.FEATURE_ATTR_MATRIX_PATH_KEY, 'Contour Statistics Attribute Matrix Path', 'The path where the Feature Attribute Matrix will be created to store the contour statistics.', nx.DataPath([])))
-    params.insert(nx.DataObjectNameParameter(ContourStatisticsFilter.CONTOUR_PERIMETERS_ARRAY_NAME_KEY, "Contour Perimeters Array Name", "The name of the Feature Data Array created to store the contour perimeters.", "Contour Perimeters"))
-    params.insert(nx.DataObjectNameParameter(ContourStatisticsFilter.CONTOUR_AREAS_ARRAY_NAME_KEY, "Contour Areas Array Name", "The name of the Feature Data Array created to store the contour areas.", "Contour Areas"))
+    params.insert(nx.DataObjectNameParameter(ContourDetectionFilter.FEATURE_ID_ARRAY_NAME_KEY, 'Feature IDs', 'FeatureID Data Array Name', "FeatureIDs"))
+    params.insert(nx.DataGroupCreationParameter(ContourDetectionFilter.FEATURE_ATTR_MATRIX_PATH_KEY, 'Contour Statistics Attribute Matrix Path', 'The path where the Feature Attribute Matrix will be created to store the contour statistics.', nx.DataPath([])))
+    params.insert(nx.DataObjectNameParameter(ContourDetectionFilter.CONTOUR_PERIMETERS_ARRAY_NAME_KEY, "Contour Perimeters Array Name", "The name of the Feature Data Array created to store the contour perimeters.", "Contour Perimeters"))
+    params.insert(nx.DataObjectNameParameter(ContourDetectionFilter.CONTOUR_AREAS_ARRAY_NAME_KEY, "Contour Areas Array Name", "The name of the Feature Data Array created to store the contour areas.", "Contour Areas"))
   
     return params
 
@@ -78,22 +82,41 @@ class ContourStatisticsFilter:
     :returns:
     :rtype: nx.IFilter.PreflightResult
     """
-    feature_attr_matrix_path: nx.DataPath = args[ContourStatisticsFilter.FEATURE_ATTR_MATRIX_PATH_KEY]
-    perimeters_array_name: str = args[ContourStatisticsFilter.CONTOUR_PERIMETERS_ARRAY_NAME_KEY]
-    areas_array_name: str = args[ContourStatisticsFilter.CONTOUR_AREAS_ARRAY_NAME_KEY]
+    # warnings = []
+    input_image_array_path: nx.DataPath = args[ContourDetectionFilter.INPUT_IMAGE_ARRAY_KEY]
+    # feature_id_array_name: str = args[ContourDetectionFilter.FEATURE_ID_ARRAY_NAME_KEY]  
+    # feature_id_array_path = str(input_image_array_path).split('/')
+    # feature_id_array_path[-1] = feature_id_array_name
+    
+
+    #warnings.append(nx.Warning(200, f"{feature_id_array_path}"))
+    #feature_id_array_path = nx.DataPath(feature_id_array_path)
+
+    feature_attr_matrix_path: nx.DataPath = args[ContourDetectionFilter.FEATURE_ATTR_MATRIX_PATH_KEY]
+    perimeters_array_name: str = args[ContourDetectionFilter.CONTOUR_PERIMETERS_ARRAY_NAME_KEY]
+    areas_array_name: str = args[ContourDetectionFilter.CONTOUR_AREAS_ARRAY_NAME_KEY]
+
+    threshold_value: float = args[ContourDetectionFilter.THRESHOLD_VALUE_KEY]
+    if threshold_value < 0 or threshold_value > 255: 
+      return nx.IFilter.PreflightResult(nx.OutputActions(), [nx.Error(-1000, f"Threshold value needs to be between 0 and 255")])
 
     output_actions = nx.OutputActions()
 
     # We don't know how many contours there are yet, so set the attribute matrix dimensions to (0)
-    output_actions.append_action(nx.CreateAttributeMatrixAction(feature_attr_matrix_path, [0]))
+    output_actions.append_action(nx.CreateAttributeMatrixAction(feature_attr_matrix_path, [10]))
 
     # Create the perimeters feature array
     perimeters_array_path = feature_attr_matrix_path.create_child_path(perimeters_array_name)
-    output_actions.append_action(nx.CreateArrayAction(nx.DataType.float64, [0], [1], perimeters_array_path))
+    output_actions.append_action(nx.CreateArrayAction(nx.DataType.float64, [10], [1], perimeters_array_path))
 
     # Create the areas feature array
     areas_array_path = feature_attr_matrix_path.create_child_path(areas_array_name)
-    output_actions.append_action(nx.CreateArrayAction(nx.DataType.uint64, [0], [1], areas_array_path))
+    output_actions.append_action(nx.CreateArrayAction(nx.DataType.uint64, [10], [1], areas_array_path))
+  
+    # Create teh feature ID array in cell data 
+    input_data_array = data_structure[input_image_array_path]
+    output_actions.append_action(nx.CreateArrayAction(nx.DataType.int32, input_data_array.tdims, [1], nx.DataPath(["ImageDataContainer", "Cell Data", "FeatureIDs"])))
+
 
     return nx.IFilter.PreflightResult(output_actions)
 
@@ -102,10 +125,11 @@ class ContourStatisticsFilter:
     :returns:
     :rtype: nx.IFilter.ExecuteResult
     """
-    input_image_array_path: nx.DataPath = args[ContourStatisticsFilter.INPUT_IMAGE_ARRAY_KEY]
-    feature_attr_matrix_path: nx.DataPath = args[ContourStatisticsFilter.FEATURE_ATTR_MATRIX_PATH_KEY]
-    perimeters_array_name: str = args[ContourStatisticsFilter.CONTOUR_PERIMETERS_ARRAY_NAME_KEY]
-    areas_array_name: str = args[ContourStatisticsFilter.CONTOUR_AREAS_ARRAY_NAME_KEY]
+    input_image_array_path: nx.DataPath = args[ContourDetectionFilter.INPUT_IMAGE_ARRAY_KEY]
+    feature_attr_matrix_path: nx.DataPath = args[ContourDetectionFilter.FEATURE_ATTR_MATRIX_PATH_KEY]
+    perimeters_array_name: str = args[ContourDetectionFilter.CONTOUR_PERIMETERS_ARRAY_NAME_KEY]
+    areas_array_name: str = args[ContourDetectionFilter.CONTOUR_AREAS_ARRAY_NAME_KEY]
+    threshold_value: float = args[ContourDetectionFilter.THRESHOLD_VALUE_KEY]
 
     perimeters_array_path = feature_attr_matrix_path.create_child_path(perimeters_array_name)
     areas_array_path = feature_attr_matrix_path.create_child_path(areas_array_name)
@@ -125,11 +149,11 @@ class ContourStatisticsFilter:
     # Apply a binary threshold to the grayscale image.
     # Pixels with intensity greater than 50 are set to the maximum value (255, white), and pixels with intensity less than or equal to 50 are set to 0 (black).
     # This operation converts the grayscale image into a binary image, enhancing the contrast between objects of interest and the background, facilitating further analysis like contour detection.
-    # ret, thresh = cv2.threshold(input_image_data, 50, 255, cv2.THRESH_BINARY)
+    ret, thresh = cv2.threshold(input_image_data, threshold_value, 255, cv2.THRESH_BINARY)
 
     # Find the contours
     message_handler(nx.IFilter.Message(nx.IFilter.Message.Type.Info, 'Finding contours...'))
-    contours, hierarchy = cv2.findContours(input_image_data, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     contour_count = len(contours)
     message_handler(nx.IFilter.Message(nx.IFilter.Message.Type.Info, f'{contour_count} contours found.'))
 
@@ -138,10 +162,19 @@ class ContourStatisticsFilter:
     perimeters_array: nx.IDataArray = data_structure[perimeters_array_path]
     areas_array: nx.IDataArray = data_structure[areas_array_path]
 
+    feature_id_array = data_structure[nx.DataPath(["ImageDataContainer", "Cell Data", "FeatureIDs"])].npview()
+    feature_id_array = np.squeeze(feature_id_array)
+
+    # Iterate over contours and draw each one with a unique label
+    for i, contour in enumerate(contours):
+    # Use the contour index as a label (ensure it's not zero to distinguish from background)
+    # Note: i+1 is used because 0 label is for background
+      cv2.drawContours(feature_id_array, [contour], -1, color=(i), thickness=cv2.FILLED)
+
     # Resize the feature attribute matrix and feature arrays
-    feature_attr_matrix.resize_tuples([contour_count])
-    perimeters_array.resize_tuples([contour_count])
-    areas_array.resize_tuples([contour_count])
+    # feature_attr_matrix.resize_tuples([contour_count])
+    # perimeters_array.resize_tuples([contour_count])
+    # areas_array.resize_tuples([contour_count])
 
     # Get numpy views of the feature arrays
     perimeters_data = perimeters_array.npview()
