e75856239bfec2c96759a83b4e841e4ad9f2fe83
nyoungbq <nathan.young@bluequartz.net>
2024-03-15 10:23:18 -0400

    make tests more comprehensive

--- test/StringUtilitiesTest.cpp
+++ test/StringUtilitiesTest.cpp
@@ -14,24 +14,21 @@ TEST_CASE("Utility Function Test: split(str, char)")
 
   std::vector<std::string> result = StringUtilities::split(inputStr, '|');
 
-  REQUIRE(result.size() == 5);
-  // Expected: {"This","Is","A","Baseline","Test"}
+  REQUIRE(result == std::vector<std::string>{"This","Is","A","Baseline","Test"});
 
   // Case 2
   inputStr = "|This|Is|A|Baseline|Test|"
 
   result = StringUtilities::split(inputStr, '|');
 
-  REQUIRE(result.size() == 5);
-  // Expected: {"This","Is","A","Baseline","Test"}
+  REQUIRE(result == std::vector<std::string>{"This","Is","A","Baseline","Test"});
 
   // Case 3
   inputStr = "|This|Is||A||Baseline|Test||"
 
   result = StringUtilities::split(inputStr, '|');
 
-  REQUIRE(result.size() == 5);
-  // Expected: {"This","Is","A","Baseline","Test"}
+  REQUIRE(result == std::vector<std::string>{"This","Is","A","Baseline","Test"});
 }
 
 TEST_CASE("Utility Function Test: split(str, char, bool)")
@@ -41,46 +38,40 @@ TEST_CASE("Utility Function Test: split(str, char, bool)")
 
   std::vector<std::string> result = StringUtilities::split(inputStr, '|', false);
 
-  REQUIRE(result.size() == 5);
-  // Expected: {"This","Is","A","Baseline","Test"}
+  REQUIRE(result == std::vector<std::string>{"This","Is","A","Baseline","Test"});
 
   // Case 2
   inputStr = "|This|Is|A|Baseline|Test|"
 
   result = StringUtilities::split(inputStr, '|', false);
 
-  REQUIRE(result.size() == 5);
-  // Expected: {"This","Is","A","Baseline","Test"}
+  REQUIRE(result == std::vector<std::string>{"This","Is","A","Baseline","Test"});
 
   // Case 3
   inputStr = "|This|Is||A||Baseline|Test||"
 
   result = StringUtilities::split(inputStr, '|', false);
 
-  REQUIRE(result.size() == 5);
-  // Expected: {"This","Is","A","Baseline","Test"}
+  REQUIRE(result == std::vector<std::string>{"This","Is","A","Baseline","Test"});
 
   // Case 4
   std::string inputStr = "This|Is|A|Baseline|Test";
 
   std::vector<std::string> result = StringUtilities::split(inputStr, '|', true);
 
-  REQUIRE(result.size() == 5);
-  // Expected: {"This","Is","A","Baseline","Test"}
+  REQUIRE(result == std::vector<std::string>{"This","Is","A","Baseline","Test"});
 
   // Case 5
   inputStr = "|This|Is|A|Baseline|Test|"
 
   result = StringUtilities::split(inputStr, '|', true);
 
-  REQUIRE(result.size() == 6);
-  // Expected: {"This","Is","A","Baseline","Test",""}
+  REQUIRE(result == std::vector<std::string>{"This","Is","A","Baseline","Test",""});
 
   // Case 6
   inputStr = "|This|Is||A||Baseline|Test||"
 
   result = StringUtilities::split(inputStr, '|', true);
 
-  REQUIRE(result.size() == 9);
-  // Expected: {"This","Is","","A","","Baseline","Test","",""}
+  REQUIRE(result == std::vector<std::string>{"This","Is","","A","","Baseline","Test","",""});
 }
