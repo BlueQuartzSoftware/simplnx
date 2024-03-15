67f26208570d8fddf7f748de56dc1b6c474bb222
nyoungbq <nathan.young@bluequartz.net>
2024-03-15 10:09:19 -0400

    Added test cases

--- test/StringUtilitiesTest.cpp
+++ test/StringUtilitiesTest.cpp
@@ -12,10 +12,66 @@ TEST_CASE("Utility Function Test: split(str, char)")
   // Case 1
   std::string inputStr = "This|Is|A|Baseline|Test";
 
-  std::vector<std::string> result1 = StringUtilities::split(inputStr, '|');
+  std::vector<std::string> result = StringUtilities::split(inputStr, '|');
 
-  REQUIRE(result1.size() == 5);
+  REQUIRE(result.size() == 5);
 
   // Case 2
-  inputStr
+  inputStr = "|This|Is|A|Baseline|Test|"
+
+  result = StringUtilities::split(inputStr, '|');
+
+  REQUIRE(result.size() == 5);
+
+  // Case 3
+  inputStr = "|This|Is||A||Baseline|Test||"
+
+  result = StringUtilities::split(inputStr, '|');
+
+  REQUIRE(result.size() == 5);
+}
+
+TEST_CASE("Utility Function Test: split(str, char, bool)")
+{
+  // Case 1
+  std::string inputStr = "This|Is|A|Baseline|Test";
+
+  std::vector<std::string> result = StringUtilities::split(inputStr, '|', false);
+
+  REQUIRE(result.size() == 5);
+
+  // Case 2
+  inputStr = "|This|Is|A|Baseline|Test|"
+
+  result = StringUtilities::split(inputStr, '|', false);
+
+  REQUIRE(result.size() == 5);
+
+  // Case 3
+  inputStr = "|This|Is||A||Baseline|Test||"
+
+  result = StringUtilities::split(inputStr, '|', false);
+
+  REQUIRE(result.size() == 5);
+
+  // Case 4
+  std::string inputStr = "This|Is|A|Baseline|Test";
+
+  std::vector<std::string> result = StringUtilities::split(inputStr, '|', true);
+
+  REQUIRE(result.size() == 5);
+
+  // Case 5
+  inputStr = "|This|Is|A|Baseline|Test|"
+
+  result = StringUtilities::split(inputStr, '|', true);
+
+  REQUIRE(result.size() == 5);
+
+  // Case 6
+  inputStr = "|This|Is||A||Baseline|Test||"
+
+  result = StringUtilities::split(inputStr, '|', true);
+
+  REQUIRE(result.size() == 8);
 }
