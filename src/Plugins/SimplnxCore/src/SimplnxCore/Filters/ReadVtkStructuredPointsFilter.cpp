6f75cc09f11987db3a3970aaaefc5517d95eb8fc
Joey Kleingers <joey.kleingers@bluequartz.net>
2024-04-02 11:22:11 -0400

    Add missing help text.
    Signed-off-by: Joey Kleingers <joey.kleingers@bluequartz.net>

--- src/Plugins/SimplnxCore/src/SimplnxCore/Filters/ReadVtkStructuredPointsFilter.cpp
+++ src/Plugins/SimplnxCore/src/SimplnxCore/Filters/ReadVtkStructuredPointsFilter.cpp
@@ -63,16 +63,16 @@ Parameters ReadVtkStructuredPointsFilter::parameters() const
 
   params.insert(std::make_unique<FileSystemPathParameter>(k_InputFile_Key, "Input VTK File", "The path to the input file", fs::path("data.vtk"), FileSystemPathParameter::ExtensionsType{".vtk"},
                                                           FileSystemPathParameter::PathType::InputFile));
-  params.insertLinkableParameter(std::make_unique<BoolParameter>(k_ReadPointData_Key, "Read Point Data", "", false));
-  params.insertLinkableParameter(std::make_unique<BoolParameter>(k_ReadCellData_Key, "Read Cell Data", "", false));
+  params.insertLinkableParameter(std::make_unique<BoolParameter>(k_ReadPointData_Key, "Read Point Data", "Determines whether or not the Point Data will be read.", false));
+  params.insertLinkableParameter(std::make_unique<BoolParameter>(k_ReadCellData_Key, "Read Cell Data", "Determines whether or not the Cell Data will be read.", false));
 
   params.insertSeparator(Parameters::Separator{"Created Point Data"});
-  params.insert(std::make_unique<DataGroupCreationParameter>(k_VertexDataContainerName_Key, "Data Container [Point Data]", "", DataPath({"VTK Point Data"})));
-  params.insert(std::make_unique<DataObjectNameParameter>(k_VertexAttributeMatrixName_Key, "Attribute Matrix Name [Point Data]", "", "Vertex Data"));
+  params.insert(std::make_unique<DataGroupCreationParameter>(k_VertexDataContainerName_Key, "Data Container [Point Data]", "The data container for the Point Data.", DataPath({"VTK Point Data"})));
+  params.insert(std::make_unique<DataObjectNameParameter>(k_VertexAttributeMatrixName_Key, "Attribute Matrix Name [Point Data]", "The attribute matrix for the Point Data.", "Vertex Data"));
 
   params.insertSeparator(Parameters::Separator{"Created Cell Data"});
-  params.insert(std::make_unique<DataGroupCreationParameter>(k_VolumeDataContainerName_Key, "Data Container [Cell Data]", "", DataPath({"VTK Cell Data"})));
-  params.insert(std::make_unique<DataObjectNameParameter>(k_CellAttributeMatrixName_Key, "Attribute Matrix Name [Cell Data]", "", "Cell Data"));
+  params.insert(std::make_unique<DataGroupCreationParameter>(k_VolumeDataContainerName_Key, "Data Container [Cell Data]", "The data container for the Cell Data.", DataPath({"VTK Cell Data"})));
+  params.insert(std::make_unique<DataObjectNameParameter>(k_CellAttributeMatrixName_Key, "Attribute Matrix Name [Cell Data]", "The attribute matrix for the Cell Data.", "Cell Data"));
 
   // Associate the Linkable Parameter(s) to the children parameters that they control
   params.linkParameters(k_ReadPointData_Key, k_VertexDataContainerName_Key, true);
