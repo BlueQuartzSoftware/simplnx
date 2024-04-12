3fa1bb57e59ec861f49c9939f17d4e13302c682f
Michael Jackson <mike.jackson@bluequartz.net>
2024-04-12 07:16:49 -0400

    BUG: CliObsever output bug, create_directories() error message (#911)
    - CliObserver was always printing warnings even if there were none.
    - If std::file_system::create_directories() returned an error, the error message was not included in the Result<> warning
    - ApproximatePointCloudHull: Resize created vertex attribute matrix to match vertex geometry
    
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- src/simplnx/Utilities/FilterUtilities.cpp
+++ src/simplnx/Utilities/FilterUtilities.cpp
@@ -18,7 +18,7 @@ Result<> CreateOutputDirectories(const fs::path& outputPath)
     // /tmp/foo but what was created was /private/tmp/foo. This logic should fix that issue.
     if(!fs::create_directories(outputPath, errorCode) && !fs::exists(outputPath))
     {
-      return MakeErrorResult(-4010, fmt::format("Unable to create output directory {}. Error code from operating system is {}", outputPath.string(), errorCode.value()));
+      return MakeErrorResult(-4010, fmt::format("Unable to create output directory {}. Error code from operating system is {}", outputPath.string(), errorCode.value(), errorCode.message()));
     }
   }
   return {};
