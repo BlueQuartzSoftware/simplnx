4e8feb5b76db270a332a4228a83acae3d26a2a11
Michael Jackson <mike.jackson@bluequartz.net>
2024-05-02 08:30:16 -0400

    Update src/simplnx/DataStructure/DataMap.cpp
    Co-authored-by: Nathan Young <nathan.young@bluequartz.net>

--- src/simplnx/DataStructure/DataMap.cpp
+++ src/simplnx/DataStructure/DataMap.cpp
@@ -32,7 +32,8 @@ DataMap DataMap::deepCopy(const DataPath& parentCopyPath) const
     const auto copy = dataObj->deepCopy(parentCopyPath.createChildPath(dataObj->getName()));
     if(copy == nullptr)
     {
-      throw std::runtime_error(fmt::format("A deep copy request for parent '{}' failed on child object '{}' ", parentCopyPath.toString(), dataObj->getName()));
+      throw std::runtime_error(
+          fmt::format("{}({}): Function {}: A deep copy request for parent '{}' failed on child object '{}'", __FILE__, __LINE__, "DataMap::DeepCopy", parentCopyPath.toString(), dataObj->getName()));
     }
     dataMap.m_Map[key] = copy;
   }
