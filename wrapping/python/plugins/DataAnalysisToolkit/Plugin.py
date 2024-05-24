db3709042eb8ac87c8cfcca875d40b98e9a82c13
Joey Kleingers <joey.kleingers@bluequartz.net>
2024-05-24 09:41:37 -0400

    FILTER: Create Peregrine Reader Filter (Python) (#965)
    Signed-off-by: Joey Kleingers <joey.kleingers@bluequartz.net>

--- wrapping/python/plugins/DataAnalysisToolkit/Plugin.py
+++ wrapping/python/plugins/DataAnalysisToolkit/Plugin.py
@@ -12,32 +12,53 @@ You may need the following python packages installed to see every filter
 
 _filters = []
 
+# FILTER_START: CalculateHistogramFilter
 try:
   from DataAnalysisToolkit.CalculateHistogramFilter import CalculateHistogramFilter
   _filters.append(CalculateHistogramFilter)
 except ImportError:
   pass
+# FILTER_END: CalculateHistogramFilter
+
+# FILTER_START: InterpolateGridDataFilter
 try:
   from DataAnalysisToolkit.InterpolateGridDataFilter import InterpolateGridDataFilter
   _filters.append(InterpolateGridDataFilter)
 except ImportError:
   pass
+# FILTER_END: InterpolateGridDataFilter
+
+# FILTER_START: CliReaderFilter
 try:
   from DataAnalysisToolkit.CliReaderFilter import CliReaderFilter
   _filters.append(CliReaderFilter)
 except ImportError:
   pass
+# FILTER_END: CliReaderFilter
+
+# FILTER_START: ContourDetectionFilter
 try:
   from DataAnalysisToolkit.ContourDetectionFilter import ContourDetectionFilter
   _filters.append(ContourDetectionFilter)
 except ImportError:
   pass
+# FILTER_END: ContourDetectionFilter
+
+# FILTER_START: NPSortArray
 try:
   from DataAnalysisToolkit.NPSortArray import NPSortArray
   _filters.append(NPSortArray)
 except ImportError:
   pass
+# FILTER_END: NPSortArray
 
+# FILTER_START: ReadPeregrineHDF5File
+try:
+  from DataAnalysisToolkit.ReadPeregrineHDF5File import ReadPeregrineHDF5File
+  _filters.append(ReadPeregrineHDF5File)
+except ImportError:
+  pass
+# FILTER_END: ReadPeregrineHDF5File
 
 import simplnx as nx
 
@@ -52,12 +73,11 @@ class DataAnalysisToolkit:
     return 'DataAnalysisToolkit'
 
   def description(self) -> str:
-    return 'A plugin that shows examples of Python DREAM3D filters that analyze data, specifically using Python libraries like numpy, scipy, and matplotlib.'
+    return 'A plugin containing Python DREAM3D filters that analyze data, specifically using Python libraries like numpy, scipy, and matplotlib.'
 
   def vendor(self) -> str:
     return 'BlueQuartz Software'
 
   def get_filters(self):
-    return _filters 
-
+    return _filters
 
