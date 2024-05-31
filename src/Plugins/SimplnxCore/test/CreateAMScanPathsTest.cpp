85f7817eaca5a17abc60c719676fd4515fa1bd88
Jessica Marquis <jessica.marquis@bluequartz.net>
2024-05-31 12:05:55 -0400

    Add CreateAMScanPaths unit test

--- src/Plugins/SimplnxCore/test/CreateAMScanPathsTest.cpp
+++ src/Plugins/SimplnxCore/test/CreateAMScanPathsTest.cpp
@@ -1,30 +1,8 @@
-/**
- * This file is auto generated from the original SimplnxCore/ScanVectorsGeneratorFilter
- * runtime information. These are the steps that need to be taken to utilize this
- * unit test in the proper way.
- *
- * 1: Validate each of the default parameters that gets created.
- * 2: Inspect the actual filter to determine if the filter in its default state
- * would pass or fail BOTH the preflight() and execute() methods
- * 3: UPDATE the ```REQUIRE(result.result.valid());``` code to have the proper
- *
- * 4: Add additional unit tests to actually test each code path within the filter
- *
- * There are some example Catch2 ```TEST_CASE``` sections for your inspiration.
- *
- * NOTE the format of the ```TEST_CASE``` macro. Please stick to this format to
- * allow easier parsing of the unit tests.
- *
- * When you start working on this unit test remove "[ScanVectorsGeneratorFilter][.][UNIMPLEMENTED]"
- * from the TEST_CASE macro. This will enable this unit test to be run by default
- * and report errors.
- */
-
 #include <catch2/catch.hpp>
 
 #include "simplnx/Parameters/ArraySelectionParameter.hpp"
 #include "simplnx/Parameters/DataGroupSelectionParameter.hpp"
-#include "simplnx/Parameters/NumberParameter.hpp"
+#include "simplnx/Parameters/DataObjectNameParameter.hpp"
 #include "simplnx/Parameters/StringParameter.hpp"
 #include "simplnx/UnitTest/UnitTestCommon.hpp"
 
@@ -33,7 +11,23 @@
 
 using namespace nx::core;
 
-TEST_CASE("SimplnxCore::ScanVectorsGeneratorFilter: Valid Filter Execution", "[SimplnxCore][ScanVectorsGeneratorFilter][.][UNIMPLEMENTED][!mayfail]")
+namespace
+{
+const nx::core::DataPath k_ExemplarEdgeGeometryPath = DataPath({"Exemplar Edge Geometry"});
+const nx::core::DataPath k_ExemplarScanVectorsPath = DataPath({"Exemplar Scan Vectors"});
+const nx::core::DataPath k_RegionIdsPath = DataPath({"Exemplar Edge Geometry", "Edge Data", "RegionIds"});
+const nx::core::DataPath k_SliceIdsPath = DataPath({"Exemplar Edge Geometry", "Edge Data", "Slice Ids"});
+
+const nx::core::DataPath k_ComputedScanVectorsPath = DataPath({"Output Scan Vectors"});
+
+const DataObjectNameParameter::ValueType k_EdgeData("Edge Data");
+const DataObjectNameParameter::ValueType k_VertexData("Vertex Data");
+const DataObjectNameParameter::ValueType k_Times("Times");
+const DataObjectNameParameter::ValueType k_Powers("Powers");
+const DataObjectNameParameter::ValueType k_RegionIdsName("Region Ids");
+} // namespace
+
+TEST_CASE("SimplnxCore::CreateAMScanPathsFilter: Valid Filter Execution", "[SimplnxCore][CreateAMScanPathsFilter]")
 {
   Application::GetOrCreateInstance()->loadPlugins(unit_test::k_BuildDir.view(), true);
 
@@ -48,26 +42,55 @@ TEST_CASE("SimplnxCore::ScanVectorsGeneratorFilter: Valid Filter Execution", "[S
   Arguments args;
 
   // Create default Parameters for the filter.
-  args.insertOrAssign(CreateAMScanPathsFilter::k_StripeWidth_Key, std::make_any<float32>(1.23345f));
-  args.insertOrAssign(CreateAMScanPathsFilter::k_HatchSpacing_Key, std::make_any<float32>(1.23345f));
-  args.insertOrAssign(CreateAMScanPathsFilter::k_Power_Key, std::make_any<float32>(1.23345f));
-  args.insertOrAssign(CreateAMScanPathsFilter::k_Speed_Key, std::make_any<float32>(1.23345f));
-  args.insertOrAssign(CreateAMScanPathsFilter::k_CADSliceDataContainerName_Key, std::make_any<DataPath>(DataPath{}));
-  args.insertOrAssign(CreateAMScanPathsFilter::k_CADSliceIdsArrayPath_Key, std::make_any<DataPath>(DataPath{}));
-  args.insertOrAssign(CreateAMScanPathsFilter::k_CADRegionIdsArrayPath_Key, std::make_any<DataPath>(DataPath{}));
-  args.insertOrAssign(CreateAMScanPathsFilter::k_HatchDataContainerName_Key, std::make_any<StringParameter::ValueType>("SomeString"));
-  args.insertOrAssign(CreateAMScanPathsFilter::k_VertexAttributeMatrixName_Key, std::make_any<StringParameter::ValueType>("SomeString"));
-  args.insertOrAssign(CreateAMScanPathsFilter::k_HatchAttributeMatrixName_Key, std::make_any<StringParameter::ValueType>("SomeString"));
-  args.insertOrAssign(CreateAMScanPathsFilter::k_TimeArrayName_Key, std::make_any<StringParameter::ValueType>("SomeString"));
-  args.insertOrAssign(CreateAMScanPathsFilter::k_RegionIdsArrayName_Key, std::make_any<StringParameter::ValueType>("SomeString"));
-  args.insertOrAssign(CreateAMScanPathsFilter::k_PowersArrayName_Key, std::make_any<StringParameter::ValueType>("SomeString"));
+  args.insertOrAssign(CreateAMScanPathsFilter::k_StripeWidth_Key, std::make_any<float32>(10.0f));
+  args.insertOrAssign(CreateAMScanPathsFilter::k_HatchSpacing_Key, std::make_any<float32>(0.33f));
+  args.insertOrAssign(CreateAMScanPathsFilter::k_Power_Key, std::make_any<float32>(100.0f));
+  args.insertOrAssign(CreateAMScanPathsFilter::k_Speed_Key, std::make_any<float32>(1000.0f));
+  args.insertOrAssign(CreateAMScanPathsFilter::k_CADSliceDataContainerName_Key, std::make_any<DataPath>(k_ExemplarEdgeGeometryPath));
+  args.insertOrAssign(CreateAMScanPathsFilter::k_CADSliceIdsArrayPath_Key, std::make_any<DataPath>(k_SliceIdsPath));
+  args.insertOrAssign(CreateAMScanPathsFilter::k_CADRegionIdsArrayPath_Key, std::make_any<DataPath>(k_RegionIdsPath));
+  args.insertOrAssign(CreateAMScanPathsFilter::k_HatchDataContainerName_Key, std::make_any<DataPath>(k_ComputedScanVectorsPath));
+  args.insertOrAssign(CreateAMScanPathsFilter::k_VertexAttributeMatrixName_Key, std::make_any<StringParameter::ValueType>(k_VertexData));
+  args.insertOrAssign(CreateAMScanPathsFilter::k_HatchAttributeMatrixName_Key, std::make_any<StringParameter::ValueType>(k_EdgeData));
+  args.insertOrAssign(CreateAMScanPathsFilter::k_TimeArrayName_Key, std::make_any<StringParameter::ValueType>(k_Times));
+  args.insertOrAssign(CreateAMScanPathsFilter::k_RegionIdsArrayName_Key, std::make_any<StringParameter::ValueType>(k_RegionIdsName));
+  args.insertOrAssign(CreateAMScanPathsFilter::k_PowersArrayName_Key, std::make_any<StringParameter::ValueType>(k_Powers));
 
   // Preflight the filter and check result
   auto preflightResult = filter.preflight(dataStructure, args);
   SIMPLNX_RESULT_REQUIRE_VALID(preflightResult.outputActions)
 
   auto result = filter.execute(dataStructure, args);
-  SIMPLNX_RESULT_REQUIRE_VALID(result.result);
+  SIMPLNX_RESULT_REQUIRE_VALID(result.result)
+
+  // Compare the exemplar and the computed outputs
+  {
+    auto exemplarGeom = dataStructure.getDataAs<IGeometry>(k_ExemplarScanVectorsPath);
+    auto computedGeom = dataStructure.getDataAs<IGeometry>(k_ComputedScanVectorsPath);
+    REQUIRE(UnitTest::CompareIGeometry(exemplarGeom, computedGeom));
+  }
+  {
+    DataPath exemplarDataArray = k_ExemplarScanVectorsPath.createChildPath(k_VertexData).createChildPath(k_Times);
+    DataPath computedDataArray = k_ComputedScanVectorsPath.createChildPath(k_VertexData).createChildPath(k_Times);
+    UnitTest::CompareArrays<float64>(dataStructure, exemplarDataArray, computedDataArray);
+  }
+
+  {
+    DataPath exemplarDataArray = k_ExemplarScanVectorsPath.createChildPath(k_EdgeData).createChildPath(k_RegionIdsName);
+    DataPath computedDataArray = k_ComputedScanVectorsPath.createChildPath(k_EdgeData).createChildPath(k_RegionIdsName);
+    UnitTest::CompareArrays<int32>(dataStructure, exemplarDataArray, computedDataArray);
+  }
+  {
+    DataPath exemplarDataArray = k_ExemplarScanVectorsPath.createChildPath(k_EdgeData).createChildPath(k_Powers);
+    DataPath computedDataArray = k_ComputedScanVectorsPath.createChildPath(k_EdgeData).createChildPath(k_Powers);
+    UnitTest::CompareArrays<float32>(dataStructure, exemplarDataArray, computedDataArray);
+  }
+
+  {
+    DataPath exemplarDataArray = k_ExemplarScanVectorsPath.createChildPath(k_EdgeData).createChildPath(k_SliceIdsPath.getTargetName());
+    DataPath computedDataArray = k_ComputedScanVectorsPath.createChildPath(k_EdgeData).createChildPath(k_SliceIdsPath.getTargetName());
+    UnitTest::CompareArrays<int32>(dataStructure, exemplarDataArray, computedDataArray);
+  }
 }
 
 // TEST_CASE("SimplnxCore::ScanVectorsGeneratorFilter: InValid Filter Execution")
--- src/Plugins/SimplnxCore/test/SliceTriangleGeometryTest.cpp
+++ src/Plugins/SimplnxCore/test/SliceTriangleGeometryTest.cpp
@@ -1,37 +1,10 @@
-/**
- * This file is auto generated from the original SimplnxCore/SliceTriangleGeometryFilter
- * runtime information. These are the steps that need to be taken to utilize this
- * unit test in the proper way.
- *
- * 1: Validate each of the default parameters that gets created.
- * 2: Inspect the actual filter to determine if the filter in its default state
- * would pass or fail BOTH the preflight() and execute() methods
- * 3: UPDATE the ```REQUIRE(result.result.valid());``` code to have the proper
- *
- * 4: Add additional unit tests to actually test each code path within the filter
- *
- * There are some example Catch2 ```TEST_CASE``` sections for your inspiration.
- *
- * NOTE the format of the ```TEST_CASE``` macro. Please stick to this format to
- * allow easier parsing of the unit tests.
- *
- * When you start working on this unit test remove "[SliceTriangleGeometryFilter][.][UNIMPLEMENTED]"
- * from the TEST_CASE macro. This will enable this unit test to be run by default
- * and report errors.
- */
-
 #include <catch2/catch.hpp>
 
 #include "simplnx/Parameters/ArrayCreationParameter.hpp"
 #include "simplnx/Parameters/ArraySelectionParameter.hpp"
-#include "simplnx/Parameters/BoolParameter.hpp"
 #include "simplnx/Parameters/ChoicesParameter.hpp"
 #include "simplnx/Parameters/DataGroupCreationParameter.hpp"
-#include "simplnx/Parameters/DataGroupSelectionParameter.hpp"
 #include "simplnx/Parameters/DataObjectNameParameter.hpp"
-#include "simplnx/Parameters/NumberParameter.hpp"
-#include "simplnx/Parameters/StringParameter.hpp"
-#include "simplnx/Parameters/VectorParameter.hpp"
 #include "simplnx/UnitTest/UnitTestCommon.hpp"
 
 #include "SimplnxCore/Filters/SliceTriangleGeometryFilter.hpp"
