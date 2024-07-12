a673fd48d9c177a14f1ee7444172ed78247e289a
Joey Kleingers <joey.kleingers@bluequartz.net>
2024-07-12 15:49:31 -0400

    Remove Python plugin error handling, and use relative imports everywhere.
    This allows import errors to percolate up to C++ and/or the NX GUI.
    
    Signed-off-by: Joey Kleingers <joey.kleingers@bluequartz.net>

--- src/Plugins/SimplnxCore/src/SimplnxCore/utils/PythonPluginInitTemplate.py
+++ src/Plugins/SimplnxCore/src/SimplnxCore/utils/PythonPluginInitTemplate.py
@@ -2,7 +2,7 @@
 """
 Insert documentation here for #PLUGIN_NAME#
 """
-from #PLUGIN_NAME#.Plugin import #PLUGIN_NAME#
+from .Plugin import #PLUGIN_NAME#
 
 __all__ = ['#PLUGIN_NAME#', 'get_plugin']
 
