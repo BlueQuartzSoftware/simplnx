aed6183930c0c849a3bf9760c729cafd9fa94ca6
Joey Kleingers <joey.kleingers@bluequartz.net>
2024-07-15 16:23:37 -0400

    BUG FIX: Remove Python plugin import error handling. (#1019)
    Remove Python plugin error handling, and use relative imports everywhere.
    
    This allows import errors to percolate up to C++ and/or the NX GUI.
    
    Signed-off-by: Joey Kleingers <joey.kleingers@bluequartz.net>

--- wrapping/python/plugins/NXDataAnalysisToolkit/src/NXDataAnalysisToolkit/__init__.py
+++ wrapping/python/plugins/NXDataAnalysisToolkit/src/NXDataAnalysisToolkit/__init__.py
@@ -7,107 +7,68 @@ This section conditionally tries to import each filter
 """
 
 # FILTER_START: CalculateHistogramFilter
-try:
-  from .CalculateHistogramFilter import CalculateHistogramFilter
-  __all__.append('CalculateHistogramFilter')
-except ImportError:
-  pass
+from .CalculateHistogramFilter import CalculateHistogramFilter
+__all__.append('CalculateHistogramFilter')
 # FILTER_END: CalculateHistogramFilter
 
 # FILTER_START: InterpolateGridDataFilter
-try:
-  from .InterpolateGridDataFilter import InterpolateGridDataFilter
-  __all__.append('InterpolateGridDataFilter')
-except ImportError:
-  pass
+from .InterpolateGridDataFilter import InterpolateGridDataFilter
+__all__.append('InterpolateGridDataFilter')
 # FILTER_END: InterpolateGridDataFilter
 
 # FILTER_START: CliReaderFilter
-try:
-  from .CliReaderFilter import CliReaderFilter
-  __all__.append('CliReaderFilter')
-except ImportError:
-  pass
+from .CliReaderFilter import CliReaderFilter
+__all__.append('CliReaderFilter')
 # FILTER_END: CliReaderFilter
 
 # FILTER_START: ContourDetectionFilter
-try:
-  from .ContourDetectionFilter import ContourDetectionFilter
-  __all__.append('ContourDetectionFilter')
-except ImportError:
-  pass
+from .ContourDetectionFilter import ContourDetectionFilter
+__all__.append('ContourDetectionFilter')
 # FILTER_END: ContourDetectionFilter
 
 # FILTER_START: NPSortArray
-try:
-  from .NPSortArray import NPSortArray
-  __all__.append('NPSortArray')
-except ImportError:
-  pass
+from .NPSortArray import NPSortArray
+__all__.append('NPSortArray')
 # FILTER_END: NPSortArray
 
 # FILTER_START: ReadPeregrineHDF5File
-try:
-  from .ReadPeregrineHDF5File import ReadPeregrineHDF5File
-  __all__.append('ReadPeregrineHDF5File')
-except ImportError:
-  pass
+from .ReadPeregrineHDF5File import ReadPeregrineHDF5File
+__all__.append('ReadPeregrineHDF5File')
 # FILTER_END: ReadPeregrineHDF5File
 
 # FILTER_START: WriteAbaqusFile
-try:
-  from .WriteAbaqusFile import WriteAbaqusFile
-  __all__.append('WriteAbaqusFile')
-except ImportError:
-  pass
+from .WriteAbaqusFile import WriteAbaqusFile
+__all__.append('WriteAbaqusFile')
 # FILTER_END: WriteAbaqusFile
 
 # FILTER_START: WriteAnsysFile
-try:
-  from .WriteAnsysFile import WriteAnsysFile
-  __all__.append('WriteAnsysFile')
-except ImportError:
-  pass
+from .WriteAnsysFile import WriteAnsysFile
+__all__.append('WriteAnsysFile')
 # FILTER_END: WriteAnsysFile
 
 # FILTER_START: WriteMedFile
-try:
-  from .WriteMedFile import WriteMedFile
-  __all__.append('WriteMedFile')
-except ImportError:
-  pass
+from .WriteMedFile import WriteMedFile
+__all__.append('WriteMedFile')
 # FILTER_END: WriteMedFile
 
 # FILTER_START: WriteGmshFile
-try:
-  from .WriteGmshFile import WriteGmshFile
-  __all__.append('WriteGmshFile')
-except ImportError:
-  pass
+from .WriteGmshFile import WriteGmshFile
+__all__.append('WriteGmshFile')
 # FILTER_END: WriteGmshFile
 
 # FILTER_START: WriteTetGenFile
-try:
-  from .WriteTetGenFile import WriteTetGenFile
-  __all__.append('WriteTetGenFile')
-except ImportError:
-  pass
+from .WriteTetGenFile import WriteTetGenFile
+__all__.append('WriteTetGenFile')
 # FILTER_END: WriteTetGenFile
 
 # FILTER_START: WriteVtuFile
-try:
-  from .WriteVtuFile import WriteVtuFile
-  __all__.append('WriteVtuFile')
-except ImportError:
-  pass
+from .WriteVtuFile import WriteVtuFile
+__all__.append('WriteVtuFile')
 # FILTER_END: WriteVtuFile
 
 # FILTER_START: ReadMeshFile
-try:
-  from .ReadMeshFile import ReadMeshFile
-  __all__.append('ReadMeshFile')
-except ImportError:
-  pass
+from .ReadMeshFile import ReadMeshFile
+__all__.append('ReadMeshFile')
 # FILTER_END: ReadMeshFile
 
 def get_plugin():
