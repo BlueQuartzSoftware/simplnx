d1dd87f233abf614c7c9b891723c00976fd9ba21
Michael Jackson <mike.jackson@bluequartz.net>
2024-08-01 11:42:17 -0400

    Update src/Plugins/OrientationAnalysis/src/OrientationAnalysis/Filters/Algorithms/EBSDSegmentFeatures.cpp
    Co-authored-by: Nathan Young <nathan.young@bluequartz.net>

--- src/Plugins/OrientationAnalysis/src/OrientationAnalysis/Filters/Algorithms/EBSDSegmentFeatures.cpp
+++ src/Plugins/OrientationAnalysis/src/OrientationAnalysis/Filters/Algorithms/EBSDSegmentFeatures.cpp
@@ -103,8 +103,6 @@ int64_t EBSDSegmentFeatures::getSeed(int32 gnum, int64 nextSeed) const
   }
   if(seed >= 0)
   {
-    //    auto& activeArray = m_DataStructure.getDataAs<UInt8Array>(m_InputValues->activeArrayPath)->getDataStoreRef();
-    //    auto& cellFeatureAM = m_DataStructure.getDataRefAs<AttributeMatrix>(m_InputValues->cellFeatureAttributeMatrixPath);
     featureIds->setValue(static_cast<usize>(seed), gnum);
     //    std::vector<usize> tDims = {static_cast<usize>(gnum) + 1};
     //    cellFeatureAM.resizeTuples(tDims); // This will resize the actives array
