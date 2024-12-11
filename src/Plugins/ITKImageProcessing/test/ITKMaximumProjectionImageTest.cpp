adaa62e78e2e1ce58811f2e77dcc16571fdbe3a7
nyoungbq <nathan.young@bluequartz.net>
2024-12-11 12:21:33 -0500

    create the additional test cases from SIMPL [6 failing]

--- src/Plugins/ITKImageProcessing/test/ITKMaximumProjectionImageTest.cpp
+++ src/Plugins/ITKImageProcessing/test/ITKMaximumProjectionImageTest.cpp
@@ -16,7 +16,7 @@ using namespace nx::core;
 using namespace nx::core::Constants;
 using namespace nx::core::UnitTest;
 
-TEST_CASE("ITKImageProcessing::ITKMaximumProjectionImageFilter(z_projection)", "[ITKImageProcessing][ITKMaximumProjectionImage][z_projection]")
+TEST_CASE("ITKImageProcessing::ITKMaximumProjectionImageFilter: Default Test", "[ITKImageProcessing][ITKMaximumProjectionImage][defaults]")
 {
   DataStructure dataStructure;
   const ITKMaximumProjectionImageFilter filter;
@@ -33,6 +33,76 @@ TEST_CASE("ITKImageProcessing::ITKMaximumProjectionImageFilter(z_projection)", "
   } // End Image Comparison Scope
 
   Arguments args;
+  args.insertOrAssign(ITKMaximumProjectionImageFilter::k_RemoveOriginalGeometry_Key, std::make_any<bool>(true));
+  args.insertOrAssign(ITKMaximumProjectionImageFilter::k_InputImageGeomPath_Key, std::make_any<DataPath>(inputGeometryPath));
+  args.insertOrAssign(ITKMaximumProjectionImageFilter::k_InputImageDataPath_Key, std::make_any<DataPath>(inputDataPath));
+  args.insertOrAssign(ITKMaximumProjectionImageFilter::k_OutputImageArrayName_Key, std::make_any<DataObjectNameParameter::ValueType>(outputArrayName));
+  args.insertOrAssign(ITKMaximumProjectionImageFilter::k_ProjectionDimension_Key, std::make_any<UInt32Parameter::ValueType>(0));
+
+  auto preflightResult = filter.preflight(dataStructure, args);
+  SIMPLNX_RESULT_REQUIRE_VALID(preflightResult.outputActions)
+
+  auto executeResult = filter.execute(dataStructure, args);
+  SIMPLNX_RESULT_REQUIRE_VALID(executeResult.result)
+
+  const std::string md5Hash = ITKTestBase::ComputeMd5Hash(dataStructure, cellDataPath.createChildPath(outputArrayName));
+  REQUIRE(md5Hash == "fb78c55635b17fc9ff38ef0ef14f0948");
+}
+
+TEST_CASE("ITKImageProcessing::ITKMaximumProjectionImageFilter: New Geometry Default Test", "[ITKImageProcessing][ITKMaximumProjectionImage][defaults]")
+{
+  DataStructure dataStructure;
+  const ITKMaximumProjectionImageFilter filter;
+
+  const DataPath inputGeometryPath({ITKTestBase::k_ImageGeometryPath});
+  const DataPath cellDataPath = inputGeometryPath.createChildPath(ITKTestBase::k_ImageCellDataName);
+  const DataPath inputDataPath = cellDataPath.createChildPath(ITKTestBase::k_InputDataName);
+  const DataObjectNameParameter::ValueType outputArrayName = ITKTestBase::k_OutputDataPath;
+  const DataObjectNameParameter::ValueType outputImageName = "New Image Geometry";
+  const DataPath outputDataPath = DataPath({outputImageName, ITKTestBase::k_ImageCellDataName, outputArrayName});
+
+  { // Start Image Comparison Scope
+    const fs::path inputFilePath = fs::path(unit_test::k_SourceDir.view()) / unit_test::k_DataDir.view() / "JSONFilters" / "Input/RA-Float.nrrd";
+    Result<> imageReadResult = ITKTestBase::ReadImage(dataStructure, inputFilePath, inputGeometryPath, ITKTestBase::k_ImageCellDataName, ITKTestBase::k_InputDataName);
+    SIMPLNX_RESULT_REQUIRE_VALID(imageReadResult)
+  } // End Image Comparison Scope
+
+  Arguments args;
+  args.insertOrAssign(ITKMaximumProjectionImageFilter::k_RemoveOriginalGeometry_Key, std::make_any<bool>(false));
+  args.insertOrAssign(ITKMaximumProjectionImageFilter::k_InputImageGeomPath_Key, std::make_any<DataPath>(inputGeometryPath));
+  args.insertOrAssign(ITKMaximumProjectionImageFilter::k_InputImageDataPath_Key, std::make_any<DataPath>(inputDataPath));
+  args.insertOrAssign(ITKMaximumProjectionImageFilter::k_OutputImageArrayName_Key, std::make_any<DataObjectNameParameter::ValueType>(outputArrayName));
+  args.insertOrAssign(ITKMaximumProjectionImageFilter::k_OutputImageGeomName_Key, std::make_any<DataObjectNameParameter::ValueType>(outputImageName));
+  args.insertOrAssign(ITKMaximumProjectionImageFilter::k_ProjectionDimension_Key, std::make_any<UInt32Parameter::ValueType>(0));
+
+  auto preflightResult = filter.preflight(dataStructure, args);
+  SIMPLNX_RESULT_REQUIRE_VALID(preflightResult.outputActions)
+
+  auto executeResult = filter.execute(dataStructure, args);
+  SIMPLNX_RESULT_REQUIRE_VALID(executeResult.result)
+
+  const std::string md5Hash = ITKTestBase::ComputeMd5Hash(dataStructure, outputDataPath);
+  REQUIRE(md5Hash == "fb78c55635b17fc9ff38ef0ef14f0948");
+}
+
+TEST_CASE("ITKImageProcessing::ITKMaximumProjectionImageFilter: Dimensional Test", "[ITKImageProcessing][ITKMaximumProjectionImage][defaults]")
+{
+  DataStructure dataStructure;
+  const ITKMaximumProjectionImageFilter filter;
+
+  const DataPath inputGeometryPath({ITKTestBase::k_ImageGeometryPath});
+  const DataPath cellDataPath = inputGeometryPath.createChildPath(ITKTestBase::k_ImageCellDataName);
+  const DataPath inputDataPath = cellDataPath.createChildPath(ITKTestBase::k_InputDataName);
+  const DataObjectNameParameter::ValueType outputArrayName = ITKTestBase::k_OutputDataPath;
+
+  { // Start Image Comparison Scope
+    const fs::path inputFilePath = fs::path(unit_test::k_SourceDir.view()) / unit_test::k_DataDir.view() / "JSONFilters" / "Input/RA-Float.nrrd";
+    Result<> imageReadResult = ITKTestBase::ReadImage(dataStructure, inputFilePath, inputGeometryPath, ITKTestBase::k_ImageCellDataName, ITKTestBase::k_InputDataName);
+    SIMPLNX_RESULT_REQUIRE_VALID(imageReadResult)
+  } // End Image Comparison Scope
+
+  Arguments args;
+  args.insertOrAssign(ITKMaximumProjectionImageFilter::k_RemoveOriginalGeometry_Key, std::make_any<bool>(true));
   args.insertOrAssign(ITKMaximumProjectionImageFilter::k_InputImageGeomPath_Key, std::make_any<DataPath>(inputGeometryPath));
   args.insertOrAssign(ITKMaximumProjectionImageFilter::k_InputImageDataPath_Key, std::make_any<DataPath>(inputDataPath));
   args.insertOrAssign(ITKMaximumProjectionImageFilter::k_OutputImageArrayName_Key, std::make_any<DataObjectNameParameter::ValueType>(outputArrayName));
@@ -44,11 +114,72 @@ TEST_CASE("ITKImageProcessing::ITKMaximumProjectionImageFilter(z_projection)", "
   auto executeResult = filter.execute(dataStructure, args);
   SIMPLNX_RESULT_REQUIRE_VALID(executeResult.result)
 
-  const fs::path baselineFilePath = fs::path(nx::core::unit_test::k_DataDir.view()) / "JSONFilters/Baseline/BasicFilters_MaximumProjectionImageFilter_z_projection.nrrd";
-  const DataPath baselineGeometryPath({ITKTestBase::k_BaselineGeometryPath});
-  const DataPath baseLineCellDataPath = baselineGeometryPath.createChildPath(ITKTestBase::k_ImageCellDataName);
-  const DataPath baselineDataPath = baseLineCellDataPath.createChildPath(ITKTestBase::k_BaselineDataPath);
-  const Result<> readBaselineResult = ITKTestBase::ReadImage(dataStructure, baselineFilePath, baselineGeometryPath, ITKTestBase::k_ImageCellDataName, ITKTestBase::k_BaselineDataPath);
-  Result<> compareResult = ITKTestBase::CompareImages(dataStructure, baselineGeometryPath, baselineDataPath, inputGeometryPath, cellDataPath.createChildPath(outputArrayName), 0.0001);
-  SIMPLNX_RESULT_REQUIRE_VALID(compareResult)
+  const std::string md5Hash = ITKTestBase::ComputeMd5Hash(dataStructure, cellDataPath.createChildPath(outputArrayName));
+  REQUIRE(md5Hash == "f3f0d97c83c6b0d92df10c28e2481520");
+}
+
+TEST_CASE("ITKImageProcessing::ITKMaximumProjectionImageFilter: Image Short Test", "[ITKImageProcessing][ITKMaximumProjectionImage][defaults]")
+{
+  DataStructure dataStructure;
+  const ITKMaximumProjectionImageFilter filter;
+
+  const DataPath inputGeometryPath({ITKTestBase::k_ImageGeometryPath});
+  const DataPath cellDataPath = inputGeometryPath.createChildPath(ITKTestBase::k_ImageCellDataName);
+  const DataPath inputDataPath = cellDataPath.createChildPath(ITKTestBase::k_InputDataName);
+  const DataObjectNameParameter::ValueType outputArrayName = ITKTestBase::k_OutputDataPath;
+
+  { // Start Image Comparison Scope
+    const fs::path inputFilePath = fs::path(unit_test::k_SourceDir.view()) / unit_test::k_DataDir.view() / "JSONFilters" / "Input/Ramp-Up-Short.nrrd";
+    Result<> imageReadResult = ITKTestBase::ReadImage(dataStructure, inputFilePath, inputGeometryPath, ITKTestBase::k_ImageCellDataName, ITKTestBase::k_InputDataName);
+    SIMPLNX_RESULT_REQUIRE_VALID(imageReadResult)
+  } // End Image Comparison Scope
+
+  Arguments args;
+  args.insertOrAssign(ITKMaximumProjectionImageFilter::k_RemoveOriginalGeometry_Key, std::make_any<bool>(true));
+  args.insertOrAssign(ITKMaximumProjectionImageFilter::k_InputImageGeomPath_Key, std::make_any<DataPath>(inputGeometryPath));
+  args.insertOrAssign(ITKMaximumProjectionImageFilter::k_InputImageDataPath_Key, std::make_any<DataPath>(inputDataPath));
+  args.insertOrAssign(ITKMaximumProjectionImageFilter::k_OutputImageArrayName_Key, std::make_any<DataObjectNameParameter::ValueType>(outputArrayName));
+  args.insertOrAssign(ITKMaximumProjectionImageFilter::k_ProjectionDimension_Key, std::make_any<UInt32Parameter::ValueType>(1));
+
+  auto preflightResult = filter.preflight(dataStructure, args);
+  SIMPLNX_RESULT_REQUIRE_VALID(preflightResult.outputActions)
+
+  auto executeResult = filter.execute(dataStructure, args);
+  SIMPLNX_RESULT_REQUIRE_VALID(executeResult.result)
+
+  const std::string md5Hash = ITKTestBase::ComputeMd5Hash(dataStructure, cellDataPath.createChildPath(outputArrayName));
+  REQUIRE(md5Hash == "5390344262c91e83bc9208b0991a2fc9");
+}
+
+TEST_CASE("ITKImageProcessing::ITKMaximumProjectionImageFilter: Image RGB Test", "[ITKImageProcessing][ITKMaximumProjectionImage][defaults]")
+{
+  DataStructure dataStructure;
+  const ITKMaximumProjectionImageFilter filter;
+
+  const DataPath inputGeometryPath({ITKTestBase::k_ImageGeometryPath});
+  const DataPath cellDataPath = inputGeometryPath.createChildPath(ITKTestBase::k_ImageCellDataName);
+  const DataPath inputDataPath = cellDataPath.createChildPath(ITKTestBase::k_InputDataName);
+  const DataObjectNameParameter::ValueType outputArrayName = ITKTestBase::k_OutputDataPath;
+
+  { // Start Image Comparison Scope
+    const fs::path inputFilePath = fs::path(unit_test::k_SourceDir.view()) / unit_test::k_DataDir.view() / "JSONFilters" / "Input/VM1111Shrink-RGB.png";
+    Result<> imageReadResult = ITKTestBase::ReadImage(dataStructure, inputFilePath, inputGeometryPath, ITKTestBase::k_ImageCellDataName, ITKTestBase::k_InputDataName);
+    SIMPLNX_RESULT_REQUIRE_VALID(imageReadResult)
+  } // End Image Comparison Scope
+
+  Arguments args;
+  args.insertOrAssign(ITKMaximumProjectionImageFilter::k_RemoveOriginalGeometry_Key, std::make_any<bool>(true));
+  args.insertOrAssign(ITKMaximumProjectionImageFilter::k_InputImageGeomPath_Key, std::make_any<DataPath>(inputGeometryPath));
+  args.insertOrAssign(ITKMaximumProjectionImageFilter::k_InputImageDataPath_Key, std::make_any<DataPath>(inputDataPath));
+  args.insertOrAssign(ITKMaximumProjectionImageFilter::k_OutputImageArrayName_Key, std::make_any<DataObjectNameParameter::ValueType>(outputArrayName));
+  args.insertOrAssign(ITKMaximumProjectionImageFilter::k_ProjectionDimension_Key, std::make_any<UInt32Parameter::ValueType>(1));
+
+  auto preflightResult = filter.preflight(dataStructure, args);
+  SIMPLNX_RESULT_REQUIRE_VALID(preflightResult.outputActions)
+
+  auto executeResult = filter.execute(dataStructure, args);
+  SIMPLNX_RESULT_REQUIRE_VALID(executeResult.result)
+
+  const std::string md5Hash = ITKTestBase::ComputeMd5Hash(dataStructure, cellDataPath.createChildPath(outputArrayName));
+  REQUIRE(md5Hash == "92134e0dd38fccdf054ff23e67a72e75");
 }
--- src/Plugins/ITKImageProcessing/test/ITKMinimumProjectionImageTest.cpp
+++ src/Plugins/ITKImageProcessing/test/ITKMinimumProjectionImageTest.cpp
@@ -16,7 +16,7 @@ using namespace nx::core;
 using namespace nx::core::Constants;
 using namespace nx::core::UnitTest;
 
-TEST_CASE("ITKImageProcessing::ITKMinimumProjectionImageFilter(z_projection)", "[ITKImageProcessing][ITKMinimumProjectionImage][z_projection]")
+TEST_CASE("ITKImageProcessing::ITKMinimumProjectionImageFilter: Default Test", "[ITKImageProcessing][ITKMinimumProjectionImage][defaults]")
 {
   DataStructure dataStructure;
   const ITKMinimumProjectionImageFilter filter;
@@ -33,6 +33,76 @@ TEST_CASE("ITKImageProcessing::ITKMinimumProjectionImageFilter(z_projection)", "
   } // End Image Comparison Scope
 
   Arguments args;
+  args.insertOrAssign(ITKMinimumProjectionImageFilter::k_RemoveOriginalGeometry_Key, std::make_any<bool>(true));
+  args.insertOrAssign(ITKMinimumProjectionImageFilter::k_InputImageGeomPath_Key, std::make_any<DataPath>(inputGeometryPath));
+  args.insertOrAssign(ITKMinimumProjectionImageFilter::k_InputImageDataPath_Key, std::make_any<DataPath>(inputDataPath));
+  args.insertOrAssign(ITKMinimumProjectionImageFilter::k_OutputImageArrayName_Key, std::make_any<DataObjectNameParameter::ValueType>(outputArrayName));
+  args.insertOrAssign(ITKMinimumProjectionImageFilter::k_ProjectionDimension_Key, std::make_any<UInt32Parameter::ValueType>(0));
+
+  auto preflightResult = filter.preflight(dataStructure, args);
+  SIMPLNX_RESULT_REQUIRE_VALID(preflightResult.outputActions)
+
+  auto executeResult = filter.execute(dataStructure, args);
+  SIMPLNX_RESULT_REQUIRE_VALID(executeResult.result)
+
+  const std::string md5Hash = ITKTestBase::ComputeMd5Hash(dataStructure, cellDataPath.createChildPath(outputArrayName));
+  REQUIRE(md5Hash == "5591e0307db733396e8cc8143e7f29f7");
+}
+
+TEST_CASE("ITKImageProcessing::ITKMinimumProjectionImageFilter: New Geometry Default Test", "[ITKImageProcessing][ITKMinimumProjectionImage][defaults]")
+{
+  DataStructure dataStructure;
+  const ITKMinimumProjectionImageFilter filter;
+
+  const DataPath inputGeometryPath({ITKTestBase::k_ImageGeometryPath});
+  const DataPath cellDataPath = inputGeometryPath.createChildPath(ITKTestBase::k_ImageCellDataName);
+  const DataPath inputDataPath = cellDataPath.createChildPath(ITKTestBase::k_InputDataName);
+  const DataObjectNameParameter::ValueType outputArrayName = ITKTestBase::k_OutputDataPath;
+  const DataObjectNameParameter::ValueType outputImageName = "New Image Geometry";
+  const DataPath outputDataPath = DataPath({outputImageName, ITKTestBase::k_ImageCellDataName, outputArrayName});
+
+  { // Start Image Comparison Scope
+    const fs::path inputFilePath = fs::path(unit_test::k_SourceDir.view()) / unit_test::k_DataDir.view() / "JSONFilters" / "Input/RA-Float.nrrd";
+    Result<> imageReadResult = ITKTestBase::ReadImage(dataStructure, inputFilePath, inputGeometryPath, ITKTestBase::k_ImageCellDataName, ITKTestBase::k_InputDataName);
+    SIMPLNX_RESULT_REQUIRE_VALID(imageReadResult)
+  } // End Image Comparison Scope
+
+  Arguments args;
+  args.insertOrAssign(ITKMinimumProjectionImageFilter::k_RemoveOriginalGeometry_Key, std::make_any<bool>(false));
+  args.insertOrAssign(ITKMinimumProjectionImageFilter::k_InputImageGeomPath_Key, std::make_any<DataPath>(inputGeometryPath));
+  args.insertOrAssign(ITKMinimumProjectionImageFilter::k_InputImageDataPath_Key, std::make_any<DataPath>(inputDataPath));
+  args.insertOrAssign(ITKMinimumProjectionImageFilter::k_OutputImageArrayName_Key, std::make_any<DataObjectNameParameter::ValueType>(outputArrayName));
+  args.insertOrAssign(ITKMinimumProjectionImageFilter::k_OutputImageGeomName_Key, std::make_any<DataObjectNameParameter::ValueType>(outputImageName));
+  args.insertOrAssign(ITKMinimumProjectionImageFilter::k_ProjectionDimension_Key, std::make_any<UInt32Parameter::ValueType>(0));
+
+  auto preflightResult = filter.preflight(dataStructure, args);
+  SIMPLNX_RESULT_REQUIRE_VALID(preflightResult.outputActions)
+
+  auto executeResult = filter.execute(dataStructure, args);
+  SIMPLNX_RESULT_REQUIRE_VALID(executeResult.result)
+
+  const std::string md5Hash = ITKTestBase::ComputeMd5Hash(dataStructure, outputDataPath);
+  REQUIRE(md5Hash == "5591e0307db733396e8cc8143e7f29f7");
+}
+
+TEST_CASE("ITKImageProcessing::ITKMinimumProjectionImageFilter: Dimensional Test", "[ITKImageProcessing][ITKMinimumProjectionImage][defaults]")
+{
+  DataStructure dataStructure;
+  const ITKMinimumProjectionImageFilter filter;
+
+  const DataPath inputGeometryPath({ITKTestBase::k_ImageGeometryPath});
+  const DataPath cellDataPath = inputGeometryPath.createChildPath(ITKTestBase::k_ImageCellDataName);
+  const DataPath inputDataPath = cellDataPath.createChildPath(ITKTestBase::k_InputDataName);
+  const DataObjectNameParameter::ValueType outputArrayName = ITKTestBase::k_OutputDataPath;
+
+  { // Start Image Comparison Scope
+    const fs::path inputFilePath = fs::path(unit_test::k_SourceDir.view()) / unit_test::k_DataDir.view() / "JSONFilters" / "Input/RA-Float.nrrd";
+    Result<> imageReadResult = ITKTestBase::ReadImage(dataStructure, inputFilePath, inputGeometryPath, ITKTestBase::k_ImageCellDataName, ITKTestBase::k_InputDataName);
+    SIMPLNX_RESULT_REQUIRE_VALID(imageReadResult)
+  } // End Image Comparison Scope
+
+  Arguments args;
+  args.insertOrAssign(ITKMinimumProjectionImageFilter::k_RemoveOriginalGeometry_Key, std::make_any<bool>(true));
   args.insertOrAssign(ITKMinimumProjectionImageFilter::k_InputImageGeomPath_Key, std::make_any<DataPath>(inputGeometryPath));
   args.insertOrAssign(ITKMinimumProjectionImageFilter::k_InputImageDataPath_Key, std::make_any<DataPath>(inputDataPath));
   args.insertOrAssign(ITKMinimumProjectionImageFilter::k_OutputImageArrayName_Key, std::make_any<DataObjectNameParameter::ValueType>(outputArrayName));
@@ -44,11 +114,72 @@ TEST_CASE("ITKImageProcessing::ITKMinimumProjectionImageFilter(z_projection)", "
   auto executeResult = filter.execute(dataStructure, args);
   SIMPLNX_RESULT_REQUIRE_VALID(executeResult.result)
 
-  const fs::path baselineFilePath = fs::path(nx::core::unit_test::k_DataDir.view()) / "JSONFilters/Baseline/BasicFilters_MinimumProjectionImageFilter_z_projection.nrrd";
-  const DataPath baselineGeometryPath({ITKTestBase::k_BaselineGeometryPath});
-  const DataPath baseLineCellDataPath = baselineGeometryPath.createChildPath(ITKTestBase::k_ImageCellDataName);
-  const DataPath baselineDataPath = baseLineCellDataPath.createChildPath(ITKTestBase::k_BaselineDataPath);
-  const Result<> readBaselineResult = ITKTestBase::ReadImage(dataStructure, baselineFilePath, baselineGeometryPath, ITKTestBase::k_ImageCellDataName, ITKTestBase::k_BaselineDataPath);
-  Result<> compareResult = ITKTestBase::CompareImages(dataStructure, baselineGeometryPath, baselineDataPath, inputGeometryPath, cellDataPath.createChildPath(outputArrayName), 0.0001);
-  SIMPLNX_RESULT_REQUIRE_VALID(compareResult)
+  const std::string md5Hash = ITKTestBase::ComputeMd5Hash(dataStructure, cellDataPath.createChildPath(outputArrayName));
+  REQUIRE(md5Hash == "6c16b87a823ca190294ac8b678ba4300");
+}
+
+TEST_CASE("ITKImageProcessing::ITKMinimumProjectionImageFilter: Image Short Test", "[ITKImageProcessing][ITKMinimumProjectionImage][defaults]")
+{
+  DataStructure dataStructure;
+  const ITKMinimumProjectionImageFilter filter;
+
+  const DataPath inputGeometryPath({ITKTestBase::k_ImageGeometryPath});
+  const DataPath cellDataPath = inputGeometryPath.createChildPath(ITKTestBase::k_ImageCellDataName);
+  const DataPath inputDataPath = cellDataPath.createChildPath(ITKTestBase::k_InputDataName);
+  const DataObjectNameParameter::ValueType outputArrayName = ITKTestBase::k_OutputDataPath;
+
+  { // Start Image Comparison Scope
+    const fs::path inputFilePath = fs::path(unit_test::k_SourceDir.view()) / unit_test::k_DataDir.view() / "JSONFilters" / "Input/Ramp-Up-Short.nrrd";
+    Result<> imageReadResult = ITKTestBase::ReadImage(dataStructure, inputFilePath, inputGeometryPath, ITKTestBase::k_ImageCellDataName, ITKTestBase::k_InputDataName);
+    SIMPLNX_RESULT_REQUIRE_VALID(imageReadResult)
+  } // End Image Comparison Scope
+
+  Arguments args;
+  args.insertOrAssign(ITKMinimumProjectionImageFilter::k_RemoveOriginalGeometry_Key, std::make_any<bool>(true));
+  args.insertOrAssign(ITKMinimumProjectionImageFilter::k_InputImageGeomPath_Key, std::make_any<DataPath>(inputGeometryPath));
+  args.insertOrAssign(ITKMinimumProjectionImageFilter::k_InputImageDataPath_Key, std::make_any<DataPath>(inputDataPath));
+  args.insertOrAssign(ITKMinimumProjectionImageFilter::k_OutputImageArrayName_Key, std::make_any<DataObjectNameParameter::ValueType>(outputArrayName));
+  args.insertOrAssign(ITKMinimumProjectionImageFilter::k_ProjectionDimension_Key, std::make_any<UInt32Parameter::ValueType>(1));
+
+  auto preflightResult = filter.preflight(dataStructure, args);
+  SIMPLNX_RESULT_REQUIRE_VALID(preflightResult.outputActions)
+
+  auto executeResult = filter.execute(dataStructure, args);
+  SIMPLNX_RESULT_REQUIRE_VALID(executeResult.result)
+
+  const std::string md5Hash = ITKTestBase::ComputeMd5Hash(dataStructure, cellDataPath.createChildPath(outputArrayName));
+  REQUIRE(md5Hash == "c4d83f61ffd5cc3a163155bb5d6a0698");
+}
+
+TEST_CASE("ITKImageProcessing::ITKMinimumProjectionImageFilter: Image RGB Test", "[ITKImageProcessing][ITKMinimumProjectionImage][defaults]")
+{
+  DataStructure dataStructure;
+  const ITKMinimumProjectionImageFilter filter;
+
+  const DataPath inputGeometryPath({ITKTestBase::k_ImageGeometryPath});
+  const DataPath cellDataPath = inputGeometryPath.createChildPath(ITKTestBase::k_ImageCellDataName);
+  const DataPath inputDataPath = cellDataPath.createChildPath(ITKTestBase::k_InputDataName);
+  const DataObjectNameParameter::ValueType outputArrayName = ITKTestBase::k_OutputDataPath;
+
+  { // Start Image Comparison Scope
+    const fs::path inputFilePath = fs::path(unit_test::k_SourceDir.view()) / unit_test::k_DataDir.view() / "JSONFilters" / "Input/VM1111Shrink-RGB.png";
+    Result<> imageReadResult = ITKTestBase::ReadImage(dataStructure, inputFilePath, inputGeometryPath, ITKTestBase::k_ImageCellDataName, ITKTestBase::k_InputDataName);
+    SIMPLNX_RESULT_REQUIRE_VALID(imageReadResult)
+  } // End Image Comparison Scope
+
+  Arguments args;
+  args.insertOrAssign(ITKMinimumProjectionImageFilter::k_RemoveOriginalGeometry_Key, std::make_any<bool>(true));
+  args.insertOrAssign(ITKMinimumProjectionImageFilter::k_InputImageGeomPath_Key, std::make_any<DataPath>(inputGeometryPath));
+  args.insertOrAssign(ITKMinimumProjectionImageFilter::k_InputImageDataPath_Key, std::make_any<DataPath>(inputDataPath));
+  args.insertOrAssign(ITKMinimumProjectionImageFilter::k_OutputImageArrayName_Key, std::make_any<DataObjectNameParameter::ValueType>(outputArrayName));
+  args.insertOrAssign(ITKMinimumProjectionImageFilter::k_ProjectionDimension_Key, std::make_any<UInt32Parameter::ValueType>(1));
+
+  auto preflightResult = filter.preflight(dataStructure, args);
+  SIMPLNX_RESULT_REQUIRE_VALID(preflightResult.outputActions)
+
+  auto executeResult = filter.execute(dataStructure, args);
+  SIMPLNX_RESULT_REQUIRE_VALID(executeResult.result)
+
+  const std::string md5Hash = ITKTestBase::ComputeMd5Hash(dataStructure, cellDataPath.createChildPath(outputArrayName));
+  REQUIRE(md5Hash == "344c2d7cf14b5e8b30b266b77a0548c2");
 }
