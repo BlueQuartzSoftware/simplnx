62ad45ef5fc2df67741256efdbd95a1fc6920786
nyoungbq <nathan.young@bluequartz.net>
2025-01-15 10:39:27 -0500

    [broken compile] code review and centralization

---
+++ src/Plugins/ITKImageProcessing/src/ITKImageProcessing/Common/ProjectionUtils.hpp
@@ -0,0 +1,211 @@
+#pragma once
+
+#include "simplnx/Common/Result.hpp"
+#include "simplnx/Common/Types.hpp"
+#include "simplnx/Filter/Actions/CreateImageGeometryAction.hpp"
+#include "simplnx/Filter/IFilter.hpp"
+
+#include "ITKArrayHelper.hpp"
+
+using namespace nx::core;
+
+namespace ProjectionUtilities
+{
+// uint8, int16, uint16, float32
+using ITKProjectionSupportedOutputTypes = ArrayTypeOptions<false, false, true, true, true, false, false, false, false, true, false>;
+
+template <class T>
+struct FixedOutputTypeHelper
+{
+  template <class PixelT>
+  using FilterOutputType = T;
+};
+
+template <class ArrayOptionsType>
+struct RunITKProjectionDataCheckFunctor
+{
+  template <class FixedOutputType>
+  auto operator()(const DataStructure& dataStructure, const DataPath& selectedInputArray, const DataPath& imageGeomPath, const DataPath& outputArrayPath) const
+  {
+    return ITK::DataCheck<ArrayOptionsType, typename FixedOutputTypeHelper<FixedOutputType>::FilterOutputType>(dataStructure, selectedInputArray, imageGeomPath, outputArrayPath);
+  }
+};
+
+template <class ArrayOptionsType, class ITKFunctorType>
+struct RunITKProjectionExecuteFunctor
+{
+  template <class FixedOutputType>
+  auto operator()(DataStructure& dataStructure, const DataPath& selectedInputArray, const DataPath& imageGeomPath, const DataPath& outputArrayPath, ITKFunctorType&& itkFunctor,
+                  const std::atomic_bool& shouldCancel) const
+  {
+    return ITK::Execute<ArrayOptionsType, typename FixedOutputTypeHelper<FixedOutputType>::FilterOutputType>(dataStructure, selectedInputArray, imageGeomPath, outputArrayPath, itkFunctor,
+                                                                                                             shouldCancel);
+  }
+};
+
+template <class ArrayTypeOptions, class FuncT, class FallbackFuncT, class... ArgsT>
+auto RunTemplateFunctor(FuncT&& func, FallbackFuncT&& fallback, DataType dataType, ArgsT&&... args)
+{
+  if constexpr(ArrayTypeOptions::UsingBoolean)
+  {
+    if(dataType == DataType::boolean)
+    {
+      return func.template operator()<bool>(std::forward<ArgsT>(args)...);
+    }
+  }
+  if constexpr(ArrayTypeOptions::UsingInt8)
+  {
+    if(dataType == DataType::int8)
+    {
+      return func.template operator()<int8>(std::forward<ArgsT>(args)...);
+    }
+  }
+  if constexpr(ArrayTypeOptions::UsingInt16)
+  {
+    if(dataType == DataType::int16)
+    {
+      return func.template operator()<int16>(std::forward<ArgsT>(args)...);
+    }
+  }
+  if constexpr(ArrayTypeOptions::UsingInt32)
+  {
+    if(dataType == DataType::int32)
+    {
+      return func.template operator()<int32>(std::forward<ArgsT>(args)...);
+    }
+  }
+  if constexpr(ArrayTypeOptions::UsingInt64)
+  {
+    if(dataType == DataType::int64)
+    {
+      return func.template operator()<int64>(std::forward<ArgsT>(args)...);
+    }
+  }
+  if constexpr(ArrayTypeOptions::UsingUInt8)
+  {
+    if(dataType == DataType::uint8)
+    {
+      return func.template operator()<uint8>(std::forward<ArgsT>(args)...);
+    }
+  }
+  if constexpr(ArrayTypeOptions::UsingUInt16)
+  {
+    if(dataType == DataType::uint16)
+    {
+      return func.template operator()<uint16>(std::forward<ArgsT>(args)...);
+    }
+  }
+  if constexpr(ArrayTypeOptions::UsingUInt32)
+  {
+    if(dataType == DataType::uint32)
+    {
+      return func.template operator()<uint32>(std::forward<ArgsT>(args)...);
+    }
+  }
+  if constexpr(ArrayTypeOptions::UsingUInt64)
+  {
+    if(dataType == DataType::uint64)
+    {
+      return func.template operator()<uint64>(std::forward<ArgsT>(args)...);
+    }
+  }
+  if constexpr(ArrayTypeOptions::UsingFloat32)
+  {
+    if(dataType == DataType::float32)
+    {
+      return func.template operator()<float32>(std::forward<ArgsT>(args)...);
+    }
+  }
+  if constexpr(ArrayTypeOptions::UsingFloat64)
+  {
+    if(dataType == DataType::float64)
+    {
+      return func.template operator()<float64>(std::forward<ArgsT>(args)...);
+    }
+  }
+  return fallback(dataType);
+}
+
+template <class ArrayOptionsType>
+IFilter::PreflightResult RunITKProjectionDataCheck(const DataStructure& dataStructure, const DataPath& selectedInputArray, const DataPath& imageGeomPath, const std::string& outputGeomName,
+                                                   bool performInPlace, const std::string& outputArrayName)
+{
+  DataPath outputArrayPath = selectedInputArray.replaceName(outputArrayName);
+  Result<OutputActions> resultOutputActions;
+  // The input geometry must be preserved, so we will just copy the needed array into newly created output geometry
+  if(!performInPlace)
+  {
+    DataPath outputGeomPath({outputGeomName});
+
+    const auto& originalGeometry = dataStructure.getDataRefAs<ImageGeom>(imageGeomPath);
+
+    // Make copy of input geometry
+    resultOutputActions.value().appendAction(std::make_unique<CreateImageGeometryAction>(
+        outputGeomPath, originalGeometry.getDimensions().toContainer<CreateImageGeometryAction::DimensionType>(), originalGeometry.getOrigin().toContainer<CreateImageGeometryAction::OriginType>(),
+        originalGeometry.getSpacing().toContainer<CreateImageGeometryAction::SpacingType>(), originalGeometry.getCellDataPath().getTargetName()));
+
+    outputArrayPath = outputGeomPath.createChildPath(originalGeometry.getCellDataPath().getTargetName()).createChildPath(outputArrayName);
+  }
+
+  auto fallbackFunc = [](DataType dataType) {
+    return MakeErrorResult<OutputActions>(-76590, fmt::format("Input {} type is not currently supported. Please reach out to devs if you have a use case.", DataTypeToString(dataType)));
+  };
+
+  DataType type = dataStructure.getDataRefAs<IDataArray>(selectedInputArray).getDataType();
+  Result<OutputActions> helperOutputActions = RunTemplateFunctor<ITKProjectionSupportedOutputTypes>(RunITKProjectionDataCheckFunctor<ArrayOptionsType>{}, fallbackFunc, type, dataStructure,
+                                                                                                    selectedInputArray, imageGeomPath, outputArrayPath);
+
+  if(helperOutputActions.invalid())
+  {
+    return {std::move(helperOutputActions)};
+  }
+
+  // Consolidate actions
+  resultOutputActions.value().actions.insert(resultOutputActions.value().actions.end(), helperOutputActions.value().actions.begin(), helperOutputActions.value().actions.end());
+
+  return {std::move(resultOutputActions)};
+}
+
+template <class ArrayOptionsType, class ITKFunctorType>
+Result<> RunITKProjectionExecute(DataStructure& dataStructure, const DataPath& selectedInputArray, const DataPath& imageGeomPath, const std::atomic_bool& shouldCancel,
+                                 const std::string& outputArrayName, bool performInPlace, ITKFunctorType&& itkFunctor, const std::string& outputImageGeomName)
+{
+  DataPath outputArrayPath = selectedInputArray.replaceName(outputArrayName);
+
+  DataPath finalImageGeomPath = imageGeomPath;
+
+  if(!performInPlace)
+  {
+    const auto& originalGeometry = dataStructure.getDataRefAs<ImageGeom>(imageGeomPath);
+
+    finalImageGeomPath = DataPath({outputImageGeomName});
+    outputArrayPath = finalImageGeomPath.createChildPath(originalGeometry.getCellDataPath().getTargetName()).createChildPath(outputArrayName);
+  }
+
+  DataType type = dataStructure.getDataRefAs<IDataArray>(selectedInputArray).getDataType();
+
+  auto fallbackFunc = [](DataType dataType) {
+    return MakeErrorResult(-76591, fmt::format("Input {} type is not currently supported. Please reach out to devs if you have a use case.", DataTypeToString(dataType)));
+  };
+
+  Result<> result = RunTemplateFunctor<ITKProjectionSupportedOutputTypes>(RunITKProjectionExecuteFunctor<ArrayOptionsType, ITKFunctorType>{}, fallbackFunc, type, dataStructure, selectedInputArray,
+                                                                          finalImageGeomPath, outputArrayPath, itkFunctor, shouldCancel);
+
+  if(result.invalid())
+  {
+    return result;
+  }
+
+  auto& imageGeom = dataStructure.getDataRefAs<ImageGeom>(finalImageGeomPath);
+  auto iArrayTupleShape = dataStructure.getDataAs<IArray>(outputArrayPath)->getTupleShape();
+
+  // Update the Image Geometry with the new dimensions
+  imageGeom.setDimensions({iArrayTupleShape[2], iArrayTupleShape[1], iArrayTupleShape[0]});
+
+  // Update the AttributeMatrix with the new tuple shape. THIS WILL ALSO CHANGE ANY OTHER DATA ARRAY THAT IS ALSO
+  // STORED IN THAT ATTRIBUTE MATRIX
+  dataStructure.getDataAs<AttributeMatrix>(outputArrayPath.getParent())->resizeTuples(iArrayTupleShape);
+
+  return {};
+}
+} // namespace ProjectionUtilities
--- src/Plugins/ITKImageProcessing/src/ITKImageProcessing/Filters/ITKMedianProjectionImageFilter.cpp
+++ src/Plugins/ITKImageProcessing/src/ITKImageProcessing/Filters/ITKMedianProjectionImageFilter.cpp
@@ -12,6 +12,8 @@
 #include "simplnx/Parameters/NumberParameter.hpp"
 #include "simplnx/Parameters/StringParameter.hpp"
 
+#include "ITKImageProcessing/Common/ProjectionUtils.hpp"
+
 #include <itkMedianProjectionImageFilter.h>
 
 using namespace nx::core;
@@ -23,18 +25,6 @@ using ArrayOptionsType = ITK::ScalarPixelIdTypeList;
 // Uncommenting below line enables RGB/ARGB images that are currently unsupported
 // using ArrayOptionsType = ITK::ArrayOptions<ITK::ArrayComponentOptions<true, false, true>, ITK::ArrayUseAllTypes>;
 // VectorPixelIDTypeList;
-
-template <class PixelT>
-using FilterOutputTypeUI8 = uint8;
-
-template <class PixelT>
-using FilterOutputTypeI16 = int16;
-
-template <class PixelT>
-using FilterOutputTypeUI16 = uint16;
-
-template <class PixelT>
-using FilterOutputTypeF32 = float32;
 
 struct ITKMedianProjectionImageFilterFunctor
 {
@@ -127,78 +117,10 @@ IFilter::PreflightResult ITKMedianProjectionImageFilter::preflightImpl(const Dat
   auto selectedInputArray = filterArgs.value<DataPath>(k_InputImageDataPath_Key);
   auto outputArrayName = filterArgs.value<DataObjectNameParameter::ValueType>(k_OutputImageArrayName_Key);
   auto projectionDimension = filterArgs.value<uint32>(k_ProjectionDimension_Key);
-  auto preformInPlace = filterArgs.value<bool>(k_RemoveOriginalGeometry_Key);
+  auto performInPlace = filterArgs.value<bool>(k_RemoveOriginalGeometry_Key);
   auto outputGeomName = filterArgs.value<std::string>(k_OutputImageGeomName_Key);
-  DataPath outputArrayPath = selectedInputArray.replaceName(outputArrayName);
 
-  Result<OutputActions> resultOutputActions;
-  // The input geometry must be preserved, so we will just copy the needed array into newly created output geometry
-  if(!preformInPlace)
-  {
-    DataPath outputGeomPath({outputGeomName});
-
-    const auto& originalGeometry = dataStructure.getDataRefAs<ImageGeom>(imageGeomPath);
-
-    // Make copy of input geometry
-    resultOutputActions.value().appendAction(std::make_unique<CreateImageGeometryAction>(
-        outputGeomPath, originalGeometry.getDimensions().toContainer<CreateImageGeometryAction::DimensionType>(), originalGeometry.getOrigin().toContainer<CreateImageGeometryAction::OriginType>(),
-        originalGeometry.getSpacing().toContainer<CreateImageGeometryAction::SpacingType>(), originalGeometry.getCellDataPath().getTargetName()));
-
-    outputArrayPath = outputGeomPath.createChildPath(originalGeometry.getCellDataPath().getTargetName()).createChildPath(outputArrayName);
-  }
-
-  Result<OutputActions> helperOutputActions = {};
-  DataType type = dataStructure.getDataAs<IDataArray>(selectedInputArray)->getDataType();
-  switch(type)
-  {
-  case DataType::uint8: {
-    helperOutputActions =
-        ITK::DataCheck<cxITKMedianProjectionImageFilter::ArrayOptionsType, cxITKMedianProjectionImageFilter::FilterOutputTypeUI8>(dataStructure, selectedInputArray, imageGeomPath, outputArrayPath);
-    break;
-  }
-  case DataType::int16: {
-    helperOutputActions =
-        ITK::DataCheck<cxITKMedianProjectionImageFilter::ArrayOptionsType, cxITKMedianProjectionImageFilter::FilterOutputTypeI16>(dataStructure, selectedInputArray, imageGeomPath, outputArrayPath);
-    break;
-  }
-  case DataType::uint16: {
-    helperOutputActions =
-        ITK::DataCheck<cxITKMedianProjectionImageFilter::ArrayOptionsType, cxITKMedianProjectionImageFilter::FilterOutputTypeUI16>(dataStructure, selectedInputArray, imageGeomPath, outputArrayPath);
-    break;
-  }
-  case DataType::float32: {
-    helperOutputActions =
-        ITK::DataCheck<cxITKMedianProjectionImageFilter::ArrayOptionsType, cxITKMedianProjectionImageFilter::FilterOutputTypeF32>(dataStructure, selectedInputArray, imageGeomPath, outputArrayPath);
-    break;
-  }
-  case DataType::int8:
-    [[fallthrough]];
-  case DataType::int32:
-    [[fallthrough]];
-  case DataType::uint32:
-    [[fallthrough]];
-  case DataType::int64:
-    [[fallthrough]];
-  case DataType::uint64: {
-    [[fallthrough]];
-  }
-  case DataType::float64: {
-    [[fallthrough]];
-  }
-  case DataType::boolean: {
-    return MakePreflightErrorResult(-76590, fmt::format("Input {} type is not currently supported. Please reach out to devs if you have a use case.", DataTypeToString(type)));
-  }
-  }
-
-  if(helperOutputActions.invalid())
-  {
-    return {std::move(helperOutputActions)};
-  }
-
-  // Consolidate actions
-  resultOutputActions.value().actions.insert(resultOutputActions.value().actions.end(), helperOutputActions.value().actions.begin(), helperOutputActions.value().actions.end());
-
-  return {std::move(resultOutputActions)};
+  return ProjectionUtilities::RunITKProjectionDataCheck<cxITKMedianProjectionImageFilter::ArrayOptionsType>(dataStructure, selectedInputArray, imageGeomPath, outputGeomName, performInPlace, outputArrayName);
 }
 
 //------------------------------------------------------------------------------
@@ -208,79 +130,13 @@ Result<> ITKMedianProjectionImageFilter::executeImpl(DataStructure& dataStructur
   auto imageGeomPath = filterArgs.value<DataPath>(k_InputImageGeomPath_Key);
   auto selectedInputArray = filterArgs.value<DataPath>(k_InputImageDataPath_Key);
   auto outputArrayName = filterArgs.value<DataObjectNameParameter::ValueType>(k_OutputImageArrayName_Key);
-  DataPath outputArrayPath = selectedInputArray.replaceName(outputArrayName);
-
-  auto preformInPlace = filterArgs.value<bool>(k_RemoveOriginalGeometry_Key);
-
-  if(!preformInPlace)
-  {
-    const auto& originalGeometry = dataStructure.getDataRefAs<ImageGeom>(imageGeomPath);
-
-    imageGeomPath = DataPath({filterArgs.value<std::string>(k_OutputImageGeomName_Key)});
-    outputArrayPath = imageGeomPath.createChildPath(originalGeometry.getCellDataPath().getTargetName()).createChildPath(outputArrayName);
-  }
-
+  auto outputImageGeomName = filterArgs.value<std::string>(k_OutputImageGeomName_Key);
+  auto performInPlace = filterArgs.value<bool>(k_RemoveOriginalGeometry_Key);
   auto projectionDimension = filterArgs.value<uint32>(k_ProjectionDimension_Key);
 
   const cxITKMedianProjectionImageFilter::ITKMedianProjectionImageFilterFunctor itkFunctor = {projectionDimension};
 
-  Result<> result = {};
-  DataType type = dataStructure.getDataAs<IDataArray>(selectedInputArray)->getDataType();
-  switch(type)
-  {
-  case DataType::uint8: {
-    result = ITK::Execute<cxITKMedianProjectionImageFilter::ArrayOptionsType, cxITKMedianProjectionImageFilter::FilterOutputTypeUI8>(dataStructure, selectedInputArray, imageGeomPath, outputArrayPath,
-                                                                                                                                     itkFunctor, shouldCancel);
-    break;
-  }
-  case DataType::int16: {
-    result = ITK::Execute<cxITKMedianProjectionImageFilter::ArrayOptionsType, cxITKMedianProjectionImageFilter::FilterOutputTypeI16>(dataStructure, selectedInputArray, imageGeomPath, outputArrayPath,
-                                                                                                                                     itkFunctor, shouldCancel);
-    break;
-  }
-  case DataType::uint16: {
-    result = ITK::Execute<cxITKMedianProjectionImageFilter::ArrayOptionsType, cxITKMedianProjectionImageFilter::FilterOutputTypeUI16>(dataStructure, selectedInputArray, imageGeomPath, outputArrayPath,
-                                                                                                                                      itkFunctor, shouldCancel);
-    break;
-  }
-  case DataType::float32: {
-    result = ITK::Execute<cxITKMedianProjectionImageFilter::ArrayOptionsType, cxITKMedianProjectionImageFilter::FilterOutputTypeF32>(dataStructure, selectedInputArray, imageGeomPath, outputArrayPath,
-                                                                                                                                     itkFunctor, shouldCancel);
-    break;
-  }
-  case DataType::int8:
-    [[fallthrough]];
-  case DataType::int32:
-    [[fallthrough]];
-  case DataType::uint32:
-    [[fallthrough]];
-  case DataType::int64:
-    [[fallthrough]];
-  case DataType::uint64: {
-    [[fallthrough]];
-  }
-  case DataType::float64: {
-    [[fallthrough]];
-  }
-  case DataType::boolean: {
-    return MakeErrorResult(-76591, fmt::format("Input {} type is not currently supported. Please reach out to devs if you have a use case.", DataTypeToString(type)));
-  }
-  }
-  if(result.invalid())
-  {
-    return result;
-  }
-
-  auto& imageGeom = dataStructure.getDataRefAs<ImageGeom>(imageGeomPath);
-  auto iArrayTupleShape = dataStructure.getDataAs<IArray>(outputArrayPath)->getTupleShape();
-
-  // Update the Image Geometry with the new dimensions
-  imageGeom.setDimensions({iArrayTupleShape[2], iArrayTupleShape[1], iArrayTupleShape[0]});
-
-  // Update the AttributeMatrix with the new tuple shape. THIS WILL ALSO CHANGE ANY OTHER DATA ARRAY THAT IS ALSO
-  // STORED IN THAT ATTRIBUTE MATRIX
-  dataStructure.getDataAs<AttributeMatrix>(outputArrayPath.getParent())->resizeTuples(iArrayTupleShape);
-
-  return {};
+  return ProjectionUtilities::RunITKProjectionExecute<cxITKMedianProjectionImageFilter::ArrayOptionsType>(dataStructure, selectedInputArray, imageGeomPath, shouldCancel, outputArrayName, performInPlace, itkFunctor,
+                                                                                     outputImageGeomName);
 }
 } // namespace nx::core
