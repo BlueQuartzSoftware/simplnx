a9506a1e9d008544950f4443b0632e8af3ad0fb1
nyoungbq <nathan.young@bluequartz.net>
2025-05-14 13:47:43 -0400

    Move unique_ptr out of loop

--- src/Plugins/SimplnxCore/src/SimplnxCore/Filters/Algorithms/ComputeArrayHistogram.cpp
+++ src/Plugins/SimplnxCore/src/SimplnxCore/Filters/Algorithms/ComputeArrayHistogram.cpp
@@ -48,6 +48,12 @@ Result<> ComputeArrayHistogram::operator()()
 
   std::atomic<usize> overflow = 0;
 
+  std::unique_ptr<MaskCompareUtilities::MaskCompare> mask = nullptr;
+  if(m_InputValues->MaskPath.has_value())
+  {
+    mask = MaskCompareUtilities::InstantiateMaskCompare(m_DataStructure, m_InputValues->MaskPath.value());
+  }
+
   for(int32 i = 0; i < selectedArrayPaths.size(); i++)
   {
     if(m_ShouldCancel)
@@ -59,20 +65,7 @@ Result<> ComputeArrayHistogram::operator()()
     auto* binRanges = m_DataStructure.getDataAs<IDataArray>(m_InputValues->CreatedBinRangeDataPaths.at(i));
     auto& counts = m_DataStructure.getDataAs<DataArray<uint64>>(m_InputValues->CreatedHistogramCountsDataPaths.at(i))->getDataStoreRef();
     auto& mostPopulated = m_DataStructure.getDataAs<DataArray<uint64>>(m_InputValues->CreatedBinMostPopulatedDataPaths.at(i))->getDataStoreRef();
-    INeighborList* modalBinRanges = nullptr;
-    if(m_InputValues->CreatedBinModalRangesDataPaths.has_value())
-    {
-      auto modalBinRangesPaths = m_InputValues->CreatedBinModalRangesDataPaths.value();
-      modalBinRanges = m_DataStructure.getDataAs<INeighborList>(modalBinRangesPaths.at(i));
-    }
 
-    std::unique_ptr<MaskCompareUtilities::MaskCompare> mask = nullptr;
-    if(m_InputValues->MaskPath.has_value())
-    {
-      mask = MaskCompareUtilities::InstantiateMaskCompare(m_DataStructure, m_InputValues->MaskPath.value());
-    }
-
-    Result<> result = {};
     if(m_InputValues->UserDefinedRange)
     {
       ExecuteParallelFunctor(HistogramUtilities::concurrent::InstantiateHistogramImplFunctor{}, inputData->getDataType(), taskRunner, inputData, binRanges,
@@ -84,21 +77,31 @@ Result<> ComputeArrayHistogram::operator()()
                              mostPopulated, mask, overflow);
     }
 
-    if(modalBinRanges != nullptr)
-    {
-      ExecuteParallelFunctor<HistogramUtilities::concurrent::CalculateModalBinRangesImplFunctor, NoBooleanType>(
-          HistogramUtilities::concurrent::CalculateModalBinRangesImplFunctor{}, inputData->getDataType(), taskRunner, inputData, binRanges, modalBinRanges, m_ShouldCancel);
-    }
-
-    if(result.invalid())
-    {
-      return result;
-    }
-
     if(overflow > 0)
     {
       const std::string arrayName = inputData->getName();
       ComputeArrayHistogram::updateProgress(fmt::format("{} values not categorized into bin for array {}", overflow.load(), arrayName));
+    }
+  }
+
+  taskRunner.wait();
+
+  for(int32 i = 0; i < selectedArrayPaths.size(); i++)
+  {
+    if(m_ShouldCancel)
+    {
+      return {};
+    }
+
+    const auto* inputData = m_DataStructure.getDataAs<IDataArray>(selectedArrayPaths[i]);
+    auto* binRanges = m_DataStructure.getDataAs<IDataArray>(m_InputValues->CreatedBinRangeDataPaths.at(i));
+    INeighborList* modalBinRanges = nullptr;
+    if(m_InputValues->CreatedBinModalRangesDataPaths.has_value())
+    {
+      auto modalBinRangesPaths = m_InputValues->CreatedBinModalRangesDataPaths.value();
+      modalBinRanges = m_DataStructure.getDataAs<INeighborList>(modalBinRangesPaths.at(i));
+      ExecuteParallelFunctor<HistogramUtilities::concurrent::CalculateModalBinRangesImplFunctor, NoBooleanType>(
+          HistogramUtilities::concurrent::CalculateModalBinRangesImplFunctor{}, inputData->getDataType(), taskRunner, inputData, binRanges, modalBinRanges, m_ShouldCancel);
     }
   }
 
