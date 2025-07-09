5871d5bf3f8e4bf86991d501639126455257929e
Joey Kleingers <joey.kleingers@bluequartz.net>
2025-07-09 15:24:37 -0400

    Fix incorrect array tuple shape and geometry in FlyingEdges3D test.
    Signed-off-by: Joey Kleingers <joey.kleingers@bluequartz.net>

--- src/Plugins/SimplnxCore/test/FlyingEdges3DTest.cpp
+++ src/Plugins/SimplnxCore/test/FlyingEdges3DTest.cpp
@@ -38,7 +38,7 @@ TEST_CASE("SimplnxCore::Image Contouring Valid Execution", "[SimplnxCore][Flying
 {
   UnitTest::LoadPlugins();
 
-  const nx::core::UnitTest::TestFileSentinel testDataSentinel(nx::core::unit_test::k_CMakeExecutable, nx::core::unit_test::k_TestFilesDir, "flying_edges_exemplar.tar.gz",
+  const nx::core::UnitTest::TestFileSentinel testDataSentinel(nx::core::unit_test::k_CMakeExecutable, nx::core::unit_test::k_TestFilesDir, "flying_edges_exemplar_v2.tar.gz",
                                                               "flying_edges_exemplar.dream3d");
 
   // Read Exemplar DREAM3D File Filter
