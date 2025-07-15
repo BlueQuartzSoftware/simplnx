6df93ee2b22f3b0b162a900ba68e5fd5cee1596d
Matthew Marine <matthew.marine@bluequartz.net>
2025-07-15 12:23:42 -0400

    Clang-Format

--- src/simplnx/DataStructure/StringArray.cpp
+++ src/simplnx/DataStructure/StringArray.cpp
@@ -234,7 +234,6 @@ void StringArray::swapTuples(usize index0, usize index1)
   (*m_Strings)[index1] = value;
 }
 
-
 void StringArray::setStore(const std::shared_ptr<AbstractStringStore>& newStore)
 {
   m_Strings = newStore;
