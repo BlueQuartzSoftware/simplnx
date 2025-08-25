9340b478cb832e7dd49ed2c2527149efca0c833e
Michael Jackson <mike.jackson@bluequartz.net>
2025-08-25 15:09:02 -0400

    API: ColorTable API added to convert from JSON to vector of structs (#1400)
    OPT: ColorTable API added to convert from JSON to Structs

--- src/simplnx/Utilities/ColorTableUtilities.cpp
+++ src/simplnx/Utilities/ColorTableUtilities.cpp
@@ -5,16 +5,112 @@
 
 #include <fmt/format.h>
 
-#include <iostream>
-
 using namespace nx::core;
+
+/* *****************************************************************************
+ *
+ * ColorTableUtilities::ColorPreset Implementation
+ *
+ ******************************************************************************/
+
+ColorTableUtilities::ColorPreset::ColorPreset() = default;
+
+ColorTableUtilities::ColorPreset::ColorPreset(const nlohmann::json& a)
+{
+  if(a.contains("Name") && a["Name"].is_string())
+  {
+    this->Name = a["Name"].get<std::string>();
+  }
+  if(a.contains("ColorSpace") && a["ColorSpace"].is_string())
+  {
+    this->ColorSpace = a["ColorSpace"].get<std::string>();
+  }
+  if(a.contains("Source") && a["Source"].is_string())
+  {
+    this->Source = a["Source"].get<std::string>();
+  }
+  if(a.contains("Licence") && a["Licence"].is_string())
+  {
+    this->License = a["Licence"].get<std::string>();
+  }
+  if(a.contains("Creator") && a["Creator"].is_string())
+  {
+    this->Creator = a["Creator"].get<std::string>();
+  }
+
+  if(a.contains("NanColor") && a["NanColor"].is_array())
+  {
+    this->NanColor = a["NanColor"].get<std::vector<float>>();
+  }
+  if(a.contains("RGBPoints") && a["RGBPoints"].is_array())
+  {
+    this->RGBPoints = a["RGBPoints"].get<std::vector<float>>();
+  }
+  if(a.contains("IndexedColors") && a["IndexedColors"].is_array())
+  {
+    this->IndexedColors = a["IndexedColors"].get<std::vector<float>>();
+  }
+
+  if(a.contains("DefaultMap") && a["DefaultMap"].is_boolean())
+  {
+    this->DefaultMap = a["DefaultMap"].get<bool>();
+  }
+
+  if(a.contains("Annotations"))
+  {
+    this->Annotations = a["Annotations"].get<nlohmann::json>();
+  }
+  this->Invalid = false;
+}
+
+nlohmann::json ColorTableUtilities::ColorPreset::toJson() const
+{
+  nlohmann::json json;
+  json["Name"] = Name;
+  json["ColorSpace"] = ColorSpace;
+  json["Source"] = Source;
+  json["License"] = License;
+  json["Creator"] = Creator;
+  json["NanColor"] = NanColor;
+  json["RGBPoints"] = RGBPoints;
+  json["IndexedColors"] = IndexedColors;
+  json["DefaultMap"] = DefaultMap;
+  json["Annotations"] = Annotations;
+  return json;
+}
+
+std::string ColorTableUtilities::ColorPreset::toJsonString(int indent) const
+{
+  nlohmann::json json = toJson();
+  return json.dump(indent);
+}
+
+/* *****************************************************************************
+ *
+ * Begin ColorTableUtilities Functions
+ *
+ ******************************************************************************/
+
+bool ColorTableUtilities::IsValidPreset(const ColorPreset& preset)
+{
+  if(!preset.RGBPoints.empty() && ((preset.ColorSpace == "RGB" || preset.ColorSpace == "Diverging" || preset.ColorSpace == "Lab" || preset.ColorSpace == "CIELAB")))
+  {
+    return true;
+  }
+  return false;
+}
+
+bool ColorTableUtilities::IsValidIndexedPreset(const ColorPreset& preset)
+{
+  return !preset.IndexedColors.empty();
+}
 
 bool ColorTableUtilities::IsValidPreset(const nlohmann::json& preset)
 {
-  bool hasRgbPoints = preset.contains("RGBPoints");
-  bool rgbPointsIsArray = (hasRgbPoints && preset["RGBPoints"].is_array() ? true : false);
-  bool hasColorSpace = preset.contains("ColorSpace");
-  std::string colorSpaceValue = (hasColorSpace ? preset["ColorSpace"].get<std::string>() : "");
+  const bool hasRgbPoints = preset.contains("RGBPoints");
+  const bool rgbPointsIsArray = (hasRgbPoints && preset["RGBPoints"].is_array() ? true : false);
+  const bool hasColorSpace = preset.contains("ColorSpace");
+  const std::string colorSpaceValue = (hasColorSpace ? preset["ColorSpace"].get<std::string>() : "");
 
   if(rgbPointsIsArray && (hasColorSpace && (colorSpaceValue == "RGB" || colorSpaceValue == "Diverging" || colorSpaceValue == "Lab" || colorSpaceValue == "CIELAB")))
   {
@@ -43,9 +139,8 @@ Result<nlohmann::json> ColorTableUtilities::LoadAllRGBPresets()
   }
 
   // Sort the presets by name
-  std::sort(rgbPresets.begin(), rgbPresets.end(), [](const nlohmann::json& a, const nlohmann::json& b) {
-    return nx::core::StringUtilities::toLower(a["Name"].get<std::string>()) < nx::core::StringUtilities::toLower(b["Name"].get<std::string>());
-  });
+  std::sort(rgbPresets.begin(), rgbPresets.end(),
+            [](const nlohmann::json& a, const nlohmann::json& b) { return StringUtilities::toLower(a["Name"].get<std::string>()) < StringUtilities::toLower(b["Name"].get<std::string>()); });
 
   return {rgbPresets};
 }
@@ -63,11 +158,27 @@ Result<nlohmann::json> ColorTableUtilities::LoadAllIndexedPresets()
   }
 
   // Sort the presets by name
-  std::sort(indexedPresets.begin(), indexedPresets.end(), [](const nlohmann::json& a, const nlohmann::json& b) {
-    return nx::core::StringUtilities::toLower(a["Name"].get<std::string>()) < nx::core::StringUtilities::toLower(b["Name"].get<std::string>());
-  });
+  std::sort(indexedPresets.begin(), indexedPresets.end(),
+            [](const nlohmann::json& a, const nlohmann::json& b) { return StringUtilities::toLower(a["Name"].get<std::string>()) < StringUtilities::toLower(b["Name"].get<std::string>()); });
 
   return {indexedPresets};
+}
+
+Result<ColorTableUtilities::ColorPresetsVectorType> ColorTableUtilities::CacheAllPresets(bool rgbPreset, bool indexedPreset)
+{
+  ColorPresetsVectorType presets;
+  for(const auto& preset : ColorTable::k_DefaultColorTableJson)
+  {
+    if((indexedPreset && IsValidIndexedPreset(preset)) || (rgbPreset && IsValidPreset(preset)))
+    {
+      presets.emplace_back(preset);
+    }
+  }
+
+  // Sort the presets by name
+  std::sort(presets.begin(), presets.end(), [](const ColorPreset& a, const ColorPreset& b) { return StringUtilities::toLower(a.Name) < StringUtilities::toLower(b.Name); });
+
+  return {presets};
 }
 
 Result<nlohmann::json> ColorTableUtilities::LoadAllPresets()
@@ -83,9 +194,8 @@ Result<nlohmann::json> ColorTableUtilities::LoadAllPresets()
   }
 
   // Sort the presets by name
-  std::sort(presets.begin(), presets.end(), [](const nlohmann::json& a, const nlohmann::json& b) {
-    return nx::core::StringUtilities::toLower(a["Name"].get<std::string>()) < nx::core::StringUtilities::toLower(b["Name"].get<std::string>());
-  });
+  std::sort(presets.begin(), presets.end(),
+            [](const nlohmann::json& a, const nlohmann::json& b) { return StringUtilities::toLower(a["Name"].get<std::string>()) < StringUtilities::toLower(b["Name"].get<std::string>()); });
 
   return {presets};
 }
--- src/simplnx/Utilities/ColorTableUtilities.hpp
+++ src/simplnx/Utilities/ColorTableUtilities.hpp
@@ -1,9 +1,8 @@
 #pragma once
 
-#include "simplnx/simplnx_export.hpp"
-
 #include "simplnx/Common/Result.hpp"
 #include "simplnx/Common/Types.hpp"
+#include "simplnx/simplnx_export.hpp"
 
 #include <nlohmann/json.hpp>
 
@@ -12,18 +11,66 @@
 
 namespace nx::core::ColorTableUtilities
 {
+
+/**
+ * @brief This holds a ColorPreset object that can be read/written to JSON
+ */
+struct SIMPLNX_EXPORT ColorPreset
+{
+  std::string Name;
+  std::string ColorSpace;
+  std::string Source;
+  std::string License;
+  std::string Creator;
+
+  std::vector<float32> NanColor;
+  std::vector<float32> RGBPoints;
+  std::vector<float32> IndexedColors;
+
+  nlohmann::json Annotations;
+  bool DefaultMap = false;
+
+  bool Invalid = true;
+
+  explicit ColorPreset(const nlohmann::json& a);
+  ColorPreset();
+
+  nlohmann::json toJson() const;
+
+  std::string toJsonString(int index = -1) const;
+};
+
+using ColorPresetsVectorType = std::vector<ColorPreset>;
+
+/**
+ * @brief Converts the default JSON color table data into the internal representation as
+ * a `ColorPreset`
+ * @param rgbPreset Load RGB Presets
+ * @param indexedPreset Load Indexed Color Preset
+ * @return
+ */
+SIMPLNX_EXPORT Result<ColorPresetsVectorType> CacheAllPresets(bool rgbPreset, bool indexedPreset);
+
 /**
  * @brief LoadRGBPresets This method will combine RGB json presets from the ColorTable.hpp JSON string
  * @return The json object result
  */
 SIMPLNX_EXPORT Result<nlohmann::json> LoadAllRGBPresets();
 
+/**
+ * @brief This method will combine Indexed Color json presets from the ColorTable.hpp JSON string
+ * @return
+ */
 SIMPLNX_EXPORT Result<nlohmann::json> LoadAllIndexedPresets();
 
+/**
+ * @brief Load all color preset objects into a JSON representation
+ * @return
+ */
 SIMPLNX_EXPORT Result<nlohmann::json> LoadAllPresets();
 
 /**
- * @brief ExtractControlPoints This method will create a 2-D array of control points based on a the name of the preset
+ * @brief ExtractControlPoints This method will create a 2-D array of control points based on the name of the preset
  * @param presetName this is a string that corresponds to a "name" of a json object
  * @return a result object holding errors and a vector<float64> that can be empty.
  */
@@ -36,11 +83,31 @@ SIMPLNX_EXPORT Result<std::vector<float32>> ExtractControlPoints(const std::stri
 SIMPLNX_EXPORT std::string GetDefaultRGBPresetName();
 
 /**
- * @brief IsValidPreset Returns true if the preset has has the "RGBPoints" and "ColorSpace" keys
+ * @brief IsValidPreset Returns true if the preset has the "RGBPoints" and "ColorSpace" keys
+ * @param preset The preset to check in JSON form
  * @return
  */
 SIMPLNX_EXPORT bool IsValidPreset(const nlohmann::json& preset);
 
+/**
+ * @brief IsValidPreset Returns true if the preset has the "RGBPoints" and "ColorSpace" keys
+ * @param preset The preset to check as a ColorPreset object
+ * @return
+ */
+SIMPLNX_EXPORT bool IsValidPreset(const ColorPreset& preset);
+
+/**
+ * @brief IsValidPreset Returns true if the preset has the "IndexedColors" keys
+ * @param preset The preset to check in JSON form
+ * @return
+ */
 SIMPLNX_EXPORT bool IsValidIndexedPreset(const nlohmann::json& preset);
+
+/**
+ * @brief IsValidPreset Returns true if the preset has the "IndexedColors" keys
+ * @param preset The preset to check as a ColorPreset object
+ * @return
+ */
+SIMPLNX_EXPORT bool IsValidIndexedPreset(const ColorPreset& preset);
 
 } // namespace nx::core::ColorTableUtilities
