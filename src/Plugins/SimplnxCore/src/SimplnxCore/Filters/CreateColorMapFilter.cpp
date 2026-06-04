d417422e94bf5859e61de283155a306779b692cc
Michael Jackson <mike.jackson@bluequartz.net>
2026-06-04 18:28:43 -0400

    VER: Update to EbsdLib version 3.0.0
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- src/Plugins/SimplnxCore/src/SimplnxCore/Filters/CreateColorMapFilter.cpp
+++ src/Plugins/SimplnxCore/src/SimplnxCore/Filters/CreateColorMapFilter.cpp
@@ -10,7 +10,6 @@
 #include "simplnx/Parameters/CreateColorMapParameter.hpp"
 #include "simplnx/Parameters/DataObjectNameParameter.hpp"
 #include "simplnx/Parameters/StringParameter.hpp"
-#include "simplnx/Parameters/CreateColorMapParameter.hpp"
 #include "simplnx/Parameters/VectorParameter.hpp"
 #include "simplnx/Utilities/ColorTableUtilities.hpp"
 #include "simplnx/Utilities/SIMPLConversion.hpp"
